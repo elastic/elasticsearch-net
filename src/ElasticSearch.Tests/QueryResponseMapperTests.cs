315030e2fa84219c6525016fd6f44d1f72b175b4
Martijn Laarman <Mpdreamz@gmail.com>
2011-02-12 16:04:06 +0100

    ResponseMapper tests

---
+++ src/ElasticSearch.Tests/QueryResponseMapperTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using ElasticSearch.Client;
+using HackerNews.Indexer.Domain;
+
+namespace ElasticSearch.Tests
+{
+	/// <summary>
+	///  Tests that test wheter the query response can be succesfully mapped
+	/// </summary>
+	public class QueryResponseMapperTests : BaseElasticSearchTests
+	{
+		[Fact]
+		public void BogusQuery()
+		{
+			var client = this.ConnectedClient;
+			QueryResponse<Post> queryResults = client.Search<Post>(
+				@"asdadsadads"
+			);
+			Assert.False(queryResults.IsValid);
+			Assert.True(queryResults.ConnectionError.HttpStatusCode == System.Net.HttpStatusCode.InternalServerError);
+		}
+		[Fact]
+		public void SimpleFuzzyQuery()
+		{
+			var client = this.ConnectedClient;
+			QueryResponse<Post> queryResults = client.Search<Post>(
+				@"query : {  ""fuzzy"" : {  ""meta.username"" : ""ki""  } }"
+			);
+			Assert.True(queryResults.IsValid);
+			Assert.Null(queryResults.ConnectionError);
+		}
+
+
+	}
+}
