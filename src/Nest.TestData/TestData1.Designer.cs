beb69d9389edd12c3074c1c808bcca71b52fdbd5
Martijn Laarman <Mpdreamz@gmail.com>
2011-03-08 21:48:37 +0100

    Cleanup + ignore commit

--- src/Nest.TestData/TestData1.Designer.cs
+++ src/Nest.TestData/TestData1.Designer.cs
@@ -25,7 +25,7 @@ namespace Nest.TestData {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("192.168.1.10")]
+        [global::System.Configuration.DefaultSettingValueAttribute("localhost")]
         public string Host {
             get {
                 return ((string)(this["Host"]));
