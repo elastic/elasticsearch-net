2264ccd8793fe2319ea53b14b64c1567317e7248
Martijn Laarman <Mpdreamz@gmail.com>
2011-11-19 21:05:15 +0100

    Restructuring project a bit

---
+++ src/ElasticSearch.Client/Domain/Connection/IConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ElasticSearch.Client
+{
+	internal interface IConnection
+	{
+		void Get(string path, Action<ConnectionStatus> callback);
+		ConnectionStatus GetSync(string path);
+
+		void Post(string path, string data, Action<ConnectionStatus> callback);
+		ConnectionStatus PostSync(string path, string data);
+
+		void Put(string path, string data, Action<ConnectionStatus> callback);
+		ConnectionStatus PutSync(string path, string data);
+
+	    void Delete(string path, Action<ConnectionStatus> callback);
+	    ConnectionStatus DeleteSync(string path);
+
+		void Delete(string path, string data, Action<ConnectionStatus> callback);
+		ConnectionStatus DeleteSync(string path, string data);
+	}
+}
