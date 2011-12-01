aae9ec44c12eac13c599ac3ad61d356a4ce04ca0
Mattias Nordberg <mattias.nordberg@gmail.com>
2011-12-01 10:27:40 +0000

    Standard analyzer settings.

---
+++ src/ElasticSearch.Client/Settings/StandardAnalyzerSettings.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace ElasticSearch.Client.Settings
+{
+    public class StandardAnalyzerSettings : AnalyzerSettings
+    {
+        public StandardAnalyzerSettings()
+        {
+            Type = "standard";
+        }
+
+        [JsonProperty("stopwords")]
+        public string StopWords { get; set; }
+    }
+}
