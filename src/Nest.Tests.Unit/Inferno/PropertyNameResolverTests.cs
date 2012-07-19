55767133bd158e3234d577ac5e146c04a40d5adc
Martijn Laarman <Mpdreamz@gmail.com>
2012-07-19 10:34:32 +0200

    Added additional test to propertyname resolver based on reports by ppdevelopment

--- src/Nest.Tests.Unit/Inferno/PropertyNameResolverTests.cs
+++ src/Nest.Tests.Unit/Inferno/PropertyNameResolverTests.cs
@@ -14,86 +14,96 @@ using System.Linq.Expressions;
 
 namespace Nest.Tests.Unit.Inferno
 {
-  [TestFixture]
-  public class PropertyNameResolverTests
-  {
-    [ElasticType(IdProperty = "Guid")]
-    internal class SomeClass
-    {
-      public MyCustomClass MyCustomClass { get; set; }
-    }
-    [ElasticType(IdProperty = "Guid")]
-    internal class SomeOtherClass
-    {
-      [ElasticProperty(Name = "custom")]
-      public MyCustomClass MyCustomClass { get; set; }
+	[TestFixture]
+	public class PropertyNameResolverTests
+	{
+		[ElasticType(IdProperty = "Guid")]
+		internal class SomeClass
+		{
+			public MyCustomClass MyCustomClass { get; set; }
+		}
+		[ElasticType(IdProperty = "Guid")]
+		internal class SomeOtherClass
+		{
+			[ElasticProperty(Name = "custom")]
+			public MyCustomClass MyCustomClass { get; set; }
+			[ElasticProperty(Name = "CreateDate")]
+			public DateTime CreateDate { get; set; }
 
-      public MyCustomOtherClass MyCustomOtherClass { get; set; }
-    }
-    internal class MyCustomClass
-    {
-      [ElasticProperty(Name = "MID")]
-      public string MyProperty { get; set; }
+			public MyCustomOtherClass MyCustomOtherClass { get; set; }
+		}
+		internal class MyCustomClass
+		{
+			[ElasticProperty(Name = "MID")]
+			public string MyProperty { get; set; }
 
-      public override string ToString()
-      {
-        return "static id ftw";
-      }
-    }
-    [ElasticType(IdProperty = "Guid", Name = "mycustomother")]
-    internal class MyCustomOtherClass
-    {
-      [ElasticProperty(Name = "MID")]
-      public string MyProperty { get; set; }
+			public override string ToString()
+			{
+				return "static id ftw";
+			}
+		}
+		[ElasticType(IdProperty = "Guid", Name = "mycustomother")]
+		internal class MyCustomOtherClass
+		{
+			[ElasticProperty(Name = "MID")]
+			public string MyProperty { get; set; }
 
-      public override string ToString()
-      {
-        return "static id ftw";
-      }
-    }
-    [Test]
-    public void TestUsesElasticProperty()
-    {
-      Expression<Func<SomeClass, object>> exp = (m) => m.MyCustomClass.MyProperty;
-      var propertyName = ElasticClient.PropertyNameResolver.Resolve(exp);
-      var expected = "myCustomClass.MID";
-      StringAssert.AreEqualIgnoringCase(expected, propertyName);
-    }
-    [Test]
-    public void TestUsesOtherElasticProperty()
-    {
-      Expression<Func<SomeOtherClass, object>> exp = (m) => m.MyCustomClass.MyProperty;
-      var propertyName = ElasticClient.PropertyNameResolver.Resolve(exp);
-      var expected = "custom.MID";
-      StringAssert.AreEqualIgnoringCase(expected, propertyName);
-    }
-    [Test]
-    public void TestUsesOtherElasticTypePropertyIsIgnored()
-    {
-      Expression<Func<SomeOtherClass, object>> exp = (m) => m.MyCustomOtherClass.MyProperty;
-      var propertyName = ElasticClient.PropertyNameResolver.Resolve(exp);
-      var expected = "myCustomOtherClass.MID";
-      StringAssert.AreEqualIgnoringCase(expected, propertyName);
-    }
-    [Test]
-    public void SearchUsesTheProperResolver()
-    {
-      var settings = new ConnectionSettings(new Uri("http://localhost:9200")).SetDefaultIndex(Test.Default.DefaultIndex);
-      var client = new ElasticClient(settings);
-      var result = client.Search<SomeOtherClass>(s => s
-        .SortDescending(f=>f.MyCustomOtherClass.MyProperty)
-        .Query(query => query
-          .Bool(bq => bq
-            .Must(
-              mq => mq.ConstantScore(cs => cs.Filter(filter => filter.Term(x => x.MyCustomClass.MyProperty, "meesageid"))),
-              mp => mp.ConstantScore(cs => cs.Filter(filter => filter.Term(x => x.MyCustomOtherClass.MyProperty, "serverid")))
-            )
-          )
-        )
-      );
-      StringAssert.Contains("custom.MID", result.ConnectionStatus.Request);
-      StringAssert.Contains("myCustomOtherClass.MID", result.ConnectionStatus.Request);
+			public override string ToString()
+			{
+				return "static id ftw";
+			}
+		}
+		[Test]
+		public void TestUsesElasticProperty()
+		{
+			Expression<Func<SomeClass, object>> exp = (m) => m.MyCustomClass.MyProperty;
+			var propertyName = ElasticClient.PropertyNameResolver.Resolve(exp);
+			var expected = "myCustomClass.MID";
+			Assert.AreEqual(expected, propertyName);
+		}
+		[Test]
+		public void TestUsesOtherElasticProperty()
+		{
+			Expression<Func<SomeOtherClass, object>> exp = (m) => m.MyCustomClass.MyProperty;
+			var propertyName = ElasticClient.PropertyNameResolver.Resolve(exp);
+			var expected = "custom.MID";
+			Assert.AreEqual(expected, propertyName);
+		}
+		[Test]
+		public void TestUsesOtherElasticTypePropertyIsIgnored()
+		{
+			Expression<Func<SomeOtherClass, object>> exp = (m) => m.MyCustomOtherClass.MyProperty;
+			var propertyName = ElasticClient.PropertyNameResolver.Resolve(exp);
+			var expected = "myCustomOtherClass.MID";
+			Assert.AreEqual(expected, propertyName);
+		}
+		[Test]
+		public void TestCreatedDate()
+		{
+			Expression<Func<SomeOtherClass, object>> exp = (m) => m.CreateDate;
+			var propertyName = ElasticClient.PropertyNameResolver.Resolve(exp);
+			var expected = "CreateDate";
+			Assert.AreEqual(expected, propertyName);
+		}
+		[Test]
+		public void SearchUsesTheProperResolver()
+		{
+			var settings = new ConnectionSettings(new Uri("http://localhost:9200")).SetDefaultIndex(Test.Default.DefaultIndex);
+			var client = new ElasticClient(settings);
+			var result = client.Search<SomeOtherClass>(s => s
+			  .SortDescending(f => f.MyCustomOtherClass.MyProperty)
+			  .Query(query => query
+				.Bool(bq => bq
+				  .Must(
+					mq => mq.ConstantScore(cs => cs.Filter(filter => filter.Term(x => x.MyCustomClass.MyProperty, "meesageid"))),
+					mp => mp.ConstantScore(cs => cs.Filter(filter => filter.Term(x => x.MyCustomOtherClass.MyProperty, "serverid")))
+				  )
+				)
+			  )
+			);
+			StringAssert.Contains("custom.MID", result.ConnectionStatus.Request);
+			StringAssert.Contains("myCustomOtherClass.MID", result.ConnectionStatus.Request);
 
-    }
-  }
+		}
+	}
 }
