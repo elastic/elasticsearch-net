66546f360a000872214783b97544205189bf29a8
mikeyoon <mike.yoon@scorebig.com>
2012-08-08 11:21:18 -0700

    Add synonym token filter settings support

---
+++ src/Nest/Domain/Settings/SynonymTokenFilter.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Nest
+{
+    public class SynonymTokenFilter : TokenFilterSettings
+    {
+        public SynonymTokenFilter() : base("synonym")
+        {
+
+        }
+
+        [JsonProperty("synonyms_path", NullValueHandling = NullValueHandling.Ignore)]
+        public string SynonymsPath { get; set; }
+
+        [JsonProperty("format", NullValueHandling=NullValueHandling.Ignore)]
+        public string Format { get; set; }
+
+        [JsonProperty("synonyms", NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<string> Synonyms { get; set; }
+    }
+}
