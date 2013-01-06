eb29fe54e3e2193be09e8b285e7b03a137746832
Martijn Laarman <Mpdreamz@gmail.com>
2013-01-06 17:27:22 +0100

    some health API uri unit tests

--- src/Nest.Tests.Unit/Cluster/HealthTests.cs
+++ src/Nest.Tests.Unit/Cluster/HealthTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Nest.Tests.Unit.Cluster
 {
@@ -9,25 +11,33 @@ namespace Nest.Tests.Unit.Cluster
 		public void ClusterHealth()
 		{
 			var r = this._client.Health(HealthLevel.Cluster);
-			Assert.True(r.IsValid);
+			var u = new Uri(r.ConnectionStatus.RequestUrl);
+			u.AbsolutePath.Should().StartWith("/_cluster/health");
+			u.Query.Should().Contain("level=cluster");
 		}
 		[Test]
 		public void ClusterHealthPerIndex()
 		{
 			var r = this._client.Health(new[] { Test.Default.DefaultIndex }, HealthLevel.Cluster);
-			Assert.True(r.IsValid);
+			var u = new Uri(r.ConnectionStatus.RequestUrl);
+			u.AbsolutePath.Should().StartWith("/_cluster/health/nest_test_data");
+			u.Query.Should().Contain("level=cluster");
 		}
 		[Test]
 		public void IndexHealth()
 		{
 			var r = this._client.Health(HealthLevel.Indices);
-			Assert.True(r.IsValid);
+			var u = new Uri(r.ConnectionStatus.RequestUrl);
+			u.AbsolutePath.Should().StartWith("/_cluster/health");
+			u.Query.Should().Contain("level=indices");
 		}
 		[Test]
 		public void ShardHealth()
 		{
 			var r = this._client.Health(HealthLevel.Shards);
-			Assert.True(r.IsValid);
+			var u = new Uri(r.ConnectionStatus.RequestUrl);
+			u.AbsolutePath.Should().StartWith("/_cluster/health");
+			u.Query.Should().Contain("level=shards");
 		}
 		[Test]
 		public void DetailedHealth()
@@ -37,22 +47,16 @@ namespace Nest.Tests.Unit.Cluster
 					CheckLevel = HealthLevel.Shards,
 					Timeout = "30s",
 					WaitForMinNodes = 1,
-					WaitForRelocatingShards = 0
+					WaitForRelocatingShards = 0,
+					WaitForStatus = HealthStatus.Green
 				});
-			Assert.True(r.IsValid);
-		}
-		[Test]
-		public void DetailedHealthPerIndex()
-		{
-			var r = this._client.Health(new[] { Test.Default.DefaultIndex },
-												new HealthParams
-													{
-														CheckLevel = HealthLevel.Shards,
-														Timeout = "30s",
-														WaitForMinNodes = 1,
-														WaitForRelocatingShards = 0
-													});
-			Assert.True(r.IsValid);
+			var u = new Uri(r.ConnectionStatus.RequestUrl);
+			u.AbsolutePath.Should().StartWith("/_cluster/health");
+			u.Query.Should().Contain("level=shards");
+			u.Query.Should().Contain("timeout=30s");
+			u.Query.Should().Contain("wait_for_nodes=1");
+			u.Query.Should().Contain("wait_for_relocating_shards=0");
+			u.Query.Should().Contain("wait_for_status=green");
 		}
 	}
 }
