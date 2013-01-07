efc54c8f1d22d2b3eccb85a9a15675211e1420a0
Martijn Laarman <Mpdreamz@gmail.com>
2013-01-07 10:12:11 +0100

    Some doubles ToString() didnt use CultureInfo.InvarantCulture

--- src/Nest/Resolvers/PathResolver.cs
+++ src/Nest/Resolvers/PathResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -314,7 +315,7 @@ namespace Nest.Resolvers
 				if (!options._LikeText.IsNullOrEmpty())
 					dict.Add("like_text", options._LikeText);
 				if (options._TermMatchPercentage != null)
-					dict.Add("percent_terms_to_match", options._TermMatchPercentage.ToString());
+					dict.Add("percent_terms_to_match", options._TermMatchPercentage.Value.ToString(CultureInfo.InvariantCulture));
 				if (options._MinTermFrequency != null)
 					dict.Add("min_term_freq", options._MinTermFrequency.ToString());
 				if (options._MaxQueryTerms != null)
@@ -328,9 +329,9 @@ namespace Nest.Resolvers
 				if (options._MaxWordLength != null)
 					dict.Add("max_word_len", options._MaxWordLength.ToString());
 				if (options._BoostTerms != null)
-					dict.Add("boost_terms", options._BoostTerms.ToString());
+					dict.Add("boost_terms", options._BoostTerms.Value.ToString(CultureInfo.InvariantCulture));
 				if (options._Boost != null)
-					dict.Add("boost", options._Boost.ToString());
+					dict.Add("boost", options._Boost.Value.ToString(CultureInfo.InvariantCulture));
 				if (!options._Analyzer.IsNullOrEmpty())
 					dict.Add("analyzer", options._Analyzer);
 			}
