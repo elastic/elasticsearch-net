3db6d5b24c28ac5ad8046e6b3e14bb8d64bb9d5a
roman <roman@delver.com>
2013-01-15 13:53:21 +0200

    Adding EdgeNgram token filter and fixing typo

---
+++ src/Nest/Domain/Settings/EdgeNgramTokenFilter.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Nest
+{
+    /// <summary>
+    /// EdgeNGram Filter settings
+    /// </summary>
+    public class EdgeNGramTokenFilter : TokenFilterSettings
+    {
+        public EdgeNGramTokenFilter()
+            : base("edgeNGram")
+        {
+            MinGram = 1;
+            MaxGram = 2;
+	        Side = "front";
+        }
+
+        [JsonProperty("min_gram")]
+        public int MinGram { get; set; }
+
+        [JsonProperty("max_gram")]
+        public int MaxGram { get; set; }
+
+        [JsonProperty("side")]
+        public string Side { get; set; }
+    }
+}
