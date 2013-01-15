e02a9a92225ca137177d74414a79b9820bff15e7
Martijn Laarman <Mpdreamz@gmail.com>
2013-01-15 22:22:29 +0100

    analysis should be written under index

--- src/Nest/Resolvers/Converters/IndexSettingsConverter.cs
+++ src/Nest/Resolvers/Converters/IndexSettingsConverter.cs
@@ -15,47 +15,66 @@ namespace Nest.Resolvers.Converters
             writer.WritePropertyName("settings");
             writer.WriteStartObject();
 
-            writer.WritePropertyName("index");
-            writer.WriteStartObject();
+			writer.WritePropertyName("index");
+			writer.WriteStartObject();
 
-            writer.WritePropertyName("number_of_shards");
-            writer.WriteValue(indexSettings.NumberOfShards);
-            writer.WritePropertyName("number_of_replicas");
-            writer.WriteValue(indexSettings.NumberOfReplicas);
-
-            foreach (var kv in indexSettings.Settings)
-            {
-                writer.WritePropertyName(kv.Key);
-                writer.WriteValue(kv.Value);
-            }
-
-            writer.WriteEndObject();
-
-            writer.WritePropertyName("analysis");
-            writer.WriteStartObject();
-
-            writer.WritePropertyName("analyzer");
-            serializer.Serialize(writer, indexSettings.Analysis.Analyzers);
-
-            if (indexSettings.Analysis.TokenFilters.Count > 0)
-            {
-                writer.WritePropertyName("filter");
-                serializer.Serialize(writer, indexSettings.Analysis.TokenFilters);
-            }
-			if (indexSettings.Analysis.Tokenizers.Count > 0)
+	 
+		    if (indexSettings.NumberOfReplicas.HasValue) 
 			{
-				writer.WritePropertyName("tokenizer");
-				serializer.Serialize(writer, indexSettings.Analysis.Tokenizers);
-			}
-			if (indexSettings.Analysis.CharFilters.Count > 0)
-			{
-				writer.WritePropertyName("char_filter");
-				serializer.Serialize(writer, indexSettings.Analysis.CharFilters);
-			}
+		        writer.WritePropertyName("number_of_shards");
+		        writer.WriteValue(indexSettings.NumberOfShards);
+	        }
+	        if (indexSettings.NumberOfShards.HasValue)
+	        {
+		        writer.WritePropertyName("number_of_replicas");
+		        writer.WriteValue(indexSettings.NumberOfReplicas);
+	        }
+	        if (indexSettings.Settings.HasAny())
+	        {
+		        foreach (var kv in indexSettings.Settings)
+		        {
+			        writer.WritePropertyName(kv.Key);
+			        writer.WriteValue(kv.Value);
+		        }
+	        }
 
-            writer.WriteEndObject();
+	        if (
+				indexSettings.Analysis.Analyzers.Count > 0
+				|| indexSettings.Analysis.TokenFilters.Count > 0
+				|| indexSettings.Analysis.Tokenizers.Count > 0
+				|| indexSettings.Analysis.CharFilters.Count > 0
+		        )
+	        {
+		        writer.WritePropertyName("analysis");
+		        writer.WriteStartObject();
+		        if (indexSettings.Analysis.Analyzers.Count > 0)
+		        {
+			        writer.WritePropertyName("analyzer");
+			        serializer.Serialize(writer, indexSettings.Analysis.Analyzers);
+		        }
 
-            writer.WriteEndObject();
+		        if (indexSettings.Analysis.TokenFilters.Count > 0)
+		        {
+			        writer.WritePropertyName("filter");
+			        serializer.Serialize(writer, indexSettings.Analysis.TokenFilters);
+		        }
+		        if (indexSettings.Analysis.Tokenizers.Count > 0)
+		        {
+			        writer.WritePropertyName("tokenizer");
+			        serializer.Serialize(writer, indexSettings.Analysis.Tokenizers);
+		        }
+		        if (indexSettings.Analysis.CharFilters.Count > 0)
+		        {
+			        writer.WritePropertyName("char_filter");
+			        serializer.Serialize(writer, indexSettings.Analysis.CharFilters);
+		        }
+
+		        writer.WriteEndObject();
+	        }
+
+	        writer.WriteEndObject();
+
+			writer.WriteEndObject();
 
             if (indexSettings.Mappings.Count > 0)
             { 
