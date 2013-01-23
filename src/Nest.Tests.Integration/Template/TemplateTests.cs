9cfcc8829b038d016f192de3d9badd5ccc4331a3
Jaap Taal <jaap@q42.nl>
2013-01-23 11:43:09 +0100

    Added first version of template mapping. Still needs fluent/Descriptor things

---
+++ src/Nest.Tests.Integration/Template/TemplateTests.cs
@@ -0,0 +1,58 @@
+﻿using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+
+namespace Nest.Tests.Integration.Mapping
+{
+	[TestFixture]
+	public class TemplateTests : IntegrationTests
+	{
+
+		[Test]
+		public void SimplePutAndGet()
+		{
+			var templateMapping = new TemplateMapping { Template = "donotinfluencothertests-*" };
+			var deleteResponse = this._client.DeleteTemplate("simple-put-and-get");
+
+			var putResponse = this._client.PutTemplate("simple-put-and-get", templateMapping);
+			Assert.IsTrue(putResponse.OK);
+
+			var templateMappingResult = this._client.GetTemplate("simple-put-and-get");
+
+			Assert.AreEqual("donotinfluencothertests-*", templateMappingResult.Template);
+		}
+
+
+		[Test]
+		public void PutTemplateWithSettings()
+		{
+			var templateMapping = new TemplateMapping { Template = "donotinfluencothertests-*", Settings = new TemplateIndexSettings { NumberOfShards = 3, NumberOfReplicas = 2 } };
+
+			this._client.DeleteTemplate("put-template-with-settings");
+			var putResponse = this._client.PutTemplate("put-template-with-settings", templateMapping);
+			Assert.IsTrue(putResponse.OK);
+
+			var templateMappingResult = this._client.GetTemplate("put-template-with-settings");
+
+			Assert.AreEqual(3, templateMappingResult.Settings.NumberOfShards);
+			Assert.AreEqual(2, templateMappingResult.Settings.NumberOfReplicas);
+		}
+
+		[Test]
+		public void PutTemplateWithMappings()
+		{
+			var mappings = new FluentDictionary<string, RootObjectMapping>()
+				.Add("mytype", new RootObjectMapping { AllFieldMapping = new AllFieldMapping { Enabled = false } });
+			var templateMapping = new TemplateMapping { Template = "donotinfluencothertests-*", Mappings = mappings };
+
+			this._client.DeleteTemplate("put-template-with-mappings");
+			var putResponse = this._client.PutTemplate("put-template-with-mappings", templateMapping);
+			Assert.IsTrue(putResponse.OK);
+
+			var templateMappingResult = this._client.GetTemplate("put-template-with-mappings");
+
+			Assert.IsTrue(templateMappingResult.Mappings.ContainsKey("mytype"), "put-template-with-mappings template should have a `mytype` mapping");
+			Assert.NotNull(templateMappingResult.Mappings["mytype"].AllFieldMapping, "`mytype` mapping should contain the _all field mapping");
+			Assert.AreEqual(false, templateMappingResult.Mappings["mytype"].AllFieldMapping.Enabled, "_all { enabled } should be set to false");
+		}
+	}
+}
---
+++ src/Nest/ElasticClient-Template.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Nest.Resolvers.Writers;
+
+namespace Nest
+{
+	public partial class ElasticClient
+	{
+
+		public TemplateMapping GetTemplate(string templateName)
+		{
+			templateName.ThrowIfNull("templateName");
+			//TODO validate templateName for invalid url chars?
+
+			string path = this.PathResolver.CreateTemplatePath(templateName);
+
+			ConnectionStatus status = this.Connection.GetSync(path);
+			try
+			{
+				var templateMappings = this.Deserialize<Dictionary<string,TemplateMapping>>(status.Result);
+
+				if (status.Success)
+				{
+					var templateMapping = templateMappings.First().Value;
+					return templateMapping;
+				}
+			}
+			catch (Exception e)
+			{
+				//TODO LOG
+			}
+			return null;
+		}
+
+		public IIndicesOperationResponse PutTemplate(string templateName, TemplateMapping templateMapping) // TODO: use descriptor
+		{
+			templateName.ThrowIfNull("templateName");
+			templateMapping.ThrowIfNull("templateMapping");
+
+			string template = JsonConvert.SerializeObject(templateMapping, Formatting.None, SerializationSettings);
+
+			return PutTemplateRaw(templateName, template);
+		}
+
+		public IIndicesOperationResponse PutTemplateRaw(string templateName, string template)
+		{
+			string path = this.PathResolver.CreateTemplatePath(templateName);
+			ConnectionStatus status = this.Connection.PutSync(path, template);
+
+			var r = this.ToParsedResponse<IndicesOperationResponse>(status);
+			return r;
+		}
+
+		public IIndicesOperationResponse DeleteTemplate(string templateName)
+		{
+			string path = this.PathResolver.CreateTemplatePath(templateName);
+			ConnectionStatus status = this.Connection.DeleteSync(path);
+
+			var r = this.ToParsedResponse<IndicesOperationResponse>(status);
+			return r;
+		}
+	}
+}
