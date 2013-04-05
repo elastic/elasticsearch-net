7e9361387fe2a1800aa9cb826ef83c25ac4cbb26
Martijn Laarman <Mpdreamz@gmail.com>
2013-04-05 15:07:55 +0200

    Added test for #204 to proof ICollections should serialize fine

--- src/Nest.Tests.Integration/Index/GetIdFromElasticsearchTests.cs
+++ src/Nest.Tests.Integration/Index/GetIdFromElasticsearchTests.cs
@@ -7,42 +7,42 @@ using NUnit.Framework;
 
 namespace Nest.Tests.Integration.Index
 {
-  [TestFixture]
-  public class GetIdFromElasticSearchTests : IntegrationTests
-  {
+	[TestFixture]
+	public class GetIdFromElasticSearchTests : IntegrationTests
+	{
 
-    public class LogClass
-    {
-      public string Message { get; set; }
-    }
-    [Test]
-    public void IndexWithoutIdShouldSetIdFromElasticseach()
-    {
-      var newProject = new LogClass
-      {
-        Message = "Some Message",
-      };
-      var response = this._client.Index(newProject);
-      Assert.IsTrue(response.IsValid);
-      Assert.IsNotNullOrEmpty(response.Id);
-      Assert.IsNotNullOrEmpty(response.Type);
-      Assert.IsNotNullOrEmpty(response.Index);
-      Assert.IsNotNullOrEmpty(response.Version);
-    }
-    [Test]
-    public void IndexmanyWithoutIdShouldSetIdFromElasticseach()
-    {
-      var newProjects = new List<LogClass>
-      {
-        new LogClass { Message = "Some Message1" },
-        new LogClass { Message = "Some Message2" },
-        new LogClass { Message = "Some Message3" }
-      };
-      var response = this._client.IndexMany(newProjects);
-      Assert.IsTrue(response.IsValid, response.ConnectionStatus.Result);
-      Assert.IsNotEmpty(response.Items);
-      Assert.True(response.Items.All(i => i.OK), response.ConnectionStatus.Result);
-      Assert.True(response.Items.All(i => !i.Id.IsNullOrEmpty()));
-    }
-  }
+		public class LogClass
+		{
+			public string Message { get; set; }
+		}
+		[Test]
+		public void IndexWithoutIdShouldSetIdFromElasticseach()
+		{
+			var newProject = new LogClass
+			{
+				Message = "Some Message",
+			};
+			var response = this._client.Index(newProject);
+			Assert.IsTrue(response.IsValid);
+			Assert.IsNotNullOrEmpty(response.Id);
+			Assert.IsNotNullOrEmpty(response.Type);
+			Assert.IsNotNullOrEmpty(response.Index);
+			Assert.IsNotNullOrEmpty(response.Version);
+		}
+		[Test]
+		public void IndexmanyWithoutIdShouldSetIdFromElasticseach()
+		{
+			var newProjects = new List<LogClass>
+			{
+				new LogClass { Message = "Some Message1" },
+				new LogClass { Message = "Some Message2" },
+				new LogClass { Message = "Some Message3" }
+			};
+			var response = this._client.IndexMany(newProjects);
+			Assert.IsTrue(response.IsValid, response.ConnectionStatus.Result);
+			Assert.IsNotEmpty(response.Items);
+			Assert.True(response.Items.All(i => i.OK), response.ConnectionStatus.Result);
+			Assert.True(response.Items.All(i => !i.Id.IsNullOrEmpty()));
+		}
+	}
 }
