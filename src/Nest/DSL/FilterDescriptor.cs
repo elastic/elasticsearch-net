61bed9cad0b0daaf3496827f68d112e0cfe60c9c
Peter Willis <a.cyberdemon@gmail.com>
2013-04-12 10:56:47 +0100

    geo distance filter unit should convert to "distance_unit"
    The geo_distance filter should output "distance_unit"
    for mi/km instead of "distance_type". "distance_type" is for
    selecting between arc and plane. Described here:
    http://www.elasticsearch.org/guide/reference/query-dsl/geo-distance-filter/

--- src/Nest/DSL/FilterDescriptor.cs
+++ src/Nest/DSL/FilterDescriptor.cs
@@ -388,7 +388,7 @@ namespace Nest
 				dd.Add("distance", filter._Distance);
 
 				if (!string.IsNullOrWhiteSpace(filter._GeoUnit))
-					dd.Add("distance_type", filter._GeoUnit);
+					dd.Add("distance_unit", filter._GeoUnit);
 
 				if (!string.IsNullOrWhiteSpace(filter._GeoOptimizeBBox))
 					dd.Add("optimize_bbox", filter._GeoOptimizeBBox);
