49d1d2babf0b9cc861f0f7267d86cffc9ce12eef
Jaap Taal <jaap@q42.nl>
2013-06-06 17:32:45 +0200

    has_parent filter was added in 0.19.10

---
+++ src/Nest/DSL/Filter/HasParentFilterDescriptor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Linq.Expressions;
+using Nest.Resolvers;
+
+namespace Nest
+{
+	[JsonObject(MemberSerialization=MemberSerialization.OptIn)]
+	public class HasParentFilterDescriptor<T> : FilterBase where T : class
+	{
+		public HasParentFilterDescriptor()
+		{
+			this._Type = new TypeNameResolver().GetTypeNameFor<T>();
+		}
+		internal override bool IsConditionless
+		{
+			get
+			{
+				return this._QueryDescriptor == null || this._QueryDescriptor.IsConditionless || this._Type.IsNullOrEmpty();
+			}
+		}
+
+		[JsonProperty("type")]
+		internal TypeNameMarker _Type { get; set; }
+
+		[JsonProperty("_scope")]
+		internal string _Scope { get; set;}
+		
+		[JsonProperty("query")]
+		internal BaseQuery _QueryDescriptor { get; set; }
+
+		public HasParentFilterDescriptor<T> Query(Func<QueryDescriptor<T>, BaseQuery> querySelector)
+		{
+			var q = new QueryDescriptor<T>();
+			this._QueryDescriptor = querySelector(q);
+			return this;
+		}
+
+		public HasParentFilterDescriptor<T> Scope(string scope)
+		{
+			this._Scope = scope;
+			return this;
+		}
+		public HasParentFilterDescriptor<T> Type(string type)
+		{
+			this._Type = type;
+			return this;
+		}
+	}
+}
