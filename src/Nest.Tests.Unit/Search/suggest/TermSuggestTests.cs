21740bebf9c26445c5d594a029946572b05958a2
Max Horstmann <max.horstmann+github@gmail.com>
2013-10-09 18:05:41 -0400

    first unit and integration test for term suggest

---
+++ src/Nest.Tests.Unit/Search/suggest/TermSuggestTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Nest.Tests.MockData.Domain;
+
+namespace Nest.Tests.Unit.Search.Query.Bool
+{
+	[TestFixture]
+	public class TermSuggestTests : BaseJsonTests
+	{
+		[Test]
+        public void TermSuggestDescriptorTest()
+		{
+		    var termSuggestDescriptor = new TermSuggestDescriptor<ElasticSearchProject>().MaxEdits(3).MaxInspections(17).OnField("field1");
+            var json = TestElasticClient.Serialize(termSuggestDescriptor);
+
+            var expected = @"{
+                              ""max_edits"": 3,
+                              ""max_inspections"": 17,
+                              ""field"": ""field1""
+                            }";
+
+            Assert.True(json.JsonEquals(expected), json);
+        }
+
+
+	} 
+}
