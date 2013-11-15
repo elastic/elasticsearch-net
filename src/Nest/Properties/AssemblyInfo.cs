abfd768242456a3a45f70b1dd3a7ded76393a281
Martijn Laarman <Mpdreamz@gmail.com>
2013-11-15 17:05:01 +0100

    fixed the build to generate proper signed libs for the *.Signed nugets

--- src/Nest/Properties/AssemblyInfo.cs
+++ src/Nest/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
 [assembly: System.Runtime.InteropServices.Guid("07E5CFA3-CF5F-4D17-874C-8D5CC6FA3E73")]
-[assembly: System.Reflection.AssemblyVersion("0.11.7.0")]
-[assembly: System.Reflection.AssemblyFileVersion("0.11.7.0")]
+[assembly: System.Reflection.AssemblyVersion("0.11.7.1")]
+[assembly: System.Reflection.AssemblyFileVersion("0.11.7.1")]
 
 
