1ee4eb4891f776d1ae54b4b75dc0ab280b7f1a31
Martijn Laarman <Mpdreamz@gmail.com>
2014-02-13 12:59:33 +0100

    fix #464 broke the build

--- src/Nest/Properties/AssemblyInfo.cs
+++ src/Nest/Properties/AssemblyInfo.cs
@@ -9,13 +9,13 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyTitle("NEST")]
-[assembly: System.Reflection.AssemblyDescription("Strongly typed Elasticsearch client.")]
+[assembly: System.Reflection.AssemblyDescription("Strongly typed ElasticSearch client.")]
 [assembly: System.Reflection.AssemblyProduct("NEST")]
 [assembly: System.Reflection.AssemblyCopyright("Martijn Laarman MIT LICENSED")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
 [assembly: System.Runtime.InteropServices.Guid("07E5CFA3-CF5F-4D17-874C-8D5CC6FA3E73")]
-[assembly: System.Reflection.AssemblyVersion("0.12.0")]
-[assembly: System.Reflection.AssemblyFileVersion("0.12.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
 
 
