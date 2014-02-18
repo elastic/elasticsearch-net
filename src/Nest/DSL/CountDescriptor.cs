5bb758469123e2c76d25b948ceda52dfa109370a
Martijn Laarman <Mpdreamz@gmail.com>
2014-02-18 14:27:48 +0100

    support for the new _count format

--- src/Nest/DSL/CountDescriptor.cs
+++ src/Nest/DSL/CountDescriptor.cs
@@ -5,19 +5,17 @@ using Nest.Resolvers.Converters;
 namespace Nest
 {
 	[DescriptorFor("Count")]
-	[JsonConverter(typeof(CustomJsonConverter))]
 	public partial class CountDescriptor<T> 
 		:	QueryPathDescriptorBase<CountDescriptor<T>, T, CountQueryString>
-		, IActAsQuery
 		, IPathInfo<CountQueryString> 
-		, ICustomJson
 		where T : class
 	{
-		BaseQuery IActAsQuery._Query { get; set; }
+		[JsonProperty("query")]
+		internal BaseQuery _Query { get; set; }
 
 		public CountDescriptor<T> Query(Func<QueryDescriptor<T>, BaseQuery> querySelector)
 		{
-			((IActAsQuery)this)._Query = querySelector(new QueryDescriptor<T>());
+			this._Query = querySelector(new QueryDescriptor<T>());
 			return this;
 		}
 
@@ -30,11 +28,6 @@ namespace Nest
 				: PathInfoHttpMethod.POST;
 				
 			return pathInfo;
-		}
-
-		object ICustomJson.GetCustomJson()
-		{
-			return ((IActAsQuery) this)._Query;
 		}
 	}
 }
