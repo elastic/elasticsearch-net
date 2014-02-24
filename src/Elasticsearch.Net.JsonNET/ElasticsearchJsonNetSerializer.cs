8a5f0b888a6552717a04c164418ad44e9d1605d8
Martijn Laarman <Mpdreamz@gmail.com>
2014-02-24 17:16:26 +0100

    Added JSON.NET serialization for Elasticsearch.NET

---
+++ src/Elasticsearch.Net.JsonNET/ElasticsearchJsonNetSerializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Elasticsearch.Net.JsonNet
+{
+	public class ElasticsearchJsonNetSerializer: IElasticsearchSerializer
+	{
+		private readonly JsonSerializerSettings _settings;
+
+		public ElasticsearchJsonNetSerializer(JsonSerializerSettings settings)
+		{
+			_settings = settings;
+		}
+
+		public T Deserialize<T>(byte[] bytes) where T : class
+		{
+			if (bytes == null) return null;
+
+			var s = Encoding.UTF8.GetString(bytes);
+			return JsonConvert.DeserializeObject<T>(s, this._settings);
+		}
+
+		public byte[] Serialize(object data, SerializationFormatting formatting = SerializationFormatting.Indented)
+		{
+			var format = formatting == SerializationFormatting.Indented
+				? Formatting.Indented
+				: Formatting.None;
+			var json = JsonConvert.SerializeObject(data, format, this._settings);
+
+			return Encoding.UTF8.GetBytes(json);
+		}
+	}
+}
