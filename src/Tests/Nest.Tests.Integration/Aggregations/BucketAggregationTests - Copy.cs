ca3578e6cc6a4507254a52875fb510862f1b36be
Martijn Laarman <Mpdreamz@gmail.com>
2014-03-03 20:59:01 +0100

    started on nested aggregation

---
+++ src/Tests/Nest.Tests.Integration/Aggregations/BucketAggregationTests - Copy.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+
+namespace Nest.Tests.Integration.Aggregations
+{
+    [TestFixture]
+    public class NestedBucketAggregationTests : IntegrationTests
+    {
+	    [Test]
+	    public void Terms()
+	    {
+		    var results = this._client.Search<ElasticsearchProject>(s=>s
+				.Size(0)
+				.Aggregations(a=>a
+					.Nested("followers", n=>n
+						.Aggregations(t=>t
+							.Terms("bucket_agg", m=>m.Field(p=>p.Country))
+						)
+					)
+				)
+			);
+		    results.IsValid.Should().BeTrue();
+		    var bucket = results.Aggs.Terms("bucket_agg");
+		    bucket.Items.Should().NotBeEmpty();
+	    }
+	
+    }
+}
