265ae14b22d7ca216598687cbb54adf24ccd6445
Martijn Laarman <Mpdreamz@gmail.com>
2014-03-03 21:46:29 +0100

    i really need to update the editorconfig visual studio extension

--- src/Tests/Nest.Tests.Integration/Aggregations/TermsAggregationTests.cs
+++ src/Tests/Nest.Tests.Integration/Aggregations/TermsAggregationTests.cs
@@ -6,59 +6,59 @@ using NUnit.Framework;
 
 namespace Nest.Tests.Integration.Aggregations
 {
-    [TestFixture]
-    public class TermsAggregationTests : IntegrationTests
-    {
-        [Test]
-        public void WrongFieldName()
-        {
+	[TestFixture]
+	public class TermsAggregationTests : IntegrationTests
+	{
+		[Test]
+		public void WrongFieldName()
+		{
 			var results = this._client.Search<ElasticsearchProject>(s => s
 				.Size(0)
-				.Aggregations(a=>a
-					.Terms("my_terms_agg", t=>t
+				.Aggregations(a => a
+					.Terms("my_terms_agg", t => t
 						.Field("this_field_name_does_not_exist")
 					)
 				)
 			);
-	        results.IsValid.Should().BeTrue();
-	        var termBucket = results.Aggs.Terms("my_terms_agg");
-	        termBucket.Should().NotBeNull();
-	        termBucket.Items.Should().BeEmpty();
-        }
-	
+			results.IsValid.Should().BeTrue();
+			var termBucket = results.Aggs.Terms("my_terms_agg");
+			termBucket.Should().NotBeNull();
+			termBucket.Items.Should().BeEmpty();
+		}
+
 		[Test]
-        public void ExistingFieldName()
-        {
+		public void ExistingFieldName()
+		{
 			var results = this._client.Search<ElasticsearchProject>(s => s
 				.Size(0)
-				.Aggregations(a=>a
-					.Terms("my_terms_agg", t=>t
-						.Field(p=>p.Name)
+				.Aggregations(a => a
+					.Terms("my_terms_agg", t => t
+						.Field(p => p.Name)
 					)
 				)
 			);
-	        results.IsValid.Should().BeTrue();
-	        var termBucket = results.Aggs.Terms("my_terms_agg");
-	        termBucket.Should().NotBeNull();
-	        termBucket.Items.Should().NotBeEmpty()
-				.And.OnlyContain(i=>!i.Key.IsNullOrEmpty())
-				.And.OnlyContain(i=>i.DocCount > 0);
-        }
-		
+			results.IsValid.Should().BeTrue();
+			var termBucket = results.Aggs.Terms("my_terms_agg");
+			termBucket.Should().NotBeNull();
+			termBucket.Items.Should().NotBeEmpty()
+				.And.OnlyContain(i => !i.Key.IsNullOrEmpty())
+				.And.OnlyContain(i => i.DocCount > 0);
+		}
+
 		[Test]
-        public void NestedEmptyAggregationThreeLevelDeeps()
-        {
+		public void NestedEmptyAggregationThreeLevelDeeps()
+		{
 			var results = this._client.Search<ElasticsearchProject>(s => s
 				.Size(0)
-				.Aggregations(a=>a
-					.Terms("countries", t=>t
-						.Field(p=>p.Country)
-						.Aggregations(aa=>aa
+				.Aggregations(a => a
+					.Terms("countries", t => t
+						.Field(p => p.Country)
+						.Aggregations(aa => aa
 							.Terms("noop", tt => tt.Field("noop"))
 							.Terms("names", tt => tt
-								.Field(p=>p.Name)
-								.Aggregations(aaa=>aaa
-									.Average("avg_loc", avg=>avg .Field(p=>p.LOC))
+								.Field(p => p.Name)
+								.Aggregations(aaa => aaa
+									.Average("avg_loc", avg => avg.Field(p => p.LOC))
 								)
 							)
 						)
@@ -93,6 +93,6 @@ namespace Nest.Tests.Integration.Aggregations
 					average.Value.Should().HaveValue();
 				}
 			}
-        }
-    }
+		}
+	}
 }
