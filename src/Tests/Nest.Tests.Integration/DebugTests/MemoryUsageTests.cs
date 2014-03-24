88c7aa026b1188de93439c5f8efb4e5ec3e5030f
Martijn Laarman <Mpdreamz@gmail.com>
2014-03-24 15:28:28 +0100

    Removed nested gitignore file under src/ and checked in missing Debug unit tests due to it matching a gitignore rule

---
+++ src/Tests/Nest.Tests.Integration/DebugTests/MemoryUsageTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+
+namespace Nest.Tests.Integration.Debug
+{
+	[TestFixture]
+	public class MemoryUsageTests : IntegrationSetup
+	{
+		[Test]
+		public void DeserializeOfStreamDoesNotHoldACopyOfTheResponse()
+		{
+			var uri = ElasticsearchConfiguration.CreateBaseUri();
+			var settings = new ConnectionSettings(uri, ElasticsearchConfiguration.DefaultIndex);
+			IElasticClient client = new ElasticClient(settings);
+			
+			var results = client.Search<ElasticsearchProject>(s => s.MatchAll());
+
+
+		}
+
+	}
+}
