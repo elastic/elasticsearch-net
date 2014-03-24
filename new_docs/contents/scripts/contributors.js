37a4e1cbc2a12dafb9380a5f27e7ff5a181d8e56
Martijn Laarman <Mpdreamz@gmail.com>
2014-03-24 15:23:42 +0100

    Made building documentation part of the build redesign to clearly indicate the difference between elasticsearch.net and nest

--- new_docs/contents/scripts/contributors.js
+++ new_docs/contents/scripts/contributors.js
@@ -1,6 +1,6 @@
 $(function() {
     $.ajax({
-        url: 'https://api.github.com/repos/Mpdreamz/NEST/contributors',
+        url: 'https://api.github.com/repos/elasticsearch/elasticsearch-net/contributors',
         dataType: 'jsonp',
         success: function(result) {
           result.data = result.data.sort(function (a, b) { 
@@ -8,7 +8,7 @@ $(function() {
             if (a.contributions < b.contributions) return 1;
             return 0;
           });
-          
+          result.count = result.data.length;
           $("#contributorTemplate").tmpl(result).appendTo("#nest-contributors");
         }
     });
