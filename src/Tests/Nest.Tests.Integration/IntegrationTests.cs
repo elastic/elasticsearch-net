79efa95db49d07be1861085c8e0755c25bfc1963
Martijn Laarman <Mpdreamz@gmail.com>
2014-04-11 20:47:47 +0200

    Added test to illustrate #579 how to combine queries.

--- src/Tests/Nest.Tests.Integration/IntegrationTests.cs
+++ src/Tests/Nest.Tests.Integration/IntegrationTests.cs
@@ -13,14 +13,15 @@ namespace Nest.Tests.Integration
 {
 	public class IntegrationTests
 	{
+
 		protected readonly IElasticClient _client = ElasticsearchConfiguration.Client;
 		protected readonly IElasticClient _clientNoRawResponse = ElasticsearchConfiguration.ClientNoRawResponse;
 		protected readonly ElasticClient _thriftClient = ElasticsearchConfiguration.ThriftClient;
 		protected readonly IConnectionSettingsValues _settings = ElasticsearchConfiguration.Settings();
-	
+
 		protected virtual void ResetIndexes()
 		{
-			
+
 		}
 
 		protected IQueryResponse<T> SearchRaw<T>(string query) where T : class
@@ -28,20 +29,20 @@ namespace Nest.Tests.Integration
 			var index = this._client.Infer.IndexName<T>();
 			var typeName = this._client.Infer.TypeName<T>();
 			var connectionStatus = this._client.Raw.Search<QueryResponse<T>>(index, typeName, query);
-			var serializer = connectionStatus.Serializer as INestSerializer; 
+			var serializer = connectionStatus.Serializer as INestSerializer;
 			return connectionStatus.Response;
-		} 
+		}
 
 		public void DoFilterTest(Func<FilterDescriptor<ElasticsearchProject>, Nest.BaseFilter> filter, ElasticsearchProject project, bool queryMustHaveResults)
 		{
 			var filterId = Filter<ElasticsearchProject>.Term(e => e.Id, project.Id);
 
 			var results = this._client.Search<ElasticsearchProject>(
-				s => s.Filter(ff => ff.And(
-						f => f.Term(e => e.Id, project.Id),
-						filter
-					))
-				);
+			  s => s.Filter(ff => ff.And(
+				  f => f.Term(e => e.Id, project.Id),
+				  filter
+				))
+			  );
 
 			var rawResponse = results.ConnectionStatus.ResponseRaw.Utf8String();
 
@@ -49,6 +50,6 @@ namespace Nest.Tests.Integration
 			Assert.True(results.ConnectionStatus.Success, rawResponse);
 			Assert.AreEqual(queryMustHaveResults ? 1 : 0, results.Total);
 		}
-	
+
 	}
 }
