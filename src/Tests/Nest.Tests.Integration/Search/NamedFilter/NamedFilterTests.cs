63192f075d7bc53af38b419eea92f907fda027bc
Chris McKee <chris.mckee@ivendi.com>
2014-04-22 14:19:26 +0100

    Tests to prove #606

--- src/Tests/Nest.Tests.Integration/Search/NamedFilter/NamedFilterTests.cs
+++ src/Tests/Nest.Tests.Integration/Search/NamedFilter/NamedFilterTests.cs
@@ -6,6 +6,9 @@ using NUnit.Framework;
 
 namespace Nest.Tests.Integration.Search.NamedFilter
 {
+	using System.Collections.Generic;
+	using Elasticsearch.Net;
+
 	[TestFixture]
 	public class NamedFilterTest : IntegrationTests
 	{
@@ -30,5 +33,29 @@ namespace Nest.Tests.Integration.Search.NamedFilter
 			//https://github.com/elasticsearch/elasticsearch/issues/3097
 		}
 
+		/// <summary>
+		/// Fields string param usage produces correct search string (ref: Nest.Tests.Unit.Search.SearchOptions.TestFieldsWithExclusionsByProperty)
+		/// Results fail to be correctly deserialized, resulting doc count is correct but docs are all null.
+		/// https://github.com/elasticsearch/elasticsearch-net/issues/606
+		/// </summary>
+		[Test]		
+		public void Search_WithFieldsRemoved_ReturnsDocuments_ButAllDocumentsAreNull()
+		{
+		  // Left in followers + contributors will cause a leaf node exception, so good test victims
+		  var fields = typeof(ElasticsearchProject).GetProperties()
+			.Select(x => x.Name.ToCamelCase())
+			.Except(new List<string> { "followers", "contributors", "nestedFollowers", "myGeoShape" }).ToList();
+
+		  var queryResults = _client.Search<ElasticsearchProject>(s =>
+				  s.Skip(0)
+				  .Take(10)
+				  .Fields(fields.ConvertAll(x => x.ToCamelCase()).ToArray())				  
+				  .AllTypes());
+
+		  Assert.True(queryResults.IsValid);
+
+		  foreach (var d in queryResults.Documents)
+			Assert.IsNotNull(d);
+		}
 	}
 }
