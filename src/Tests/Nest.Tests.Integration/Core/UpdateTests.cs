47b03a524e4dc342cabf09795def2ad55e08a580
Martijn Laarman <Mpdreamz@gmail.com>
2014-05-06 15:06:36 +0200

    fix #638 try to infer id of Document() if Object() is missing

--- src/Tests/Nest.Tests.Integration/Core/UpdateTests.cs
+++ src/Tests/Nest.Tests.Integration/Core/UpdateTests.cs
@@ -23,20 +23,31 @@ namespace Nest.Tests.Integration.Core
 			Assert.AreEqual(project.LOC, loc + 10);
 			Assert.AreNotEqual(project.Version, "1");
 		}
+		
+		public class ElasticsearchProjectLocUpdate
+		{
+			public int Id { get; set; }
+			[ElasticProperty(Name="loc",AddSortField=true)]
+			public int LOC { get; set; }
+		}
 
 		[Test]
 		public void DocAsUpsert()
 		{
-			var project = this._client.Source<ElasticsearchProject>(s => s.Id(1));
+			var project = this._client.Source<ElasticsearchProject>(s => s.Id(2));
 			Assert.NotNull(project);
 			Assert.Greater(project.LOC, 0);
 			var loc = project.LOC;
-			this._client.Update<ElasticsearchProject>(u => u
-				.Document(project)
+			this._client.Update<ElasticsearchProject, ElasticsearchProjectLocUpdate>(u => u
+				.Document(new ElasticsearchProjectLocUpdate
+				{
+					Id = project.Id,
+					LOC = project.LOC + 10
+				})
 				.DocAsUpsert()
 				.Refresh()
 			);
-			project = this._client.Source<ElasticsearchProject>(s => s.Id(1));
+			project = this._client.Source<ElasticsearchProject>(s => s.Id(2));
 			Assert.AreEqual(project.LOC, loc + 10);
 			Assert.AreNotEqual(project.Version, "1");
 		}
