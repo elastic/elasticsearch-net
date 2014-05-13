749d17915a59345acdd09f218f2bec3d4e1f6ad6
Martijn Laarman <Mpdreamz@gmail.com>
2014-05-13 16:49:17 +0200

    started on reproducing #654 and in the process found what made #658 / #659 fail

---
+++ src/Tests/Nest.Tests.Integration/Reproduce/Reproduce654Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Elasticsearch.Net;
+using Elasticsearch.Net.Connection;
+using Nest.Tests.MockData;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+using System.Diagnostics;
+using FluentAssertions;
+
+namespace Nest.Tests.Integration.Reproduce
+{
+	[TestFixture]
+	public class Reproduce654Tests : IntegrationTests
+	{
+
+		/// <summary>
+		/// https://github.com/Mpdreamz/NEST/issues/654
+		/// </summary>
+		[Test]
+		public async void AsyncMethodsShouldNotThrowOnHttpError()
+		{
+			Assert.DoesNotThrow(async () =>
+			{
+				var searchResult = await this._client.DeleteAsync("index", "type", "id");
+			});
+		}
+		[Test]
+		public void SyncMethodsShouldNotThrowOnHttpError()
+		{
+			Assert.DoesNotThrow(() =>
+			{
+				var searchResult = this._client.Delete("index", "type", "id");
+			});
+		}
+	}
+}
