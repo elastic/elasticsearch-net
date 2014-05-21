db8924b634eb497c8520c6e3d6450855a06339d9
Daniel Low <daniel.low@zazzle.com>
2014-05-21 15:33:46 -0700

    Add DelimitedPayloadTokenFilter according to www.elasticsearch.org/guide/en/elasticsearch/reference/current/analysis-delimited-payload-tokenfilter.html

---
+++ src/Nest/Domain/Analysis/TokenFilter/DelimitedPayloadTokenFilter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Nest
+{
+    /// <summary>
+    /// A token filter of type delimited_token_filter. Splits tokens into tokens and payload whenever a delimiter character is found.
+    /// </summary>
+    public class DelimitedPayloadTokenFilter : TokenFilterBase
+    {
+
+        public DelimitedPayloadTokenFilter()
+            : base("delimited_payload_filter")
+        { }
+
+        /// <summary>
+        /// Character used for splitting the tokens. Default is '|'.
+        /// </summary>
+        [JsonProperty("delimiter")]
+        public char? Delimiter { get; set; }
+
+        /// <summary>
+        /// The type of the payload. 'int' for integer, 'float' for float and 'identity' for characters. Default is 'float.'
+        /// </summary>
+        [JsonProperty("encoding")]
+        public string Encoding { get; set; }
+
+    }
+}
