54ecb8ac0e2ccc42e4970f4ed1a8e7e04ecdf53b
Martijn Laarman <Mpdreamz@gmail.com>
2014-06-10 10:06:17 +0200

    updated nested aggregation tests to include getting to the aggregations without the dictonary

--- src/Tests/Nest.Tests.Integration/Aggregations/NestedBucketAggregationTests.cs
+++ src/Tests/Nest.Tests.Integration/Aggregations/NestedBucketAggregationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Security.AccessControl;
 using Elasticsearch.Net;
 using FluentAssertions;
 using Nest.Tests.MockData.Domain;
@@ -25,11 +26,22 @@ namespace Nest.Tests.Integration.Aggregations
 					)
 				)
 			);
+
+			//using the helper to return typed aggregation buckets
 		    results.IsValid.Should().BeTrue();
 		    var bucket = results.Aggs.Nested("contributors");
 		    bucket.DocCount.Should().BeGreaterThan(1);
 
 		    var averageAge = bucket.Average("avg_age");
+		    averageAge.Should().NotBeNull();
+		    averageAge.Value.Should().HaveValue()
+			    .And.BeGreaterOrEqualTo(18);
+
+			//Using the .Aggregation dictionary.
+			var contributors = results.Aggregations["contributors"] as SingleBucket;
+			contributors.Should().NotBeNull();
+			contributors.DocCount.Should().BeGreaterThan(1);
+			averageAge = contributors.Aggregations["avg_age"] as ValueMetric;
 		    averageAge.Should().NotBeNull();
 		    averageAge.Value.Should().HaveValue()
 			    .And.BeGreaterOrEqualTo(18);
