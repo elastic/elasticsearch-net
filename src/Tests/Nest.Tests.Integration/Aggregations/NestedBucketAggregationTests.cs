a4e77ce0732c60ea98426d12909a42a017d4b6ba
Martijn Laarman <Mpdreamz@gmail.com>
2014-06-10 10:08:51 +0200

    spacing on NestedBucketAggregationTests

--- src/Tests/Nest.Tests.Integration/Aggregations/NestedBucketAggregationTests.cs
+++ src/Tests/Nest.Tests.Integration/Aggregations/NestedBucketAggregationTests.cs
@@ -7,20 +7,20 @@ using NUnit.Framework;
 
 namespace Nest.Tests.Integration.Aggregations
 {
-    [TestFixture]
-    public class NestedBucketAggregationTests : IntegrationTests
-    {
-	    [Test]
-	    public void Terms()
-	    {
-		    var results = this._client.Search<ElasticsearchProject>(s=>s
+	[TestFixture]
+	public class NestedBucketAggregationTests : IntegrationTests
+	{
+		[Test]
+		public void Terms()
+		{
+			var results = this._client.Search<ElasticsearchProject>(s => s
 				.Size(0)
-				.Aggregations(a=>a
-					.Nested("contributors", n=>n
-						.Path(p=>p.Contributors)
-						.Aggregations(t=>t
-							.Average("avg_age", m=>m
-								.Field(p=>p.Contributors.First().Age)
+				.Aggregations(a => a
+					.Nested("contributors", n => n
+						.Path(p => p.Contributors)
+						.Aggregations(t => t
+							.Average("avg_age", m => m
+								.Field(p => p.Contributors.First().Age)
 							)
 						)
 					)
@@ -28,24 +28,24 @@ namespace Nest.Tests.Integration.Aggregations
 			);
 
 			//using the helper to return typed aggregation buckets
-		    results.IsValid.Should().BeTrue();
-		    var bucket = results.Aggs.Nested("contributors");
-		    bucket.DocCount.Should().BeGreaterThan(1);
+			results.IsValid.Should().BeTrue();
+			var bucket = results.Aggs.Nested("contributors");
+			bucket.DocCount.Should().BeGreaterThan(1);
 
-		    var averageAge = bucket.Average("avg_age");
-		    averageAge.Should().NotBeNull();
-		    averageAge.Value.Should().HaveValue()
-			    .And.BeGreaterOrEqualTo(18);
+			var averageAge = bucket.Average("avg_age");
+			averageAge.Should().NotBeNull();
+			averageAge.Value.Should().HaveValue()
+				.And.BeGreaterOrEqualTo(18);
 
 			//Using the .Aggregation dictionary.
 			var contributors = results.Aggregations["contributors"] as SingleBucket;
 			contributors.Should().NotBeNull();
 			contributors.DocCount.Should().BeGreaterThan(1);
 			averageAge = contributors.Aggregations["avg_age"] as ValueMetric;
-		    averageAge.Should().NotBeNull();
-		    averageAge.Value.Should().HaveValue()
-			    .And.BeGreaterOrEqualTo(18);
-	    }
-	
-    }
+			averageAge.Should().NotBeNull();
+			averageAge.Value.Should().HaveValue()
+				.And.BeGreaterOrEqualTo(18);
+		}
+
+	}
 }
