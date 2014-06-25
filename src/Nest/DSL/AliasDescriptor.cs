74e0694c9def039a7d404dfccc2a74e9267004f7
Martijn Laarman <Mpdreamz@gmail.com>
2014-06-25 21:19:05 +0200

    cleaned up some unused namespaces

--- src/Nest/DSL/AliasDescriptor.cs
+++ src/Nest/DSL/AliasDescriptor.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using Elasticsearch.Net;
 using Newtonsoft.Json;
-using System.Linq.Expressions;
-using Nest.Resolvers;
-using Nest.Domain;
 
 namespace Nest
 {
