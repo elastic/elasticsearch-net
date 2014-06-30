a6ef1c83761e7051b8e87c0002f759f1ba4eb6f6
gmarz <greg.marzouka@gmail.com>
2014-06-30 09:10:33 -0400

    Fluent support for configuring similarity modules

---
+++ src/Nest/Domain/Similarity/DFRSimilarity.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nest
+{
+	public class DFRSimilarity : SimilarityBase
+	{
+		public DFRSimilarity()
+		{
+			this.Type = "DFR";
+		}
+
+		[JsonProperty("basic_model")]
+		public string BasicModel { get; set; }
+
+		[JsonProperty("after_effect")]
+		public string AfterEffect { get; set; }
+	}
+}
