cf32ce77231695cc3f67b0c517db456efca9fcb6
Greg Marzouka <greg.marzouka@gmail.com>
2014-07-03 16:26:37 -0400

    OIS support for Snapshot

--- src/Nest/DSL/SnapshotDescriptor.cs
+++ src/Nest/DSL/SnapshotDescriptor.cs
@@ -8,18 +8,96 @@ using Newtonsoft.Json;
 
 namespace Nest
 {
-	[DescriptorFor("SnapshotCreate")]
-	public partial class SnapshotDescriptor : RepositorySnapshotPathDescriptor<SnapshotDescriptor, SnapshotRequestParameters>
-	{
-		[JsonProperty("indices")]
-		internal IEnumerable<IndexNameMarker> _Indices { get; set; }
+    public interface ISnapshotRequest : IRepositorySnapshotPath<SnapshotRequestParameters>
+    {
+        [JsonProperty("indices")]
+		IEnumerable<IndexNameMarker> Indices { get; set; }
+
 		[JsonProperty("ignore_unavailable")]
-		internal bool? _IgnoreUnavailable { get; set; }
+		bool? IgnoreUnavailable { get; set; }
+
 		[JsonProperty("include_global_state")]
-		internal bool? _IncludeGlobalState { get; set; }
+		bool? IncludeGlobalState { get; set; }
+
 		[JsonProperty("partial")]
-		internal bool? _Partial { get; set; }
-		
+		bool? Partial { get; set; }
+    }
+
+    public interface ISnapshotRequest<T> : ISnapshotRequest where T : class
+    {
+        [JsonProperty("indices")]
+		IEnumerable<IndexNameMarker> Indices { get; set; }
+
+		[JsonProperty("ignore_unavailable")]
+		bool? IgnoreUnavailable { get; set; }
+
+		[JsonProperty("include_global_state")]
+		bool? IncludeGlobalState { get; set; }
+
+		[JsonProperty("partial")]
+		bool? Partial { get; set; }
+    }
+
+    internal static class SnapshotPathInfo
+    {
+        public static void Update(IConnectionSettingsValues settings, ElasticsearchPathInfo<SnapshotRequestParameters> pathInfo)
+        {
+            pathInfo.HttpMethod = PathInfoHttpMethod.PUT;
+        }
+    }
+
+    public partial class SnapshotRequest : RepositorySnapshotPathBase<SnapshotRequestParameters>, ISnapshotRequest
+    {
+        public IEnumerable<IndexNameMarker> Indices { get; set; }
+
+        public bool? IgnoreUnavailable { get; set; }
+
+        public bool? IncludeGlobalState { get; set; }
+
+        public bool? Partial { get; set; }
+
+        protected override void UpdatePathInfo(IConnectionSettingsValues settings, ElasticsearchPathInfo<SnapshotRequestParameters> pathInfo)
+        {
+            SnapshotPathInfo.Update(settings, pathInfo);
+        }
+    }
+
+    public partial class SnapshotRequest<T> : RepositorySnapshotPathBase<SnapshotRequestParameters>, ISnapshotRequest<T>
+        where T : class
+    {
+        public IEnumerable<IndexNameMarker> Indices { get; set; }
+
+        public bool? IgnoreUnavailable { get; set; }
+
+        public bool? IncludeGlobalState { get; set; }
+
+        public bool? Partial { get; set; }
+        
+        protected override void UpdatePathInfo(IConnectionSettingsValues settings, ElasticsearchPathInfo<SnapshotRequestParameters> pathInfo)
+        {
+            SnapshotPathInfo.Update(settings, pathInfo);
+        }
+    }
+
+	[DescriptorFor("SnapshotCreate")]
+	public partial class SnapshotDescriptor 
+        : RepositorySnapshotPathDescriptor<SnapshotDescriptor, SnapshotRequestParameters>, ISnapshotRequest
+	{
+        private ISnapshotRequest Self { get { return this; } }
+
+        IEnumerable<IndexNameMarker> ISnapshotRequest.Indices { get; set; }
+        bool? ISnapshotRequest.IgnoreUnavailable { get; set; }
+
+        bool? ISnapshotRequest.IncludeGlobalState { get; set; }
+
+        bool? ISnapshotRequest.Partial { get; set; }
+
+        string IRepositorySnapshotPath<SnapshotRequestParameters>.Repository { get; set; }
+
+        string IRepositorySnapshotPath<SnapshotRequestParameters>.Snapshot { get; set; }
+
+        SnapshotRequestParameters IRequest<SnapshotRequestParameters>.RequestParameters { get; set; }
+
 		public SnapshotDescriptor Index(string index)
 		{
 			return this.Indices(index);
@@ -32,28 +110,28 @@ namespace Nest
 			
 		public SnapshotDescriptor Indices(params string[] indices)
 		{
-			this._Indices = indices.Select(s=>(IndexNameMarker)s);
+			this.Self.Indices = indices.Select(s=>(IndexNameMarker)s);
 			return this;
 		}
 
 		public SnapshotDescriptor Indices(params Type[] indicesTypes)
 		{
-			this._Indices = indicesTypes.Select(s=>(IndexNameMarker)s);
+			this.Self.Indices = indicesTypes.Select(s=>(IndexNameMarker)s);
 			return this;
 		}
 		public SnapshotDescriptor IgnoreUnavailable(bool ignoreUnavailable = true)
 		{
-			this._IgnoreUnavailable = ignoreUnavailable;
+			this.Self.IgnoreUnavailable = ignoreUnavailable;
 			return this;
 		}
 		public SnapshotDescriptor IncludeGlobalstate(bool includeGlobalState = true)
 		{
-			this._IncludeGlobalState = includeGlobalState;
+			this.Self.IncludeGlobalState = includeGlobalState;
 			return this;
 		}
 		public SnapshotDescriptor Partial(bool partial = true)
 		{
-			this._Partial = partial;
+			this.Self.Partial = partial;
 			return this;
 		}
 
@@ -61,6 +139,5 @@ namespace Nest
 		{
 			pathInfo.HttpMethod = PathInfoHttpMethod.PUT;
 		}
-
-	}
+    }
 }
