5e53a376b6e789cd0cc0740219750026fe8ff21a
Martijn Laarman <Mpdreamz@gmail.com>
2014-07-04 12:03:20 +0200

    cleaned up test

--- src/Tests/Nest.Tests.Unit/ObjectInitializer/Snapshots/RestoreRequestTests.cs
+++ src/Tests/Nest.Tests.Unit/ObjectInitializer/Snapshots/RestoreRequestTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Elasticsearch.Net;
 using FluentAssertions;
 using Nest;
 using NUnit.Framework;
@@ -12,36 +13,36 @@ namespace Nest.Tests.Unit.ObjectInitializer.Snapshots
 	[TestFixture]
 	public class RestoreRequestTests : BaseJsonTests
 	{
-		[Test]
-		public void Url()
-		{
-			var response = this._client.Restore(new RestoreRequest("repos", "snap")
-			{
-				IgnoreUnavailable = true,
-				IncludeGlobalState = true,
-				WaitForCompletion = true
-			});
+		private IRestoreRequest _request;
+		private IRestoreResponse _response;
+		private IElasticsearchResponse _status;
 
-			var status = response.ConnectionStatus;
-			var url = status.RequestUrl;
-			url.Should().EndWith("/_snapshot/repos/snap/_restore?wait_for_completion=true");
-			status.RequestMethod.Should().Be("POST");
-		}
-		
-		[Test]
-		public void RestoreBody()
+		public RestoreRequestTests()
 		{
-			var response = this._client.Restore(new RestoreRequest("repos", "snap")
+			this._request = new RestoreRequest("repos", "snap")
 			{
 				IgnoreUnavailable = true,
 				IncludeGlobalState = true,
 				WaitForCompletion = true,
 				RenamePattern = "index_(.+)",
 				RenameReplacement = "index_restored_$1"
-			});
-			var status = response.ConnectionStatus;
+			};
+			this._response = this._client.Restore(this._request);
+			this._status = this._response.ConnectionStatus;
+		}
 
-			this.JsonEquals(status.Request, MethodBase.GetCurrentMethod());
+		[Test]
+		public void Url()
+		{
+			var url = this._status.RequestUrl;
+			url.Should().EndWith("/_snapshot/repos/snap/_restore?wait_for_completion=true");
+			this._status.RequestMethod.Should().Be("POST");
+		}
+		
+		[Test]
+		public void RestoreBody()
+		{
+			this.JsonEquals(this._status.Request, MethodBase.GetCurrentMethod());
 		}
 	}
 }
