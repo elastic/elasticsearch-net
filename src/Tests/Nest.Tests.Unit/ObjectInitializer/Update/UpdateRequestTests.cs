2fb3d42e975374ba1221cb3214ab66a27db466bf
Greg Marzouka <greg.marzouka@gmail.com>
2014-07-07 13:26:29 -0400

    Update() OIS tests

---
+++ src/Tests/Nest.Tests.Unit/ObjectInitializer/Update/UpdateRequestTests.cs
@@ -0,0 +1,44 @@
+﻿using Elasticsearch.Net;
+using FluentAssertions;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nest.Tests.Unit.ObjectInitializer.Update
+{
+	public class UpdateRequestTests : BaseJsonTests
+	{
+		private readonly IElasticsearchResponse _status;
+		public UpdateRequestTests()
+		{
+			var project = new ElasticsearchProject { Id = 1 };
+
+			var request = new UpdateRequest<ElasticsearchProject, dynamic>(project)
+				{
+					Document = new { Name = "NEST" },
+					DocAsUpsert = true
+				};
+
+			var response = this._client.Update<ElasticsearchProject, dynamic>(request);
+			this._status = response.ConnectionStatus;
+		}
+
+		[Test]
+		public void Url()
+		{
+			this._status.RequestUrl.Should().EndWith("/_update");
+			this._status.RequestMethod.Should().Be("POST");
+		}
+
+		[Test]
+		public void UpdateBody()
+		{
+			this.JsonEquals(this._status.Request, MethodInfo.GetCurrentMethod());
+		}
+	}
+}
