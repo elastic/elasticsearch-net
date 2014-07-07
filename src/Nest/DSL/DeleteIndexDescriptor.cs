5b25d024b3fdfff27f82e6148c334c1f8706cd69
Martijn Laarman <Mpdreamz@gmail.com>
2014-07-07 13:08:42 +0200

    added DeleteIndex OIS unit tests

--- src/Nest/DSL/DeleteIndexDescriptor.cs
+++ src/Nest/DSL/DeleteIndexDescriptor.cs
@@ -8,7 +8,7 @@ using Newtonsoft.Json;
 namespace Nest
 {
 	[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
-	public interface IDeleteIndexRequest : IIndicesOptionalPath<DeleteIndexRequestParameters> { }
+	public interface IDeleteIndexRequest : IIndicesOptionalExplicitAllPath<DeleteIndexRequestParameters> { }
 
 	internal static class DeleteIndexPathInfo
 	{
@@ -18,19 +18,32 @@ namespace Nest
 		}
 	}
 	
-	public partial class DeleteIndexRequest : IndicesOptionalPathBase<DeleteIndexRequestParameters>, IDeleteIndexRequest
+	public partial class DeleteIndexRequest : IndicesOptionalExplicitAllPathBase<DeleteIndexRequestParameters>, IDeleteIndexRequest
 	{
+
+		public DeleteIndexRequest(IEnumerable<IndexNameMarker> indices)
+		{
+			this.Indices = indices;
+			this.AllIndices = false;
+		}
+		public DeleteIndexRequest(IndexNameMarker index)
+		{
+			this.Indices = new [] { index };
+			this.AllIndices = false;
+		}
+
 		protected override void UpdatePathInfo(IConnectionSettingsValues settings, ElasticsearchPathInfo<DeleteIndexRequestParameters> pathInfo)
 		{
 			DeleteIndexPathInfo.Update(pathInfo, this);
 		}
 	}
 	[DescriptorFor("IndicesDelete")]
-	public partial class DeleteIndexDescriptor : IndicesOptionalPathDescriptor<DeleteIndexDescriptor, DeleteIndexRequestParameters>, IDeleteIndexRequest
+	public partial class DeleteIndexDescriptor : IndicesOptionalExplicitAllPathDescriptor<DeleteIndexDescriptor, DeleteIndexRequestParameters>, IDeleteIndexRequest
 	{
 		protected override void UpdatePathInfo(IConnectionSettingsValues settings, ElasticsearchPathInfo<DeleteIndexRequestParameters> pathInfo)
 		{
 			DeleteIndexPathInfo.Update(pathInfo, this);
 		}
+
 	}
 }
---
+++ src/Tests/Nest.Tests.Unit/ObjectInitializer/DeleteIndex/DeleteIndexRequestTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+
+namespace Nest.Tests.Unit.ObjectInitializer.DeleteIndex
+{
+	[TestFixture]
+	public class DeleteIndexRequestTests : BaseJsonTests
+	{
+		private readonly IElasticsearchResponse _status;
+
+		public DeleteIndexRequestTests()
+		{
+			var request = new DeleteIndexRequest(new IndexNameMarker[] { "postfixed-index-*", typeof(ElasticsearchProject)})
+			{
+				Timeout = "2m"
+			};
+			var response = this._client.DeleteIndex(request);
+			this._status = response.ConnectionStatus;
+		}
+
+		[Test]
+		public void Url()
+		{
+			this._status.RequestUrl.Should().EndWith("/postfixed-index-*%2Cnest_test_data?timeout=2m");
+			this._status.RequestMethod.Should().Be("DELETE");
+		}
+		
+	}
+}
