45f23947b5d1441b0809d1f8b0ea77a3ab5b062c
Greg Marzouka <greg.marzouka@gmail.com>
2014-07-07 11:51:23 -0400

    UpdateSettings() OIS tests

---
+++ src/Tests/Nest.Tests.Unit/ObjectInitializer/Update/UpdateSettingsRequestTests.cs
@@ -0,0 +1,73 @@
+﻿using Elasticsearch.Net;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nest.Tests.Unit.ObjectInitializer.Update
+{
+	[TestFixture]
+	public class UpdateSettingsRequestTests : BaseJsonTests
+	{
+		private readonly IElasticsearchResponse _status;
+
+		public UpdateSettingsRequestTests()
+		{
+			var request = new UpdateSettingsRequest
+				{
+					NumberOfReplicas = 5,
+					AutoExpandReplicas = false,
+					BlocksReadOnly = true,
+					BlocksRead = true,
+					BlocksWrite = false,
+					BlocksMetadata = true,
+					RefreshInterval = "30s",
+					IndexConcurrency = 5,
+					Codec = "default",
+					CodecBloomLoad = false,
+					FailOnMergeFailure = false,
+					TranslogFlushTreshHoldOps = "10s",
+					TranslogFlushThresholdSize = "2048",
+					TranslogFlushThresholdPeriod = "5s",
+					TranslogDisableFlush = true,
+					CacheFilterMaxSize = "-1",
+					CacheFilterExpire = "-1",
+					GatewaySnapshotInterval = "5s",
+					RoutingAllocationInclude = new Dictionary<string, object> { { "tag", "value1,value2" } },
+					RoutingAllocationExclude = new Dictionary<string, object> { { "tag", "value3" } },
+					RoutingAllocationRequire = new Dictionary<string, object> { { "group4", "aaa" } },
+					RoutingAllocationDisableAllication = false,
+					RoutingAllocationDisableNewAllocation = false,
+					RoutingAllocationDisableReplicaAllocation = false,
+					RoutingAllocationTotalShardsPerNode = 1,
+					RecoveryInitialShards = "full",
+					TtlDisablePurge = true,
+					GcDeletes = true,
+					TranslogFsType = "simple",
+					CompoundFormat = true,
+					CompoundOnFlush = true,
+					WarmersEnabled = true
+				};
+
+			var response = this._client.UpdateSettings(request);
+			this._status = response.ConnectionStatus;
+		}
+
+		[Test]
+		public void Url()
+		{
+			this._status.RequestUrl.Should().EndWith("/_settings");
+			this._status.RequestMethod.Should().Be("PUT");
+		}
+
+		[Test]
+		public void UpdateSettingsBody()
+		{
+			this.JsonEquals(this._status.Request, MethodInfo.GetCurrentMethod());
+		}
+	}
+}
