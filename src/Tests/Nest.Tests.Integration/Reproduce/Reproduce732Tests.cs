11cbbee67ba581ec7670ff0f0664be5cb654eacb
Greg Marzouka <greg.marzouka@gmail.com>
2014-07-09 10:36:48 -0400

    Add test in attempt to reproduce #732

---
+++ src/Tests/Nest.Tests.Integration/Reproduce/Reproduce732Tests.cs
@@ -0,0 +1,36 @@
+﻿using Nest.Tests.MockData;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nest.Tests.Integration.Reproduce
+{
+	[TestFixture]
+	public class Reproduce732Tests : IntegrationTests
+	{
+		[Test]
+		public void UpdateUsingDynamicObject()
+		{
+			var id = NestTestData.Data.Last().Id;
+			var project = this._client.Source<ElasticsearchProject>(s => s.Id(id));
+			Assert.NotNull(project);
+			var loc = project.LOC;
+			this._client.Update<ElasticsearchProject, dynamic>(u => u
+				.Id(id)
+				.Document(new
+				{
+					Id = project.Id,
+					LOC = project.LOC + 10
+				})
+				.Refresh()
+			);
+			project = this._client.Source<ElasticsearchProject>(s => s.Id(id));
+			Assert.AreEqual(project.LOC, loc + 10);
+			Assert.AreNotEqual(project.Version, "1");
+		}
+	}
+}
