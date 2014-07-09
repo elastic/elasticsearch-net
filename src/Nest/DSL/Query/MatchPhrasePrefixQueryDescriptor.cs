a398f0e75d4c52bd308c0573b39ba6d4cbc09495
Martijn Laarman <Mpdreamz@gmail.com>
2014-07-09 11:28:26 +0200

    fix #741 missing minimum_should_match on match queries

--- src/Nest/DSL/Query/MatchPhrasePrefixQueryDescriptor.cs
+++ src/Nest/DSL/Query/MatchPhrasePrefixQueryDescriptor.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System.Linq.Expressions;
 
 namespace Nest
 {
