2f426c97846321eea68229d0252c410e7e0cf7e6
Greg Marzouka <greg.marzouka@gmail.com>
2014-07-10 10:16:32 -0400

    Bumping version to 1.0.0-RC1

--- src/Elasticsearch.Net/Properties/AssemblyInfo.cs
+++ src/Elasticsearch.Net/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("1.0.0")]
 [assembly: AssemblyFileVersionAttribute("1.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0-ci20140710110619")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0-rc1")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
