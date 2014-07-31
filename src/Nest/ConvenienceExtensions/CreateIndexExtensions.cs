ddf4fd0a95663d0aebd6c87732dcba51a8a4989b
David Pfeffer <bytenik@bytenik.com>
2014-07-31 14:09:12 -0400

    added back typo for backward compat

--- src/Nest/ConvenienceExtensions/CreateIndexExtensions.cs
+++ src/Nest/ConvenienceExtensions/CreateIndexExtensions.cs
@@ -23,6 +23,20 @@ namespace Nest
 			createIndexSelector = createIndexSelector ?? (c => c);
 			return client.CreateIndex(c=> createIndexSelector(c).Index(index));
 		}
+		
+		/// <summary>
+		/// The create index API allows to instantiate an index. Elasticsearch provides support for multiple indices, 
+		/// including executing operations across several indices.
+		/// <para> </para>http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/indices-create-index.html
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="index">The name of the index to be created</param>
+		/// <param name="createIndexSelector">A descriptor that further describes the parameters for the create index operation</param>
+		public static IIndicesOperationResponse CreatIndex(this IElasticClient client, string index, 
+			Func<CreateIndexDescriptor, CreateIndexDescriptor> createIndexSelector = null )
+		{
+			return CreateIndex(client, index, createIndexSelector);
+		}
 
 		/// <summary>
 		/// The create index API allows to instantiate an index. Elasticsearch provides support for multiple indices, 
