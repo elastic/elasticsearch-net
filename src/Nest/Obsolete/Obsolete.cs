cd4e28e26929a70582dbb3111be7b4e6de87f7d7
Martijn Laarman <Mpdreamz@gmail.com>
2014-08-06 17:06:14 +0200

    added NEST bwc placeholders for renamed IndicesExistsAlias* classes

---
+++ src/Nest/Obsolete/Obsolete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nest.Obsolete
+{
+	[Obsolete("Scheduled to be removed in 2.0, renamed to AliasExistsDescriptor")]
+	public class IndicesExistsAliasDescriptor : AliasExistsDescriptor {}
+
+	[Obsolete("Scheduled to be removed in 2.0, renamed to AliasExistsRequest")]
+	public class IndicesExistsAliasRequest : AliasExistsRequest 
+	{
+		public IndicesExistsAliasRequest() : base("") {}
+		public IndicesExistsAliasRequest(string name) : base(name) { }
+		public IndicesExistsAliasRequest(IndexNameMarker index, string name) : base(index, name) { }
+	}
+
+
+
+}
