2b000d0d9df5a08c487b51a4312333572a50b84a
Anders Østhus <anders.osthus@outlook.com>
2014-09-16 16:46:04 +0200

    Updated BaseConverterTest to inherit BaseJonTests

--- src/Tests/Nest.Tests.Unit/Converters/BaseConverterTest.cs
+++ src/Tests/Nest.Tests.Unit/Converters/BaseConverterTest.cs
@@ -1,33 +1,16 @@
-﻿using System.Globalization;
-using System.IO;
-using System.Threading;
-using Elasticsearch.Net.Connection;
+﻿using System.IO;
 
 namespace Nest.Tests.Unit.Converters
 {
-	public abstract class BaseConverterTest
+	public abstract class BaseConverterTest : BaseJsonTests
 	{
-		protected IElasticClient Client;
-
-		protected BaseConverterTest()
-		{
-			Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-us");
-			IConnectionSettingsValues settings = new ConnectionSettings(UnitTestDefaults.Uri, UnitTestDefaults.DefaultIndex)
-				.DisablePing()
-				.ExposeRawResponse();
-
-			var connection = new InMemoryConnection(settings);
-
-			Client = new ElasticClient(settings, connection);
-		}
-
 		protected T SerializeAndDeserialize<T>(T expected)
 		{
-			var json = Client.Serializer.Serialize(expected);
+			var json = _client.Serializer.Serialize(expected);
 
 			using (var ms = new MemoryStream(json))
 			{
-				return Client.Serializer.Deserialize<T>(ms);
+				return _client.Serializer.Deserialize<T>(ms);
 			}
 		}
 
