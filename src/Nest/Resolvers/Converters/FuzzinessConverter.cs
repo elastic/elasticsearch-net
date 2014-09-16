323e0fd75034601570f705b04c59280581a15322
Anders Østhus <anders.osthus@outlook.com>
2014-09-16 17:07:52 +0200

    Made the MatchQueryJsonConverter use the FuzzinessConverter
    Now the MatchQueryJsonConverter uses the FuzzinessConverter to
    deserialize IFuzziness.
    This removes the ugly object creation in FuzzinessConverter to get the
    object into a unified format.

--- src/Nest/Resolvers/Converters/FuzzinessConverter.cs
+++ src/Nest/Resolvers/Converters/FuzzinessConverter.cs
@@ -22,10 +22,19 @@ namespace Nest.Resolvers.Converters
 
 		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 		{
-			if (reader.TokenType == JsonToken.None) return null;
-
-			var jsonPath = reader.Path.Split('.').Last();
-			return FuzzinessConverterHelper.ReadJson(new JObject(new JProperty(jsonPath, reader.Value)));
+			if (reader.TokenType == JsonToken.String)
+				return Fuzziness.Auto;
+			if (reader.TokenType == JsonToken.Integer)
+			{
+				var editDistance = Convert.ToInt32(reader.Value);
+				return Fuzziness.EditDistance(editDistance);
+			}
+			if (reader.TokenType == JsonToken.Float)
+			{
+				var ratio = (reader.Value as double?).GetValueOrDefault(0);
+				return Fuzziness.Ratio(ratio);
+			}
+			return null;
 		}
 
 		public override bool CanConvert(Type objectType)
