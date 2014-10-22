107989be7830f8b45227960a422e92fc8cf196a4
Greg Marzouka <greg.marzouka@gmail.com>
2014-10-22 18:13:54 -0400

    Bump version to 1.2.1

--- src/Tests/Elasticsearch.Net.Tests.Unit/Properties/AssemblyInfo.cs
+++ src/Tests/Elasticsearch.Net.Tests.Unit/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.2.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.2.0")]
+[assembly: AssemblyFileVersionAttribute("1.2.1")]
+[assembly: AssemblyInformationalVersionAttribute("1.2.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
