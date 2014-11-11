d3e8df6e8b09566c51310b6523d84e2e18cf2123
gmarz <greg.marzouka@gmail.com>
2014-11-11 16:37:23 -0500

    Separate file for ITransportDelegator

--- src/Elasticsearch.Net/Connection/Transport.cs
+++ src/Elasticsearch.Net/Connection/Transport.cs
@@ -20,31 +20,6 @@ using PurifyNet;
 
 namespace Elasticsearch.Net.Connection
 {
-	internal interface ITransportDelegator
-	{
-		bool Ping(ITransportRequestState requestState);
-		Task<bool> PingAsync(ITransportRequestState requestState);
-		IList<Uri> Sniff(ITransportRequestState ownerState = null);
-		void SniffClusterState(ITransportRequestState requestState = null);
-		void SniffIfInformationIsTooOld(ITransportRequestState requestState);
-		void SniffOnConnectionFailure(ITransportRequestState requestState);
-
-		/// <summary>
-		/// Returns either the fixed maximum set on the connection configuration settings or the number of nodes
-		/// </summary>
-		/// <param name="requestState"></param>
-		int GetMaximumRetries(IRequestConfiguration requestConfiguration);
-
-		bool SniffingDisabled(IRequestConfiguration requestConfiguration);
-		bool SniffOnFaultDiscoveredMoreNodes(ITransportRequestState requestState, int retried, ElasticsearchResponse<Stream> streamResponse);
-
-		/// <summary>
-		/// Selects next node uri on request state
-		/// </summary>
-		/// <returns>bool hint whether the new current node needs to pinged first</returns>
-		bool SelectNextNode(ITransportRequestState requestState);
-	}
-
 	public class Transport : ITransport, ITransportDelegator
 	{
 
