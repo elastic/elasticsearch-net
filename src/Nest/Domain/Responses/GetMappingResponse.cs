9ef2536f69c7afc2605e2d034255b86e22adeecd
Martijn Laarman <mpdreamz@gmail.com>
2014-11-25 12:37:10 +0100

    Implemented support for getmapping to return the results for multiple mappings fix #1066

--- src/Nest/Domain/Responses/GetMappingResponse.cs
+++ src/Nest/Domain/Responses/GetMappingResponse.cs
@@ -7,8 +7,15 @@ namespace Nest
 {
 	public interface IGetMappingResponse : IResponse
 	{
+		Dictionary<string, IList<TypeMapping>> Mappings { get; }
 		RootObjectMapping Mapping { get; }
 		void Accept(IMappingVisitor visitor);
+	}
+
+	public class TypeMapping
+	{
+		public string TypeName { get; internal set; }
+		public RootObjectMapping Mapping  { get; internal set; }
 	}
 
 	internal class GetRootObjectMappingWrapping : Dictionary<string, Dictionary<string, Dictionary<string, RootObjectMapping>>>
@@ -21,31 +28,43 @@ namespace Nest
 		public GetMappingResponse()
 		{
 			this.IsValid = true;
+			this.Mappings = new Dictionary<string, IList<TypeMapping>>();
 		}
+
 
 		internal GetMappingResponse(IElasticsearchResponse status, GetRootObjectMappingWrapping dict)
 		{
+			this.Mappings = new Dictionary<string, IList<TypeMapping>>();
 			this.IsValid = status.Success && dict != null && dict.Count > 0;
 			if (!this.IsValid) return;
-			var firstNode = dict.First();
-			if (!firstNode.Value.HasAny())
-				return;
-			var mappingNode = firstNode.Value["mappings"];
-			if (mappingNode == null)
+			foreach (var index in dict)
 			{
-				this.IsValid = false;
-				return;
+				if (index.Value == null || !index.Value.ContainsKey("mappings"))
+					continue;
+
+				var mappings = index.Value["mappings"];
+				this.Mappings.Add(index.Key, new List<TypeMapping>());
+				if (mappings == null) continue;
+				foreach (var mapping in mappings)
+				{
+					if (mapping.Value == null) continue;
+					var typeMapping = new TypeMapping
+					{
+						TypeName = mapping.Key,
+						Mapping = mapping.Value
+					};
+					mapping.Value.Name = mapping.Key;
+					this.Mappings[index.Key].Add(typeMapping);
+				}
 			}
-			var mapping = mappingNode.First();
-			if (mapping.Value == null)
-			{
-				this.IsValid = false;
-				return;
-			}
-			mapping.Value.Name = mapping.Key;
-			this.Mapping = mapping.Value;
+			
+			this.Mapping = this.Mappings.Where(kv=>kv.Value.HasAny(v=>v.Mapping != null))
+				.SelectMany(kv=>kv.Value)
+				.Select(tm=>tm.Mapping)
+				.FirstOrDefault(t=>t != null);
 		}
 
+		public Dictionary<string, IList<TypeMapping>> Mappings { get; internal set; }
 		public RootObjectMapping Mapping { get; internal set; }
 
 		public void Accept(IMappingVisitor visitor)
