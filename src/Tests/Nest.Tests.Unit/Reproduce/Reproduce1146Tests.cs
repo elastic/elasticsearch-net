acfb02be89880621810d054682d86b8c7e322ef8
Martijn Laarman <mpdreamz@gmail.com>
2014-12-15 14:31:44 +0100

    added test to showcase inference for #1146

---
+++ src/Tests/Nest.Tests.Unit/Reproduce/Reproduce1146Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest.Tests.MockData.Domain;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Nest.Tests.Unit.Reproduce
+{
+	/// <summary>
+	/// tests to reproduce reported errors
+	/// </summary>
+	[TestFixture]
+	public class Reproduce1146Tests : BaseJsonTests
+	{
+		[Test]
+		public void DeleteByQueryInference()
+		{
+			var ids = new[] {"1", "2", "3"};
+			IResponse response = this._client.DeleteByQuery<string>(s => s
+				.Query(q => q.Terms("string.ProgramId", ids))
+			);
+			var status = response.ConnectionStatus;
+			status.RequestUrl.Should().EndWith("/nest_test_data/string/_query");
+			
+			response = this._client.DeleteByQuery<JObject>(s => s
+				.Query(q => q.Terms("string.ProgramId", ids))
+			);
+			status = response.ConnectionStatus;
+			status.RequestUrl.Should().EndWith("/nest_test_data/jobject/_query");
+		}
+	}
+}
