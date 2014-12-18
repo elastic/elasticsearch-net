370662a2f86f8611eee08ca4db764af8c93b3cdf
eddyzanda <edsonzandam@gmail.com>
2014-12-18 09:38:53 +0200

    Thrift connection timeout #1159
    Changed the Tsocket to timeout using the property from the
    connection-settings.

--- src/Connections/Elasticsearch.Net.Connection.Thrift/Transport/TSocket.cs
+++ src/Connections/Elasticsearch.Net.Connection.Thrift/Transport/TSocket.cs
@@ -117,8 +117,16 @@ namespace Elasticsearch.Net.Connection.Thrift.Transport
 			{
 				InitSocket();
 			}
+            
+		    var connectionRequest = client.BeginConnect(host, port, null, null);
+		    
+            var connected = connectionRequest.AsyncWaitHandle.WaitOne(timeout);
 
-			client.Connect(host, port);
+		    if (!connected)
+		    {
+		        throw new Exception("Failed to connect");
+		    }
+
 			inputStream = client.GetStream();
 			outputStream = client.GetStream();
 		}
