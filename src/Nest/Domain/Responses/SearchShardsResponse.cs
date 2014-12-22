a93b22803dfb1384bb7055f0798b0f55f5212985
udi <udi@sears.co.il>
2014-12-23 00:29:18 +0200

    Fix wrong property mapping in SearchShard

--- src/Nest/Domain/Responses/SearchShardsResponse.cs
+++ src/Nest/Domain/Responses/SearchShardsResponse.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections.Generic;
-using Nest.Domain;
+﻿using Nest.Domain;
 using Newtonsoft.Json;
-using System.Linq.Expressions;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Nest
 {
 	[JsonObject(MemberSerialization.OptIn)]
-	public interface ISearchShardsResponse : IResponse 
+	public interface ISearchShardsResponse : IResponse
 	{
 		[JsonProperty("shards")]
 		IEnumerable<IEnumerable<SearchShard>> Shards { get; }
@@ -20,34 +20,39 @@ namespace Nest
 	public class SearchShardsResponse : BaseResponse, ISearchShardsResponse
 	{
 		public IEnumerable<IEnumerable<SearchShard>> Shards { get; internal set; }
+
 		public IDictionary<string, SearchNode> Nodes { get; internal set; }
 	}
-	
 
 	[JsonObject(MemberSerialization.OptIn)]
 	public class SearchNode
 	{
 		[JsonProperty("name")]
 		public string Name { get; set; }
+
 		[JsonProperty("transport_address")]
 		public string TransportAddress { get; set; }
-
 	}
 
 	[JsonObject(MemberSerialization.OptIn)]
 	public class SearchShard
 	{
-		[JsonProperty("name")]
-		public string State { get; set;}		
+		[JsonProperty("state")]
+		public string State { get; set; }
+
 		[JsonProperty("primary")]
-		public bool Primary { get; set;}		
+		public bool Primary { get; set; }
+
 		[JsonProperty("node")]
-		public string Node { get; set;}		
+		public string Node { get; set; }
+
 		[JsonProperty("relocating_node")]
-		public string RelocatingNode { get; set;}		
+		public string RelocatingNode { get; set; }
+
 		[JsonProperty("shard")]
-		public int Shard { get; set;}		
+		public int Shard { get; set; }
+
 		[JsonProperty("index")]
-		public string Index { get; set;}		
+		public string Index { get; set; }
 	}
 }
