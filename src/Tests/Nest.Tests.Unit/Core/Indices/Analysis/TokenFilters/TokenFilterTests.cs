76c47b1843ae9caa1e750ad2d527a3d23d8b7680
Daniel Silva <dsilva@passeidireto.com>
2015-02-02 16:49:08 -0200

    Add support for predefined language specific stopwords

---
+++ src/Tests/Nest.Tests.Unit/Core/Indices/Analysis/TokenFilters/TokenFilterTests.cs
@@ -0,0 +1,43 @@
+﻿using Nest.Tests.Unit.Core.Indices.Analysis.Tokenizers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nest.Tests.Unit.Core.Indices.Analysis.TokenFilters
+{
+	[TestFixture]
+	public class TokenFilterTests : BaseAnalysisTests
+	{
+		[Test]
+		public void TestStopTokenFilterWithArray()
+		{
+			var result = this.Analysis(a => a
+				.TokenFilters(t => t
+					.Add("my_stop", new StopTokenFilter
+					{
+						Stopwords = new string[] { "and", "is", "the" }
+					})
+				)
+			);
+			this.JsonEquals(result.ConnectionStatus.Request, MethodInfo.GetCurrentMethod());
+		}
+
+		[Test]
+		public void TestStopTokenFilterWithPredefinedLanguageList()
+		{
+			var result = this.Analysis(a => a
+				.TokenFilters(t => t
+					.Add("my_stop", new StopTokenFilter
+					{
+						Stopwords = "_english_"
+					})
+				)
+			);
+			this.JsonEquals(result.ConnectionStatus.Request, MethodInfo.GetCurrentMethod());
+		}
+	}
+}
