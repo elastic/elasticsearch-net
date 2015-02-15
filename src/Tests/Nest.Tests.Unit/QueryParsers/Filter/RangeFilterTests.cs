07a97d14db70595372651465c5958a37ad977130
udi <udi@sears.co.il>
2015-02-15 18:34:28 +0200

    Add support for execution in range filter:
    http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/query-dsl-range-filter.html#_execution

--- src/Tests/Nest.Tests.Unit/QueryParsers/Filter/RangeFilterTests.cs
+++ src/Tests/Nest.Tests.Unit/QueryParsers/Filter/RangeFilterTests.cs
@@ -1,21 +1,21 @@
-using System;
-using System.Globalization;
 using FluentAssertions;
 using NUnit.Framework;
+using System;
+using System.Globalization;
 
 namespace Nest.Tests.Unit.QueryParsers.Filter
 {
 	[TestFixture]
-	public class RangeFilterTests : ParseFilterTestsBase 
+	public class RangeFilterTests : ParseFilterTestsBase
 	{
 		[Test]
 		[TestCase("cacheName", "cacheKey", true)]
 		public void Range_Deserializes(string cacheName, string cacheKey, bool cache)
 		{
-			var rangeFilter = this.SerializeThenDeserialize(cacheName, cacheKey, cache, 
-				f=>f.Range,
-				f=>f.Range(n => n
-					.OnField(p=>p.LOC)
+			var rangeFilter = this.SerializeThenDeserialize(cacheName, cacheKey, cache,
+				f => f.Range,
+				f => f.Range(n => n
+					.OnField(p => p.LOC)
 					.GreaterOrEquals("10")
 					.LowerOrEquals("20")
 					)
@@ -24,17 +24,16 @@ namespace Nest.Tests.Unit.QueryParsers.Filter
 			rangeFilter.Field.Should().Be("loc");
 			rangeFilter.LowerThanOrEqualTo.Should().Be("20");
 			rangeFilter.GreaterThanOrEqualTo.Should().Be("10");
-
 		}
 
 		[Test]
 		[TestCase("cacheName", "cacheKey", true)]
 		public void Range_Long_Deserializes(string cacheName, string cacheKey, bool cache)
 		{
-			var rangeFilter = this.SerializeThenDeserialize(cacheName, cacheKey, cache, 
-				f=>f.Range,
-				f=>f.Range(n => n
-					.OnField(p=>p.LOC)
+			var rangeFilter = this.SerializeThenDeserialize(cacheName, cacheKey, cache,
+				f => f.Range,
+				f => f.Range(n => n
+					.OnField(p => p.LOC, FilterRangeExecutionType.FieldData)
 					.GreaterOrEquals(10)
 					.LowerOrEquals(20)
 					)
@@ -42,34 +41,37 @@ namespace Nest.Tests.Unit.QueryParsers.Filter
 
 			rangeFilter.GreaterThanOrEqualTo.Should().Be("10");
 			rangeFilter.LowerThanOrEqualTo.Should().Be("20");
+			rangeFilter.ExecutionType.Should().Be(FilterRangeExecutionType.FieldData);
 		}
-		
+
 		[Test]
 		[TestCase("cacheName", "cacheKey", true)]
 		public void Range_DateTime_Deserializes(string cacheName, string cacheKey, bool cache)
 		{
 			var dateFrom = DateTime.UtcNow.AddDays(-1);
 			var dateTo = DateTime.UtcNow.AddDays(1);
-			var rangeFilter = this.SerializeThenDeserialize(cacheName, cacheKey, cache, 
-				f=>f.Range,
-				f=>f.Range(n => n
-					.OnField(p=>p.LOC)
+			var rangeFilter = this.SerializeThenDeserialize(cacheName, cacheKey, cache,
+				f => f.Range,
+				f => f.Range(n => n
+					.OnField(p => p.LOC)
 					.GreaterOrEquals(dateFrom)
 					.LowerOrEquals(dateTo)
 					)
 				);
-			rangeFilter.GreaterThanOrEqualTo.Should().Be(dateFrom.ToString("yyyy-MM-dd'T'HH:mm:ss.fff", CultureInfo.InvariantCulture));
-			rangeFilter.LowerThanOrEqualTo.Should().Be(dateTo.ToString("yyyy-MM-dd'T'HH:mm:ss.fff", CultureInfo.InvariantCulture));
+			rangeFilter.GreaterThanOrEqualTo.Should()
+				.Be(dateFrom.ToString("yyyy-MM-dd'T'HH:mm:ss.fff", CultureInfo.InvariantCulture));
+			rangeFilter.LowerThanOrEqualTo.Should()
+				.Be(dateTo.ToString("yyyy-MM-dd'T'HH:mm:ss.fff", CultureInfo.InvariantCulture));
 		}
-		
+
 		[Test]
 		[TestCase("cacheName", "cacheKey", true)]
 		public void Range_Double_Deserializes(string cacheName, string cacheKey, bool cache)
 		{
-			var rangeFilter = this.SerializeThenDeserialize(cacheName, cacheKey, cache, 
-				f=>f.Range,
-				f=>f.Range(n => n
-					.OnField(p=>p.LOC)
+			var rangeFilter = this.SerializeThenDeserialize(cacheName, cacheKey, cache,
+				f => f.Range,
+				f => f.Range(n => n
+					.OnField(p => p.LOC)
 					.GreaterOrEquals(20.1)
 					.LowerOrEquals(20.22)
 					)
@@ -77,6 +79,5 @@ namespace Nest.Tests.Unit.QueryParsers.Filter
 			rangeFilter.GreaterThanOrEqualTo.Should().Be("20.1");
 			rangeFilter.LowerThanOrEqualTo.Should().Be("20.22");
 		}
-
 	}
 }
