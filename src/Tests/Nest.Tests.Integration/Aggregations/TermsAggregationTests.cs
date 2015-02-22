d746480d487f95dc6567f7cab8a9d6eb4e6168ca
Manish Kumar Gupta <manishkg@microsoft.com>
2015-02-21 22:22:53 -0800

    Adding collect_mode to terms aggretation descriptor

--- src/Tests/Nest.Tests.Integration/Aggregations/TermsAggregationTests.cs
+++ src/Tests/Nest.Tests.Integration/Aggregations/TermsAggregationTests.cs
@@ -95,5 +95,56 @@ namespace Nest.Tests.Integration.Aggregations
 				}
 			}
 		}
+
+		[Test]
+		public void NestedEmptyAggregationThreeLevelDeepsWithBFS()
+		{
+			var results = this.Client.Search<ElasticsearchProject>(s => s
+				.Size(0)
+				.Aggregations(a => a
+					.Terms("countries", t => t
+						.Field(p => p.Country)
+						.CollectMode(TermsAggregationCollectMode.BreadthFirst)
+						.Aggregations(aa => aa
+							.Terms("noop", tt => tt.Field("noop"))
+							.Terms("names", tt => tt
+								.Field(p => p.Name)
+								.Aggregations(aaa => aaa
+									.Average("avg_loc", avg => avg.Field(p => p.LOC))
+								)
+							)
+						)
+					)
+				)
+			);
+			results.IsValid.Should().BeTrue();
+			var termBucket = results.Aggs.Terms("countries");
+			termBucket.Should().NotBeNull();
+			termBucket.Items.Should().NotBeEmpty()
+				.And.OnlyContain(i => !i.Key.IsNullOrEmpty())
+				.And.OnlyContain(i => i.DocCount > 0);
+
+			foreach (var term in termBucket.Items)
+			{
+				var country = term.Key;
+				country.Should().NotBeNullOrWhiteSpace();
+
+				var noop = term.Terms("noop");
+				noop.Should().NotBeNull();
+				noop.Items.Should().BeEmpty();
+
+				var names = term.Terms("names");
+				names.Should().NotBeNull();
+				names.Items.Should().NotBeEmpty();
+
+				foreach (var nameTerm in names.Items)
+				{
+					var name = nameTerm.Key;
+					var average = nameTerm.Average("avg_loc");
+					average.Should().NotBeNull();
+					average.Value.Should().HaveValue();
+				}
+			}
+		}
 	}
 }
