d03b7226a1ded34a4668d3632306f7286f6ce0ec
Martijn Laarman <mpdreamz@gmail.com>
2015-02-23 12:22:38 +0100

    added unit tests for the various terms aggregation options

---
+++ src/Tests/Nest.Tests.Unit/Search/Aggregations/Terms/TermAggregationTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+
+namespace Nest.Tests.Unit.Search.Aggregations.Terms
+{
+	[TestFixture]
+	public class TermAggregationTests : BaseJsonTests
+	{
+		[Test]
+		public void TermAggregationSerializes()
+		{
+			var s = new TermsAggregationDescriptor<ElasticsearchProject>()
+				.CollectMode(TermsAggregationCollectMode.BreadthFirst)
+				.ExecutionHint(TermsAggregationExecutionHint.GlobalOrdinalsLowCardinality)
+				.Field(p => p.Country)
+				.MinimumDocumentCount(1)
+				.OrderAscending("_count");
+
+			this.JsonEquals(s, MethodBase.GetCurrentMethod());
+		}
+	}
+}
