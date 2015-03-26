4e628c10a4a6187cd888f34b98a407f62a4cdd39
Martijn Laarman <mpdreamz@gmail.com>
2015-03-26 17:39:50 +0100

    added unit test to illustrate #991

---
+++ src/Tests/Nest.Tests.Unit/Reproduce/Reproduce991Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Elasticsearch.Net;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+
+namespace Nest.Tests.Unit.Reproduce
+{
+	/// <summary>
+	/// tests to reproduce reported errors
+	/// </summary>
+	[TestFixture]
+	public class Reproduce991Tests : BaseJsonTests
+	{
+		private class MyClass
+		{
+			public MyEnum MyEnum { get; set; }
+		}
+
+		private enum MyEnum
+		{
+			Value1,
+			Value2
+		}
+
+		[Test]
+		public void EnumQueryDefaultsToInt()
+		{
+			var query = new SearchDescriptor<MyClass>()
+				.Query(q => q.Term(p => p.MyEnum, MyEnum.Value2));
+			this.JsonEquals(query, MethodBase.GetCurrentMethod());
+		}
+	}
+}
