d4cdaa3174824ec15d187458f8e72779d30fb80c
Martijn Laarman <mpdreamz@gmail.com>
2015-05-01 16:28:17 +0200

    started with IDocument approach, wrote a unit test against Search to showcase a more general usecase

--- src/Nest/Domain/DSL/IDocument.cs
+++ src/Nest/Domain/DSL/IDocument.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Nest
 {
+	[JsonConverter(typeof(DocumentConverter))]
 	public interface IDocument
 	{
 		/// <summary>
@@ -12,18 +15,40 @@ namespace Nest
 		T OfType<T>() where T : class;
 	}
 
+	public class DocumentConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			var d = value as Document;
+			if (d == null || d._Value == null)
+				return;
+			writer.WriteToken(d._Value.CreateReader());
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			var document = serializer.Deserialize(reader) as JToken;
+			return new Document {_Value = document};
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return true;
+		}
+	}
 	public class Document : IDocument
 	{
-		private Lazy<object> _value;
+		internal JToken _Value { get; set; }
 
-		public Document(Func deserializer)
+		public Document()
 		{
-			_value = new Lazy<object>(deserializer);
+			//_value = new Lazy<object>(deserializer);
 		}
 
 		public T OfType<T>() where T : class
 		{
-			return this._value.Value as T;
+			var jToken = this._Value;
+			return jToken != null ? jToken.ToObject<T>() : null;
 		}
 	}
 }
