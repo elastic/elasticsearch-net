f8901425a0e990bd41df0c713dded534937c17d1
Martijn Laarman <mpdreamz@gmail.com>
2015-05-07 13:00:40 +0200

    fix #1363 added more translog stats on recovery api and cat recovery

--- src/Nest/Domain/Recovery/RecoveryTranslogStatus.cs
+++ src/Nest/Domain/Recovery/RecoveryTranslogStatus.cs
@@ -7,6 +7,19 @@ namespace Nest
 		[JsonProperty("recovered")]
 		public long Recovered { get; internal set; }
 
+		[JsonProperty("total")]
+		public long Total { get; internal set; }
+
+		[JsonProperty("percent")]
+		public string Percent { get; internal set; }
+
+		[JsonProperty("total_on_start")]
+		public long TotalOnStart { get; internal set; }
+
+		[JsonProperty("total_time")]
+		public string TotalTime { get; internal set; }
+
+		//TODO should be a long in 2.0
 		[JsonProperty("total_time_in_millis")]
 		public string TotalTimeInMilliseconds { get; internal set; }
 	}
