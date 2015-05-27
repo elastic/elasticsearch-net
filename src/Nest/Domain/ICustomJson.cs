9d48ab762979b9af6ccfdad8a57462188d6e6ff3
gmarz <greg.marzouka@gmail.com>
2015-05-27 12:47:42 -0400

    Rename IDomainObject => INestSerializable

--- src/Nest/Domain/ICustomJson.cs
+++ src/Nest/Domain/ICustomJson.cs
@@ -2,14 +2,12 @@
 
 namespace Nest
 {
-	//If an object implements this then it can handle its own json representation
+	// If an object implements this then it can handle its own json representation
 	public interface ICustomJson
 	{
 		object GetCustomJson();
 	}
 
-	public interface IDomainObject
-	{
-		
-	}
+	// Any object that implements this will automatically be added to the serialization contract
+	public interface INestSerializable { }
 }
