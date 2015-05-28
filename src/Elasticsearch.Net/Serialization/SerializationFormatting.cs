652732e9b8b0f5fcf19654419dccdc5531d6b566
Martijn Laarman <mpdreamz@gmail.com>
2015-05-28 09:52:57 +0200

    moved ElasticsearchNetJsonStrategy

--- src/Elasticsearch.Net/Serialization/SerializationFormatting.cs
+++ src/Elasticsearch.Net/Serialization/SerializationFormatting.cs
@@ -1,25 +1,8 @@
-using System;
-using System.Collections.Generic;
-
 namespace Elasticsearch.Net.Serialization
 {
 	public enum SerializationFormatting
 	{
 		None,
 		Indented
-	}
-
-	internal class ElasticsearchNetJsonStrategy : PocoJsonSerializerStrategy
-	{
-		public override object DeserializeObject(object value, Type type)
-		{
-			if (type == typeof (DynamicDictionary))
-			{
-				var dict = base.DeserializeObject(value, typeof (Dictionary<string, object>)) as IDictionary<string, object>;
-				return dict == null ? null : DynamicDictionary.Create(dict);
-			}
-			return base.DeserializeObject(value, type);
-		}
-
 	}
 }
