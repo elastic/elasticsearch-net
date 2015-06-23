0db92a27e0ce98a897a2e4848a79ef4fadecce30
gmarz <greg.marzouka@gmail.com>
2015-06-23 09:29:05 -0400

    Fix #1463: ScriptFile and ScriptId not serialized

--- src/Nest/Domain/Bulk/BulkUpdateBody.cs
+++ src/Nest/Domain/Bulk/BulkUpdateBody.cs
@@ -18,6 +18,12 @@ namespace Nest
 
 		[JsonProperty(PropertyName = "script")]
 		internal string _Script { get; set; }
+
+		[JsonProperty(PropertyName = "script_id")]
+		internal string _ScriptId { get; set; }
+
+		[JsonProperty(PropertyName = "script_file")]
+		internal string _ScriptFile { get; set; }
 		
 		[JsonProperty(PropertyName = "params")]
 		[JsonConverter(typeof(DictionaryKeysAreNotPropertyNamesJsonConverter))]
