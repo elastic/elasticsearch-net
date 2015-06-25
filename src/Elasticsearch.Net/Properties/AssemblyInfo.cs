4b579a96afa55e7aab8ee9de158cb1ccb506044a
gmarz <greg.marzouka@gmail.com>
2015-06-25 12:57:41 -0400

    Bump version to 1.6.1

--- src/Elasticsearch.Net/Properties/AssemblyInfo.cs
+++ src/Elasticsearch.Net/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.6.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.6.0")]
+[assembly: AssemblyFileVersionAttribute("1.6.1")]
+[assembly: AssemblyInformationalVersionAttribute("1.6.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
