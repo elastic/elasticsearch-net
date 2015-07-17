23ef718c30136e48394bf27c218f6fd8bebdb607
Martijn Laarman <mpdreamz@gmail.com>
2015-07-17 13:14:36 +0200

    added a test trying to reproduce #1440 to no avail

---
+++ src/Tests/Nest.Tests.Unit/Reproduce/Reproduce1440Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+
+namespace Nest.Tests.Unit.Reproduce
+{
+	/// <summary>
+	/// tests to reproduce reported errors
+	/// </summary>
+	[TestFixture]
+	public class Reproduce1440Tests : BaseJsonTests
+	{
+		[Test]
+		public void CountShouldNotThrowWhenNoDefaultIndexSpecified()
+		{
+			var client = new ElasticClient();
+			var request = client.Count<ElasticsearchProject>();
+			var path = new Uri(request.ConnectionStatus.RequestUrl).AbsolutePath;
+			path.Should().Be("/_all/elasticsearchprojects/_count");
+			request = client.Count<ElasticsearchProject>(c=>c.Index("x"));
+			path = new Uri(request.ConnectionStatus.RequestUrl).AbsolutePath;
+			path.Should().Be("/x/elasticsearchprojects/_count");
+		}
+
+	}
+}
