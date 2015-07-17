5a1e12d3cb31ee43a0d9ad272bbdf868da77344e
Martijn Laarman <mpdreamz@gmail.com>
2015-07-17 13:57:21 +0200

    updated test #1464 to show correct usage

--- src/Tests/Nest.Tests.Unit/Reproduce/Reproduce1464Tests.cs
+++ src/Tests/Nest.Tests.Unit/Reproduce/Reproduce1464Tests.cs
@@ -3,10 +3,12 @@ using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Elasticsearch.Net.Connection;
 
 namespace Nest.Tests.Unit.Reproduce
 {
@@ -16,17 +18,24 @@ namespace Nest.Tests.Unit.Reproduce
 		[Test]
 		public void Issue1464()
 		{
+			var rawClient = new ElasticsearchClient(connection: new InMemoryConnection(new ConnectionSettings()));
+
 			var bulkEntries = new List<object>();
 			bulkEntries.Add(new { index = new { _index = "ourIndex", _type = "ourType", _id = "ourId" } });
 
-			var doc = JsonConvert.DeserializeObject<dynamic>("{ \"id\": \"3\"}");
+			var doc = rawClient.Serializer.Deserialize<dynamic>(new MemoryStream("{ \"id\": \"3\"}".Utf8Bytes()));
 			doc.someprop = "some value";
 			bulkEntries.Add(doc);
 
 			var response = this._client.Raw.Bulk(bulkEntries);
 			var actual = response.Request.Utf8String();
-
 			this.BulkJsonEquals(actual, MethodBase.GetCurrentMethod());
+
+			response = rawClient.Bulk(bulkEntries);
+			actual = response.Request.Utf8String();
+			this.BulkJsonEquals(actual, MethodBase.GetCurrentMethod());
+
+
 		}
 	}
 }
