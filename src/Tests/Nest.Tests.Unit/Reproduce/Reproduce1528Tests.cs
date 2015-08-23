19382bae209ec12786335f062e509ea5b89d9d5d
Martijn Laarman <mpdreamz@gmail.com>
2015-08-23 12:06:20 +0200

    trying to reproduce #1528, no luck yet though

--- src/Tests/Nest.Tests.Unit/Reproduce/Reproduce1528Tests.cs
+++ src/Tests/Nest.Tests.Unit/Reproduce/Reproduce1528Tests.cs
@@ -21,7 +21,6 @@ namespace Nest.Tests.Unit.Reproduce
 			public string Name { get; set; }
 		}
 
-
 		[Test]
 		public void Issue1528()
 		{
@@ -56,6 +55,46 @@ namespace Nest.Tests.Unit.Reproduce
 			Assert.Throws<NullReferenceException>(()=>{
 				client.Bulk(descriptor);
 			});
+		}
+
+		[Test]
+		public void Issue1528_ExplicitTypeAndIndex()
+		{
+			var settings = new ConnectionSettings(defaultIndex: "default-index");
+			var client = new ElasticClient(settings, new InMemoryConnection(settings));
+			var descriptor = new BulkDescriptor();
+
+			foreach (var i in Enumerable.Range(0, 5))
+			{
+				descriptor.Index<MyClass>(op => op
+					.Index("myindex")
+					.Type("sometype")
+					.Document(new MyClass { Name = "my_id_" + i })
+				);
+			}
+			var result = client.Bulk(descriptor);
+			var jsonRequest = result.ConnectionStatus.Request;
+			this.BulkJsonEquals(jsonRequest.Utf8String(), MethodBase.GetCurrentMethod());
+		}
+
+		[Test]
+		public void Issue1528_ExplicitTypeAndIndex_NoDefaultIndex()
+		{
+			var settings = new ConnectionSettings();
+			var client = new ElasticClient(settings, new InMemoryConnection(settings));
+			var descriptor = new BulkDescriptor();
+
+			foreach (var i in Enumerable.Range(0, 5))
+			{
+				descriptor.Index<MyClass>(op => op
+					.Index("myindex")
+					.Type("sometype")
+					.Document(new MyClass { Name = "my_id_" + i })
+				);
+			}
+			var result = client.Bulk(descriptor);
+			var jsonRequest = result.ConnectionStatus.Request;
+			this.BulkJsonEquals(jsonRequest.Utf8String(), MethodBase.GetCurrentMethod(), "Issue1528_ExplicitTypeAndIndex");
 		}
 
 	}
