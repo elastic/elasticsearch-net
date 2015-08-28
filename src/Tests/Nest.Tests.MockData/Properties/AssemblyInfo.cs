1318b68cf119cf5cbc906fee0901926fbb2d3d93
gmarz <greg.marzouka@gmail.com>
2015-08-28 12:48:00 -0400

    Bump version to 1.7.1

--- src/Tests/Nest.Tests.MockData/Properties/AssemblyInfo.cs
+++ src/Tests/Nest.Tests.MockData/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.7.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.7.0")]
+[assembly: AssemblyFileVersionAttribute("1.7.1")]
+[assembly: AssemblyInformationalVersionAttribute("1.7.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
