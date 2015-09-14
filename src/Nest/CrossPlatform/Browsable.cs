cff885d483e942dadec8f0a78b9117bbfe762cde
Martijn Laarman <mpdreamz@gmail.com>
2015-09-14 17:15:34 +0200

    trying to port NEST over but can't add reference Elasticsearch.Net without upsetting dnu restore

---
+++ src/Nest/CrossPlatform/Browsable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+#if DOTNETCORE
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace System.ComponentModel
+{
+	public class Browsable : Attribute
+	{
+		public Browsable(bool browsable) { }
+	}
+}
+#endif
