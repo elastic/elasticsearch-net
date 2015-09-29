7f1a2365e2045367934a6883f55f6270943661f0
Martijn Laarman <mpdreamz@gmail.com>
2015-09-29 11:43:14 +0200

    added GET/PUT/DELETE search template url tests

---
+++ src/Tests/Search/SearchTemplate/DeleteSearchTemplate/DeleteSearchTemplateUrlTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+using Tests.Framework;
+using Tests.Framework.MockData;
+using static Tests.Framework.UrlTester;
+
+namespace Tests.Search.SearchTemplate.DeleteSearchTemplate
+{
+	public class DeleteSearchTemplateUrlTests
+	{
+		[U] public async Task Urls()
+		{
+			var id = "the-id";
+			await DELETE("/_search/template/the-id")
+				.Fluent(c => c.DeleteSearchTemplate(id))
+				.Request(c=>c.DeleteSearchTemplate(new DeleteSearchTemplateRequest(id)))
+				.FluentAsync(c => c.DeleteSearchTemplateAsync(id))
+				.RequestAsync(c=>c.DeleteSearchTemplateAsync(new DeleteSearchTemplateRequest(id)))
+				;
+		}
+	}
+}
