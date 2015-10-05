87fd636d37ffe35ce0b6538d3f58819cddb17072
Greg Marzouka <greg.marzouka@gmail.com>
2015-10-05 12:15:25 -0400

    Separate folder for Metrics and IndexMetrics

--- src/Nest/CommonAbstractions/Infer/TODO.cs
+++ src/Nest/CommonAbstractions/Infer/TODO.cs
@@ -9,31 +9,8 @@ using Elasticsearch.Net.Serialization;
 
 namespace Nest
 {
-	public class Metrics : IUrlParameter
-	{
-		private readonly Enum _enumValue;
 
-		public string GetString(IConnectionConfigurationValues settings) => KnownEnums.Resolve(this._enumValue);
-		internal Metrics(IndicesStatsMetric metric) { _enumValue = metric; }
-		internal Metrics(NodesStatsMetric metric){ _enumValue = metric; }
-		internal Metrics(NodesInfoMetric metric){ _enumValue = metric; }
-		internal Metrics(ClusterStateMetric metric){ _enumValue = metric; }
 
-		public static implicit operator Metrics(IndicesStatsMetric metric) => new Metrics(metric);
-		public static implicit operator Metrics(NodesStatsMetric metric) => new Metrics(metric);
-		public static implicit operator Metrics(NodesInfoMetric metric) => new Metrics(metric);
-		public static implicit operator Metrics(ClusterStateMetric metric) => new Metrics(metric);
-	}
-
-	public class IndexMetrics : IUrlParameter
-	{
-		private readonly Enum _enumValue;
-
-		public string GetString(IConnectionConfigurationValues settings) => KnownEnums.Resolve(this._enumValue);
-		internal IndexMetrics(NodesStatsIndexMetric metric) { _enumValue = metric; }
-
-		public static implicit operator IndexMetrics(NodesStatsIndexMetric metric) => new IndexMetrics(metric);
-	}
 
 	public class Features : IUrlParameter
 	{
