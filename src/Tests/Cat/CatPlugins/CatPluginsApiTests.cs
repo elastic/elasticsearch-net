d300f15d186357452725f7d9efd103fa74ed8b1c
Martijn Laarman <mpdreamz@gmail.com>
2015-10-08 15:58:20 +0200

    Finished cat API

---
+++ src/Tests/Cat/CatPlugins/CatPluginsApiTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Tests.Framework;
+using Tests.Framework.Integration;
+using Xunit;
+
+namespace Tests.Cat.CatPlugins
+{
+	[Collection(IntegrationContext.ReadOnly)]
+	public class CatPluginsApiTests : ApiTestBase<ICatResponse<CatPluginsRecord>, ICatPluginsRequest, CatPluginsDescriptor, CatPluginsRequest>
+	{
+		public CatPluginsApiTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.CatPlugins(),
+			fluentAsync: (client, f) => client.CatPluginsAsync(),
+			request: (client, r) => client.CatPlugins(r),
+			requestAsync: (client, r) => client.CatPluginsAsync(r)
+		);
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 200;
+		protected override HttpMethod HttpMethod => HttpMethod.GET;
+		protected override string UrlPath => "/_cat/plugins";
+
+		[I] public async Task HasPlugins() => await this.AssertOnAllResponses(r =>
+		{
+			r.Records.Should().NotBeEmpty().And.Contain(a => !string.IsNullOrEmpty(a.Name) && a.Component == "delete-by-query");
+		});
+
+	}
+
+}
---
+++ src/Tests/Cat/CatSegments/CatSegmentsApiTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Tests.Framework;
+using Tests.Framework.Integration;
+using Xunit;
+
+namespace Tests.Cat.CatSegments
+{
+	[Collection(IntegrationContext.ReadOnly)]
+	public class CatSegmentsApiTests : ApiTestBase<ICatResponse<CatSegmentsRecord>, ICatSegmentsRequest, CatSegmentsDescriptor, CatSegmentsRequest>
+	{
+		public CatSegmentsApiTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.CatSegments(),
+			fluentAsync: (client, f) => client.CatSegmentsAsync(),
+			request: (client, r) => client.CatSegments(r),
+			requestAsync: (client, r) => client.CatSegmentsAsync(r)
+		);
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 200;
+		protected override HttpMethod HttpMethod => HttpMethod.GET;
+		protected override string UrlPath => "/_cat/segments";
+
+		[I] public async Task HasSegments() => await this.AssertOnAllResponses(r =>
+		{
+			r.Records.Should().NotBeEmpty().And.Contain(a => !string.IsNullOrEmpty(a.Index));
+		});
+
+	}
+
+}
