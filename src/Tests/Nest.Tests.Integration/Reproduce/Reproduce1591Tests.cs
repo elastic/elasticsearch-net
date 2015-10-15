8ff89b272c0c183bea02e579da9f773a74109c73
Robert Łysoń <robert@theministryofideas.com>
2015-10-15 23:36:43 +0200

    Fix for CatAliases API #1591 - build fix

---
+++ src/Tests/Nest.Tests.Integration/Reproduce/Reproduce1591Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Nest.Tests.Integration.Reproduce
+{
+	[TestFixture]
+	public class Reproduce1591Tests : IntegrationTests
+	{
+		private readonly string _aliasName = Guid.NewGuid().ToString();
+		private readonly string _indexName = Guid.NewGuid().ToString();
+
+		[SetUp]
+		public void SetUp()
+		{
+			Client.CreateIndex(_indexName);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			Client.DeleteIndex(_indexName);
+		}
+
+		[Test]
+		public void Test()
+		{
+			var putAliasResponse = Client.PutAlias(d => d
+				.Index(_indexName)
+				.Name(_aliasName)
+				.IndexRouting("indexrouting")
+				.SearchRouting("searchrouting"));
+
+			putAliasResponse.IsValid.Should().BeTrue();
+
+			var catResponse = Client.CatAliases(d => d.V()).Records
+				.FirstOrDefault(x => x.Index == _indexName);
+
+			catResponse.Should().NotBeNull();
+			catResponse.Alias.Should().Be(_aliasName);
+			catResponse.IndexRouting.Should().Be("indexrouting");
+			catResponse.SearchRouting.Should().Be("searchrouting");
+		}
+	}
+}
