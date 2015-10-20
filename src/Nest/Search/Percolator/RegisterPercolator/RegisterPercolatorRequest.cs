fa183ffc58717945bbbee7aaab9744e30b810d44
Greg Marzouka <greg.marzouka@gmail.com>
2015-10-20 16:04:39 -0400

    Started on percolator api tests, added new DictionaryToPropertiesJsonConverter to eventually replace ICustomJson

--- src/Nest/Search/Percolator/RegisterPercolator/RegisterPercolatorRequest.cs
+++ src/Nest/Search/Percolator/RegisterPercolator/RegisterPercolatorRequest.cs
@@ -6,47 +6,36 @@ using Newtonsoft.Json;
 
 namespace Nest
 {
-	//this does not represent a dedicated API in elasticsearch, its syntactic sugar for indexing percolate documents
+	// This does not represent a dedicated API in elasticsearch, its syntactic sugar for indexing percolate documents
 
 	[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
-	[JsonConverter(typeof(CustomJsonConverter))]
-	public interface IRegisterPercolatorRequest : IRequest<IndexRequestParameters>, ICustomJson
+	[JsonConverter(typeof(ReadAsTypeJsonConverter<RegisterPercolatorRequest>))]
+	public interface IRegisterPercolatorRequest : IRequest<IndexRequestParameters> 
 	{
+		[JsonConverter(typeof(DictionaryToPropertiesJsonConverter<object>))]
 		IDictionary<string, object> MetaData { get; set; }
+
+		[JsonProperty("query")]
 		QueryContainer Query { get; set; }
 	}
 
-	//TODO  Route parameters are complex need to be ported
-	
-	//internal static class RegisterPercolatorPathInfo
-	//{
-	//	public static void Update(RouteValues pathInfo, IRegisterPercolatorRequest request)
-	//	{
-	//		pathInfo.Id = pathInfo.Name;
-	//		pathInfo.Type = ".percolator";
-	//	}
-	//}
-
 	public class RegisterPercolatorRequest : RequestBase<IndexRequestParameters>, IRegisterPercolatorRequest
 	{
+		internal RegisterPercolatorRequest() { }
+
 		public IDictionary<string, object> MetaData { get; set; }
 		public QueryContainer Query { get; set; }
 
 		public RegisterPercolatorRequest(IndexName index, Name name) 
 			: base(r=>r.Required("index", index).Required("type", (TypeName)".percolator").Required("id", name)) { }
-
-		public object GetCustomJson()
-		{
-			return new FluentDictionary<string, object>(this.MetaData)
-				.Add("query", this.Query);
-		}
-
 	}
 
 	public class RegisterPercolatorDescriptor<T> 
 		: RequestDescriptorBase<RegisterPercolatorDescriptor<T>, IndexRequestParameters, IRegisterPercolatorRequest>, IRegisterPercolatorRequest
 		where T : class
 	{
+		internal RegisterPercolatorDescriptor() { }
+
 		public RegisterPercolatorDescriptor(Name name) 
 			: base(r=>r.Required("index", (IndexName)typeof(T)).Required("type", (TypeName)".percolator").Required("id", name)) { }
 
@@ -57,7 +46,8 @@ namespace Nest
 		IDictionary<string, object> IRegisterPercolatorRequest.MetaData { get; set; }
 
 		/// <summary>
-		/// Add metadata associated with this percolator query document
+		/// Add metadata associated with this percolator query document that can
+		/// later be used to query or filter specific percolated queries
 		/// </summary>
 		public RegisterPercolatorDescriptor<T> AddMetadata(Func<FluentDictionary<string, object>, FluentDictionary<string, object>> selector)
 		{
@@ -77,12 +67,6 @@ namespace Nest
 			var d = querySelector(new QueryContainerDescriptor<T>());
 			Self.Query = d;
 			return this;
-		}
-
-		public object GetCustomJson()
-		{
-			return new FluentDictionary<string, object>(Self.MetaData)
-				.Add("query", Self.Query);
 		}
 	}
 }
