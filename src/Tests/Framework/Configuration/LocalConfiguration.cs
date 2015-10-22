7b24b876c8f9e32394c02092493f0efbf0051ded
Greg Marzouka <greg.marzouka@gmail.com>
2015-10-22 09:44:26 -0400

    Moved integration override and version to a config file

---
+++ src/Tests/Framework/Configuration/LocalConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Framework.Configuration
+{
+	public class LocalConfiguration
+	{
+		public bool IntegrationOverride { get; private set; } = false;
+		public string ManualOverrideVersion { get; private set; } = "2.0.0-rc1";
+
+		public LocalConfiguration(string configurationFile)
+		{
+			if (!File.Exists(configurationFile)) return;
+
+			var config = File.ReadAllLines(configurationFile)
+				.ToDictionary(l => l.Split(':')[0], l => l.Split(':')[1]);
+
+			this.IntegrationOverride = bool.Parse(config["integration_override"]);
+			this.ManualOverrideVersion = config["override_version"];
+		}
+	}
+}
