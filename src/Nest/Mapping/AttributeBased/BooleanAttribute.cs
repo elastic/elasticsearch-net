ed04c5236f756e6f25395d98261161b27b828820
gmarz <greg.marzouka@gmail.com>
2015-12-01 20:34:16 -0500

    Remove unnecessary json attribute

--- src/Nest/Mapping/AttributeBased/BooleanAttribute.cs
+++ src/Nest/Mapping/AttributeBased/BooleanAttribute.cs
@@ -3,7 +3,6 @@ using System;
 
 namespace Nest
 {
-	[JsonObject(ItemTypeNameHandling = TypeNameHandling.Auto)]
 	public class BooleanAttribute : ElasticsearchPropertyAttribute, IBooleanProperty
 	{
 		IBooleanProperty Self => this;
