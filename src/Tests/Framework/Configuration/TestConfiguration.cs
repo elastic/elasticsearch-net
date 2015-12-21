448c5f39459ecbf237a160723849c26256337837
Mpdreamz <mpdreamz@gmail.com>
2015-12-21 18:32:18 +0100

    added the option back to override integration mode by specffying an env var, FAKE uses this to kick off integration tests

--- src/Tests/Framework/Configuration/TestConfiguration.cs
+++ src/Tests/Framework/Configuration/TestConfiguration.cs
@@ -7,15 +7,29 @@ namespace Tests.Framework.Configuration
 	public class TestConfiguration
 	{
 		public TestMode Mode { get; private set; } = TestMode.Unit;
-		public string ElasticsearchVersion { get; private set; } = "2.0.0-rc1";
-		public bool ForceReseed { get; private set; }
-		public bool DoNotSpawnIfAlreadyRunning { get; private set; }
+		public string ElasticsearchVersion { get; private set; } = "2.0.01";
+		public bool ForceReseed { get; private set; } = false;
+		public bool DoNotSpawnIfAlreadyRunning { get; private set; } = true;
 
 		public bool RunIntegrationTests => Mode == TestMode.Mixed || Mode == TestMode.Integration;
 		public bool RunUnitTests => Mode == TestMode.Mixed || Mode == TestMode.Unit;
 
+		private static string ElasticVersionInEnvironment = Environment.GetEnvironmentVariable("NEST_INTEGRATION_VERSION");
+
 		public TestConfiguration(string configurationFile)
 		{
+			//if env var NEST_INTEGRATION_VERSION is set assume integration mode
+			//used by the build script FAKE
+			if (!string.IsNullOrWhiteSpace(ElasticVersionInEnvironment))
+			{
+				this.ElasticsearchVersion = ElasticVersionInEnvironment;
+				this.Mode = TestMode.Integration;
+				this.ForceReseed = false;
+				this.DoNotSpawnIfAlreadyRunning = false;
+				return;
+			}
+
+
 			if (!File.Exists(configurationFile)) return;
 
 			var config = File.ReadAllLines(configurationFile)
