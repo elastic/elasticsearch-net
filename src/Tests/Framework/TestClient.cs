c29d46f556b892c1515e9eedf65433e4650376ee
gmarz <greg.marzouka@gmail.com>
2015-12-21 11:07:42 -0500

    Only set a fixed response explicitly, normalized fixed response arguments to byte[]

--- src/Tests/Framework/TestClient.cs
+++ src/Tests/Framework/TestClient.cs
@@ -55,11 +55,11 @@ namespace Tests.Framework
 		public static IElasticClient GetFixedReturnClient(object responseJson)
 		{
 			var serializer = new NestSerializer(new ConnectionSettings());
-			string fixedResult;
+			byte[] fixedResult;
 			using (var ms = new MemoryStream())
 			{
 				serializer.Serialize(responseJson, ms);
-				fixedResult =Encoding.UTF8.GetString(ms.ToArray());
+				fixedResult = ms.ToArray();
 			}
 			var connection = new InMemoryConnection(fixedResult);
 			var connectionPool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
