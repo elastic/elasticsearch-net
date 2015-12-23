5577ad79edf859443685052939341fbf3211a06e
gmarz <greg.marzouka@gmail.com>
2015-12-23 12:26:43 -0500

    Seeder cleanup, moved reseeding to collection fixture to reduce the number of times it's called

--- src/Tests/Framework/Integration/Clusters/ReadOnlyCluster.cs
+++ src/Tests/Framework/Integration/Clusters/ReadOnlyCluster.cs
@@ -1,11 +1,17 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Tests.Framework.Integration
 {
 	[CollectionDefinition(IntegrationContext.ReadOnly)]
 	public class ReadOnlyCluster : ClusterBase, ICollectionFixture<ReadOnlyCluster>, IClassFixture<EndpointUsage>
 	{
+		public ReadOnlyCluster()
+		{
+			new Seeder(this.Node.Port).SeedNode();
+		}
+
 		protected override bool DoNotSpawnIfAlreadyRunning => true;
-		public override void Boostrap() => new Seeder(this.Node.Port).SeedNode();
+		//public override void Boostrap() => new Seeder(this.Node.Port).SeedNode();
 	}
 }
