66ca9d9f606ab8a84fc72b03d712eb852c173d49
Mpdreamz <mpdreamz@gmail.com>
2015-12-30 20:41:28 +0100

    another attempt for TC to play nice

--- build/scripts/Testing.fsx
+++ build/scripts/Testing.fsx
@@ -6,16 +6,16 @@ open Fake
 open Paths
 open System.IO
 
-// Normalizes path for different OS 
-let inline normalizePath (path : string) =  
-    path.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar) 
+// Normalizes path for different OS
+let inline normalizePath (path : string) =
+    path.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar)
 
 type Tests() =
     static member RunAll() =
         !! Paths.Source("**/bin/Release/*.Tests.Unit.dll")
         |> NUnit (fun p ->
           {p with
-             ToolPath = "build/tools/Nunit.Runners/tools"
+             ToolPath = normalizePath("build/tools/Nunit.Runners/tools")
              DisableShadowCopy = true;
              OutputFile = normalizePath(Paths.Output("TestResults.xml")) }
          )
