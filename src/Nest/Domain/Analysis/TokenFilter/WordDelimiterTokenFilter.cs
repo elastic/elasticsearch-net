405d60d9ecb4188e2f171fceeb0e47c63fb6d635
Mpdreamz <mpdreamz@gmail.com>
2016-01-06 15:00:21 +0100

    possible fix for #1555, make class json serialization option to prevent adding obsolete TypeTable property

--- src/Nest/Domain/Analysis/TokenFilter/WordDelimiterTokenFilter.cs
+++ src/Nest/Domain/Analysis/TokenFilter/WordDelimiterTokenFilter.cs
@@ -8,6 +8,7 @@ namespace Nest
 	/// <summary>
 	/// Named word_delimiter, it Splits words into subwords and performs optional transformations on subword groups.
 	/// </summary>
+	[JsonObject(MemberSerialization.OptIn)]
     public class WordDelimiterTokenFilter : TokenFilterBase
     {
         public WordDelimiterTokenFilter()
