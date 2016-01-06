ef46d05fc8776a319408d42f122fd9afe501666a
Mpdreamz <mpdreamz@gmail.com>
2016-01-06 12:32:49 +0100

    add Reproduce1566Tests.cs

---
+++ src/Tests/Nest.Tests.Unit/Reproduce/Reproduce1566Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest.Tests.MockData.Domain;
+using NUnit.Framework;
+
+namespace Nest.Tests.Unit.Reproduce
+{
+	/// <summary>
+	/// tests to reproduce reported errors
+	/// </summary>
+	[TestFixture]
+	public class Reproduce1566Tests : BaseJsonTests
+	{
+		[Test]
+		public void RawSurvives()
+		{
+			var searchRequest = new SearchRequest()
+			{
+				Aggregations = new Dictionary<string, IAggregationContainer>(),
+			};
+
+			var filterContainer = new AggregationContainer()
+			{
+				Filter = new FilterAggregator
+				{
+					Filter = new FilterDescriptor<dynamic>().Raw(@"{""test"":""test""")
+				},
+				Aggregations = new Dictionary<string, IAggregationContainer>()
+			};
+
+			searchRequest.Aggregations["base_filter"] = filterContainer;
+			var serialized = _client.Serializer.Serialize(searchRequest).Utf8String();
+			serialized.Should().Contain(@"""test""");
+
+		}
+	}
+}
