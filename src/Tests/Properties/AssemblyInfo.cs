0d2c1dd9705f13107e10dd5870c6478ce0dd48da
Russ Cam <russ.cam@elastic.co>
2016-01-20 10:16:40 +1100

    Bump version to 2.0.0-alpha2

--- src/Tests/Properties/AssemblyInfo.cs
+++ src/Tests/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("2.0.0")]
 [assembly: AssemblyFileVersionAttribute("2.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.0.0-alpha1")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.0-alpha2")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "2.0.0";
