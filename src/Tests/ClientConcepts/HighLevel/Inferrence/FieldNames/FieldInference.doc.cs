3cc83afa7c852918dd0dbb6858304cf6a43ddda6
gmarz <greg.marzouka@gmail.com>
2016-01-27 15:38:38 -0500

    Move property name tests to separate folder

--- src/Tests/ClientConcepts/HighLevel/Inferrence/FieldNames/FieldInference.doc.cs
+++ src/Tests/ClientConcepts/HighLevel/Inferrence/FieldNames/FieldInference.doc.cs
@@ -332,20 +332,6 @@ namespace Tests.ClientConcepts.HighLevel.Inferrence.FieldNames
 				DefaultFieldNameInferrer = "shouting much?"
 			});
 
-		}
-
-		public class PropertyNames
-		{
-			[U] public void PropertyNamesAreResolvedToLastToken()
-			{
-				Expression<Func<Project, object>> expression = p => p.Name.Suffix("raw");
-				Expect("raw").WhenSerializing<PropertyName>(expression);
-			}
-
-			[U] public void StringsContainingDotsIsAnException()
-			{
-				Assert.Throws<ArgumentException>(() => Expect("exception!").WhenSerializing<PropertyName>("name.raw"));
-			}
-		}
+		}	
 	}
 }
