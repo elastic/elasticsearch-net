3420eaa829db3b5b2286e2869e1d07690faf8ca9
Russ Cam <russ.cam@elastic.co>
2016-02-02 14:32:50 +1100

    Add example use case for aggregating over a collection of aggregation functions
    Closes https://github.com/elastic/elasticsearch-net/issues/1740
    Run litterateur

--- src/Tests/Aggregations/WritingAggregations.doc.cs
+++ src/Tests/Aggregations/WritingAggregations.doc.cs
@@ -3,6 +3,8 @@ using Nest;
 using Tests.Framework;
 using Tests.Framework.MockData;
 using static Nest.Infer;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tests.Aggregations
 {
@@ -43,28 +45,28 @@ namespace Tests.Aggregations
 			 * It benefits from types that are carried over to sub aggregations
 			 */
 			protected override Func<SearchDescriptor<Project>, ISearchRequest> Fluent => s => s
-				.Aggregations(aggs=>aggs
+				.Aggregations(aggs => aggs
 					.Children<CommitActivity>("name_of_child_agg", child => child
-						.Aggregations(childAggs=>childAggs
-							.Average("average_per_child", avg=>avg.Field(p=>p.ConfidenceFactor))
-							.Max("max_per_child", avg=>avg.Field(p=>p.ConfidenceFactor))
+						.Aggregations(childAggs => childAggs
+							.Average("average_per_child", avg => avg.Field(p => p.ConfidenceFactor))
+							.Max("max_per_child", avg => avg.Field(p => p.ConfidenceFactor))
 						)
 					)
 				);
-			
+
 			/**
 			 * The object initializer syntax (OIS) is a one-to-one mapping with how aggregations 
 			 * have to be represented in the Elasticsearch API. While it has the benefit of being a one-to-one 
 			 * mapping, being dictionary based in C# means it can grow exponentially in complexity rather quickly.
 			 */
-			protected override SearchRequest<Project> Initializer => 
+			protected override SearchRequest<Project> Initializer =>
 				new SearchRequest<Project>
 				{
 					Aggregations = new ChildrenAggregation("name_of_child_agg", typeof(CommitActivity))
 					{
-						Aggregations = 
-							new AverageAggregation("average_per_child", "confidenceFactor") &&
-							new MaxAggregation("max_per_child", "confidenceFactor")
+						Aggregations =
+							new AverageAggregation("average_per_child", "confidenceFactor") 
+							&& new MaxAggregation("max_per_child", "confidenceFactor")
 					}
 				};
 		}
@@ -83,11 +85,41 @@ namespace Tests.Aggregations
 				{
 					Aggregations = new ChildrenAggregation("name_of_child_agg", typeof(CommitActivity))
 					{
-						Aggregations = 
-							new AverageAggregation("average_per_child", Field<CommitActivity>(p=>p.ConfidenceFactor))
-							&& new MaxAggregation("max_per_child", Field<CommitActivity>(p=>p.ConfidenceFactor))
+						Aggregations =
+							new AverageAggregation("average_per_child", Field<CommitActivity>(p => p.ConfidenceFactor))
+							&& new MaxAggregation("max_per_child", Field<CommitActivity>(p => p.ConfidenceFactor))
 					}
 				};
+		}
+
+		public class AdvancedAggregationDslUsage : Usage
+		{
+			/**
+			 * An advanced scenario may involve an existing collection of aggregation functions that should be set as aggregations 
+			 * on the request. Using LINQ's `.Aggregate()` method, each function can be applied to the aggregation descriptor
+			 * (`childAggs` below) in turn, returning the descriptor after each function application.
+			 *
+			 */
+			protected override Func<SearchDescriptor<Project>, ISearchRequest> Fluent
+			{   
+				get
+				{
+					var aggregations = new List<Func<AggregationContainerDescriptor<CommitActivity>, IAggregationContainer>>
+					{
+						a => a.Average("average_per_child", avg => avg.Field(p => p.ConfidenceFactor)),
+						a => a.Max("max_per_child", avg => avg.Field(p => p.ConfidenceFactor))
+					};
+
+					return s => s
+						.Aggregations(aggs => aggs
+							.Children<CommitActivity>("name_of_child_agg", child => child
+								.Aggregations(childAggs =>
+									aggregations.Aggregate(childAggs, (acc, agg) => { agg(acc); return acc; })
+								)
+							)
+						);
+				}
+			}
 		}
 	}
 }
