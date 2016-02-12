3b68d34291ebd23d1acdd1d11dc578bcd6f5dbc1
Romanx <mcauliffe.alex@gmail.com>
2016-02-12 10:19:17 +0000

    Fix test logic and naming #1810

--- src/Tests/ClientConcepts/ConnectionPooling/Sticky/Sticky.doc.cs
+++ src/Tests/ClientConcepts/ConnectionPooling/Sticky/Sticky.doc.cs
@@ -11,9 +11,13 @@ namespace Tests.ClientConcepts.ConnectionPooling.Sticky
 {
     public class Sticky
     {
+        /** Sticky
+		 * Each connection pool returns the first `live` node so that it is sticky between requests
+		*/
+
         protected int NumberOfNodes = 10;
 
-        [U] public void EachViewStartsAtNexPositionAndWrapsOver()
+        [U] public void EachViewStartsAtNextPositionAndWrapsOver()
         {
             var uris = Enumerable.Range(9200, NumberOfNodes).Select(p => new Uri("http://localhost:" + p));
             var staticPool = new StickyConnectionPool(uris);
@@ -23,12 +27,20 @@ namespace Tests.ClientConcepts.ConnectionPooling.Sticky
 
         public void AssertCreateView(StickyConnectionPool pool)
         {
+            /**
+			* Here we have setup a static connection pool seeded with 10 nodes.
+			* So what order we expect? Imagine the following:
+			*
+			* Thread A calls GetNext and gets returned the first live node
+			* Thread B calls GetNext() and gets returned the same node as it's still the first live.
+			*/
+
             var startingPositions = Enumerable.Range(0, NumberOfNodes)
                 .Select(i => pool.CreateView().First())
                 .Select(n => n.Uri.Port)
                 .ToList();
 
-            var expectedOrder = Enumerable.Range(9200, NumberOfNodes);
+            var expectedOrder = Enumerable.Repeat(9200, NumberOfNodes);
             startingPositions.Should().ContainInOrder(expectedOrder);
         }
     }
