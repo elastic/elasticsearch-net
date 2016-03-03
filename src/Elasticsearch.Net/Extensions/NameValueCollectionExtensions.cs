b57c01136484573d27696cc407946429ce337b79
Maxim Dubrovkin <dubrovkinmaxim@gmail.com>
2016-03-03 13:15:46 +0500

    NameValueCollectionExtensions was refactored.

--- src/Elasticsearch.Net/Extensions/NameValueCollectionExtensions.cs
+++ src/Elasticsearch.Net/Extensions/NameValueCollectionExtensions.cs
@@ -5,41 +5,44 @@ using System.Linq;
 
 namespace Elasticsearch.Net
 {
-	internal static class NameValueCollectionExtensions
-	{
-		internal static void CopyKeyValues(this NameValueCollection source, NameValueCollection dest)
-		{
-			foreach (var key in source.AllKeys)
-			{
-				if (dest[key] != null) throw new Exception(string.Format("Attempted to add duplicate key '{0}'", key));
+    internal static class NameValueCollectionExtensions
+    {
+        internal static void CopyKeyValues(this NameValueCollection source, NameValueCollection dest)
+        {
+            foreach (var key in source.AllKeys)
+            {
+                if (dest[key] != null) throw new Exception($"Attempted to add duplicate key '{key}'");
 
-				dest.Add(key, source[key]);
-			}
-		}
-		internal static string ToQueryString(this NameValueCollection self, string prefix = "?")
-		{
-			if (self == null)
-				return null;
+                dest.Add(key, source[key]);
+            }
+        }
 
-			if (self.AllKeys.Length == 0) return string.Empty;
-			
-			return prefix + string.Join("&", self.AllKeys.Select(key => $"{Encode(key)}={Encode(self[key])}"));
-		}
+        internal static string ToQueryString(this NameValueCollection self, string prefix = "?")
+        {
+            if (self == null)
+                return null;
 
-		private static string Encode(string s) => s;
-		//private static string Encode(string s) => s == null ? null : Uri.EscapeDataString(s);
+            return self.AllKeys.Length == 0
+                ? string.Empty
+                : $"{prefix}{string.Join("&", self.AllKeys.Select(key => $"{Encode(key)}={Encode(self[key])}"))}";
+        }
 
-		internal static NameValueCollection ToNameValueCollection(this IDictionary<string, object> dict, IFormatProvider provider)
-		{
-			if (dict == null || dict.Count < 0)
-				return null;
-			
-			var nv = new NameValueCollection();
-			foreach (var kv in dict.Where(kv => !kv.Key.IsNullOrEmpty()))
-			{
-				nv.Add(kv.Key, string.Format(provider, "{0}", kv.Value));
-			}
-			return nv;
-		}
-	}
+        //TODO: It's not placeholder function ;)
+        private static string Encode(string s) => s;
+        //private static string Encode(string s) => s == null ? null : Uri.EscapeDataString(s);
+
+        internal static NameValueCollection ToNameValueCollection(this IDictionary<string, object> originalDictionary,
+            IFormatProvider provider)
+        {
+            if (originalDictionary == null || originalDictionary.Count < 0)
+                return null;
+
+            var nameValueCollection = new NameValueCollection();
+
+            foreach (var keyValuePair in originalDictionary.Where(kv => !kv.Key.IsNullOrEmpty()))
+                nameValueCollection.Add(keyValuePair.Key, string.Format(provider, "{0}", keyValuePair.Value));
+
+            return nameValueCollection;
+        }
+    }
 }
