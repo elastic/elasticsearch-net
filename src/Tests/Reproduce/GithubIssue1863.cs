b6057152b4eb86e962415468e7773f886f762a6e
gmarz <greg.marzouka@gmail.com>
2016-03-18 14:17:57 -0400

    Fix exception in ConcreteTypeConverter when _score is null
    Closes #1863
    Closes #1929

---
+++ src/Tests/Reproduce/GithubIssue1863.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using Nest;
+using FluentAssertions;
+using Tests.Framework;
+using Xunit;
+using Tests.Framework.Integration;
+using Tests.Framework.MockData;
+
+namespace Tests.Reproduce
+{
+	[Collection(IntegrationContext.ReadOnly)]
+	public class GithubIssue1863
+	{
+		[I]
+		public void ConcreteTypeConverterThrowsExceptionOnNullScore()
+		{
+			var client = TestClient.GetClient();
+			var response = client.Search<Project>(s => s
+				.ConcreteTypeSelector((d,h) => typeof(Project))
+				.Sort(srt => srt.Ascending(p => p.StartedOn))
+			);
+			response.Hits.Count().Should().BeGreaterThan(0);
+			response.Hits.All(h => h.Score == 0).Should().BeTrue();
+		}
+	}
+}
