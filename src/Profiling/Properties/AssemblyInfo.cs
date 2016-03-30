36bba337f173a3962dba37d439c9a55c30f2f587
Russ Cam <russ.cam@elastic.co>
2016-03-30 12:06:17 +1100

    Bump assembly versions to 2.1.0

--- src/Profiling/Properties/AssemblyInfo.cs
+++ src/Profiling/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("2.0.0")]
-[assembly: AssemblyFileVersionAttribute("2.0.5")]
-[assembly: AssemblyInformationalVersionAttribute("2.0.5")]
+[assembly: AssemblyFileVersionAttribute("2.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.1.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "2.0.0";
