e8e83d18e28a90048ce16f111fa85f2e59e2b993
Russ Cam <russ.cam@elastic.co>
2016-04-20 10:44:25 +1000

    Score is now a boolean on HasParent query
    Closes https://github.com/elastic/elasticsearch-net/issues/2044

--- src/Nest/QueryDsl/Joining/HasParent/HasParentQuery.cs
+++ src/Nest/QueryDsl/Joining/HasParent/HasParentQuery.cs
@@ -10,8 +10,12 @@ namespace Nest
 		[JsonProperty("type")]
 		TypeName Type { get; set; }
 
-		[JsonProperty("score_mode")]
-		ParentScoreMode? ScoreMode { get; set; }
+		/// <summary>
+		/// Determines whether the score of the matching parent document is aggregated into the child documents belonging to the matching parent document.
+		/// The default is false which ignores the score from the parent document.
+		/// </summary>
+		[JsonProperty("score")]
+		bool? Score { get; set; }
 
 		[JsonProperty("query")]
 		QueryContainer Query { get; set; }
@@ -25,7 +29,12 @@ namespace Nest
 	{
 		protected override bool Conditionless => IsConditionless(this);
 		public TypeName Type { get; set; }
-		public ParentScoreMode? ScoreMode { get; set; }
+
+		/// <summary>
+		/// Determines whether the score of the matching parent document is aggregated into the child documents belonging to the matching parent document.
+		/// The default is false which ignores the score from the parent document.
+		/// </summary>
+		public bool? Score{ get; set; }
 		public QueryContainer Query { get; set; }
 		public IInnerHits InnerHits { get; set; }
 
@@ -39,7 +48,12 @@ namespace Nest
 	{
 		protected override bool Conditionless => HasParentQuery.IsConditionless(this);
 		TypeName IHasParentQuery.Type { get; set; }
-		ParentScoreMode? IHasParentQuery.ScoreMode { get; set; }
+
+		/// <summary>
+		/// Determines whether the score of the matching parent document is aggregated into the child documents belonging to the matching parent document.
+		/// The default is false which ignores the score from the parent document.
+		/// </summary>
+		bool? IHasParentQuery.Score { get; set; }
 		IInnerHits IHasParentQuery.InnerHits { get; set; }
 		QueryContainer IHasParentQuery.Query { get; set; }
 
@@ -50,7 +64,11 @@ namespace Nest
 
 		public HasParentQueryDescriptor<T> Type(string type) => Assign(a => a.Type = type);
 
-		public HasParentQueryDescriptor<T> ScoreMode(ParentScoreMode? scoreMode = ParentScoreMode.Score) => Assign(a => a.ScoreMode = scoreMode);
+		/// <summary>
+		/// Determines whether the score of the matching parent document is aggregated into the child documents belonging to the matching parent document.
+		/// The default is false which ignores the score from the parent document.
+		/// </summary>
+		public HasParentQueryDescriptor<T> Score(bool score) => Assign(a => a.Score = score);
 
 		public HasParentQueryDescriptor<T> InnerHits(Func<InnerHitsDescriptor<T>, IInnerHits> selector = null) =>
 			Assign(a => a.InnerHits = selector.InvokeOrDefault(new InnerHitsDescriptor<T>()));
