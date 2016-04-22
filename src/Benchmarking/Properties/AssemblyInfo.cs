7ffbc4f01ea1dfaadc8311d8e78398ced483ae95
gmarz <greg.marzouka@gmail.com>
2016-04-22 16:44:24 -0400

    Bump version to 2.3.0

--- src/Benchmarking/Properties/AssemblyInfo.cs
+++ src/Benchmarking/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("2.0.0")]
-[assembly: AssemblyFileVersionAttribute("2.2.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.2.0")]
+[assembly: AssemblyFileVersionAttribute("2.3.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.3.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "2.0.0";
