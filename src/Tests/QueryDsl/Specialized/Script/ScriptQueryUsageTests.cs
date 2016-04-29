f84d5350e999f8f19fc9446aa5d0cbf7751e4c01
Russ Cam <russ.cam@elastic.co>
2016-04-29 11:07:56 +1000

    Integration test for Script Query usage

--- src/Tests/QueryDsl/Specialized/Script/ScriptQueryUsageTests.cs
+++ src/Tests/QueryDsl/Specialized/Script/ScriptQueryUsageTests.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
 using Nest;
+using Tests.Framework;
 using Tests.Framework.Integration;
 using Tests.Framework.MockData;
+using Xunit;
 
 namespace Tests.QueryDsl.Specialized.Script
 {
-	public class ScriptUsageTests : QueryDslUsageTestsBase
+	[Collection(IntegrationContext.ReadOnly)]
+	public class ScriptQueryUsageTests : QueryDslUsageTestsBase
 	{
-		public ScriptUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
+		public ScriptQueryUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
 
-		private static readonly string _templateString = "doc['num1'].value > param1";
+		private static readonly string _templateString = "doc['numberOfCommits'].value > param1";
+
+		protected override bool ForceInMemory => false;
 
 		protected override object QueryJson => new
 		{
@@ -19,8 +28,8 @@ namespace Tests.QueryDsl.Specialized.Script
 				boost = 1.1,
 				script = new
 				{
-					inline = "doc['num1'].value > param1",
-					@params = new { param1 = 1 }
+					inline = "doc['numberOfCommits'].value > param1",
+					@params = new { param1 = 50 }
 				}
 			}
 		};
@@ -32,7 +41,7 @@ namespace Tests.QueryDsl.Specialized.Script
 			Inline = _templateString,
 			Params = new Dictionary<string, object>
 			{
-				{ "param1", 1 }
+				{ "param1", 50 }
 			}
 		};
 
@@ -41,8 +50,17 @@ namespace Tests.QueryDsl.Specialized.Script
 				.Name("named_query")
 				.Boost(1.1)
 				.Inline(_templateString)
-				.Params(p=>p.Add("param1", 1))
+				.Params(p=>p.Add("param1", 50))
 			);
+
+		protected void ExpectResponse(ISearchResponse<Project> response)
+		{
+			response.IsValid.Should().BeTrue();
+			response.Documents.Count().Should().BeGreaterThan(0);
+		}
+
+		[I]
+		protected async Task ReturnsExpectedResponse() => await this.AssertOnAllResponses(ExpectResponse);
 
 		protected override ConditionlessWhen ConditionlessWhen => new ConditionlessWhen<IScriptQuery>(a => a.Script)
 		{
