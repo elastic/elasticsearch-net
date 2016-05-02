0857eeb54256c3fb9a67912aa0ab6608ced4fe21
Russ Cam <russ.cam@forloop.co.uk>
2016-05-02 18:16:09 +1000

    Add support for cluster allocation explain
    Closes https://github.com/elastic/elasticsearch-net/issues/1987

---
+++ src/Nest/Cluster/ClusterAllocationExplain/ClusterAllocationExplainRequest.cs
@@ -0,0 +1,71 @@
+using Newtonsoft.Json;
+
+namespace Nest
+{
+	[JsonConverter(typeof(ReadAsTypeJsonConverter<ClusterAllocationExplainRequest>))]
+	public partial interface IClusterAllocationExplainRequest
+	{
+		/// <summary>
+		/// The name of the index to provide an explanation for
+		/// </summary>
+		[JsonProperty("index")]
+		IndexName Index { get; set; }
+
+		/// <summary>
+		/// The shard id to provide an explanation for
+		/// </summary>
+		[JsonProperty("shard")]
+		int Shard { get; set; }
+
+		/// <summary>
+		/// Whether to explain a primary or replica shard
+		/// </summary>
+		[JsonProperty("primary")]
+		bool? Primary { get; set; }
+	}
+
+	public partial class ClusterAllocationExplainRequest
+	{
+		/// <summary>
+		/// The name of the index to provide an explanation for
+		/// </summary>
+		public IndexName Index { get; set; }
+
+		/// <summary>
+		/// The shard id to provide an explanation for
+		/// </summary>
+		public int Shard { get; set; }
+
+		/// <summary>
+		/// Whether to explain a primary or replica shard
+		/// </summary>
+		public bool? Primary { get; set; }
+	}
+
+	public partial class ClusterAllocationExplainDescriptor
+	{
+		IndexName IClusterAllocationExplainRequest.Index { get; set; }
+		int IClusterAllocationExplainRequest.Shard { get; set; }
+		bool? IClusterAllocationExplainRequest.Primary { get; set; }
+
+		/// <summary>
+		/// The name of the index to provide an explanation for
+		/// </summary>
+		public ClusterAllocationExplainDescriptor Index(IndexName index) => Assign(a => a.Index = index);
+
+		/// <summary>
+		/// The name of the index to provide an explanation for
+		/// </summary>
+		public ClusterAllocationExplainDescriptor Index<TDocument>() => Assign(a => a.Index = typeof(TDocument));
+
+		/// <summary>
+		/// Whether to explain a primary or replica shard
+		/// </summary>
+		public ClusterAllocationExplainDescriptor Primary(bool primary = true) => Assign(a => a.Primary = primary);
+
+		/// <summary>
+		/// The shard id to provide an explanation for
+		/// </summary>
+		public ClusterAllocationExplainDescriptor Shard(int shard) => Assign(a => a.Shard = shard);
+	}
+}
---
+++ src/Nest/Cluster/ClusterAllocationExplain/ElasticClient-ClusterAllocationExplain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+
+namespace Nest
+{
+	public partial interface IElasticClient
+	{
+		/// <summary>
+		/// The cluster allocation explanation API is designed to assist in answering the question "why is this shard unassigned?"
+		/// <para> </para><a href="https://www.elastic.co/guide/en/elasticsearch/reference/master/cluster-allocation-explain.html">https://www.elastic.co/guide/en/elasticsearch/reference/master/cluster-allocation-explain.html</a>
+		/// </summary>
+		/// <param name="selector">An optional descriptor to further describe the cluster allocation explain operation</param>
+		IClusterAllocationExplainResponse ClusterAllocationExplain(Func<ClusterAllocationExplainDescriptor, IClusterAllocationExplainRequest> selector = null);
+
+		/// <inheritdoc/>
+		IClusterAllocationExplainResponse ClusterAllocationExplain(IClusterAllocationExplainRequest request);
+
+		/// <inheritdoc/>
+		Task<IClusterAllocationExplainResponse> ClusterAllocationExplainAsync(Func<ClusterAllocationExplainDescriptor, IClusterAllocationExplainRequest> selector = null);
+
+		/// <inheritdoc/>
+		Task<IClusterAllocationExplainResponse> ClusterAllocationExplainAsync(IClusterAllocationExplainRequest request);
+	}
+
+	public partial class ElasticClient
+	{
+		/// <inheritdoc/>
+		public IClusterAllocationExplainResponse ClusterAllocationExplain(Func<ClusterAllocationExplainDescriptor, IClusterAllocationExplainRequest> selector = null) =>
+			this.ClusterAllocationExplain(selector.InvokeOrDefault(new ClusterAllocationExplainDescriptor()));
+
+		/// <inheritdoc/>
+		public IClusterAllocationExplainResponse ClusterAllocationExplain(IClusterAllocationExplainRequest request) =>
+			this.Dispatcher.Dispatch<IClusterAllocationExplainRequest, ClusterAllocationExplainRequestParameters, ClusterAllocationExplainResponse>(
+				request,
+				(p, d) => this.LowLevelDispatch.ClusterAllocationExplainDispatch<ClusterAllocationExplainResponse>(p, d)
+			);
+
+		/// <inheritdoc/>
+		public Task<IClusterAllocationExplainResponse> ClusterAllocationExplainAsync(Func<ClusterAllocationExplainDescriptor, IClusterAllocationExplainRequest> selector = null) =>
+			this.ClusterAllocationExplainAsync(selector.InvokeOrDefault(new ClusterAllocationExplainDescriptor()));
+
+		/// <inheritdoc/>
+		public Task<IClusterAllocationExplainResponse> ClusterAllocationExplainAsync(IClusterAllocationExplainRequest request) =>
+			this.Dispatcher.DispatchAsync<IClusterAllocationExplainRequest, ClusterAllocationExplainRequestParameters, ClusterAllocationExplainResponse, IClusterAllocationExplainResponse>(
+				request,
+				(p, d) => this.LowLevelDispatch.ClusterAllocationExplainDispatchAsync<ClusterAllocationExplainResponse>(p, d)
+			);
+	}
+}
