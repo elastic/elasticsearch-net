b909694d8eba33979a5e3032bb13581426057021
Russ Cam <russ.cam@elastic.co>
2016-06-17 15:54:15 +1000

    Update percolate query docs to include field
    Update Indices Paths
    Closes https://github.com/elastic/elasticsearch-net/pull/2094

--- src/Tests/ClientConcepts/HighLevel/Inference/IndicesPaths.doc.cs
+++ src/Tests/ClientConcepts/HighLevel/Inference/IndicesPaths.doc.cs
@@ -12,7 +12,7 @@ namespace Tests.ClientConcepts.HighLevel.Inference
 		/**== Indices paths
 		*
 		* Some APIs in Elasticsearch take one or many index name or a special `_all` marker to send the request to all the indices
-		* In nest this is encoded using `Indices`.
+		* In NEST, this is encoded using `Indices`.
 		*
 		*=== Implicit Conversion
 		* Several types implicitly convert to `Indices`
