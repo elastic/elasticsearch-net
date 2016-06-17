087481c4ed5a247f023142d92d004bec764b2195
Russ Cam <russ.cam@elastic.co>
2016-06-17 15:54:35 +1000

    Update Indices Paths
    Closes https://github.com/elastic/elasticsearch-net/pull/2094

--- src/Tests/ClientConcepts/HighLevel/Inference/IndicesPaths.doc.cs
+++ src/Tests/ClientConcepts/HighLevel/Inference/IndicesPaths.doc.cs
@@ -11,7 +11,7 @@ namespace Tests.ClientConcepts.HighLevel.Inference
 		/**== Indices paths
 		*
 		* Some APIs in Elasticsearch take one or many index names or a special `_all` marker to send the request to all the indices.
-		* In NEST this is encoded using `Indices`.
+		* In NEST, this is encoded using `Indices`.
 		*
 		*=== Implicit Conversion
 		* Several types implicitly convert to `Indices`
