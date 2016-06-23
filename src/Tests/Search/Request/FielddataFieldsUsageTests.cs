31d6e6dd76705f6b1cda47db83109f753eba593b
Russ Cam <russ.cam@elastic.co>
2016-06-23 19:36:53 +1000

    Remove link to removed fielddata_fields page

--- src/Tests/Search/Request/FielddataFieldsUsageTests.cs
+++ src/Tests/Search/Request/FielddataFieldsUsageTests.cs
@@ -7,7 +7,6 @@ namespace Tests.Search.Request
 {
 	/** Allows to return the field data representation of a field for each hit.
 	*
-	* See the Elasticsearch documentation on {ref_current}/search-request-fielddata-fields.html[Field Data Fields] for more detail.
 	*/
 	public class FielddataFieldsUsageTests : SearchUsageTestBase
 	{
