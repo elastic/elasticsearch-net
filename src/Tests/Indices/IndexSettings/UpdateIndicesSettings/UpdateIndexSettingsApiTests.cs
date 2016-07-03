4e5202437c8e0ceedeccfba93611dbf457eaf616
Russ Cam <russ.cam@elastic.co>
2016-07-04 09:04:58 +1000

    Update replicas only on single index

--- src/Tests/Indices/IndexSettings/UpdateIndicesSettings/UpdateIndexSettingsApiTests.cs
+++ src/Tests/Indices/IndexSettings/UpdateIndicesSettings/UpdateIndexSettingsApiTests.cs
@@ -13,9 +13,22 @@ namespace Tests.Indices.IndexSettings.UpdateIndicesSettings
 	public class UpdateIndexSettingsApiTests : ApiIntegrationTestBase<IUpdateIndexSettingsResponse, IUpdateIndexSettingsRequest, UpdateIndexSettingsDescriptor, UpdateIndexSettingsRequest>
 	{
 		public UpdateIndexSettingsApiTests(IndexingCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		protected override void IntegrationSetup(IElasticClient client, CallUniqueValues values)
+		{
+			foreach (var value in values)
+			{
+				var index = value.Value;
+				var createIndexResponse = client.CreateIndex(index);
+
+				if (!createIndexResponse.IsValid)
+					throw new Exception($"Invalid response when setting up index for integration test {this.GetType().Name}");
+			}
+		}
+
 		protected override LazyResponses ClientUsage() => Calls(
-			fluent: (client, f) => client.UpdateIndexSettings(AllIndices, f),
-			fluentAsync: (client, f) => client.UpdateIndexSettingsAsync(AllIndices, f),
+			fluent: (client, f) => client.UpdateIndexSettings(CallIsolatedValue, f),
+			fluentAsync: (client, f) => client.UpdateIndexSettingsAsync(CallIsolatedValue, f),
 			request: (client, r) => client.UpdateIndexSettings(r),
 			requestAsync: (client, r) => client.UpdateIndexSettingsAsync(r)
 		);
@@ -23,7 +36,7 @@ namespace Tests.Indices.IndexSettings.UpdateIndicesSettings
 		protected override bool ExpectIsValid => true;
 		protected override int ExpectStatusCode => 200;
 		protected override HttpMethod HttpMethod => HttpMethod.PUT;
-		protected override string UrlPath => $"/_settings";
+		protected override string UrlPath => $"{CallIsolatedValue}/_settings";
 
 		protected override object ExpectJson { get; } = new Dictionary<string, object>
 		{
@@ -32,12 +45,13 @@ namespace Tests.Indices.IndexSettings.UpdateIndicesSettings
 		};
 
 		protected override Func<UpdateIndexSettingsDescriptor, IUpdateIndexSettingsRequest> Fluent => d => d
+			.Index(CallIsolatedValue)
 			.IndexSettings(i => i
 				.BlocksWrite(false)
 				.NumberOfReplicas(2)
 			);
 
-		protected override UpdateIndexSettingsRequest Initializer => new UpdateIndexSettingsRequest
+		protected override UpdateIndexSettingsRequest Initializer => new UpdateIndexSettingsRequest(CallIsolatedValue)
 		{
 			IndexSettings = new Nest.IndexSettings
 			{
