a73e8b01f7cd86570c04ef329376e21f969595a7
Russ Cam <russ.cam@elastic.co>
2016-07-07 17:15:20 +1000

    Bump assemblies to 5.0.0-alpha3

--- src/Nest/Properties/AssemblyInfo.cs
+++ src/Nest/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Reflection;
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("5.0.0")]
 [assembly: AssemblyFileVersionAttribute("5.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.0.0-alpha2")]
+[assembly: AssemblyInformationalVersionAttribute("5.0.0-alpha3")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "5.0.0";
-        internal const string InformationalVersion = "5.0.0-alpha2";
+        internal const string InformationalVersion = "5.0.0-alpha3";
     }
 }
