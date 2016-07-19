82c4edd320a0943224bbb4fc6feac32b94799220
Mpdreamz <mpdreamz@gmail.com>
2016-07-19 14:08:23 +0200

    fix #2052 simplejson barfs on thrown exceptions

--- src/Elasticsearch.Net/Serialization/ElasticsearchNetJsonStrategy.cs
+++ src/Elasticsearch.Net/Serialization/ElasticsearchNetJsonStrategy.cs
@@ -1,11 +1,82 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
 
 namespace Elasticsearch.Net
 {
 	internal class ElasticsearchNetJsonStrategy : PocoJsonSerializerStrategy
 	{
 		public static readonly ElasticsearchNetJsonStrategy Instance = new ElasticsearchNetJsonStrategy();
+
+
+		public override bool TrySerializeNonPrimitiveObject(object input, out object output)
+		{
+			if (input is Exception)
+			{
+				var e = input as Exception;
+				var o = new JsonObject();
+
+				var si = new SerializationInfo(e.GetType(), new FormatterConverter());
+				var sc = new StreamingContext();
+				e.GetObjectData(si, sc);
+
+				var helpUrl = si.GetString("HelpURL");
+				var stackTrace = si.GetString("StackTraceString");
+				var remoteStackTrace = si.GetString("RemoteStackTraceString");
+				var remoteStackIndex = si.GetInt32("RemoteStackIndex");
+				var exceptionMethod = si.GetString("ExceptionMethod");
+				var hresult = si.GetInt32("HResult");
+				var source = si.GetString("Source");
+				var className = si.GetString("ClassName");
+
+				//TODO Loop over ISerializable data
+
+				o.Add("ClassName", className);
+				o.Add("Message", e.Message);
+				o.Add("Source", source);
+				o.Add("StackTraceString", stackTrace);
+				o.Add("RemoteStackTraceString", remoteStackTrace);
+				o.Add("RemoteStackIndex", remoteStackIndex);
+				o.Add("HResult", hresult);
+				o.Add("HelpURL", helpUrl);
+				this.WriteStructuredExceptionMethod(o, exceptionMethod);
+
+				output = o;
+				return true;
+
+			}
+			return base.TrySerializeNonPrimitiveObject(input, out output);
+		}
+
+		private void WriteStructuredExceptionMethod(JsonObject o, string exceptionMethodString)
+		{
+			if (string.IsNullOrWhiteSpace(exceptionMethodString)) return;
+
+			var args = exceptionMethodString.Split('\0', '\n');
+
+			if (args.Length != 5) return;
+
+			var memberType = Int32.Parse(args[0], CultureInfo.InvariantCulture);
+			var name = args[1];
+			var assemblyName = args[2];
+			var className = args[3];
+			var signature = args[4];
+			var an = new AssemblyName(assemblyName);
+			var exceptionMethod = new JsonObject();
+
+			exceptionMethod.Add("Name", name);
+			exceptionMethod.Add("AssemblyName", an.Name);
+			exceptionMethod.Add("AssemblyVersion", an.Version.ToString());
+			exceptionMethod.Add("AssemblyCulture", an.CultureName);
+			exceptionMethod.Add("ClassName", className);
+			exceptionMethod.Add("Signature", signature);
+			exceptionMethod.Add("MemberType", memberType);
+			o.Add("ExceptionMethod", exceptionMethod);
+
+		}
+
 
 		public override object DeserializeObject(object value, Type type)
 		{
--- src/Tests/Reproduce/GithubIssue2052.cs
+++ src/Tests/Reproduce/GithubIssue2052.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
 using Elasticsearch.Net;
 using FluentAssertions;
 using Tests.Framework;
@@ -9,30 +13,77 @@ namespace Tests.Reproduce
 	public class GithubIssue2052
 	{
 
-		[U] public void BadSimpleJsonDeserialization()
+		[U]
+		public void BadSimpleJsonDeserialization()
 		{
 
 			var ex = this.GimmeACaughtException();
 
-			var document = new Dictionary<string,object>{
-			    { "message", "My message"},
-			    { "exception", ex }
+			var document = new Dictionary<string, object>{
+				{ "message", "My message"},
+				{ "exception", ex }
 			};
 
 			var pool = new StaticConnectionPool(new List<Uri> { new Uri("http://localhost:9200") });
 			var memoryConnection = new InMemoryConnection();
-			var connectionSettings = new ConnectionConfiguration(pool, memoryConnection);
+			var connectionSettings = new ConnectionConfiguration(pool, memoryConnection)
+				.DisableDirectStreaming();
 			var client = new ElasticLowLevelClient(connectionSettings);
 
-			List<object> payload = new List<object>{
-			    new { index = new { _index = "myIndex", _type = "myDocumentType" } },
-			    document
+
+			var header = new { index = new { _index = "myIndex", _type = "myDocumentType" } };
+			var payload = new List<object>{
+				header,
+				document
 			};
-			Action act = () => client.Bulk<byte[]>(payload);
-			var e = act.ShouldThrow<UnexpectedElasticsearchClientException>();
+			var response = client.Bulk<byte[]>(payload);
 
-			//throw new Exception(e.Subject.First().DebugInformation);
 
+			var request = Encoding.UTF8.GetString(response.RequestBodyInBytes);
+			var a = new AssemblyName(this.GetType().Assembly.FullName);
+
+			var si = new SerializationInfo(ex.GetType(), new FormatterConverter());
+			var sc = new StreamingContext();
+			ex.GetObjectData(si, sc);
+			PostData<object> postData = new List<object>
+			{
+				header,
+				new
+				{
+					message = "My message",
+					exception = new
+					{
+						ClassName = "System.Exception",
+						Message = "Some exception",
+						Source = "Tests",
+						StackTraceString = ex.StackTrace,
+						RemoteStackTraceString = si.GetString("RemoteStackTraceString"),
+						RemoteStackIndex = 0,
+						HResult = si.GetInt32("HResult"),
+						HelpURL = si.GetString("HelpURL"),
+						ExceptionMethod = new
+						{
+							Name = nameof(GimmeACaughtException),
+							AssemblyName = a.Name,
+							AssemblyVersion = a.Version.ToString(),
+							AssemblyCulture = a.CultureName,
+							ClassName = this.GetType().FullName,
+							Signature = $"System.Exception {nameof(GimmeACaughtException)}()",
+							MemberType = 8
+						}
+					},
+				}
+			};
+
+
+
+
+			using (var ms = new MemoryStream())
+			{
+				postData.Write(ms, client.Settings);
+				var expectedString = Encoding.UTF8.GetString(ms.ToArray());
+				request.Should().Be(expectedString);
+			}
 		}
 
 		private Exception GimmeACaughtException()
@@ -41,7 +92,7 @@ namespace Tests.Reproduce
 			{
 				throw new Exception("Some exception");
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
 				return e;
 			}
