b690d8ae3e69dab763af9fce9fdd329283d6fe1f
Mpdreamz <mpdreamz@gmail.com>
2016-07-20 12:38:12 +0200

    Simple json writes exception (+ max 20 inner exceptions) as flattened array, mimicing serilog behavior

--- src/Tests/Reproduce/GithubIssue2052.cs
+++ src/Tests/Reproduce/GithubIssue2052.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text;
@@ -12,75 +14,149 @@ namespace Tests.Reproduce
 {
 	public class GithubIssue2052
 	{
+		private const string _objectMessage = "My message";
+		private static object _bulkHeader =
+			 new { index = new { _index = "myIndex", _type = "myDocumentType" } };
+		private readonly ElasticLowLevelClient _client;
+		private AssemblyName _assemblyName = new AssemblyName(typeof(GithubIssue2052).Assembly.FullName);
 
-		[U]
-		public void BadSimpleJsonDeserialization()
+		public GithubIssue2052()
 		{
-
-			var ex = this.GimmeACaughtException();
-
-			var document = new Dictionary<string, object>{
-				{ "message", "My message"},
-				{ "exception", ex }
-			};
 
 			var pool = new StaticConnectionPool(new List<Uri> { new Uri("http://localhost:9200") });
 			var memoryConnection = new InMemoryConnection();
 			var connectionSettings = new ConnectionConfiguration(pool, memoryConnection)
 				.DisableDirectStreaming();
-			var client = new ElasticLowLevelClient(connectionSettings);
+			this._client = new ElasticLowLevelClient(connectionSettings);
+		}
 
 
-			var header = new { index = new { _index = "myIndex", _type = "myDocumentType" } };
-			var payload = new List<object>{
-				header,
-				document
-			};
-			var response = client.Bulk<byte[]>(payload);
+		[U] public void SingleThrownExceptionCanBeSerializedUsingSimpleJson()
+		{
 
+			var ex = this.GimmeACaughtException();
 
-			var request = Encoding.UTF8.GetString(response.RequestBodyInBytes);
-			var a = new AssemblyName(this.GetType().Assembly.FullName);
+			var request = this.CreateRequest(ex);
+			var postData = this.CreatePostData(ex);
 
-			var si = new SerializationInfo(ex.GetType(), new FormatterConverter());
-			var sc = new StreamingContext();
-			ex.GetObjectData(si, sc);
+			this.AssertRequestEquals(request, postData);
+		}
+
+		[U] public void MultipleThrownExceptionCanBeSerializedUsingSimpleJson()
+		{
+
+			var ex = this.GimmeAnExceptionWithInnerException();
+
+			var request = this.CreateRequest(ex);
+			var postData = this.CreatePostData(ex);
+
+			this.AssertRequestEquals(request, postData);
+		}
+
+		private PostData<object> CreatePostData(Exception e)
+		{
 			PostData<object> postData = new List<object>
 			{
-				header,
+				_bulkHeader,
 				new
 				{
 					message = "My message",
-					exception = new
-					{
-						ClassName = "System.Exception",
-						Message = "Some exception",
-						Source = "Tests",
-						StackTraceString = ex.StackTrace,
-						RemoteStackTraceString = si.GetString("RemoteStackTraceString"),
-						RemoteStackIndex = 0,
-						HResult = si.GetInt32("HResult"),
-						HelpURL = si.GetString("HelpURL"),
-						ExceptionMethod = new
-						{
-							Name = nameof(GimmeACaughtException),
-							AssemblyName = a.Name,
-							AssemblyVersion = a.Version.ToString(),
-							AssemblyCulture = a.CultureName,
-							ClassName = this.GetType().FullName,
-							Signature = $"System.Exception {nameof(GimmeACaughtException)}()",
-							MemberType = 8
-						}
-					},
+					exception = this.ExceptionJson(e).ToArray(),
 				}
+			};
+			return postData;
+		}
+
+		private IEnumerable<object> ExceptionJson(Exception e)
+		{
+			int depth = 0;
+			int maxExceptions = 20;
+			do
+			{
+
+				var si = new SerializationInfo(e.GetType(), new FormatterConverter());
+				var sc = new StreamingContext();
+				e.GetObjectData(si, sc);
+
+				var helpUrl = si.GetString("HelpURL");
+				var stackTrace = si.GetString("StackTraceString");
+				var remoteStackTrace = si.GetString("RemoteStackTraceString");
+				var remoteStackIndex = si.GetInt32("RemoteStackIndex");
+				var exceptionMethod = si.GetString("ExceptionMethod");
+				var hresult = si.GetInt32("HResult");
+				var source = si.GetString("Source");
+				var className = si.GetString("ClassName");
+
+				yield return new
+				{
+					Depth = depth,
+					ClassName = className,
+					Message = e.Message,
+					Source = source,
+					StackTraceString = stackTrace,
+					RemoteStackTraceString = remoteStackTrace,
+					RemoteStackIndex = remoteStackIndex,
+					HResult = hresult,
+					HelpURL = helpUrl,
+					ExceptionMethod = this.WriteStructuredExceptionMethod(exceptionMethod)
+				};
+				depth++;
+				e = e.InnerException;
+
+			}
+			while (depth < maxExceptions && e != null);
+		}
+
+		private object WriteStructuredExceptionMethod(string exceptionMethodString)
+		{
+			if (string.IsNullOrWhiteSpace(exceptionMethodString)) return null;
+
+			var args = exceptionMethodString.Split('\0', '\n');
+
+			if (args.Length != 5) return null;
+
+			var memberType = Int32.Parse(args[0], CultureInfo.InvariantCulture);
+			var name = args[1];
+			var assemblyName = args[2];
+			var className = args[3];
+			var signature = args[4];
+			var an = new AssemblyName(assemblyName);
+			return new
+			{
+				Name = name,
+				AssemblyName = an.Name,
+				AssemblyVersion = an.Version.ToString(),
+				AssemblyCulture = an.CultureName,
+				ClassName = className,
+				Signature = signature,
+				MemberType = memberType,
+			};
+		}
+
+		private string CreateRequest(Exception ex)
+		{
+			var document = new Dictionary<string, object>{
+				{ "message", _objectMessage},
+				{ "exception", ex }
 			};
 
 
+			var payload = new List<object>{
+				_bulkHeader,
+				document
+			};
+			var response = this._client.Bulk<byte[]>(payload);
 
 
+			var request = Encoding.UTF8.GetString(response.RequestBodyInBytes);
+			return request;
+		}
+
+		private void AssertRequestEquals(string request, PostData<object> postData)
+		{
 			using (var ms = new MemoryStream())
 			{
-				postData.Write(ms, client.Settings);
+				postData.Write(ms, this._client.Settings);
 				var expectedString = Encoding.UTF8.GetString(ms.ToArray());
 				request.Should().Be(expectedString);
 			}
@@ -98,5 +174,18 @@ namespace Tests.Reproduce
 			}
 		}
 
+
+		private Exception GimmeAnExceptionWithInnerException()
+		{
+			try
+			{
+				var e = this.GimmeACaughtException();
+				throw new Exception("Some exception", e);
+			}
+			catch (Exception e)
+			{
+				return e;
+			}
+		}
 	}
 }
