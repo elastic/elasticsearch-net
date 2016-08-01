5e548e97175937722a1b79297bf251c4c13a2b1a
Greg Marzouka <greg.marzouka@gmail.com>
2016-08-01 13:08:36 -0400

    Fix flaky repro test

--- src/Tests/Reproduce/GithubIssue2173.cs
+++ src/Tests/Reproduce/GithubIssue2173.cs
@@ -5,17 +5,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tests.Framework;
+using Tests.Framework.Integration;
 using Tests.Framework.MockData;
+using Xunit;
 
 namespace Tests.Reproduce
 {
+	[Collection(TypeOfCluster.Indexing)]
 	public class GithubIssue2173
 	{
-		[I]
-		public void UpdateByQueryWithInvalidScript()
+		private readonly IndexingCluster _cluster;
+		public GithubIssue2173(IndexingCluster cluster)
 		{
-			var client = TestClient.GetClient();
-			var response = client.UpdateByQuery<Project>(typeof(Project), typeof(Project), u => u
+			_cluster = cluster;
+		}
+
+		[I] public void UpdateByQueryWithInvalidScript()
+		{
+			var client = _cluster.Client();
+			var response = client.UpdateByQuery<Project>(u => u
 				.Script("invalid groovy")
 			);
 			response.IsValid.Should().BeFalse();
