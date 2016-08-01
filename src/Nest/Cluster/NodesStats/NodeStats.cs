0550c3ce944cf9e965139f2892400987d8a76e14
Matthias Mertens <matthias0212@gmail.com>
2016-08-01 13:52:21 +0200

    Add Percent metric in OperatingSystemStats

--- src/Nest/Cluster/NodesStats/NodeStats.cs
+++ src/Nest/Cluster/NodesStats/NodeStats.cs
@@ -87,6 +87,8 @@ namespace Nest
 		public long Timestamp { get; internal set; }
 		[JsonProperty("load_average")]
 		public float LoadAverage { get; internal set; }
+		[JsonProperty("percent")]
+		public float Percent { get; internal set; }
 
 		[JsonProperty("mem")]
 		public ExtendedMemoryStats Memory { get; internal set; }
