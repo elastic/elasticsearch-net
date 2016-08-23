760bc005bb03dad4ba5fcf30ddd0c6843845bb30
Russ Cam <russ.cam@elastic.co>
2016-08-23 15:19:35 +1000

    Add tasks list status and description
    Closes #2133

--- src/Tests/Cluster/TaskManagement/TasksList/TasksListApiTests.cs
+++ src/Tests/Cluster/TaskManagement/TasksList/TasksListApiTests.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using Nest;
 using Tests.Framework;
 using Tests.Framework.Integration;
+using Tests.Framework.MockData;
 using Xunit;
 
 namespace Tests.Cluster.TaskManagement.TasksList
@@ -56,8 +57,86 @@ namespace Tests.Cluster.TaskManagement.TasksList
 			var parentTask = taskExecutingNode.Tasks[task.ParentTaskId];
 			parentTask.Should().NotBeNull();
 			parentTask.ParentTaskId.Should().BeNull();
+		}
+	}
 
+	[SkipVersion("<2.3.0", "")]
+	public class TasksListDetailedApiTests : ApiIntegrationTestBase<IntrusiveOperationCluster, ITasksListResponse, ITasksListRequest, TasksListDescriptor, TasksListRequest>
+	{
+		private static TaskId _taskId = new TaskId("fakeid:1");
 
+		public TasksListDetailedApiTests(IntrusiveOperationCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.TasksList(f),
+			fluentAsync: (client, f) => client.TasksListAsync(f),
+			request: (client, r) => client.TasksList(r),
+			requestAsync: (client, r) => client.TasksListAsync(r)
+		);
+
+		protected override void IntegrationSetup(IElasticClient client, CallUniqueValues values)
+		{
+			var seeder = new Seeder(this.Cluster.Node);
+			seeder.SeedNode();
+
+			// get a suitable load of projects in order to get a decent task status out
+			var bulkResponse = client.IndexMany(Project.Generator.Generate(20000));
+			if (!bulkResponse.IsValid)
+				throw new Exception("failure in setting up integration");
+
+			var response = client.ReindexOnServer(r => r
+				.Source(s => s
+					.Index(Infer.Index<Project>())
+					.Type(typeof(Project))
+				)
+				.Destination(d => d
+					.Index("tasks-list-projects")
+					.OpType(OpType.Create)
+				)
+				.Conflicts(Conflicts.Proceed)
+				.WaitForCompletion(false)
+				.Refresh()
+			);
+
+			_taskId = response.Task;
+		}
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 200;
+		protected override HttpMethod HttpMethod => HttpMethod.GET;
+		protected override string UrlPath => $"/_tasks/{Uri.EscapeDataString(_taskId.ToString())}?detailed=true";
+
+		protected override Func<TasksListDescriptor, ITasksListRequest> Fluent => s => s
+			.TaskId(_taskId)
+			.Detailed();
+
+		protected override TasksListRequest Initializer => new TasksListRequest(_taskId)
+		{
+			Detailed = true
+		};
+
+		protected override void ExpectResponse(ITasksListResponse response)
+		{
+			response.Nodes.Should().NotBeEmpty();
+			var taskExecutingNode = response.Nodes.First().Value;
+			taskExecutingNode.Host.Should().NotBeNullOrWhiteSpace();
+			taskExecutingNode.Ip.Should().NotBeNullOrWhiteSpace();
+			taskExecutingNode.Name.Should().NotBeNullOrWhiteSpace();
+			taskExecutingNode.TransportAddress.Should().NotBeNullOrWhiteSpace();
+			taskExecutingNode.Tasks.Should().NotBeEmpty();
+			taskExecutingNode.Tasks.Count().Should().Be(1);
+
+			var task = taskExecutingNode.Tasks[_taskId];
+			task.Action.Should().NotBeNullOrWhiteSpace();
+			task.Type.Should().NotBeNullOrWhiteSpace();
+			task.Id.Should().BePositive();
+			task.Node.Should().NotBeNullOrWhiteSpace();
+			task.RunningTimeInNanoSeconds.Should().BeGreaterThan(0);
+			task.StartTimeInMilliseconds.Should().BeGreaterThan(0);
+
+			var status = task.Status;
+			status.Should().NotBeNull();
+			status.Total.Should().BeGreaterOrEqualTo(0);
+			status.Batches.Should().BeGreaterOrEqualTo(0);
 		}
 	}
 }
