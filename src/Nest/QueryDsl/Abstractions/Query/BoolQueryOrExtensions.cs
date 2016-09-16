c848b4160ffdef91e36b0aea98ec936cc4e689e7
Martijn Laarman <Mpdreamz@gmail.com>
2016-09-16 11:03:11 +0200

    fix #2209 Several improvements in some edgecase querydsl scenarios (#2235)
    * This commit brings about several improvements in some edgecase querydsl
    scenarios
    
    We always flattened situations like:
    
    query1 && query2 && query3 into a single bool query with 3 must clauses.
    
    But if the queries are booleans themselves that can't be merged we would
    generate deeply nested pairs of two.
    
    For instance we have 3 bool queries with only should clauses
    
    bq1 && bq2 && bq3
    
    ideally we'd generate:
    
    bool:
      must:
         bool: should:,
         bool: should:,
         bool: should:
    
    However due to the bitwise binary nature the way they were combined was:
    
    Step1:
    
    bq1 && b2 - both sides only have should clauses merging them would be
    wrong so we wrap to `result1`
    
    bool:
      must:
         bool: should:,
         bool: should:
    
    now we do `result1 && bq2` since result1 is a must we can not merge the
    should from `bq2` on it and thus we need to wrap to `result2`
    
    bool:
      should:
        bool:
          must:
            bool: should:,
    	bool: should:
        bool: should: //bq2
    
    This is still sementically correct but now immagine you are doing this
    in a loop combining thousands of these. Admittingly an edgecase it would
    cause a nasty stackoverflow and query graphs that are way too deep
    unnecessary as is showcased by #2209
    
    This is now fixed in a way that does not need constant traversal of to
    see if a bool must or should clause consists of only bools.
    
    Similary the same happened if you'd || or a bunch of booleanqueries only
    containing musts.
    
    A third fix is that we no longer modify the original query when stealing
    clauses. This could cause nasty errors when reusing a static bool query
    e.g
    
    ```csharp
    var q = Query<Project>.Bool(b => b.Should(ATermQuery).Must(ATermQuery));
    var c = new QueryContainer();
    for(int i = 0;i<1000;i++) c &= q;
    ```
    
    The & or | bitwise operators were not pure and could potentially steal clauses from
    `q` and adding them to the new result. Cuasing problems when we want to
    apply q again and again and again.
    
    * attempt to minimize allocations in the bool dsl by reusing lists and already instantiated bool queries
    
    * documentation commit, hacked around asciidotnet not supporting tables just yet :) russcam/asciidotnet#2
    
    * updated bool dsl performance section in the documentation
    
    * inverted if statement for readabillity and removed some of the tests in the documentation output
    
    Conflicts:
    	docs/client-concepts/low-level/connecting.asciidoc
    	src/Nest/Nest.csproj
    	src/Nest/QueryDsl/Abstractions/Container/IQueryContainer.cs
    	src/Nest/QueryDsl/Visitor/DslPrettyPrintVisitor.cs
    	src/Tests/Framework/SerializationTests/SerializationTestBase.cs
    	src/Tests/QueryDsl/QueryDslUsageTestsBase.cs
    	src/Tests/Tests.csproj

---
+++ src/Nest/QueryDsl/Abstractions/Query/BoolQueryOrExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nest
+{
+	using Containers = System.Collections.Generic.List<QueryContainer>;
+	internal static class BoolQueryOrExtensions
+	{
+		internal static QueryContainer CombineAsShould(this QueryContainer leftContainer, QueryContainer rightContainer)
+		{
+			QueryContainer c = null;
+			var leftBool = leftContainer.Self()?.Bool;
+			var rightBool = rightContainer.Self()?.Bool;
+			if (TryFlattenShould(leftContainer, rightContainer, leftBool, rightBool, out c))
+				return c;
+
+			var lBoolQuery = leftContainer.Self().Bool;
+			var rBoolQuery = rightContainer.Self().Bool;
+
+			var lHasShouldQueries = lBoolQuery != null && lBoolQuery.Should.HasAny();
+			var rHasShouldQueries = rBoolQuery != null && rBoolQuery.Should.HasAny();
+
+			var lq = lHasShouldQueries ? lBoolQuery.Should : new[] { leftContainer };
+			var rq = rHasShouldQueries ? rBoolQuery.Should : new[] { rightContainer };
+
+			var shouldClauses = lq.EagerConcat(rq);
+			return CreateShouldContainer(shouldClauses);
+		}
+		private static bool TryFlattenShould(
+			QueryContainer leftContainer, QueryContainer rightContainer, IBoolQuery leftBool, IBoolQuery rightBool, out QueryContainer c)
+		{
+			c = null;
+			var leftCanMerge = leftContainer.CanMergeShould();
+			var rightCanMerge = rightContainer.CanMergeShould();
+			if (!leftCanMerge && !rightCanMerge) c = CreateShouldContainer(new Containers { leftContainer, rightContainer });
+
+			//left can merge but right's bool can not instead of wrapping into a new bool we inject the whole bool into left
+			else if (leftCanMerge && !rightCanMerge && rightBool != null)
+			{
+				leftBool.Should = leftBool.Should.AddIfNotNull(rightContainer);
+				c = leftContainer;
+			}
+			else if (rightCanMerge && !leftCanMerge && leftBool != null)
+			{
+				rightBool.Should = rightBool.Should.AddIfNotNull(leftContainer);
+				c = rightContainer;
+			}
+			return c != null;
+
+		}
+
+		private static bool CanMergeShould(this IQueryContainer container) => container.Bool.CanMergeShould();
+
+		private static bool CanMergeShould(this IBoolQuery boolQuery) =>
+			boolQuery != null && boolQuery.IsWritable && !boolQuery.Locked && boolQuery.HasOnlyShouldClauses();
+
+		private static QueryContainer CreateShouldContainer(List<QueryContainer> shouldClauses) =>
+			new BoolQuery(createdByBoolDsl: true)
+			{
+				Should = shouldClauses.ToListOrNullIfEmpty()
+			};
+
+	}
+}
