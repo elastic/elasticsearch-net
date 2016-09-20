f1c9d9984b791734909b6f5613d11dd58a4dc198
Russ Cam <russ.cam@forloop.co.uk>
2016-09-20 13:57:24 +1000

    Do not use IsoDateTimeConverter to handle DateTimeOffset
    Forward port of 4ea007cb7129939d512083d252875f5557a6b139
    
    IsoDateTimeConverter deserializes a DateTimeOffset represented in an ISO8601 into a DateTimeOffset with a local offset. For example "1999-01-01T01:01:01.001+05:00" deserializes to a DateTimeOffset with a value of 01/01/1999 07:01:01 +11:00. The +11 offset is my local offset (Australia). The default deserialization for a DateTimeOffset not using IsoDateTimeConverter deserializes into a DateTimeOffset with an offset that reflects the one in the ISO8601 string representation. This seems more appropriate behaviour.
    
    Use InvariantCulture in the IsoDateTimeConverter when deserializing DateTime

---
+++ src/Tests/Reproduce/DateSerialization.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Text;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Tests.Framework;
+
+namespace Tests.Reproduce
+{
+    public class DateSerialization
+    {
+        [U]
+        public void ShouldRoundtripDateTimeAndDateTimeOffsetWithSameKindAndOffset()
+        {
+            var dates = new Dates
+            {
+                DateTimeUtcKind = new DateTime(2016,1,1,1,1,1,DateTimeKind.Utc),
+                DateTimeOffset = new DateTimeOffset(1999, 1, 1, 1, 1, 1, 1, TimeSpan.FromHours(5))
+            };
+
+            var client = new ElasticClient();
+            var serializedDates = client.Serializer.SerializeToString(dates,SerializationFormatting.None);
+
+            serializedDates.Should()
+                .Be("{\"dateTimeUtcKind\":\"2016-01-01T01:01:01Z\",\"dateTimeOffset\":\"1999-01-01T01:01:01.001+05:00\"}");
+
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(serializedDates)))
+            {
+                var deserializedDates = client.Serializer.Deserialize<Dates>(stream);
+
+                deserializedDates.DateTimeUtcKind.Should().Be(dates.DateTimeUtcKind);
+                deserializedDates.DateTimeUtcKind.Kind.Should().Be(dates.DateTimeUtcKind.Kind);
+
+                deserializedDates.DateTimeOffset.Should().Be(dates.DateTimeOffset);
+                deserializedDates.DateTimeOffset.Offset.Should().Be(dates.DateTimeOffset.Offset);
+                deserializedDates.DateTimeOffset.Date.Kind.Should().Be(dates.DateTimeOffset.Date.Kind);
+            }
+        }
+
+        private class Dates
+        {
+            public DateTime DateTimeUtcKind { get; set; }
+            public DateTimeOffset DateTimeOffset { get; set; }
+        }
+    }
+}
