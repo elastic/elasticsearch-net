25c039507cf6df018bb5069f29c653325b411ca7
Greg Marzouka <greg.marzouka@gmail.com>
2016-09-27 13:31:00 -0400

    Bump version to 5.0.0-beta1

--- src/CodeGeneration/ApiGenerator/Properties/AssemblyInfo.cs
+++ src/CodeGeneration/ApiGenerator/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Reflection;
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("5.0.0")]
 [assembly: AssemblyFileVersionAttribute("5.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.0.0-alpha3")]
+[assembly: AssemblyInformationalVersionAttribute("5.0.0-beta1")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "5.0.0";
-        internal const string InformationalVersion = "5.0.0-alpha3";
+        internal const string InformationalVersion = "5.0.0-beta1";
     }
 }
