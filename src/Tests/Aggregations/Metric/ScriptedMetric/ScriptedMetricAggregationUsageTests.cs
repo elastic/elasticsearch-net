c148dd5ceb6c194deab8d28e60ef385a788743bc
Mpdreamz <mpdreamz@gmail.com>
2016-09-27 02:48:54 +0200

    scripted metric did not specify lang

--- src/Tests/Aggregations/Metric/ScriptedMetric/ScriptedMetricAggregationUsageTests.cs
+++ src/Tests/Aggregations/Metric/ScriptedMetric/ScriptedMetricAggregationUsageTests.cs
@@ -43,10 +43,10 @@ namespace Tests.Aggregations.Metric.ScriptedMetric
 		protected override Func<SearchDescriptor<Project>, ISearchRequest> Fluent => s => s
 			.Aggregations(a => a
 				.ScriptedMetric("sum_the_hard_way", sm => sm
-					.InitScript("_agg['commits'] = []")
-					.MapScript("if (doc['state'].value == \"Stable\") { _agg.commits.add(doc['numberOfCommits']) }")
-					.CombineScript("sum = 0; for (c in _agg.commits) { sum += c }; return sum")
-					.ReduceScript("sum = 0; for (a in _aggs) { sum += a }; return sum")
+					.InitScript(ss=>ss.Inline("_agg['commits'] = []").Lang("groovy"))
+					.MapScript(ss=>ss.Inline("if (doc['state'].value == \"Stable\") { _agg.commits.add(doc['numberOfCommits']) }").Lang("groovy"))
+					.CombineScript(ss=>ss.Inline("sum = 0; for (c in _agg.commits) { sum += c }; return sum").Lang("groovy"))
+					.ReduceScript(ss=>ss.Inline("sum = 0; for (a in _aggs) { sum += a }; return sum").Lang("groovy"))
 				)
 			);
 
@@ -55,10 +55,10 @@ namespace Tests.Aggregations.Metric.ScriptedMetric
 			{
 				Aggregations = new ScriptedMetricAggregation("sum_the_hard_way")
 				{
-					InitScript = new InlineScript("_agg['commits'] = []"),
-					MapScript = new InlineScript("if (doc['state'].value == \"Stable\") { _agg.commits.add(doc['numberOfCommits']) }"),
-					CombineScript = new InlineScript("sum = 0; for (c in _agg.commits) { sum += c }; return sum"),
-					ReduceScript = new InlineScript("sum = 0; for (a in _aggs) { sum += a }; return sum")
+					InitScript = new InlineScript("_agg['commits'] = []") { Lang = "groovy" },
+					MapScript = new InlineScript("if (doc['state'].value == \"Stable\") { _agg.commits.add(doc['numberOfCommits']) }"){ Lang = "groovy" },
+					CombineScript = new InlineScript("sum = 0; for (c in _agg.commits) { sum += c }; return sum"){ Lang = "groovy" },
+					ReduceScript = new InlineScript("sum = 0; for (a in _aggs) { sum += a }; return sum"){ Lang = "groovy" }
 				}
 			};
 
