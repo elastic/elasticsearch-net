84f11fdd346f2ffa252ea3b77176126f24ed63ef
Russ Cam <russ.cam@elastic.co>
2016-11-09 12:46:13 +1100

    Integration tests for not found Get and Delete

--- src/Tests/Document/Single/Delete/DeleteApiTests.cs
+++ src/Tests/Document/Single/Delete/DeleteApiTests.cs
@@ -44,4 +44,34 @@ namespace Tests.Document.Single.Delete
 			response.Result.Should().Be(Result.Deleted);
 		}
 	}
+
+	public class DeleteNonExistentDocumentApiTests : ApiIntegrationTestBase<ReadOnlyCluster, IDeleteResponse, IDeleteRequest, DeleteDescriptor<Project>, DeleteRequest<Project>>
+	{
+		public DeleteNonExistentDocumentApiTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.Delete<Project>(CallIsolatedValue),
+			fluentAsync: (client, f) => client.DeleteAsync<Project>(CallIsolatedValue),
+			request: (client, r) => client.Delete(r),
+			requestAsync: (client, r) => client.DeleteAsync(r)
+		);
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 404;
+		protected override HttpMethod HttpMethod => HttpMethod.DELETE;
+		protected override string UrlPath => $"/project/project/{CallIsolatedValue}";
+
+		protected override bool SupportsDeserialization => false;
+
+		protected override Func<DeleteDescriptor<Project>, IDeleteRequest> Fluent => null;
+		protected override DeleteRequest<Project> Initializer => new DeleteRequest<Project>(CallIsolatedValue);
+
+		protected override void ExpectResponse(IDeleteResponse response)
+		{
+			response.Found.Should().BeFalse();
+			response.Index.Should().Be("project");
+			response.Type.Should().Be("project");
+			response.Id.Should().Be(this.CallIsolatedValue);
+		}
+	}
 }
