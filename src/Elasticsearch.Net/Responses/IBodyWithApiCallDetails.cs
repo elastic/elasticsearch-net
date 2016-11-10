96c803cdbe7305c6500debf5ca80d6c21e072eec
Martijn Laarman <Mpdreamz@gmail.com>
2016-11-10 10:09:27 +0100

    fix #2286 normalize ApiCall and CallDetails and add better xmldocs to IResponse (#2381)

--- src/Elasticsearch.Net/Responses/IBodyWithApiCallDetails.cs
+++ src/Elasticsearch.Net/Responses/IBodyWithApiCallDetails.cs
@@ -1,7 +1,14 @@
 namespace Elasticsearch.Net
 {
+	/// <summary>
+	/// Implementing this interface on your response objects will cause the low level client to
+	/// automatically set <see cref="IApiCallDetails"/> diagnostic information on the object.
+	/// </summary>
 	public interface IBodyWithApiCallDetails
 	{
-		IApiCallDetails CallDetails { get; set; }
+		/// <summary>
+		/// Sets and returns the <see cref="IApiCallDetails"/> diagnostic information
+		/// </summary>
+		IApiCallDetails ApiCall { get; set; }
 	}
 }
--- src/Nest/CommonAbstractions/Response/ResponseBase.cs
+++ src/Nest/CommonAbstractions/Response/ResponseBase.cs
@@ -7,20 +7,45 @@ using Newtonsoft.Json;
 
 namespace Nest
 {
+	/// <summary>
+	/// Shared interface by all elasticsearch responses
+	/// </summary>
 	public interface IResponse : IBodyWithApiCallDetails
 	{
+		/// <summary>
+		/// This property can be used to check if a response is functionally valid or not.
+		/// This is a NEST abstraction to have a single point to check whether something wrong happend with the request.
+		/// <para>For instance an elasticsearch bulk response always returns 200 and individual bulk items may fail, <see cref="IsValid"/> will be false in that case</para>
+		/// <para>You can also configure the client to always throw an <see cref="ElasticsearchClientException"/> using <see cref="IConnectionConfigurationValues.ThrowExceptions"/> if the response is not valid</para>
+		/// </summary>
 		[JsonIgnore]
 		bool IsValid { get; }
 
+		/// <summary>
+		/// Returns the <see cref="IApiCallDetails"/> diagnostic information
+		/// </summary>
 		[JsonIgnore]
-		IApiCallDetails ApiCall { get; }
+		new IApiCallDetails ApiCall { get; }
 
+		/// <summary>
+		/// If the response results in an error on elasticsearch's side an <pre>error</pre> element will be returned, this is mapped to <see cref="ServerError"/> in NEST.
+		/// <para>This property is a shortcut to <see cref="ApiCall"/>'s <see cref="IApiCallDetails.ServerError"/> and is possibly set when <see cref="IsValid"/> is false depending on the cause of the error</para>
+		/// <para>You can also configure the client to always throw an <see cref="ElasticsearchClientException"/> using <see cref="IConnectionConfigurationValues.ThrowExceptions"/> if the response is not valid</para>
+		/// </summary>
 		[JsonIgnore]
 		ServerError ServerError { get; }
 
+		/// <summary>
+		/// If the request resulted in an exception on the client side this will hold the exception that was thrown.
+		/// <para>This property is a shortcut to <see cref="ApiCall"/>'s <see cref="IApiCallDetails.OriginalException"/> and is possibly set when <see cref="IsValid"/> is false depending on the cause of the error</para>
+		/// <para>You can also configure the client to always throw an <see cref="ElasticsearchClientException"/> using <see cref="IConnectionConfigurationValues.ThrowExceptions"/> if the response is not valid</para>
+		/// </summary>
 		[JsonIgnore]
 		Exception OriginalException { get; }
 
+		/// <summary>
+		/// A lazy human readable string representation of what happened during this request for both succesful and failed requests, very useful while developing or to log when you get <see cref="IsValid"/> = false responses.
+		/// </summary>
 		[JsonIgnore]
 		string DebugInformation { get; }
 
@@ -28,16 +53,21 @@ namespace Nest
 
 	public abstract class ResponseBase : IResponse
 	{
+		/// <inheritdoc/>
 		public virtual bool IsValid => (this.ApiCall?.Success ?? false) && (this.ServerError == null);
 
-		IApiCallDetails IBodyWithApiCallDetails.CallDetails { get; set; }
+		IApiCallDetails IBodyWithApiCallDetails.ApiCall { get; set; }
 
-		public virtual IApiCallDetails ApiCall => ((IBodyWithApiCallDetails)this).CallDetails;
+		/// <inheritdoc/>
+		public virtual IApiCallDetails ApiCall => ((IBodyWithApiCallDetails)this).ApiCall;
 
+		/// <inheritdoc/>
 		public virtual ServerError ServerError => this.ApiCall?.ServerError;
 
+		/// <inheritdoc/>
 		public Exception OriginalException => this.ApiCall?.OriginalException;
 
+		/// <inheritdoc/>
 		public string DebugInformation
 		{
 			get
