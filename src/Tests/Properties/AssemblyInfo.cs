e83d5287159c49c1543f9881f4844ae115947c76
Russ Cam <russ.cam@elastic.co>
2016-11-23 16:20:38 +1100

    Bump assembly versions to 5.0.0-rc2

--- src/Tests/Properties/AssemblyInfo.cs
+++ src/Tests/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Reflection;
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("5.0.0")]
 [assembly: AssemblyFileVersionAttribute("5.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.0.0-rc1")]
+[assembly: AssemblyInformationalVersionAttribute("5.0.0-rc2")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "5.0.0";
-        internal const string InformationalVersion = "5.0.0-rc1";
+        internal const string InformationalVersion = "5.0.0-rc2";
     }
 }
