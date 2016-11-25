593997a931f6d778a8c2b1efc7e121032ea3cf59
Mpdreamz <mpdreamz@gmail.com>
2016-11-25 14:43:40 +0100

    tone max default connections down to 80, add integration tests that proofs connections reuse

--- src/Tests/Reproduce/DateSerialization.cs
+++ src/Tests/Reproduce/DateSerialization.cs
@@ -8,40 +8,40 @@ using Tests.Framework;
 
 namespace Tests.Reproduce
 {
-    public class DateSerialization
-    {
-        [U]
-        public void ShouldRoundtripDateTimeAndDateTimeOffsetWithSameKindAndOffset()
-        {
-            var dates = new Dates
-            {
-                DateTimeUtcKind = new DateTime(2016,1,1,1,1,1,DateTimeKind.Utc),
-                DateTimeOffset = new DateTimeOffset(1999, 1, 1, 1, 1, 1, 1, TimeSpan.FromHours(5))
-            };
+	public class DateSerialization
+	{
+		[U]
+		public void ShouldRoundtripDateTimeAndDateTimeOffsetWithSameKindAndOffset()
+		{
+			var dates = new Dates
+			{
+				DateTimeUtcKind = new DateTime(2016, 1, 1, 1, 1, 1, DateTimeKind.Utc),
+				DateTimeOffset = new DateTimeOffset(1999, 1, 1, 1, 1, 1, 1, TimeSpan.FromHours(5))
+			};
 
-            var client = new ElasticClient();
-            var serializedDates = client.Serializer.SerializeToString(dates,SerializationFormatting.None);
+			var client = new ElasticClient();
+			var serializedDates = client.Serializer.SerializeToString(dates, SerializationFormatting.None);
 
-            serializedDates.Should()
-                .Be("{\"dateTimeUtcKind\":\"2016-01-01T01:01:01Z\",\"dateTimeOffset\":\"1999-01-01T01:01:01.001+05:00\"}");
+			serializedDates.Should()
+				.Be("{\"dateTimeUtcKind\":\"2016-01-01T01:01:01Z\",\"dateTimeOffset\":\"1999-01-01T01:01:01.001+05:00\"}");
 
-            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(serializedDates)))
-            {
-                var deserializedDates = client.Serializer.Deserialize<Dates>(stream);
+			using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(serializedDates)))
+			{
+				var deserializedDates = client.Serializer.Deserialize<Dates>(stream);
 
-                deserializedDates.DateTimeUtcKind.Should().Be(dates.DateTimeUtcKind);
-                deserializedDates.DateTimeUtcKind.Kind.Should().Be(dates.DateTimeUtcKind.Kind);
+				deserializedDates.DateTimeUtcKind.Should().Be(dates.DateTimeUtcKind);
+				deserializedDates.DateTimeUtcKind.Kind.Should().Be(dates.DateTimeUtcKind.Kind);
 
-                deserializedDates.DateTimeOffset.Should().Be(dates.DateTimeOffset);
-                deserializedDates.DateTimeOffset.Offset.Should().Be(dates.DateTimeOffset.Offset);
-                deserializedDates.DateTimeOffset.Date.Kind.Should().Be(dates.DateTimeOffset.Date.Kind);
-            }
-        }
+				deserializedDates.DateTimeOffset.Should().Be(dates.DateTimeOffset);
+				deserializedDates.DateTimeOffset.Offset.Should().Be(dates.DateTimeOffset.Offset);
+				deserializedDates.DateTimeOffset.Date.Kind.Should().Be(dates.DateTimeOffset.Date.Kind);
+			}
+		}
 
-        private class Dates
-        {
-            public DateTime DateTimeUtcKind { get; set; }
-            public DateTimeOffset DateTimeOffset { get; set; }
-        }
-    }
+		private class Dates
+		{
+			public DateTime DateTimeUtcKind { get; set; }
+			public DateTimeOffset DateTimeOffset { get; set; }
+		}
+	}
 }
