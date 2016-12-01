3fb075307ab70865169a812b712895177efdec1f
Russ Cam <russ.cam@elastic.co>
2016-12-01 12:17:03 +1100

    Add missing reference

--- src/Elasticsearch.Net/Connection/HttpConnection.cs
+++ src/Elasticsearch.Net/Connection/HttpConnection.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 // ReSharper disable VirtualMemberNeverOverriden.Global
