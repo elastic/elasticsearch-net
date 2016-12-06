ac85121fa267c7fb6da3582ea7db9ff834ebccc7
Russ Cam <russ.cam@elastic.co>
2016-12-06 18:45:20 +1100

    Convert NotImplementedException to NotSupportedException
    Semantically correct in these cases and means they do not get flagged as TODOs

--- src/Nest/CommonAbstractions/SerializationBehavior/GenericJsonConverters/JsonConverterBase.cs
+++ src/Nest/CommonAbstractions/SerializationBehavior/GenericJsonConverters/JsonConverterBase.cs
@@ -20,7 +20,7 @@ namespace Nest
 
 		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 
 	}
