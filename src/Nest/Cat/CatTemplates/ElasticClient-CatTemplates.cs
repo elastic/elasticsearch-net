efe128237c2d241bbccf8db4308da79edcf8e05e
Russ Cam <russ.cam@elastic.co>
2016-12-09 17:11:45 +1100

    add support for cat templates which is forthcomming in 5.1
    (cherry picked from commit 5f0db4aa6642ec29da14f51b3041db49f8d88a2c)

---
+++ src/Nest/Cat/CatTemplates/ElasticClient-CatTemplates.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using System.Threading;
+
+namespace Nest
+{
+	public partial interface IElasticClient
+	{
+		/// <inheritdoc/>
+		ICatResponse<CatTemplatesRecord> CatTemplates(Func<CatTemplatesDescriptor, ICatTemplatesRequest> selector = null);
+
+		/// <inheritdoc/>
+		ICatResponse<CatTemplatesRecord> CatTemplates(ICatTemplatesRequest request);
+
+		/// <inheritdoc/>
+		Task<ICatResponse<CatTemplatesRecord>> CatTemplatesAsync(
+			Func<CatTemplatesDescriptor, ICatTemplatesRequest> selector = null,
+			CancellationToken cancellationToken = default(CancellationToken)
+		);
+
+		/// <inheritdoc/>
+		Task<ICatResponse<CatTemplatesRecord>> CatTemplatesAsync(ICatTemplatesRequest request, CancellationToken cancellationToken = default(CancellationToken));
+
+	}
+
+	public partial class ElasticClient
+	{
+		/// <inheritdoc/>
+		public ICatResponse<CatTemplatesRecord> CatTemplates(Func<CatTemplatesDescriptor, ICatTemplatesRequest> selector = null) =>
+			this.CatTemplates(selector.InvokeOrDefault(new CatTemplatesDescriptor()));
+
+		/// <inheritdoc/>
+		public ICatResponse<CatTemplatesRecord> CatTemplates(ICatTemplatesRequest request) =>
+			this.DoCat<ICatTemplatesRequest, CatTemplatesRequestParameters, CatTemplatesRecord>(request, this.LowLevelDispatch.CatTemplatesDispatch<CatResponse<CatTemplatesRecord>>);
+
+		/// <inheritdoc/>
+		public Task<ICatResponse<CatTemplatesRecord>> CatTemplatesAsync(
+			Func<CatTemplatesDescriptor, ICatTemplatesRequest> selector = null,
+			CancellationToken cancellationToken = default(CancellationToken)
+		) => this.CatTemplatesAsync(selector.InvokeOrDefault(new CatTemplatesDescriptor()), cancellationToken);
+
+		/// <inheritdoc/>
+		public Task<ICatResponse<CatTemplatesRecord>> CatTemplatesAsync(ICatTemplatesRequest request, CancellationToken cancellationToken = default(CancellationToken)) =>
+			this.DoCatAsync<ICatTemplatesRequest, CatTemplatesRequestParameters, CatTemplatesRecord>(request, cancellationToken, this.LowLevelDispatch.CatTemplatesDispatchAsync<CatResponse<CatTemplatesRecord>>);
+
+	}
+}
