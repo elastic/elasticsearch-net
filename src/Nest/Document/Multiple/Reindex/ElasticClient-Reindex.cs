4a1dfa77b8f30d3a2dbb894b69985d91e3567ac4
Martijn Laarman <Mpdreamz@gmail.com>
2016-12-13 00:34:36 +0100

    Reindex now allows the target type to be different (#2480)
    * Reindex now allows the target type to be different
    
    User has to supply an explicit map between TSource and TTarget, the old
    overloads where TSource == TTarget still exists.
    
    The simplified helpers for reindex contained a bug, out integration
    tests only reindexed 2 documents but if you had more the backpressure
    sanity checks would through, these now run with sane defaults.
    
    * fix exception messages and ConsistentFluentParamNames tests
    
    * Simplify exception messages

--- src/Nest/Document/Multiple/Reindex/ElasticClient-Reindex.cs
+++ src/Nest/Document/Multiple/Reindex/ElasticClient-Reindex.cs
@@ -9,71 +9,174 @@ namespace Nest
 		/// Helper method that allows you to reindex from one index into another using ScrollAll and BulkAll.
 		/// </summary>
 		/// <returns>An IObservable&lt;IReindexResponse&lt;T&gt;$gt; you can subscribe to to listen to the progress of the reindex process</returns>
-		IObservable<IBulkAllResponse> Reindex<T>(
-			Func<ReindexDescriptor<T>, IReindexRequest<T>> selector,
+		IObservable<IBulkAllResponse> Reindex<TSource,TTarget>(
+			Func<TSource, TTarget> mapper,
+			Func<ReindexDescriptor<TSource,TTarget>, IReindexRequest<TSource,TTarget>> selector,
 			CancellationToken cancellationToken = default(CancellationToken)
-		) where T : class;
+		)
+			where TSource : class
+			where TTarget : class;
 
 		/// <summary>
-		/// Simplified form for reindex which will cover 80% of its usecases. Allows you to index all documents of type T from <paramref name="fromIndex" /> to <paramref name="toIndex" />
-		/// optionally limitting the documents found in <paramref name="fromIndex" /> by using <paramref name="selector"/>.
+		/// Helper method that allows you to reindex from one index into another using ScrollAll and BulkAll.
 		/// </summary>
-		/// <param name="fromIndex">The source index, from which all types will be returned</param>
-		/// <param name="toIndex">The target index, if it does not exist already will be created using the same settings of <paramref name="fromIndex"/></param>
-		/// <param name="selector">an optional query limitting the documents found in <paramref name="fromIndex"/></param>
-		IObservable<IBulkAllResponse> Reindex<T>(
-			IndexName fromIndex,
-			IndexName toIndex,
-			Func<QueryContainerDescriptor<T>, QueryContainer> selector = null,
+		/// <returns>An IObservable&lt;IReindexResponse&lt;T&gt;$gt; you can subscribe to to listen to the progress of the reindex process</returns>
+		IObservable<IBulkAllResponse> Reindex<TSource>(
+			Func<ReindexDescriptor<TSource, TSource>, IReindexRequest<TSource, TSource>> selector,
 			CancellationToken cancellationToken = default(CancellationToken)
-		) where T : class;
+		)
+			where TSource : class;
 
 		/// <summary>
 		/// Helper method that allows you to reindex from one index into another using ScrollAll and BulkAll.
 		/// </summary>
 		/// <param name="request">a request object to describe the reindex operation</param>
 		/// <returns>An IObservable&lt;IReindexResponse&lt;T&gt;$gt; you can subscribe to to listen to the progress of the reindex process</returns>
-		IObservable<IBulkAllResponse> Reindex<T>(
-			IReindexRequest<T> request,
+		IObservable<IBulkAllResponse> Reindex<TSource,TTarget>(
+			IReindexRequest<TSource,TTarget> request,
 			CancellationToken cancellationToken = default(CancellationToken)
-		) where T : class;
+		)
+			where TSource : class
+			where TTarget : class;
+
+		/// <summary>
+		/// Helper method that allows you to reindex from one index into another using ScrollAll and BulkAll.
+		/// </summary>
+		/// <param name="request">a request object to describe the reindex operation</param>
+		/// <returns>An IObservable&lt;IReindexResponse&lt;T&gt;$gt; you can subscribe to to listen to the progress of the reindex process</returns>
+		IObservable<IBulkAllResponse> Reindex<TSource>(
+			IReindexRequest<TSource> request,
+			CancellationToken cancellationToken = default(CancellationToken)
+		)
+			where TSource : class;
+
+		/// <summary>
+		/// Simplified form for reindex which will cover 80% of its usecases. Allows you to index all documents of type T from <paramref name="fromIndex" /> to <paramref name="toIndex" />
+		/// optionally limiting the documents found in <paramref name="fromIndex" /> by using <paramref name="selector"/>.
+		/// </summary>
+		/// <param name="fromIndex">The source index, from which all types will be returned</param>
+		/// <param name="toIndex">The target index, if it does not exist already will be created using the same settings of <paramref name="fromIndex"/></param>
+		/// <param name="selector">an optional query limiting the documents found in <paramref name="fromIndex"/></param>
+		IObservable<IBulkAllResponse> Reindex<TSource,TTarget>(
+			IndexName fromIndex,
+			IndexName toIndex,
+			Func<TSource, TTarget> mapper,
+			Func<QueryContainerDescriptor<TSource>, QueryContainer> selector = null,
+			CancellationToken cancellationToken = default(CancellationToken)
+		)
+			where TSource : class
+			where TTarget : class;
+
+		/// <summary>
+		/// Simplified form for reindex which will cover 80% of its use cases. Allows you to index all documents of type T from <paramref name="fromIndex" /> to <paramref name="toIndex" />
+		/// optionally limiting the documents found in <paramref name="fromIndex" /> by using <paramref name="selector"/>.
+		/// </summary>
+		/// <param name="fromIndex">The source index, from which all types will be returned</param>
+		/// <param name="toIndex">The target index, if it does not exist already will be created using the same settings of <paramref name="fromIndex"/></param>
+		/// <param name="selector">an optional query limiting the documents found in <paramref name="fromIndex"/></param>
+		IObservable<IBulkAllResponse> Reindex<TSource>(
+			IndexName fromIndex,
+			IndexName toIndex,
+			Func<QueryContainerDescriptor<TSource>, QueryContainer> selector = null,
+			CancellationToken cancellationToken = default(CancellationToken)
+		)
+			where TSource : class;
+
 	}
 
 	public partial class ElasticClient
 	{
 		/// <inheritdoc />
-		public IObservable<IBulkAllResponse> Reindex<T>(
-			Func<ReindexDescriptor<T>, IReindexRequest<T>> selector,
+		public IObservable<IBulkAllResponse> Reindex<TSource>(
+			Func<ReindexDescriptor<TSource,TSource>, IReindexRequest<TSource,TSource>> selector,
 			CancellationToken cancellationToken = default(CancellationToken)
-		) where T : class =>
-			this.Reindex<T>(selector.InvokeOrDefault(new ReindexDescriptor<T>()));
+		)
+			where TSource : class =>
+			this.Reindex(selector.InvokeOrDefault(new ReindexDescriptor<TSource,TSource>(s=>s)));
 
 		/// <inheritdoc />
-		public IObservable<IBulkAllResponse> Reindex<T>(
-			IndexName fromIndex,
-			IndexName toIndex,
-			Func<QueryContainerDescriptor<T>, QueryContainer> selector = null,
+		public IObservable<IBulkAllResponse> Reindex<TSource,TTarget>(
+			Func<TSource, TTarget> mapper,
+			Func<ReindexDescriptor<TSource,TTarget>, IReindexRequest<TSource,TTarget>> selector,
 			CancellationToken cancellationToken = default(CancellationToken)
-		) where T : class =>
-			this.Reindex<T>(r => r
-					.ScrollAll("1m", -1, search => search.Search(ss => ss.Index(fromIndex).Query(selector)))
-					.BulkAll(b => b.Index(toIndex))
-			, cancellationToken);
+		)
+			where TTarget : class
+			where TSource : class =>
+			this.Reindex(selector.InvokeOrDefault(new ReindexDescriptor<TSource,TTarget>(mapper)));
+
+		/// <inheritdoc />
+		public IObservable<IBulkAllResponse> Reindex<TSource>(
+			IReindexRequest<TSource> request,
+			CancellationToken cancellationToken = default(CancellationToken)
+		)
+			where TSource : class =>
+			this.Reindex<TSource, TSource>(request, cancellationToken);
 
 
 		/// <inheritdoc />
-		public IObservable<IBulkAllResponse> Reindex<T>(
-			IReindexRequest<T> request,
+		public IObservable<IBulkAllResponse> Reindex<TSource,TTarget>(
+			IReindexRequest<TSource,TTarget> request,
 			CancellationToken cancellationToken = default(CancellationToken)
-		) where T : class
+		)
+			where TTarget : class
+			where TSource : class
 		{
 			if (request.ScrollAll == null)
 				throw new ArgumentException(
-					"Reindex must have ScrollAll property set so we know how to get the source of the reindex operation");
+					"ScrollAll property must be set in order to get the source of a Reindex operation");
 			if (request.BulkAll == null)
 				throw new ArgumentException(
-					"Reindex must have BulkAll property set so we know how to get the target of the reindex operation");
-			return new ReindexObservable<T>(this, ConnectionSettings, request, cancellationToken);
+					"BulkAll property must set in order to get the target of a Reindex operation");
+			return new ReindexObservable<TSource,TTarget>(this, ConnectionSettings, request, cancellationToken);
 		}
+
+		/// <inheritdoc />
+		public IObservable<IBulkAllResponse> Reindex<TSource,TTarget>(
+			IndexName fromIndex,
+			IndexName toIndex,
+			Func<TSource, TTarget> mapper,
+			Func<QueryContainerDescriptor<TSource>, QueryContainer> selector = null,
+			CancellationToken cancellationToken = default(CancellationToken)
+		)
+			where TTarget : class
+			where TSource : class =>
+			this.Reindex(
+				mapper,
+				SimplifiedReindexer<TSource, TTarget>(fromIndex, toIndex, selector)
+			, cancellationToken);
+
+		/// <inheritdoc />
+		public IObservable<IBulkAllResponse> Reindex<TSource>(
+			IndexName fromIndex,
+			IndexName toIndex,
+			Func<QueryContainerDescriptor<TSource>, QueryContainer> selector = null,
+			CancellationToken cancellationToken = default(CancellationToken)
+		)
+			where TSource : class =>
+			this.Reindex(
+				s=>s,
+				SimplifiedReindexer<TSource, TSource>(fromIndex, toIndex, selector)
+			, cancellationToken);
+
+		private static Func<ReindexDescriptor<TSource,TTarget>, IReindexRequest<TSource,TTarget>> SimplifiedReindexer<TSource, TTarget>(
+			IndexName fromIndex,
+			IndexName toIndex,
+			Func<QueryContainerDescriptor<TSource>, QueryContainer> selector
+		)
+			where TTarget : class
+			where TSource : class
+		{
+			return r => r
+				.ScrollAll("1m", -1, search => search
+					.Search(ss => ss
+						.Size(CoordinatedRequestDefaults.ReindexScrollSize)
+						.Index(fromIndex)
+						.Query(selector)
+					)
+				)
+				.BulkAll(b => b.Index(toIndex));
+		}
+
+
 	}
 }
--- src/Nest/Document/Multiple/Reindex/ReindexObservable.cs
+++ src/Nest/Document/Multiple/Reindex/ReindexObservable.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -8,9 +7,20 @@ using static Nest.Infer;
 
 namespace Nest
 {
-	public class ReindexObservable<T> : IDisposable, IObservable<IBulkAllResponse> where T : class
+	public class ReindexObservable<TSource> : ReindexObservable<TSource, TSource>
+		where TSource : class
 	{
-		private readonly IReindexRequest<T> _reindexRequest;
+		public ReindexObservable(IElasticClient client, IConnectionSettingsValues connectionSettings, IReindexRequest<TSource, TSource> reindexRequest, CancellationToken cancellationToken)
+			: base(client, connectionSettings, reindexRequest, cancellationToken)
+		{
+		}
+	}
+
+	public class ReindexObservable<TSource, TTarget> : IDisposable, IObservable<IBulkAllResponse>
+		where TSource : class
+		where TTarget : class
+	{
+		private readonly IReindexRequest<TSource, TTarget> _reindexRequest;
 		private readonly IConnectionSettingsValues _connectionSettings;
 		private readonly IElasticClient _client;
 
@@ -19,7 +29,11 @@ namespace Nest
 		private readonly CancellationTokenSource _compositeCancelTokenSource;
 		private readonly CancellationToken _compositeCancelToken;
 
-		public ReindexObservable(IElasticClient client, IConnectionSettingsValues connectionSettings, IReindexRequest<T> reindexRequest, CancellationToken cancellationToken)
+		public ReindexObservable(
+			IElasticClient client,
+			IConnectionSettingsValues connectionSettings,
+			IReindexRequest<TSource, TTarget> reindexRequest,
+			CancellationToken cancellationToken)
 		{
 			this._connectionSettings = connectionSettings;
 			this._reindexRequest = reindexRequest;
@@ -28,7 +42,7 @@ namespace Nest
 			this._compositeCancelToken = this._compositeCancelTokenSource.Token;
 		}
 
-		public IDisposable Subscribe(ReindexObserver<T> observer)
+		public IDisposable Subscribe(ReindexObserver observer)
 		{
 			this._incrementSeenDocuments = observer.IncrementSeenScrollDocuments;
 			this._incrementSeenScrollOperations = observer.IncrementSeenScrollOperations;
@@ -51,7 +65,7 @@ namespace Nest
 
 		private void Reindex(IObserver<IBulkAllResponse> observer)
 		{
-			var bulkMeta = this._reindexRequest.BulkAll?.Invoke(Enumerable.Empty<IHit<T>>());
+			var bulkMeta = this._reindexRequest.BulkAll?.Invoke(Enumerable.Empty<IHitMetadata<TTarget>>());
 			var scrollAll = this._reindexRequest.ScrollAll;
 			var toIndex = bulkMeta?.Index.Resolve(this._connectionSettings);
 
@@ -60,7 +74,9 @@ namespace Nest
 			var backPressure = this.CreateBackPressure(bulkMeta, scrollAll, slices);
 
 			var scrollDocuments = this.ScrollAll(slices, backPressure)
-				.SelectMany(r => r.SearchResponse.Hits);
+				.SelectMany(r => r.SearchResponse.Hits)
+				.Select(r=> r.Copy(this._reindexRequest.Map));
+
 
 			var observableBulk = this.BulkAll(scrollDocuments, backPressure, toIndex);
 
@@ -71,18 +87,18 @@ namespace Nest
 			else observableBulk.Subscribe(observer);
 		}
 
-		private BulkAllObservable<IHit<T>> BulkAll(IEnumerable<IHit<T>> scrollDocuments, ProducerConsumerBackPressure backPressure, string toIndex)
+		private BulkAllObservable<IHitMetadata<TTarget>> BulkAll(IEnumerable<IHitMetadata<TTarget>> scrollDocuments, ProducerConsumerBackPressure backPressure, string toIndex)
 		{
 			var bulkAllRequest = this._reindexRequest.BulkAll?.Invoke(scrollDocuments);
 			if (bulkAllRequest == null)
-				throw new Exception("Reindex can not commence BulkAll was not defined, we have no way of knowing how to index the documents now.");
+				throw new Exception("BulkAll must set on ReindexRequest in order to get the target of a Reindex operation");
 
 			bulkAllRequest.BackPressure = backPressure;
 			bulkAllRequest.BufferToBulk = (bulk, hits) =>
 			{
 				foreach (var hit in hits)
 				{
-					var item = new BulkIndexOperation<T>(hit.Source)
+					var item = new BulkIndexOperation<TTarget>(hit.Source)
 					{
 						Type = hit.Type,
 						Index = toIndex,
@@ -98,9 +114,9 @@ namespace Nest
 			return observableBulk;
 		}
 
-		private ProducerConsumerBackPressure CreateBackPressure(IBulkAllRequest<IHit<T>> bulkMeta, IScrollAllRequest scrollAll, int slices)
+		private ProducerConsumerBackPressure CreateBackPressure(IBulkAllRequest<IHitMetadata<TTarget>> bulkMeta, IScrollAllRequest scrollAll, int slices)
 		{
-			var backPressureFactor = this._reindexRequest.BackPressureFactor;
+			var backPressureFactor = this._reindexRequest.BackPressureFactor ?? CoordinatedRequestDefaults.ReindexBackPressureFactor;
 			var maxConcurrentConsumers = bulkMeta?.MaxDegreeOfParallelism ?? CoordinatedRequestDefaults.BulkAllMaxDegreeOfParallelismDefault;
 			var maxConcurrentProducers = scrollAll?.MaxDegreeOfParallelism ?? slices;
 			var maxConcurrency = Math.Min(maxConcurrentConsumers, maxConcurrentProducers);
@@ -110,17 +126,16 @@ namespace Nest
 			var producerBandwidth = searchSize * maxConcurrency * backPressureFactor;
 			var funnelTooSmall =  producerBandwidth < bulkSize;
 			if (funnelTooSmall)
-				throw new Exception("The back pressure settings are squeezing too hard "
+				throw new Exception("The back pressure settings are too conservative in providing enough documents for a single bulk operation. "
 					+ $"searchSize:{searchSize} * maxConcurrency:{maxConcurrency} * backPressureFactor:{backPressureFactor} = {producerBandwidth}"
-					+ $" which is smaller then the bulkSize:{bulkSize}. Meaning the producers won't have the chance to produce enough documents "
-					+ "for a single bulk operation"
+					+ $" which is smaller then the bulkSize:{bulkSize}."
 				);
 
 			var backPressure = new ProducerConsumerBackPressure(backPressureFactor, maxConcurrency);
 			return backPressure;
 		}
 
-		private IEnumerable<IScrollAllResponse<T>> ScrollAll(int slices, ProducerConsumerBackPressure backPressure)
+		private IEnumerable<IScrollAllResponse<TSource>> ScrollAll(int slices, ProducerConsumerBackPressure backPressure)
 		{
 			var scrollAll = this._reindexRequest.ScrollAll;
 			var scroll = this._reindexRequest.ScrollAll?.ScrollTime ?? TimeSpan.FromMinutes(2);
@@ -133,13 +148,13 @@ namespace Nest
 				BackPressure = backPressure
 			};
 
-			var scrollObservable = this._client.ScrollAll<T>(scrollAllRequest, this._compositeCancelToken);
-			return new GetEnumerator<IScrollAllResponse<T>>()
+			var scrollObservable = this._client.ScrollAll<TSource>(scrollAllRequest, this._compositeCancelToken);
+			return new GetEnumerator<IScrollAllResponse<TSource>>()
 				.ToEnumerable(scrollObservable)
 				.Select(ObserveScrollAllResponse);
 		}
 
-		private IScrollAllResponse<T> ObserveScrollAllResponse(IScrollAllResponse<T> response)
+		private IScrollAllResponse<TSource> ObserveScrollAllResponse(IScrollAllResponse<TSource> response)
 		{
 			this._incrementSeenScrollOperations();
 			this._incrementSeenDocuments(response.SearchResponse.Hits.Count);
@@ -151,10 +166,10 @@ namespace Nest
 
 		private int CreateIndex(string toIndex, IScrollAllRequest scrollAll)
 		{
-			var fromIndices = this._reindexRequest.ScrollAll?.Search?.Index ?? Indices<T>();
+			var fromIndices = this._reindexRequest.ScrollAll?.Search?.Index ?? Indices<TSource>();
 
 			if (string.IsNullOrEmpty(toIndex))
-				throw new Exception($"Can not resolve the target index name to reindex to make sure the bulk all operation describes one");
+				throw new Exception($"Could not resolve the target index name to reindex to make sure the bulk all operation describes one");
 
 			var numberOfShards = this.CreateIndexIfNeeded(fromIndices, toIndex);
 
@@ -164,8 +179,8 @@ namespace Nest
 			else if (scrollAll?.Slices < 0)
 				throw new Exception("Slices is a negative number and no sane default could be inferred from the origin index's number_of_shards");
 			if (!slices.HasValue)
-				throw new Exception("Reindex can not commence because slices is not specified and we could not get a number of "
-				                    + "shards hint from the source, usually this could happen if the scroll all points to multiple indices and no slices have been set");
+				throw new Exception("Slices is not specified and could not be inferred from the number of "
+				                    + "shards hint from the source. This could happen if the scroll all points to multiple indices and no slices have been set");
 			return slices.Value;
 		}
 
@@ -208,7 +223,7 @@ namespace Nest
 			var createIndexResponse = this._client.CreateIndex(createIndexRequest);
 			this._compositeCancelToken.ThrowIfCancellationRequested();
 			if (!createIndexResponse.IsValid)
-				throw Throw($"Failed to create destination index {resolvedTo}.", createIndexResponse.ApiCall);
+				throw Throw($"Could not create destination index {resolvedTo}.", createIndexResponse.ApiCall);
 
 			return createIndexRequest.Settings?.NumberOfShards;
 		}
