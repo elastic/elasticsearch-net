1151a63921077000ac7e0cb98e3ab905f006cd9e
Mpdreamz <mpdreamz@gmail.com>
2016-12-28 11:23:48 +0100

    moved manual deserializing to RoundTripper

--- src/Tests/Reproduce/GithubIssue2503.cs
+++ src/Tests/Reproduce/GithubIssue2503.cs
@@ -3,79 +3,62 @@ using System.Text;
 using FluentAssertions;
 using Nest;
 using Tests.Framework;
+using static Tests.Framework.RoundTripper;
 
 namespace Tests.Reproduce
 {
 	public class GithubIssue2503
 	{
-		[U]
-		public void DeserializeTermsIncludeExcludeValues()
+		[U] public void DeserializeTermsIncludeExcludeValues()
 		{
-			var json = @"{
-						  ""aggs"": {
-							""sizes"": {
-							  ""terms"": {
-								""field"": ""size"",
-								""size"": 20,
-								""include"": [
-								  ""35"",
-								  ""50"",
-								  ""70"",
-								  ""75"",
-								  ""100""
-								]
-							  }
-							}
-						  }
-						}";
-
-			var client = TestClient.GetInMemoryClient();
-			SearchRequest request;
-
-			using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+			Expect(new
 			{
-				request = client.Serializer.Deserialize<SearchRequest>(stream);
-			}
-
-			request.Should().NotBeNull();
-			request.Aggregations.Should().NotBeNull().And.HaveCount(1);
-			var termsAggregation = request.Aggregations["sizes"].Terms;
-			termsAggregation.Should().NotBeNull();
-			termsAggregation.Include.Should().NotBeNull();
-			termsAggregation.Include.Values.Should().NotBeNull().And.HaveCount(5);
+				aggs = new
+				{
+					sizes = new
+					{
+						terms =  new
+						{
+							field= "size",
+							size= 20,
+							include= new [] { "35", "50", "70", "75", "100" }
+						}
+					}
+				}
+			}).DeserializesTo<SearchRequest>((message, request) =>
+			{
+				request.Should().NotBeNull(message);
+				request.Aggregations.Should().NotBeNull(message).And.HaveCount(1, message);
+				var termsAggregation = request.Aggregations["sizes"].Terms;
+				termsAggregation.Should().NotBeNull(message);
+				termsAggregation.Include.Should().NotBeNull(message);
+				termsAggregation.Include.Values.Should().NotBeNull(message).And.HaveCount(5, message);
+			});
 		}
 
-		[U]
-		public void DeserializeTermsIncludeExcludePattern()
+		[U] public void DeserializeTermsIncludeExcludePattern()
 		{
-			var json = @"{
-						  ""aggs"": {
-							""sizes"": {
-							  ""terms"": {
-								""field"": ""size"",
-								""size"": 20,
-								""include"": {
-								  ""pattern"" : ""\\d+""
-								}
-							  }
-							}
-						  }
-						}";
-
-			var client = TestClient.GetInMemoryClient();
-			SearchRequest request;
-
-			using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+			Expect(@"{
+			  ""aggs"": {
+				""sizes"": {
+				  ""terms"": {
+					""field"": ""size"",
+					""size"": 20,
+					""include"": {
+					  ""pattern"" : ""\\d+""
+					}
+				  }
+				}
+			  }
+			}").DeserializesTo<SearchRequest>((message, request) =>
 			{
-				request = client.Serializer.Deserialize<SearchRequest>(stream);
-			}
-
-			request.Should().NotBeNull();
-			request.Aggregations.Should().NotBeNull().And.HaveCount(1);
-			var termsAggregation = request.Aggregations["sizes"].Terms;
-			termsAggregation.Should().NotBeNull();
-			termsAggregation.Include.Should().NotBeNull();
-			termsAggregation.Include.Pattern.Should().NotBeNull();
+				request.Should().NotBeNull(message);
+				request.Aggregations.Should().NotBeNull(message).And.HaveCount(1, message);
+				var termsAggregation = request.Aggregations["sizes"].Terms;
+				termsAggregation.Should().NotBeNull(message);
+				termsAggregation.Include.Should().NotBeNull(message);
+				termsAggregation.Include.Pattern.Should().NotBeNull(message);
+			});
 		}
 	}
 }
