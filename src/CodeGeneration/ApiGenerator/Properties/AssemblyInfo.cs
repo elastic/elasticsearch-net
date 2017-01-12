76b7cd9db62191b97c8bb0b9f68297588353776a
Russ Cam <russ.cam@elastic.co>
2017-01-13 01:07:14 +1100

    Bump assembly version for release

--- src/CodeGeneration/ApiGenerator/Properties/AssemblyInfo.cs
+++ src/CodeGeneration/ApiGenerator/Properties/AssemblyInfo.cs
@@ -2,16 +2,22 @@
 using System.Reflection;
 
 [assembly: AssemblyTitleAttribute("ApiGenerator")]
-[assembly: AssemblyCopyrightAttribute("Elasticsearch 2016")]
+[assembly: AssemblyCopyrightAttribute("Elasticsearch 2017")]
 [assembly: AssemblyDescriptionAttribute("")]
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("5.0.0")]
-[assembly: AssemblyFileVersionAttribute("5.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.0.0")]
+[assembly: AssemblyFileVersionAttribute("5.0.1")]
+[assembly: AssemblyInformationalVersionAttribute("5.0.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "5.0.0";
-        internal const string InformationalVersion = "5.0.0";
+        internal const System.String AssemblyTitle = "ApiGenerator";
+        internal const System.String AssemblyCopyright = "Elasticsearch 2017";
+        internal const System.String AssemblyDescription = "";
+        internal const System.String AssemblyCompany = "Elasticsearch";
+        internal const System.String AssemblyConfiguration = "Release";
+        internal const System.String AssemblyVersion = "5.0.0";
+        internal const System.String AssemblyFileVersion = "5.0.1";
+        internal const System.String AssemblyInformationalVersion = "5.0.1";
     }
 }
--- src/Elasticsearch.Net/Properties/AssemblyInfo.cs
+++ src/Elasticsearch.Net/Properties/AssemblyInfo.cs
@@ -2,16 +2,22 @@
 using System.Reflection;
 
 [assembly: AssemblyTitleAttribute("Elasticsearch.Net")]
-[assembly: AssemblyCopyrightAttribute("Elasticsearch 2016")]
+[assembly: AssemblyCopyrightAttribute("Elasticsearch 2017")]
 [assembly: AssemblyDescriptionAttribute("Elasticsearch.Net - official low level elasticsearch client")]
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("5.0.0")]
-[assembly: AssemblyFileVersionAttribute("5.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.0.0")]
+[assembly: AssemblyFileVersionAttribute("5.0.1")]
+[assembly: AssemblyInformationalVersionAttribute("5.0.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "5.0.0";
-        internal const string InformationalVersion = "5.0.0";
+        internal const System.String AssemblyTitle = "Elasticsearch.Net";
+        internal const System.String AssemblyCopyright = "Elasticsearch 2017";
+        internal const System.String AssemblyDescription = "Elasticsearch.Net - official low level elasticsearch client";
+        internal const System.String AssemblyCompany = "Elasticsearch";
+        internal const System.String AssemblyConfiguration = "Release";
+        internal const System.String AssemblyVersion = "5.0.0";
+        internal const System.String AssemblyFileVersion = "5.0.1";
+        internal const System.String AssemblyInformationalVersion = "5.0.1";
     }
 }
