d3d9f0ff92575272a7beb55317fbe729d49b37ba
Russ Cam <russ.cam@elastic.co>
2017-01-12 23:27:55 +1100

    Bump assembly versions for release

--- src/CodeGeneration/CodeGeneration.LowLevelClient/Properties/AssemblyInfo.cs
+++ src/CodeGeneration/CodeGeneration.LowLevelClient/Properties/AssemblyInfo.cs
@@ -2,13 +2,13 @@
 using System.Reflection;
 
 [assembly: AssemblyTitleAttribute("CodeGeneration.LowLevelClient")]
-[assembly: AssemblyCopyrightAttribute("Elasticsearch 2016")]
+[assembly: AssemblyCopyrightAttribute("Elasticsearch 2017")]
 [assembly: AssemblyDescriptionAttribute("")]
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.9.1")]
-[assembly: AssemblyInformationalVersionAttribute("1.9.1")]
+[assembly: AssemblyFileVersionAttribute("1.9.2")]
+[assembly: AssemblyInformationalVersionAttribute("1.9.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
--- src/Nest/Properties/AssemblyInfo.cs
+++ src/Nest/Properties/AssemblyInfo.cs
@@ -2,13 +2,13 @@
 using System.Reflection;
 
 [assembly: AssemblyTitleAttribute("Nest")]
-[assembly: AssemblyCopyrightAttribute("Elasticsearch 2016")]
+[assembly: AssemblyCopyrightAttribute("Elasticsearch 2017")]
 [assembly: AssemblyDescriptionAttribute("NEST - official high level elasticsearch client")]
 [assembly: AssemblyCompanyAttribute("Elasticsearch")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 [assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.9.1")]
-[assembly: AssemblyInformationalVersionAttribute("1.9.1")]
+[assembly: AssemblyFileVersionAttribute("1.9.2")]
+[assembly: AssemblyInformationalVersionAttribute("1.9.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
