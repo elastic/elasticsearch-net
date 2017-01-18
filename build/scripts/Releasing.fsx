b2db416c81ba22bc935278d824ee8d7c20985ada
Russ Cam <russ.cam@elastic.co>
2017-01-18 19:57:18 +1100

    Limit NEST 5.x compatibility to Elasticsearch.Net 5.x versions (#2537)
    Pass next major version and current year as paramaters to nuget pack

--- build/scripts/Releasing.fsx
+++ build/scripts/Releasing.fsx
@@ -1,12 +1,19 @@
 ﻿#I @"../../packages/build/FAKE/tools"
+#I @"../../packages/SemanticVersioning/lib/net45"
 #r @"FakeLib.dll"
+#r @"SemVer.dll"
 
 #load @"Projects.fsx"
 #load @"Paths.fsx"
 #load @"Tooling.fsx"
 #load @"Versioning.fsx"
 
-open Fake 
+open System
+open System.Text
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+open Fake
+open SemVer
 
 open Paths
 open Projects
@@ -23,7 +30,33 @@ module Release =
             let nuspec = (sprintf @"build\%s.nuspec" name)
             let nugetOutFile =  Paths.Output(sprintf "%s.%s.nupkg" name Versioning.FileVersion)
 
-            Tooling.Nuget.Exec ["pack"; nuspec; "-version"; Versioning.FileVersion; "-outputdirectory"; Paths.BuildOutput; ] |> ignore
+            let nextMajorVersion = 
+                let version = new Version(Versioning.FileVersion)
+                let nextMajor = version.Major + 1
+                sprintf "%i" nextMajor
+
+            let year = sprintf "%i" DateTime.UtcNow.Year
+
+            let properties =
+                let addKeyValue (e:Expr<string>) (builder:StringBuilder) =
+                    // the binding for this tuple looks like key/value should 
+                    // be round the other way (but it's correct as is)...
+                    let (value,key) = 
+                        match e with
+                        | PropertyGet (eo, pi, li) -> (pi.Name, (pi.GetValue(e) |> string))
+                        | ValueWithName (obj,ty,nm) -> ((obj |> string), nm)
+                        | _ -> failwith (sprintf "%A is not a let-bound value. %A" e (e.GetType()))
+                    builder.AppendFormat("{0}=\"{1}\";", key, value);
+                new StringBuilder()
+                |> addKeyValue <@nextMajorVersion@>
+                |> addKeyValue <@year@>
+                |> toText
+            
+            Tooling.Nuget.Exec [ "pack"; nuspec; 
+                                 "-version"; Versioning.FileVersion; 
+                                 "-outputdirectory"; Paths.BuildOutput; 
+                                 "-properties"; properties; 
+                               ] |> ignore
             traceFAKE "%s" Paths.BuildOutput
             MoveFile Paths.NugetOutput nugetOutFile
         )
