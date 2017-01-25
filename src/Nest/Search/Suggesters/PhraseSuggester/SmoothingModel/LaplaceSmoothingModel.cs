00c544ba8960b3a0cf8294a0dcc3893922e3f264
Mpdreamz <mpdreamz@gmail.com>
2017-01-25 10:44:53 +0100

    backported suggester smoothing to 2.x

---
+++ src/Nest/Search/Suggesters/PhraseSuggester/SmoothingModel/LaplaceSmoothingModel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Nest
+{
+	[JsonObject]
+	[JsonConverter(typeof(ReadAsTypeJsonConverter<LaplaceSmoothingModel>))]
+	public interface ILaplaceSmoothingModel : ISmoothingModel
+	{
+		[JsonProperty("alpha")]
+		double? Alpha { get; set; }
+	}
+
+	public class LaplaceSmoothingModel : SmoothingModelBase, ILaplaceSmoothingModel
+	{
+		public double? Alpha { get; set; }
+
+		internal override void WrapInContainer(ISmoothingModelContainer container) => container.Laplace = this;
+	}
+
+	public class LaplaceSmoothingModelDescriptor : DescriptorBase<LaplaceSmoothingModelDescriptor, ILaplaceSmoothingModel>, ILaplaceSmoothingModel
+	{
+		double? ILaplaceSmoothingModel.Alpha{ get; set; }
+
+		public LaplaceSmoothingModelDescriptor Alpha(double? alpha) => Assign(a => a.Alpha = alpha);
+	}
+}
