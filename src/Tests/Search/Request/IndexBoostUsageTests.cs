face0e859b575c5e01fc532f115a0d46b84886cb
Mpdreamz <mpdreamz@gmail.com>
2017-02-05 13:59:00 +0100

    fix index boos usage integration test

--- src/Tests/Search/Request/IndexBoostUsageTests.cs
+++ src/Tests/Search/Request/IndexBoostUsageTests.cs
@@ -14,15 +14,15 @@ namespace Tests.Search.Request
 		{
 			indices_boost = new
 			{
-				index1 = 1.4,
-				index2 = 1.3
+				project = 1.4,
+				devs = 1.3
 			}
 		};
 
 		protected override Func<SearchDescriptor<Project>, ISearchRequest> Fluent => s => s
 			.IndicesBoost(b => b
-				.Add("index1", 1.4)
-				.Add("index2", 1.3)
+				.Add("project", 1.4)
+				.Add("devs", 1.3)
 			);
 
 		protected override SearchRequest<Project> Initializer =>
@@ -30,8 +30,8 @@ namespace Tests.Search.Request
 			{
 				IndicesBoost = new Dictionary<IndexName, double>
 				{
-						{ "index1", 1.4 },
-						{ "index2", 1.3 }
+					{ "project", 1.4 },
+					{ "devs", 1.3 }
 				}
 			};
 	}
