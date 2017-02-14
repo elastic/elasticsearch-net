f926b9736ea49f418fabecb78d43b2f1724a173f
Mpdreamz <mpdreamz@gmail.com>
2017-02-14 15:34:40 +0100

    error:, should be deserialized into ServerError as well

--- src/Tests/Indices/AliasManagement/AliasExists/AliasExistsApiTests.cs
+++ src/Tests/Indices/AliasManagement/AliasExists/AliasExistsApiTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Elasticsearch.Net;
+using FluentAssertions;
 using Nest;
 using Tests.Framework;
 using Tests.Framework.Integration;
@@ -13,13 +14,15 @@ namespace Tests.Indices.AliasManagement.AliasExists
 		: ApiIntegrationTestBase<WritableCluster, IExistsResponse, IAliasExistsRequest, AliasExistsDescriptor, AliasExistsRequest>
 
 	{
-		public AliasExistsApiTests(WritableCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+		public AliasExistsApiTests(WritableCluster cluster, EndpointUsage usage) : base(cluster, usage)
+		{
+		}
 
 		protected override void IntegrationSetup(IElasticClient client, CallUniqueValues values)
 		{
 			foreach (var index in values.Values)
-				client.CreateIndex(index, c=>c
-					.Aliases(aa=>aa.Alias(index + "-alias"))
+				client.CreateIndex(index, c => c
+					.Aliases(aa => aa.Alias(index + "-alias"))
 				);
 		}
 
@@ -41,5 +44,39 @@ namespace Tests.Indices.AliasManagement.AliasExists
 			.Name(CallIsolatedValue + "-alias");
 
 		protected override AliasExistsRequest Initializer => new AliasExistsRequest(Names(CallIsolatedValue + "-alias"));
+	}
+
+	public class AliasExistsNotFoundApiTests
+		: ApiIntegrationTestBase<ReadOnlyCluster, IExistsResponse, IAliasExistsRequest, AliasExistsDescriptor, AliasExistsRequest>
+
+	{
+		public AliasExistsNotFoundApiTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(cluster, usage)
+		{
+		}
+
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.AliasExists(f),
+			fluentAsync: (client, f) => client.AliasExistsAsync(f),
+			request: (client, r) => client.AliasExists(r),
+			requestAsync: (client, r) => client.AliasExistsAsync(r)
+		);
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 404;
+		protected override HttpMethod HttpMethod => HttpMethod.HEAD;
+		protected override string UrlPath => $"/_alias/unknown-alias";
+
+		protected override bool SupportsDeserialization => false;
+
+		protected override Func<AliasExistsDescriptor, IAliasExistsRequest> Fluent => d => d
+			.Name("unknown-alias");
+
+		protected override AliasExistsRequest Initializer => new AliasExistsRequest(Names("unknown-alias"));
+
+		protected override void ExpectResponse(IExistsResponse response)
+		{
+			response.ServerError.Should().BeNull();
+			response.Exists.Should().BeFalse();
+		}
 	}
 }
