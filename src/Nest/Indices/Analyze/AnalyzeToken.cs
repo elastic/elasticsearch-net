f37a3cb206cbd7b09c2861045a64e2cfb4d3d4b7
Martijn Laarman <Mpdreamz@gmail.com>
2017-02-17 15:53:44 +0100

    fix #2604 add position_length on analyze api response (#2622)

--- src/Nest/Indices/Analyze/AnalyzeToken.cs
+++ src/Nest/Indices/Analyze/AnalyzeToken.cs
@@ -7,16 +7,22 @@ namespace Nest
 	{
 		[JsonProperty(PropertyName = "token")]
 		public string Token { get; internal set; }
+
 		[JsonProperty(PropertyName = "type")]
 		public string Type { get; internal set; }
-		
+
+		//TODO change to long in 6.0
 		[JsonProperty(PropertyName = "start_offset")]
 		public int StartOffset { get; internal set; }
+
 		[JsonProperty(PropertyName = "end_offset")]
 		public int EndPostion { get; internal set; }
 
 		[JsonProperty(PropertyName = "position")]
 		public int Position { get; internal set; }
+
+		[JsonProperty(PropertyName = "position_length")]
+		public long? PositionLength { get; internal set; }
 
 	}
 }
