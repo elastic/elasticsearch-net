d3cd40c342c379dc647d150b56327887e053c634
Martijn Laarman <Mpdreamz@gmail.com>
2017-02-17 15:50:24 +0100

    fix #2602 typed routing partition size on index settings (#2623)

---
+++ src/Nest/IndexModules/IndexSettings/Settings/FixedIndexSettings.cs
@@ -0,0 +1,12 @@
+namespace Nest
+{
+	/// <summary>
+	/// const string collection of known Elasticsearch index settings that can only be provided at
+	/// index creation time
+	/// </summary>
+	public static class FixedIndexSettings
+	{
+		public const string NumberOfShards = "index.number_of_shards";
+		public const string RoutingPartitionSize = "index.routing_partition_size";
+	}
+}
