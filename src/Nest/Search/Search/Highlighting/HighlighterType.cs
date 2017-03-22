3662b38450e0c4f1b0514f90855ab1d404429d40
Martijn Laarman <Mpdreamz@gmail.com>
2017-03-22 10:30:13 +0100

    fix #2592 add unified highlighter support (#2672)

--- src/Nest/Search/Search/Highlighting/HighlighterType.cs
+++ src/Nest/Search/Search/Highlighting/HighlighterType.cs
@@ -12,8 +12,8 @@ namespace Nest
 	{
 		/// <summary>
 		/// Plain Highlighter.
-		/// The default choice of highlighter is of type plain and uses the Lucene highlighter. 
-		/// It tries hard to reflect the query matching logic in terms of understanding word 
+		/// The default choice of highlighter is of type plain and uses the Lucene highlighter.
+		/// It tries hard to reflect the query matching logic in terms of understanding word
 		/// importance and any word positioning criteria in phrase queries.
 		/// </summary>
 		[EnumMember(Value = "plain")]
@@ -21,7 +21,7 @@ namespace Nest
 
 		/// <summary>
 		/// Postings Highlighter.
-		/// If index_options is set to offsets in the mapping the postings highlighter 
+		/// If index_options is set to offsets in the mapping the postings highlighter
 		/// will be used instead of the plain highlighter
 		/// </summary>
 		[EnumMember(Value = "postings")]
@@ -29,10 +29,22 @@ namespace Nest
 
 		/// <summary>
 		/// Fast Vector Highlighter.
-		/// If term_vector information is provided by setting term_vector to with_positions_offsets 
+		/// If term_vector information is provided by setting term_vector to with_positions_offsets
 		/// in the mapping then the fast vector highlighter will be used instead of the plain highlighter
 		/// </summary>
 		[EnumMember(Value = "fvh")]
-		Fvh
+		Fvh,
+
+
+		/// <summary>
+		/// Unified Highlighter.
+		/// The unified highlighter can extract offsets from either postings, term vectors, or via re-analyzing text.
+		/// Under the hood it uses Lucene UnifiedHighlighter which picks its strategy depending on the field and the query to highlight.
+		/// Independently of the strategy this highlighter breaks the text into sentences and scores individual sentences as if
+		/// they were documents in this corpus, using the BM25 algorithm. It supports accurate phrase and multi-term
+		/// (fuzzy, prefix, regex) highlighting
+		/// </summary>
+		[EnumMember(Value = "unified")]
+		Unified
 	}
 }
