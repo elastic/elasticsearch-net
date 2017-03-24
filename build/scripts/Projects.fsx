0593b835ebe624f063363c99443276f62ef2a299
Russ Cam <russ.cam@forloop.co.uk>
2017-03-24 16:20:44 +1100

    fix mono constant in project build script

--- build/scripts/Projects.fsx
+++ build/scripts/Projects.fsx
@@ -21,7 +21,7 @@ module Projects =
         member this.Identifier = 
             match this with
             | Net45 -> { MSBuild = "v4.5"; Nuget = "net45"; DefineConstants = if not isMono then "TRACE;NET45" else "NET45"; }
-            | Net46 -> { MSBuild = "v4.6"; Nuget = "net46"; DefineConstants = if not isMono then "TRACE;NET46" else "NET45"; }
+            | Net46 -> { MSBuild = "v4.6"; Nuget = "net46"; DefineConstants = if not isMono then "TRACE;NET46" else "NET46"; }
             | NetStandard1_3 -> { MSBuild = "netstandard1.3"; Nuget = "netstandard1.3"; DefineConstants = if not isMono then "TRACE;DOTNETCORE" else "DOTNETCORE"; }
 
     type Project =
