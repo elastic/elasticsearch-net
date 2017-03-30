88ca9f5cfdad3dea0feb72fcd413dc4d75d5d9be
Russ Cam <russ.cam@forloop.co.uk>
2017-03-30 17:58:55 +1100

    Introduce EndOffset to Analyze token
    Deprecate misspelt EndPostion
    
    (cherry-picked from commit 5e4e0e5f357edbc5c8cef3630d2f4fb40a9066d6)

--- src/Nest/Indices/Analyze/AnalyzeToken.cs
+++ src/Nest/Indices/Analyze/AnalyzeToken.cs
@@ -1,22 +1,27 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace Nest
 {
 	[JsonObject]
 	public class AnalyzeToken
 	{
-		[JsonProperty(PropertyName = "token")]
+		[JsonProperty("token")]
 		public string Token { get; internal set; }
-		[JsonProperty(PropertyName = "type")]
+
+		[JsonProperty("type")]
 		public string Type { get; internal set; }
-		
-		[JsonProperty(PropertyName = "start_offset")]
+
+		[JsonProperty("start_offset")]
 		public int StartOffset { get; internal set; }
-		[JsonProperty(PropertyName = "end_offset")]
-		public int EndPostion { get; internal set; }
 
-		[JsonProperty(PropertyName = "position")]
+        [Obsolete("Use EndOffset")]
+	    public int EndPostion => EndOffset;
+
+		[JsonProperty("end_offset")]
+		public int EndOffset { get; internal set; }
+
+		[JsonProperty("position")]
 		public int Position { get; internal set; }
-
 	}
 }
