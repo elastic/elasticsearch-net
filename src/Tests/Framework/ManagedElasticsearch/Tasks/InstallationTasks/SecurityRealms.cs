cb6ce33083ddf1a877fea3128ee039ed3d2476c8
Russ Cam <russ.cam@forloop.co.uk>
2017-03-30 12:24:54 +1100

    Fix ClearCachedRealms test
    Introduce constants for File and PKI realm names

---
+++ src/Tests/Framework/ManagedElasticsearch/Tasks/InstallationTasks/SecurityRealms.cs
@@ -0,0 +1,10 @@
+namespace Tests.Framework.ManagedElasticsearch.Tasks.InstallationTasks
+{
+    public static class SecurityRealms
+    {
+        public const string FileRealm = "file1";
+
+        public const string PkiRealm = "pki1";
+
+    }
+}
