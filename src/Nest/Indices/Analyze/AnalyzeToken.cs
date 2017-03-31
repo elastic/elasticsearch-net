eee58224dfb27060b9dee9cab3fe5d3d10f18f43
Russ Cam <russ.cam@elastic.co>
2017-03-31 12:30:39 +1100

    Introduce EndOffset to Analyze token
    remove misspelt EndPostion
    
    (cherry picked from commit 5e4e0e5f357edbc5c8cef3630d2f4fb40a9066d6)

--- src/Nest/Indices/Analyze/AnalyzeToken.cs
+++ src/Nest/Indices/Analyze/AnalyzeToken.cs
@@ -1,28 +1,28 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace Nest
 {
 	[JsonObject]
 	public class AnalyzeToken
 	{
-		[JsonProperty(PropertyName = "token")]
+		[JsonProperty("token")]
 		public string Token { get; internal set; }
 
-		[JsonProperty(PropertyName = "type")]
+		[JsonProperty("type")]
 		public string Type { get; internal set; }
 
-		//TODO change to long in 6.0
-		[JsonProperty(PropertyName = "start_offset")]
+		//TODO change to long in 6.0... RC: (this is int in Elasticsearch codebase)
+		[JsonProperty("start_offset")]
 		public int StartOffset { get; internal set; }
 
-		[JsonProperty(PropertyName = "end_offset")]
-		public int EndPostion { get; internal set; }
+		[JsonProperty("end_offset")]
+		public int EndOffset { get; internal set; }
 
-		[JsonProperty(PropertyName = "position")]
+		[JsonProperty("position")]
 		public int Position { get; internal set; }
 
-		[JsonProperty(PropertyName = "position_length")]
+		[JsonProperty("position_length")]
 		public long? PositionLength { get; internal set; }
-
 	}
 }
