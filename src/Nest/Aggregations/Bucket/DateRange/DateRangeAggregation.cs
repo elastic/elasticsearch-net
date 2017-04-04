34eb98c746595a41c48e41ca8c2048fb82303c0e
Adam McCoy <adam@forsedomani.com>
2017-04-04 16:12:50 +1000

    Add support for 'time_zone' parameter in date range aggregation.

--- src/Nest/Aggregations/Bucket/DateRange/DateRangeAggregation.cs
+++ src/Nest/Aggregations/Bucket/DateRange/DateRangeAggregation.cs
@@ -18,6 +18,9 @@ namespace Nest
 
 		[JsonProperty(PropertyName = "ranges")]
 		IEnumerable<IDateRangeExpression> Ranges { get; set; }
+
+		[JsonProperty(PropertyName = "time_zone")]
+		string TimeZone { get; set; }
 	}
 
 	public class DateRangeAggregation : BucketAggregationBase, IDateRangeAggregation
@@ -25,6 +28,7 @@ namespace Nest
 		public Field Field { get; set; }
 		public string Format { get; set; }
 		public IEnumerable<IDateRangeExpression> Ranges { get; set; }
+		public string TimeZone { get; set; }
 
 		internal DateRangeAggregation() { }
 
@@ -44,6 +48,8 @@ namespace Nest
 
 		IEnumerable<IDateRangeExpression> IDateRangeAggregation.Ranges { get; set; }
 
+		string IDateRangeAggregation.TimeZone { get; set; }
+
 		public DateRangeAggregationDescriptor<T> Field(Field field) => Assign(a => a.Field = field);
 
 		public DateRangeAggregationDescriptor<T> Field(Expression<Func<T, object>> field) => Assign(a => a.Field = field);
@@ -52,6 +58,8 @@ namespace Nest
 
 		public DateRangeAggregationDescriptor<T> Ranges(params IDateRangeExpression[] ranges) =>
 			Assign(a=>a.Ranges = ranges.ToListOrNullIfEmpty());
+
+		public DateRangeAggregationDescriptor<T> TimeZone(string timeZone) => Assign(a => a.TimeZone = timeZone);
 
 		public DateRangeAggregationDescriptor<T> Ranges(params Func<DateRangeExpressionDescriptor, IDateRangeExpression>[] ranges) =>
 			Assign(a=>a.Ranges = ranges?.Select(r=>r(new DateRangeExpressionDescriptor())).ToListOrNullIfEmpty());
