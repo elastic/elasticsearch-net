31addfeb0486c127acf4aef2e8dd2c3941e049d7
Mpdreamz <mpdreamz@gmail.com>
2017-05-01 16:26:17 +0200

    add skipversion, eventhough this API existed it was undocumented prior to 5.4
    Conflicts:
    	src/Tests/Document/Single/SourceExists/SourceExistsApiTests.cs

---
+++ src/Tests/Document/Single/SourceExists/SourceExistsApiTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Tests.Framework;
+using Tests.Framework.Integration;
+using Tests.Framework.ManagedElasticsearch.Clusters;
+using Tests.Framework.ManagedElasticsearch.NodeSeeders;
+using Tests.Framework.MockData;
+using Xunit;
+
+using static Nest.Infer;
+
+namespace Tests.Document.Single.SourceExists
+{
+	[SkipVersion("<5.4.0", "API was documented from 5.4.0 and over")]
+	public class SourceExistsApiTests : ApiIntegrationTestBase<WritableCluster, IExistsResponse, ISourceExistsRequest, SourceExistsDescriptor<Project>, SourceExistsRequest<Project>>
+	{
+		public SourceExistsApiTests(WritableCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		protected override void IntegrationSetup(IElasticClient client, CallUniqueValues values)
+		{
+			foreach (var id in values.Values)
+				this.Client.Index(Project.Instance, i=>i.Id(id));
+		}
+
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.SourceExists<Project>(CallIsolatedValue),
+			fluentAsync: (client, f) => client.SourceExistsAsync<Project>(CallIsolatedValue),
+			request: (client, r) => client.SourceExists(r),
+			requestAsync: (client, r) => client.SourceExistsAsync(r)
+		);
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 200;
+		protected override HttpMethod HttpMethod => HttpMethod.HEAD;
+		protected override string UrlPath => $"/project/project/{CallIsolatedValue}/_source";
+
+		protected override bool SupportsDeserialization => false;
+
+		protected override Func<SourceExistsDescriptor<Project>, ISourceExistsRequest> Fluent => null;
+		protected override SourceExistsRequest<Project> Initializer => new SourceExistsRequest<Project>(CallIsolatedValue);
+	}
+	public class SourceExistsNotFoundApiTests : ApiIntegrationTestBase<WritableCluster, IExistsResponse, ISourceExistsRequest, SourceExistsDescriptor<Project>, SourceExistsRequest<Project>>
+	{
+		public SourceExistsNotFoundApiTests(WritableCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		private static IndexName IndexWithNoSource { get; } = "project-with-no-source";
+		private static DocumentPath<Project> Doc(string id) => new DocumentPath<Project>(id).Index(IndexWithNoSource);
+
+		protected override void IntegrationSetup(IElasticClient client, CallUniqueValues values)
+		{
+			var index = client.CreateIndex(IndexWithNoSource, i=>i
+				.Mappings(m=>m
+					.Map<Project>(mm=>mm
+						.SourceField(sf=>sf.Enabled(false))
+					)
+				)
+			);
+			index.IsValid.Should().BeTrue(index.DebugInformation);
+
+			foreach (var id in values.Values)
+				this.Client.Index(Project.Instance, i=>i.Id(id).Index(IndexWithNoSource));
+		}
+
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.SourceExists<Project>(Doc(CallIsolatedValue)),
+			fluentAsync: (client, f) => client.SourceExistsAsync<Project>(Doc(CallIsolatedValue)),
+			request: (client, r) => client.SourceExists(r),
+			requestAsync: (client, r) => client.SourceExistsAsync(r)
+		);
+
+		protected override bool ExpectIsValid => false;
+		protected override int ExpectStatusCode => 404;
+		protected override HttpMethod HttpMethod => HttpMethod.HEAD;
+		protected override string UrlPath => $"/{IndexWithNoSource.Name}/project/{CallIsolatedValue}/_source";
+
+		protected override bool SupportsDeserialization => false;
+
+		protected override Func<SourceExistsDescriptor<Project>, ISourceExistsRequest> Fluent => null;
+		protected override SourceExistsRequest<Project> Initializer => new SourceExistsRequest<Project>(Doc(CallIsolatedValue));
+	}
+}
