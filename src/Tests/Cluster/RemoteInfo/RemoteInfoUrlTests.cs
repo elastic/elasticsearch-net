f716d6d3e94f58be9fc2e5116fd5c9b428d05e30
Mpdreamz <mpdreamz@gmail.com>
2017-05-01 15:50:19 +0200

    add support for RemoteInfo API and adds cross cluster support to IndexName

---
+++ src/Tests/Cluster/RemoteInfo/RemoteInfoUrlTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Nest;
+using Tests.Framework;
+using Tests.Framework.MockData;
+using static Tests.Framework.UrlTester;
+
+namespace Tests.Cluster.RemoteInfo
+{
+	public class RemoteInfoUrlTests : IUrlTests
+	{
+		[U] public async Task Urls()
+		{
+			await GET("/_remote/info")
+				.Fluent(c => c.RemoteInfo())
+				.Request(c => c.RemoteInfo(new RemoteInfoRequest()))
+				.FluentAsync(c => c.RemoteInfoAsync())
+				.RequestAsync(c => c.RemoteInfoAsync(new RemoteInfoRequest()))
+				;
+
+		}
+	}
+}
