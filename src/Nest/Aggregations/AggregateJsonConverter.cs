28cd15546830b399f7f5b7e16734b9e2da90c3c9
Mpdreamz <mpdreamz@gmail.com>
2017-05-04 11:47:54 +0200

    update spelling
    Conflicts:
    	src/Nest/Aggregations/AggregateJsonConverter.cs

--- src/Nest/Aggregations/AggregateJsonConverter.cs
+++ src/Nest/Aggregations/AggregateJsonConverter.cs
@@ -84,9 +84,9 @@ namespace Nest
 			var allKeys = string.Join(", ", AllReservedAggregationNames);
 			UsingReservedAggNameFormat =
 				"'{0}' is one of the reserved aggregation keywords"
-			    + " we use a heuristics based response parser and using these reserved keywords"
-			    + " could throw its heuritics off course. We are working on a solution in elasticsearch itself to make"
-			    + " the response parsable. For now these are all the reserved keywords: "
+				+ " we use a heuristics based response parser and using these reserved keywords"
+				+ " could throw its heuristics off course. We are working on a solution in elasticsearch itself to make"
+				+ " the response parseable. For now these are all the reserved keywords: "
 				+ allKeys;
 		}
 
