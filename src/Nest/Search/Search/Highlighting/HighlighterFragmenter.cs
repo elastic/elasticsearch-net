284d784fc016051cf18026c2a941e4d344ce172c
Mpdreamz <mpdreamz@gmail.com>
2017-05-04 11:43:08 +0200

    add support for fragmenter on highlighters

---
+++ src/Nest/Search/Search/Highlighting/HighlighterFragmenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Nest
+{
+	/// <summary>
+	/// Fragmenter can control how text should be broken up in highlight snippets. However, this option is
+	/// applicable only for the Plain Highlighter
+	/// </summary>
+	[JsonConverter(typeof(StringEnumConverter))]
+	public enum HighlighterFragmenter
+	{
+		/// <summary>
+		/// Breaks up text into same sized fragments.
+		/// </summary>
+		[EnumMember(Value = "simple")]
+		Simple,
+		/// <summary>
+		/// Same as the simple fragmenter, but tries not to break up text between highlighted terms (this is applicable
+		/// when using phrase like queries). This is the default.
+		/// </summary>
+		[EnumMember(Value = "span")]
+		Span
+	}
+}
