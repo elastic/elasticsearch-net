d55c589311683e90e7589898d01b1974604f5dae
Adam Clarke <adam.clarke@thefilter.com>
2017-05-09 13:19:00 +0100

    SniffingSortedStickyConnectionPool inherits from SniffingConnectionPool, uses scoring function for node priority

--- src/Elasticsearch.Net/ConnectionPool/SniffingSortedStickyConnectionPool.cs
+++ src/Elasticsearch.Net/ConnectionPool/SniffingSortedStickyConnectionPool.cs
@@ -5,74 +5,31 @@ using System.Threading;
 
 namespace Elasticsearch.Net
 {
-	public class SniffingSortedStickyConnectionPool : IConnectionPool
+	public class SniffingSortedStickyConnectionPool : SniffingConnectionPool
 	{
-		private readonly ReaderWriterLockSlim _readerWriter = new ReaderWriterLockSlim();
+		public override bool SupportsPinging => true;
+		public override bool SupportsReseeding => true;
 
-		private readonly Func<IEnumerable<Node>, IEnumerable<Node>> _sortMethod;
+		private Func<Node, float> _nodeScorer;
 
-		protected IDateTimeProvider DateTimeProvider { get; }
-
-		protected List<Node> InternalNodes { get; set; }
-
-		/// <inheritdoc/>
-		public bool UsingSsl { get; }
-
-		/// <inheritdoc/>
-		public bool SniffedOnStartup { get; set; }
-
-		/// <inheritdoc/>
-		public IReadOnlyCollection<Node> Nodes => this.InternalNodes;
-
-		/// <inheritdoc/>
-		public int MaxRetries => this.InternalNodes.Count - 1;
-
-		/// <inheritdoc/>
-		public bool SupportsReseeding => true;
-
-		/// <inheritdoc/>
-		public bool SupportsPinging => true;
-
-		/// <inheritdoc/>
-		public DateTime LastUpdate { get; protected set; }
-
-		public SniffingSortedStickyConnectionPool(IEnumerable<Uri> uris, Func<IEnumerable<Node>, IEnumerable<Node>> sortMethod, IDateTimeProvider dateTimeProvider = null)
-			: this(uris.Select(uri => new Node(uri)), sortMethod, dateTimeProvider)
+		public SniffingSortedStickyConnectionPool(IEnumerable<Uri> uris, Func<Node, float> nodeScorer, IDateTimeProvider dateTimeProvider = null)
+			: base(uris.Select(uri => new Node(uri)), false, dateTimeProvider)
 		{
+			this._nodeScorer = nodeScorer ?? DefaultNodeScore;
 		}
 
-		public SniffingSortedStickyConnectionPool(IEnumerable<Node> nodes, Func<IEnumerable<Node>, IEnumerable<Node>> sortMethod, IDateTimeProvider dateTimeProvider = null)
+		public SniffingSortedStickyConnectionPool(IEnumerable<Node> nodes, Func<Node, float> nodeScorer, IDateTimeProvider dateTimeProvider = null)
+			: base(nodes, false, dateTimeProvider)
 		{
-			nodes.ThrowIfEmpty(nameof(nodes));
-
-			this.DateTimeProvider = dateTimeProvider ?? Elasticsearch.Net.DateTimeProvider.Default;
-
-			var nn = nodes.ToList();
-			var uris = nn.Select(n => n.Uri).ToList();
-			if (uris.Select(u => u.Scheme).Distinct().Count() > 1)
-				throw new ArgumentException("Trying to instantiate a connection pool with mixed URI Schemes");
-
-			this.UsingSsl = uris.Any(uri => uri.Scheme == "https");
-
-			this.InternalNodes = nn
-				.DistinctBy(n => n.Uri)
-				.ToList();
-			this.LastUpdate = this.DateTimeProvider.Now();
-
-			this._sortMethod = sortMethod;
+			this._nodeScorer = nodeScorer ?? DefaultNodeScore;
 		}
 
-		protected int GlobalCursor = -1;
-
-		/// <summary>
-		/// Creates a view of all the live nodes preserving their order,
-		/// if there are no live nodes yields a different dead node to try once
-		/// </summary>
-		public IEnumerable<Node> CreateView(Action<AuditEvent, Node> audit = null)
+		public override IEnumerable<Node> CreateView(Action<AuditEvent, Node> audit = null)
 		{
 			var now = this.DateTimeProvider.Now();
 			var nodes = this.InternalNodes.Where(n => n.IsAlive || n.DeadUntil <= now)
 				.ToList();
+
 			var count = nodes.Count;
 			Node node;
 
@@ -111,35 +68,14 @@ namespace Elasticsearch.Net
 			}
 		}
 
-		/// <summary>
-		/// Update the node list and sort it
-		/// </summary>
-		public void Reseed(IEnumerable<Node> nodes)
+		protected override IOrderedEnumerable<Node> SortNodes(IEnumerable<Node> nodes)
 		{
-			if (!nodes.HasAny()) return;
-
-			try
-			{
-				this._readerWriter.EnterWriteLock();
-				List<Node> sortedNodes = _sortMethod(nodes).ToList();
-				this.InternalNodes = sortedNodes;
-				this.LastUpdate = this.DateTimeProvider.Now();
-			}
-			finally
-			{
-				this._readerWriter.ExitWriteLock();
-			}
+			return nodes.OrderByDescending(_nodeScorer);
 		}
 
-		public void Dispose()
+		private static float DefaultNodeScore(Node node)
 		{
-			this._readerWriter?.Dispose();
-			this.DisposeManagedResources();
-		}
-
-		protected void DisposeManagedResources()
-		{
-			this._readerWriter?.Dispose();
+			return 0f;
 		}
 	}
 }
