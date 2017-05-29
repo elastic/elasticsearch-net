240ec87e336e2611d6e9bf2af5321fe673867e24
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-05-29 19:01:45 +0100

    Add the pklugin code from the test project.

---
+++ src/Managed/Plugins/ElasticsearchPlugin.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+
+namespace Elasticsearch.Managed.Plugins
+{
+	public enum ElasticsearchPlugin
+	{
+		[Moniker("delete-by-query")] DeleteByQuery,
+		[Moniker("cloud-azure")] CloudAzure,
+		[Moniker("mapper-attachments")] MapperAttachments,
+		[Moniker("mapper-murmur3")] MapperMurmer3,
+		[Moniker("x-pack")] XPack,
+		[Moniker("ingest-geoip")] IngestGeoIp,
+		[Moniker("ingest-attachment")] IngestAttachment,
+		[Moniker("analysis-kuromoji")] AnalysisKuromoji,
+		[Moniker("analysis-icu")] AnalysisIcu
+	}
+}
+
