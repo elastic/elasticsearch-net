92652a093fa8cd7150cfca460b63e77e6e5dc3af
Russ Cam <russ.cam@elastic.co>
2017-06-05 12:16:33 -0700

    Add remove_index operation to Alias operations
    Closes #2774

---
+++ src/Nest/Indices/AliasManagement/Alias/Actions/AliasRemoveIndex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Nest
+{
+	[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+	public interface IAliasRemoveIndexAction : IAliasAction
+	{
+		[JsonProperty("remove_index")]
+		AliasRemoveIndexOperation RemoveIndex { get; set; }
+	}
+
+	public class AliasRemoveIndexAction : IAliasRemoveIndexAction
+	{
+		public AliasRemoveIndexOperation RemoveIndex { get; set; }
+	}
+
+	public class AliasRemoveIndexDescriptor : DescriptorBase<AliasRemoveIndexDescriptor, IAliasRemoveIndexAction>, IAliasRemoveIndexAction
+	{
+		AliasRemoveIndexOperation IAliasRemoveIndexAction.RemoveIndex { get; set; }
+
+		public AliasRemoveIndexDescriptor()
+		{
+			Self.RemoveIndex = new AliasRemoveIndexOperation();
+		}
+
+		public AliasRemoveIndexDescriptor Index(IndexName index)
+		{
+			Self.RemoveIndex.Index = index;
+			return this;
+		}
+
+		public AliasRemoveIndexDescriptor Index(Type index)
+		{
+			Self.RemoveIndex.Index = index;
+			return this;
+		}
+
+		public AliasRemoveIndexDescriptor Index<T>() where T : class
+		{
+			Self.RemoveIndex.Index = typeof(T);
+			return this;
+		}
+	}
+}
--- src/Nest/Indices/AliasManagement/Alias/BulkAliasRequest.cs
+++ src/Nest/Indices/AliasManagement/Alias/BulkAliasRequest.cs
@@ -4,22 +4,22 @@ using Newtonsoft.Json;
 
 namespace Nest
 {
-	public partial interface IBulkAliasRequest 
+	public partial interface IBulkAliasRequest
 	{
 		[JsonProperty("actions")]
 		IList<IAliasAction> Actions { get; set; }
 	}
 
-	public partial class BulkAliasRequest 
+	public partial class BulkAliasRequest
 	{
 		public IList<IAliasAction> Actions { get; set; }
 	}
 
 
 	[DescriptorFor("IndicesUpdateAliases")]
-	public partial class BulkAliasDescriptor 
+	public partial class BulkAliasDescriptor
 	{
-		public BulkAliasDescriptor Add(IAliasAction action) => 
+		public BulkAliasDescriptor Add(IAliasAction action) =>
 			Fluent.Assign<BulkAliasDescriptor, IBulkAliasRequest>(this, a=> a.Actions.AddIfNotNull(action));
 
 		IList<IAliasAction> IBulkAliasRequest.Actions { get; set; } = new List<IAliasAction>();
@@ -27,5 +27,8 @@ namespace Nest
 		public BulkAliasDescriptor Add(Func<AliasAddDescriptor, IAliasAddAction> addSelector) => Add(addSelector?.Invoke(new AliasAddDescriptor()));
 
 		public BulkAliasDescriptor Remove(Func<AliasRemoveDescriptor, IAliasRemoveAction> removeSelector)=> Add(removeSelector?.Invoke(new AliasRemoveDescriptor()));
+
+		public IBulkAliasRequest RemoveIndex(Func<AliasRemoveIndexDescriptor, IAliasRemoveIndexAction> removeIndexSelector) =>
+			Add(removeIndexSelector?.Invoke(new AliasRemoveIndexDescriptor()));
 	}
 }
