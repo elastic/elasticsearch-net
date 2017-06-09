0b9255aa829ceaf4db60f23be40fe393ee1fa64f
Mpdreamz <mpdreamz@gmail.com>
2017-06-09 11:58:36 -0700

    Order of conditional targetframeworks matters to VisualStudio because its dumb in how it resolves the TFMS's (works on commandline and straight up MsBuild)

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-.paket\paket.bootstrapper.exe
+.paket\paket.bootstrapper.exe 
 IF EXIST paket.lock (.paket\paket.exe restore)
 IF NOT EXIST paket.lock (.paket\paket.exe install)
 "packages\build\FAKE\tools\Fake.exe" "build\\scripts\\Targets.fsx" "cmdline=%*"
