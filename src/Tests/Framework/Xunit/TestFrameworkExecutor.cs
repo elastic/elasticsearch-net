45042a25a31f6946e09468f63d9d86372b0bdddf
Mpdreamz <mpdreamz@gmail.com>
2017-09-28 17:20:52 +0200

    small update to test framework, allows multiple tests filters and integration tests now prints failed collections AND the build command to replay only the failed integration tests

--- src/Tests/Framework/Xunit/TestFrameworkExecutor.cs
+++ src/Tests/Framework/Xunit/TestFrameworkExecutor.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Threading;
+using Tests.Framework;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
@@ -8,17 +12,59 @@ namespace Xunit
 {
 	class TestFrameworkExecutor : XunitTestFrameworkExecutor
 	{
-		public TestFrameworkExecutor(AssemblyName assemblyName, ISourceInformationProvider sourceInformationProvider, IMessageSink diagnosticMessageSink)
-			: base(assemblyName, sourceInformationProvider, diagnosticMessageSink)
-		{
-		}
+		public TestFrameworkExecutor(AssemblyName a, ISourceInformationProvider sip, IMessageSink d) : base(a, sip, d) { }
 
-		protected override async void RunTestCases(IEnumerable<IXunitTestCase> testCases, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
+		protected override async void RunTestCases(
+			IEnumerable<IXunitTestCase> testCases, IMessageSink sink, ITestFrameworkExecutionOptions options
+		)
 		{
 			try
 			{
-                using (var assemblyRunner = new TestAssemblyRunner(TestAssembly, testCases, DiagnosticMessageSink, executionMessageSink, executionOptions))
-                    await assemblyRunner.RunAsync();
+				using (var runner = new TestAssemblyRunner(TestAssembly, testCases, DiagnosticMessageSink, sink, options))
+				{
+					await runner.RunAsync();
+					Console.Out.Flush();
+					Console.WriteLine("--------");
+					Console.WriteLine("Individual cluster running times:");
+					foreach (var kv in runner.ClusterTotals)
+						Console.WriteLine($"- {kv.Key}: {kv.Value.Elapsed}");
+					Console.WriteLine("--------");
+
+					if (TestClient.Configuration.RunIntegrationTests && runner.FailedCollections.Count > 0)
+					{
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.WriteLine("Failed collections:");
+                        foreach (var t in runner.FailedCollections)
+                        {
+                            var cluster = t.Item1;
+                            Console.WriteLine($" - {cluster}: {t.Item2}");
+                        }
+						if (runner.FailedCollections.Count < 30)
+						{
+							Console.ForegroundColor = ConsoleColor.Yellow;
+							Console.WriteLine("--------");
+							var sb = new StringBuilder("build integrate ");
+							sb.Append(TestClient.Configuration.ElasticsearchVersion);
+							sb.Append(" \"");
+							var clusters = string.Join(",", runner.FailedCollections
+								.Select(c => c.Item1.ToLowerInvariant()).Distinct());
+							sb.Append(clusters);
+							sb.Append("\" \"");
+							var tests = string.Join(",", runner.FailedCollections
+								.Select(c => c.Item2.ToLowerInvariant().Split('.').Last()
+									.Replace("apitests", "")
+									.Replace("usagetests", "")
+									.Replace("tests", "")
+								));
+							sb.Append(tests);
+							sb.Append("\"");
+							Console.WriteLine(sb.ToString());
+						}
+
+					}
+
+					Console.ResetColor();
+				}
 
 			}
 			catch (Exception e)
