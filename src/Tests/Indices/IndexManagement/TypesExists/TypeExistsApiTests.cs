6042ddc72799e8d82af6f21ab1599c204434d2b4
Mpdreamz <mpdreamz@gmail.com>
2017-10-11 10:50:27 +0200

    type exists api tests was still using multiple types

--- src/Tests/Indices/IndexManagement/TypesExists/TypeExistsApiTests.cs
+++ src/Tests/Indices/IndexManagement/TypesExists/TypeExistsApiTests.cs
@@ -10,12 +10,13 @@ using static Nest.Infer;
 
 namespace Tests.Indices.IndexManagement.TypesExists
 {
-	public class TypeExistsApiTests : ApiIntegrationTestBase<ReadOnlyCluster, IExistsResponse, ITypeExistsRequest, TypeExistsDescriptor, TypeExistsRequest>
+	public class TypeExistsApiTests
+		: ApiIntegrationTestBase<ReadOnlyCluster, IExistsResponse, ITypeExistsRequest, TypeExistsDescriptor, TypeExistsRequest>
 	{
 		public TypeExistsApiTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
 		protected override LazyResponses ClientUsage() => Calls(
-			fluent: (client, f) => client.TypeExists(Index<Project>(), Type<Project>().And<CommitActivity>(), f),
-			fluentAsync: (client, f) => client.TypeExistsAsync(Index<Project>(), Type<Project>().And<CommitActivity>(), f),
+			fluent: (client, f) => client.TypeExists(Index<Project>(), Type<Project>(), f),
+			fluentAsync: (client, f) => client.TypeExistsAsync(Index<Project>(), Type<Project>(), f),
 			request: (client, r) => client.TypeExists(r),
 			requestAsync: (client, r) => client.TypeExistsAsync(r)
 		);
@@ -23,14 +24,14 @@ namespace Tests.Indices.IndexManagement.TypesExists
 		protected override bool ExpectIsValid => true;
 		protected override int ExpectStatusCode => 200;
 		protected override HttpMethod HttpMethod => HttpMethod.HEAD;
-		protected override string UrlPath => $"/project/_mapping/project%2Ccommits?ignore_unavailable=true";
+		protected override string UrlPath => $"/project/_mapping/project?ignore_unavailable=true";
 
-		protected override TypeExistsDescriptor NewDescriptor() => new TypeExistsDescriptor(Index<Project>(), Type<Project>().And<CommitActivity>());
+		protected override TypeExistsDescriptor NewDescriptor() => new TypeExistsDescriptor(Index<Project>(), Type<Project>());
 
 		protected override Func<TypeExistsDescriptor, ITypeExistsRequest> Fluent => d => d
 			.IgnoreUnavailable();
 
-		protected override TypeExistsRequest Initializer => new TypeExistsRequest(Index<Project>(), Type<Project>().And<CommitActivity>())
+		protected override TypeExistsRequest Initializer => new TypeExistsRequest(Index<Project>(), Type<Project>())
 		{
 			IgnoreUnavailable = true
 		};
