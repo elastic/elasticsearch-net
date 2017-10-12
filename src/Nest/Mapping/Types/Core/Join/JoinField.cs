9cf1dba537959bf6a22c7b0d80819d269bd9128d
Mpdreamz <mpdreamz@gmail.com>
2017-10-12 16:08:57 +0200

    Add documentation on how to move to the new join data type' '

--- src/Nest/Mapping/Types/Core/Join/JoinField.cs
+++ src/Nest/Mapping/Types/Core/Join/JoinField.cs
@@ -1,42 +1,46 @@
+using System.CodeDom;
 using Newtonsoft.Json;
 
 namespace Nest
 {
 	[JsonConverter(typeof(JoinFieldJsonConverter))]
-	public class JoinField : Union<ParentJoinField, ChildJoinField>
+	public class JoinField : Union<JoinField.Parent, JoinField.Child>
 	{
-		public JoinField(ParentJoinField parentName) : base(parentName) { }
+		public JoinField(Parent parentName) : base(parentName) { }
 
-		public JoinField(ChildJoinField child) : base(child) { }
+		public JoinField(Child child) : base(child) { }
 
-		public static JoinField OfParent(TypeName parent) => new ParentJoinField(parent);
-		public static JoinField OfChild(TypeName childName, Id parentId) => new ChildJoinField(childName, parentId);
-		public static JoinField OfChild<TDocument>(TypeName childName, TDocument parent) where TDocument : class =>
-			new ChildJoinField(childName, Id.From<TDocument>(parent));
+		public static JoinField Root<TParent>() => new Parent(typeof(TParent));
+		public static JoinField Root(TypeName parent) => new Parent(parent);
 
-		public static implicit operator JoinField(ParentJoinField parent) => new JoinField(parent);
-		public static implicit operator JoinField(string parentName) => new ParentJoinField(parentName);
-		public static implicit operator JoinField(ChildJoinField child) => new JoinField(child);
+		public static JoinField Link(TypeName childName, Id parentId) => new Child(childName, parentId);
+		public static JoinField Link<TChild, TParentDocument>(TParentDocument parent) where TParentDocument : class =>
+			new Child(typeof(TChild), Id.From<TParentDocument>(parent));
+		public static JoinField Link<TChild>(Id parentId) => new Child(typeof(TChild), parentId);
 
-	}
-	public class ParentJoinField
-	{
-		internal TypeName Name { get; }
+		public static implicit operator JoinField(Parent parent) => new JoinField(parent);
+		public static implicit operator JoinField(string parentName) => new JoinField(new Parent(parentName));
+		public static implicit operator JoinField(Child child) => new JoinField(child);
 
-		public ParentJoinField(TypeName name)
-		{
-			Name = name;
-		}
-	}
-	public class ChildJoinField
-	{
-		internal Id Parent { get; }
-		internal TypeName Name { get; }
+        public class Parent
+        {
+            internal TypeName Name { get; }
 
-		public ChildJoinField(TypeName name, Id parent)
-		{
-			Name = name;
-			Parent = parent;
-		}
+            public Parent(TypeName name)
+            {
+                Name = name;
+            }
+        }
+        public class Child
+        {
+            internal Id Parent { get; }
+            internal TypeName Name { get; }
+
+            public Child(TypeName name, Id parent)
+            {
+                Name = name;
+                Parent = parent;
+            }
+        }
 	}
 }
