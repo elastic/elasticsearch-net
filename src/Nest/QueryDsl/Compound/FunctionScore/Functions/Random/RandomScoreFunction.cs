99f9bcd4097906a7f9f5ce799e9299383088ad82
Mpdreamz <mpdreamz@gmail.com>
2017-11-09 22:11:36 +0100

    random score function now requires a field to be provided explicitly when a seed is passed along as well

--- src/Nest/QueryDsl/Compound/FunctionScore/Functions/Random/RandomScoreFunction.cs
+++ src/Nest/QueryDsl/Compound/FunctionScore/Functions/Random/RandomScoreFunction.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Linq.Expressions;
+using Newtonsoft.Json;
 
 namespace Nest
 {
@@ -8,20 +10,29 @@ namespace Nest
 	{
 		[JsonProperty(PropertyName = "seed")]
 		Union<long, string> Seed { get; set; }
+
+		[JsonProperty(PropertyName = "field")]
+		Field Field { get; set; }
 	}
 
 	public class RandomScoreFunction: FunctionScoreFunctionBase, IRandomScoreFunction
 	{
 		public Union<long, string> Seed { get; set; }
+
+		public Field Field { get; set; }
 	}
 
 	public class RandomScoreFunctionDescriptor<T> : FunctionScoreFunctionDescriptorBase<RandomScoreFunctionDescriptor<T>, IRandomScoreFunction,T>, IRandomScoreFunction
 		where T : class
 	{
 		Union<long, string> IRandomScoreFunction.Seed { get; set; }
+		Field IRandomScoreFunction.Field { get; set; }
 
 		public RandomScoreFunctionDescriptor<T> Seed(long? seed) => Assign(a => a.Seed = seed);
 		public RandomScoreFunctionDescriptor<T> Seed(string seed) => Assign(a => a.Seed = seed);
+		public RandomScoreFunctionDescriptor<T> Field(Field field) => Assign(a => a.Field = field);
+		public RandomScoreFunctionDescriptor<T> Field(Expression<Func<T, object>> objectPath) =>
+			Assign(a => a.Field = objectPath);
 
 	}
 }
