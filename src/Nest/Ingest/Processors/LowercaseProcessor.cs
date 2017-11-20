12339162379b35b29ddacfa98286799c11daeaeb
Mpdreamz <mpdreamz@gmail.com>
2017-11-20 19:12:20 +0100

    attempt 2

--- src/Nest/Ingest/Processors/LowercaseProcessor.cs
+++ src/Nest/Ingest/Processors/LowercaseProcessor.cs
@@ -1,10 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nest
 {
