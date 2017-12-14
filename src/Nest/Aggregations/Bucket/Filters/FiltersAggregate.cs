d2c745cc9c41461d91f88f39c5bc40cec9c08274
Mpdreamz <mpdreamz@gmail.com>
2017-12-14 14:46:48 +0100

    can now deserialize AggregateDictionary, MultiBucketsAggregate only stores nested aggregations on the buckets not itself so shouldn't extend AggregateDictionary (or AggregationsHelper as it does currently on master which is even odder)

--- src/Nest/Aggregations/Bucket/Filters/FiltersAggregate.cs
+++ src/Nest/Aggregations/Bucket/Filters/FiltersAggregate.cs
@@ -11,10 +11,27 @@ namespace Nest
 		public long DocCount { get; set; }
 	}
 
-	public class FiltersAggregate : MultiBucketAggregate<FiltersBucketItem>
+	//TODO this is mapped rather odly we always deserialize as if this is
+	// {
+	//    "agg1" : { ...},
+	//	  "agg2" : { ... }
+	//}
+	// while its actually a buckets response
+	// {
+	//   "buckets" : {} || []
+	//}
+	// where buckets is either an array or object. We fix this in the helper where we
+	// move the aggs from itself into a *new* filters aggregate using the parameterless constructor
+	public class FiltersAggregate : BucketAggregateBase
 	{
+		public FiltersAggregate() : base(EmptyReadOnly<string, IAggregate>.Dictionary) { }
+		public FiltersAggregate(IReadOnlyDictionary<string, IAggregate> aggregations) : base(aggregations) { }
+
 		public SingleBucketAggregate NamedBucket(string key) => this.Global(key);
 
 		public IList<FiltersBucketItem> AnonymousBuckets() => this.Buckets?.ToList();
+
+		public IReadOnlyCollection<FiltersBucketItem> Buckets { get; set; } = EmptyReadOnly<FiltersBucketItem>.Collection;
+
 	}
 }
