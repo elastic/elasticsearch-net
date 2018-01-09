9c73c9f49a79fa5cd88ec6e0f993d0eacf8345ee
Mpdreamz <mpdreamz@gmail.com>
2018-01-09 08:43:31 +0100

    finished XmlDoc on GlobalOverrides.RenderPartial

--- src/CodeGeneration/ApiGenerator/Overrides/GlobalOverrides.cs
+++ src/CodeGeneration/ApiGenerator/Overrides/GlobalOverrides.cs
@@ -7,7 +7,7 @@ namespace ApiGenerator.Overrides
 	{
 		/// <summary>
 		/// Force these be rendered as interface properties only, so that they'd have to be implemented manually
-		/// and become part of the body. This only takes affect for reques
+		/// and become part of the body. This only takes affect on requests that take a body (e.g not GET or HEAD).
 		/// </summary>
 		public override IEnumerable<string> RenderPartial => new[]
 		{
