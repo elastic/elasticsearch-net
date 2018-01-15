aceee2ba556de4d87feb212f57fb75957f69ac1d
Mpdreamz <mpdreamz@gmail.com>
2018-01-15 21:59:18 +0100

    remove throttle time from store stats

--- src/Nest/CommonOptions/Stats/StoreStats.cs
+++ src/Nest/CommonOptions/Stats/StoreStats.cs
@@ -11,8 +11,6 @@ namespace Nest
 		[JsonProperty("size_in_bytes")]
 		public double SizeInBytes { get; set; }
 
-		[JsonProperty("throttle_time_in_millis")]
-		public long ThrottleTimeInMilliseconds { get; set; }
 	}
 
 }
