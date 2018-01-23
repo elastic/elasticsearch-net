7d99e0e8ef316e064109c66cdbe71f7a3ad48247
Russ Cam <russ.cam@elastic.co>
2018-01-23 19:52:40 +1100

    Documentation for Union type (#3059)

--- src/Nest/Aggregations/Bucket/Terms/TermsInclude.cs
+++ src/Nest/Aggregations/Bucket/Terms/TermsInclude.cs
@@ -4,12 +4,21 @@ using Newtonsoft.Json;
 
 namespace Nest
 {
+	/// <summary>
+	/// Filters which terms to include in the response
+	/// </summary>
 	[JsonConverter(typeof(TermsIncludeJsonConverter))]
 	public class TermsInclude
 	{
+		/// <summary>
+		/// The regular expression pattern to determine terms to include in the response
+		/// </summary>
 		[JsonIgnore]
 		public string Pattern { get; set; }
 
+		/// <summary>
+		/// Collection of terms to include in the response
+		/// </summary>
 		[JsonIgnore]
 		public IEnumerable<string> Values { get; set; }
 
@@ -25,16 +34,26 @@ namespace Nest
 		[JsonProperty("num_partitions")]
 		public long? NumberOfPartitions { get; set; }
 
-		public TermsInclude(string pattern)
-		{
-			Pattern = pattern;
-		}
+		/// <summary>
+		/// Creates an instance of <see cref="TermsInclude"/> that uses a regular expression pattern
+		/// to determine the terms to include in the response
+		/// </summary>
+		/// <param name="pattern">The regular expression pattern</param>
+		public TermsInclude(string pattern) => Pattern = pattern;
 
-		public TermsInclude(IEnumerable<string> values)
-		{
-			Values = values;
-		}
+		/// <summary>
+		/// Creates an instance of <see cref="TermsInclude"/> that uses a collection of terms
+		/// to include in the response
+		/// </summary>
+		/// <param name="values">The exact terms to include</param>
+		public TermsInclude(IEnumerable<string> values) => Values = values;
 
+		/// <summary>
+		/// Creates an instance of <see cref="TermsInclude"/> that partitions the terms into a number of
+		/// partitions to receive in multiple requests. Used to process many unique terms
+		/// </summary>
+		/// <param name="partition">The 0-based partition number for this request</param>
+		/// <param name="numberOfPartitions">The total number of partitions</param>
 		public TermsInclude(long partition, long numberOfPartitions)
 		{
 			Partition = partition;
--- src/Nest/CommonAbstractions/Union/Union.cs
+++ src/Nest/CommonAbstractions/Union/Union.cs
@@ -3,6 +3,12 @@ using Newtonsoft.Json;
 
 namespace Nest
 {
+	/// <summary>
+	/// Represents the union of two types, <typeparamref name="TFirst"/> and <typeparamref name="TSecond"/>. Used
+	/// in scenarios where an Elasticsearch API may accept more than one different input data structure.
+	/// </summary>
+	/// <typeparam name="TFirst">The first type</typeparam>
+	/// <typeparam name="TSecond">The second type</typeparam>
 	[JsonConverter(typeof(UnionJsonConverter))]
 	public class Union<TFirst, TSecond>
 	{
@@ -10,9 +16,23 @@ namespace Nest
 		internal readonly TSecond Item2;
 		internal readonly int _tag;
 
+		/// <summary>
+		/// Creates an new instance of <see cref="Union{TFirst,TSecond}"/> that encapsulates <paramref name="item"/> value
+		/// </summary>
+		/// <param name="item">The value to encapsulate</param>
 		public Union(TFirst item) { Item1 = item; _tag = 0; }
+
+		/// <summary>
+		/// Creates an new instance of <see cref="Union{TFirst,TSecond}"/> that encapsulates <paramref name="item"/> value
+		/// </summary>
+		/// <param name="item">The value to encapsulate</param>
 		public Union(TSecond item) { Item2 = item; _tag = 1; }
 
+		/// <summary>
+		/// Runs an <see cref="Action{T}"/> delegate against the encapsulated value
+		/// </summary>
+		/// <param name="first">The delegate to run when this instance encapsulates an instance of <typeparamref name="TFirst"/></param>
+		/// <param name="second">The delegate to run when this instance encapsulates an instance of <typeparamref name="TSecond"/></param>
 		public void Match(Action<TFirst> first, Action<TSecond> second)
 		{
 			switch (_tag)
@@ -26,6 +46,12 @@ namespace Nest
 				default: throw new Exception($"Unrecognized tag value: {_tag}");
 			}
 		}
+
+		/// <summary>
+		/// Runs a <see cref="Func{T,TResult}"/> delegate against the encapsulated value
+		/// </summary>
+		/// <param name="first">The delegate to run when this instance encapsulates an instance of <typeparamref name="TFirst"/></param>
+		/// <param name="second">The delegate to run when this instance encapsulates an instance of <typeparamref name="TSecond"/></param>
 		public T Match<T>(Func<TFirst, T> first, Func<TSecond, T> second)
 		{
 			switch (_tag)
