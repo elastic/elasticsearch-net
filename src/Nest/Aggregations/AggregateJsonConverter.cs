d6eb33bc65555422b02191b8b1d4ca825aeed5f7
Russ Cam <russ.cam@elastic.co>
2018-02-05 13:41:39 +1100

    Deserialize aggregation metadata to Dictionary<string,object> (#3073)
    This commit uses the JsonSerializer to deserialize the aggregation metadata to a Dictionary<string,object> from the JsonReader.
    Add integration test to assert behaviour.
    
    Closes #3072
    (cherry picked from commit 188d2d70410dc450e971cb0e1540928c88206d80)

--- src/Nest/Aggregations/AggregateJsonConverter.cs
+++ src/Nest/Aggregations/AggregateJsonConverter.cs
@@ -108,7 +108,7 @@ namespace Nest
 				aggregate = GetPercentilesAggregate(reader, serializer, oldFormat: true);
 
 			var meta = propertyName == Parser.Meta
-				? GetMetadata(reader)
+				? GetMetadata(serializer, reader)
 				: null;
 
 			if (aggregate != null)
@@ -189,19 +189,12 @@ namespace Nest
 			return item;
 		}
 
-		private Dictionary<string, object> GetMetadata(JsonReader reader)
+		private Dictionary<string, object> GetMetadata(JsonSerializer serializer, JsonReader reader)
 		{
-			var meta = new Dictionary<string, object>();
+			// read past "meta" property name to start of object
 			reader.Read();
-			reader.Read();
-			while (reader.TokenType != JsonToken.EndObject)
-			{
-				var key = (string) reader.Value;
-				reader.Read();
-				var value = reader.Value;
-				meta.Add(key, value);
-				reader.Read();
-			}
+			var meta = serializer.Deserialize<Dictionary<string, object>>(reader);
+			// read past the closing end object of "meta" object
 			reader.Read();
 			return meta;
 		}
