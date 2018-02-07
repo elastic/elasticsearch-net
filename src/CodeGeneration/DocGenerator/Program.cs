3abdb568501d5109144c16f0e3bf55e07db94bda
Russ Cam <russ.cam@forloop.co.uk>
2018-02-07 21:20:01 +1100

    Use branch name in documentation

--- src/CodeGeneration/DocGenerator/Program.cs
+++ src/CodeGeneration/DocGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace DocGenerator
@@ -11,12 +12,11 @@ namespace DocGenerator
 			{
 				return path.Replace(@"\", Path.DirectorySeparatorChar.ToString());
 			}
-			
+
 			var currentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
             if (currentDirectory.Name == "DocGenerator" && currentDirectory.Parent.Name == "CodeGeneration")
 			{
 				Console.WriteLine("IDE: " + currentDirectory);
-				
                 InputDirPath = P(@"..\..\");
 				OutputDirPath = P(@"..\..\..\docs");
                 BuildOutputPath = P(@"..\..\..\src");
@@ -28,6 +28,36 @@ namespace DocGenerator
 				OutputDirPath = P(@"..\..\..\..\docs");
                 BuildOutputPath = P(@"..\..\..\..\build\output");
 			}
+
+			var process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					FileName = "git.exe",
+					CreateNoWindow = true,
+					WorkingDirectory = Environment.CurrentDirectory,
+					Arguments = "rev-parse --abbrev-ref HEAD"
+				}
+			};
+
+			try
+			{
+				process.Start();
+				BranchName = process.StandardOutput.ReadToEnd().Trim();
+				Console.WriteLine($"Using branch name {BranchName} in documentation");
+				process.WaitForExit();
+			}
+			catch (Exception)
+			{
+				BranchName = "master";
+				Console.WriteLine($"Could not get the git branch name. Assuming {BranchName}");
+			}
+			finally
+			{
+				process.Dispose();
+			}
         }
 
         public static string BuildOutputPath { get; }
@@ -35,6 +65,10 @@ namespace DocGenerator
 		public static string InputDirPath { get; }
 
 		public static string OutputDirPath { get; }
+
+		public static string BranchName { get; }
+
+		public static string DocVersion => "6.1";
 
 		static int Main(string[] args)
 		{
@@ -52,3 +86,5 @@ namespace DocGenerator
 		}
 	}
 }
+
+
