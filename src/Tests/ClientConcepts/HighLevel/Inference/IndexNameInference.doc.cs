2e47cb26b684774cab8b4b0870fa85ddd99a65e3
Mpdreamz <mpdreamz@gmail.com>
2018-03-01 10:45:28 -0800

    fix #3116 instances of gethashcode in equals implementation

--- src/Tests/ClientConcepts/HighLevel/Inference/IndexNameInference.doc.cs
+++ src/Tests/ClientConcepts/HighLevel/Inference/IndexNameInference.doc.cs
@@ -170,6 +170,7 @@ namespace Tests.ClientConcepts.HighLevel.Inference
 		{
 			var clusterIndex = (IndexName)"cluster_one:p";
 			var index = (IndexName)"p";
+			Index<Project>("cluster_one").Should().NotBe(Index<Project>("cluster_two"));
 
 			clusterIndex.Should().NotBe(index);
 			clusterIndex.Should().Be("cluster_one:p");
@@ -177,7 +178,6 @@ namespace Tests.ClientConcepts.HighLevel.Inference
 
 			Index<Project>().Should().Be(Index<Project>());
 			Index<Project>().Should().NotBe(Index<Project>("cluster_two"));
-			Index<Project>("cluster_one").Should().NotBe(Index<Project>("cluster_two"));
 			Index<Project>("cluster_one").Should().NotBe("cluster_one:project");
 			Index<Project>().Should().NotBe(Index<Developer>());
 			Index<Project>("cluster_one").Should().NotBe(Index<Developer>("cluster_one"));
