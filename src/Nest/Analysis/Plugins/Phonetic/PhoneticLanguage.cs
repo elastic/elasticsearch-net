27b8813662690446c3053b71faa9b5ea6b113fbe
Stuart Cam <stuart@codebrain.co.uk>
2018-04-12 14:26:10 +1000

    Add support for Daitch-Mokotoff algorithm (#3190)
    * Add support for Daitch-Mokotoff algorithm
    
    https://github.com/elastic/elasticsearch-net/issues/3174
    
    * Add additional settings for certain phonetic token filters
    
    This commit adds additional settings for the double_metaphone and beider_morse encoders
    
    (cherry picked from commit a11c4a4052daf5fe5ff5f94db5703836acca2c87)

---
+++ src/Nest/Analysis/Plugins/Phonetic/PhoneticLanguage.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Nest
+{
+	[JsonConverter(typeof(StringEnumConverter))]
+	public enum PhoneticLanguage
+	{
+		[EnumMember(Value = "any")]
+		Any,
+
+		[EnumMember(Value = "comomon")]
+		Comomon,
+
+		[EnumMember(Value = "cyrillic")]
+		Cyrillic,
+
+		[EnumMember(Value = "english")]
+		English,
+
+		[EnumMember(Value = "french")]
+		French,
+
+		[EnumMember(Value = "german")]
+		German,
+
+		[EnumMember(Value = "hebrew")]
+		Hebrew,
+
+		[EnumMember(Value = "hungarian")]
+		Hungarian,
+
+		[EnumMember(Value = "polish")]
+		Polish,
+
+		[EnumMember(Value = "romanian")]
+		Romanian,
+
+		[EnumMember(Value = "russian")]
+		Russian,
+
+		[EnumMember(Value = "spanish")]
+		Spanish,
+	}
+}
