511319de47a9063d96471d6c5b03325add6242db
Stuart Cam <stuart.cam@elastic.co>
2018-04-13 15:47:01 +1000

    Fix broken BucketSortAggregationUsageTests integration tests, mark for ES6.1+ only

--- src/Tests/Aggregations/Pipeline/BucketSort/BucketSortAggregationUsageTests.cs
+++ src/Tests/Aggregations/Pipeline/BucketSort/BucketSortAggregationUsageTests.cs
@@ -9,6 +9,7 @@ using Tests.Framework.MockData;
 
 namespace Tests.Aggregations.Pipeline.BucketSort
 {
+	[SkipVersion("<6.1.0", "Only valid in Elasticsearch 6.1.0+")]
 	public class BucketSortAggregationUsageTests : AggregationUsageTestBase
 	{
 		public BucketSortAggregationUsageTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
