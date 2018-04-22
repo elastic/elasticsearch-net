b3af5b9b25f046d11e3660685d49271058f9366a
Stuart Cam <stuart@codebrain.co.uk>
2018-04-22 15:39:06 +1000

    Indices ShardsStats in 5.x (#3194)
    Fixes #3185

---
+++ src/Nest/Indices/Monitoring/IndicesStats/ShardRouting.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Nest
+{
+	[JsonObject]
+	public class ShardRouting
+	{
+		[JsonProperty("state")]
+		public ShardRoutingState State { get; internal set; }
+
+		[JsonProperty("primary")]
+		public bool Primary { get; internal set; }
+
+		[JsonProperty("node")]
+		public string Node { get; internal set; }
+
+		[JsonProperty("relocating_node")]
+		public string RelocatingNode { get; internal set; }
+	}
+}
