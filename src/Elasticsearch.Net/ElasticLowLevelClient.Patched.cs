026dea498b3f8c91da0c07c29e974e6fcf8b874c
Russ Cam <russ.cam@forloop.co.uk>
2018-04-26 17:59:32 +1000

    Maintain BWC for XPackDeprecationInfo
    This commit adds back XpackDeprecationInfo* methods to the low level client
    to maintain backwards binary compatibility, and deprecates them through
    Obsolete attribute to point users to the new method name.
    
    The new X-Pack REST API specs moved the deprecation info API specs
    
    from
    
    https://github.com/elastic/elasticsearch-net/blob/6.0.2/src/CodeGeneration/ApiGenerator/RestSpecification/XPack/Info/xpack.deprecation.info.json
    
    to
    
    https://github.com/elastic/elasticsearch-net/blob/6.x/src/CodeGeneration/ApiGenerator/RestSpecification/XPack/Migration/xpack.migration.deprecations.json
    
    and renamed from "xpack.deprecation.info" to "xpack.migration.deprecations".

---
+++ src/Elasticsearch.Net/ElasticLowLevelClient.Patched.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static Elasticsearch.Net.HttpMethod;
+
+namespace Elasticsearch.Net
+{
+	public partial class ElasticLowLevelClient
+	{
+		///<summary>GET on /_xpack/migration/deprecations <para>http://www.elastic.co/guide/en/migration/current/migration-api-deprecation.html</para></summary>
+		///<param name="requestParameters">A func that allows you to describe the querystring parameters &amp; request specific connection settings.</param>
+		[Obsolete("Use XpackMigrationDeprecations")]
+		public TResponse XpackDeprecationInfo<TResponse>(DeprecationInfoRequestParameters requestParameters = null)
+			where TResponse : class, IElasticsearchResponse, new() => this.DoRequest<TResponse>(GET, Url($"_xpack/migration/deprecations"), null, _params(requestParameters));
+
+		///<summary>GET on /_xpack/migration/deprecations <para>http://www.elastic.co/guide/en/migration/current/migration-api-deprecation.html</para></summary>
+		///<param name="requestParameters">A func that allows you to describe the querystring parameters &amp; request specific connection settings.</param>
+		[Obsolete("Use XpackMigrationDeprecationsAsync")]
+		public Task<TResponse> XpackDeprecationInfoAsync<TResponse>(DeprecationInfoRequestParameters requestParameters = null, CancellationToken ctx = default(CancellationToken))
+			where TResponse : class, IElasticsearchResponse, new() => this.DoRequestAsync<TResponse>(GET, Url($"_xpack/migration/deprecations"), ctx, null, _params(requestParameters));
+
+		///<summary>GET on /{index}/_xpack/migration/deprecations <para>http://www.elastic.co/guide/en/migration/current/migration-api-deprecation.html</para></summary>
+		///<param name="index">Index pattern</param>
+		///<param name="requestParameters">A func that allows you to describe the querystring parameters &amp; request specific connection settings.</param>
+		[Obsolete("Use XpackMigrationDeprecations")]
+		public TResponse XpackDeprecationInfo<TResponse>(string index, DeprecationInfoRequestParameters requestParameters = null)
+			where TResponse : class, IElasticsearchResponse, new() => this.DoRequest<TResponse>(GET, Url($"{index.NotNull("index")}/_xpack/migration/deprecations"), null, _params(requestParameters));
+
+		///<summary>GET on /{index}/_xpack/migration/deprecations <para>http://www.elastic.co/guide/en/migration/current/migration-api-deprecation.html</para></summary>
+		///<param name="index">Index pattern</param>
+		///<param name="requestParameters">A func that allows you to describe the querystring parameters &amp; request specific connection settings.</param>
+		[Obsolete("Use XpackMigrationDeprecationsAsync")]
+		public Task<TResponse> XpackDeprecationInfoAsync<TResponse>(string index, DeprecationInfoRequestParameters requestParameters = null, CancellationToken ctx = default(CancellationToken))
+			where TResponse : class, IElasticsearchResponse, new() => this.DoRequestAsync<TResponse>(GET, Url($"{index.NotNull("index")}/_xpack/migration/deprecations"), ctx, null, _params(requestParameters));
+	}
+}
---
+++ src/Elasticsearch.Net/IElasticLowLevelClient.Patched.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elasticsearch.Net
+{
+	public partial interface IElasticLowLevelClient
+	{
+		///<summary>GET on /_xpack/migration/deprecations <para>http://www.elastic.co/guide/en/migration/current/migration-api-deprecation.html</para></summary>
+		///<param name="requestParameters">A func that allows you to describe the querystring parameters &amp; request specific connection settings.</param>
+		[Obsolete("Use XpackMigrationDeprecations")]
+		TResponse XpackDeprecationInfo<TResponse>(DeprecationInfoRequestParameters requestParameters = null) where TResponse : class, IElasticsearchResponse, new();
+
+		///<summary>GET on /_xpack/migration/deprecations <para>http://www.elastic.co/guide/en/migration/current/migration-api-deprecation.html</para></summary>
+		///<param name="requestParameters">A func that allows you to describe the querystring parameters &amp; request specific connection settings.</param>
+		[Obsolete("Use XpackMigrationDeprecationsAsync")]
+		Task<TResponse> XpackDeprecationInfoAsync<TResponse>(DeprecationInfoRequestParameters requestParameters = null, CancellationToken ctx = default(CancellationToken)) where TResponse : class, IElasticsearchResponse, new();
+
+		///<summary>GET on /{index}/_xpack/migration/deprecations <para>http://www.elastic.co/guide/en/migration/current/migration-api-deprecation.html</para></summary>
+		///<param name="index">Index pattern</param>
+		///<param name="requestParameters">A func that allows you to describe the querystring parameters &amp; request specific connection settings.</param>
+		[Obsolete("Use XpackMigrationDeprecations")]
+		TResponse XpackDeprecationInfo<TResponse>(string index, DeprecationInfoRequestParameters requestParameters = null) where TResponse : class, IElasticsearchResponse, new();
+
+		///<summary>GET on /{index}/_xpack/migration/deprecations <para>http://www.elastic.co/guide/en/migration/current/migration-api-deprecation.html</para></summary>
+		///<param name="index">Index pattern</param>
+		///<param name="requestParameters">A func that allows you to describe the querystring parameters &amp; request specific connection settings.</param>
+		[Obsolete("Use XpackMigrationDeprecationsAsync")]
+		Task<TResponse> XpackDeprecationInfoAsync<TResponse>(string index, DeprecationInfoRequestParameters requestParameters = null, CancellationToken ctx = default(CancellationToken)) where TResponse : class, IElasticsearchResponse, new();
+	}
+}
