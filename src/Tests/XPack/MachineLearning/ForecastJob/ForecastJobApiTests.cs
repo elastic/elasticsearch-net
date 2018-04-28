bea988e82267984febbcf514cb88933e70563b60
Russ Cam <russ.cam@elastic.co>
2018-04-28 21:23:57 +1000

    Post job data for forecast job integration tests

--- src/Tests/XPack/MachineLearning/ForecastJob/ForecastJobApiTests.cs
+++ src/Tests/XPack/MachineLearning/ForecastJob/ForecastJobApiTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Elasticsearch.Net;
 using FluentAssertions;
 using Nest;
@@ -11,22 +12,63 @@ namespace Tests.XPack.MachineLearning.ForecastJob
 	[SkipVersion("<6.1.0", "Only exists in Elasticsearch 6.1.0+")]
 	public class ForecastJobApiTests : MachineLearningIntegrationTestBase<IForecastJobResponse, IForecastJobRequest, ForecastJobDescriptor, ForecastJobRequest>
 	{
+		private const int BucketSpanSeconds = 3600;
+
 		public ForecastJobApiTests(XPackMachineLearningCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
 
 		protected override void IntegrationSetup(IElasticClient client, CallUniqueValues values)
 		{
 			foreach (var callUniqueValue in values)
 			{
-				PutJob(client, callUniqueValue.Value);
-				IndexAnomalyRecord(client, callUniqueValue.Value, new DateTimeOffset(2016, 6, 2, 00, 00, 00, TimeSpan.Zero));
+				var putJobResponse = client.PutJob<object>(callUniqueValue.Value, f => f
+					.Description("ForecastJobApiTests")
+					.AnalysisConfig(a => a
+						.BucketSpan($"{BucketSpanSeconds}s")
+						.Detectors(d => d
+							.Mean(m => m
+								.FieldName("value")
+							)
+						)
+					)
+					.DataDescription(d => d
+						.TimeFormat("epoch")
+					)
+				);
+
+				if (!putJobResponse.IsValid)
+					throw new Exception($"Problem putting job {callUniqueValue.Value} for integration test: {putJobResponse.DebugInformation}");
+
 				OpenJob(client, callUniqueValue.Value);
+
+				var epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+				var now = (DateTime.UtcNow - epoch).TotalSeconds;
+				var timestamp = now - 50 * BucketSpanSeconds;
+				var data = new List<object>(50);
+				while (timestamp < now)
+				{
+					data.AddRange(new []
+					{
+						new { time = timestamp, value = 10d },
+						new { time = timestamp, value = 30d }
+					});
+					timestamp += BucketSpanSeconds;
+				}
+
+				var postJobDataResponse = client.PostJobData(callUniqueValue.Value, d => d.Data(data));
+				if (!postJobDataResponse.IsValid)
+					throw new Exception($"Problem posting data for integration test: {postJobDataResponse.DebugInformation}");
+
+				FlushJob(client, callUniqueValue.Value, false);
 			}
 		}
 
 		protected override void IntegrationTeardown(IElasticClient client, CallUniqueValues values)
 		{
 			foreach (var callUniqueValue in values)
+			{
 				CloseJob(client, callUniqueValue.Value);
+				DeleteJob(client, callUniqueValue.Value);
+			}
 		}
 
 		protected override LazyResponses ClientUsage() => Calls(
