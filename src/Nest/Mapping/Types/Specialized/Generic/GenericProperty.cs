243173aeb86d5000339209fac053c72b323aeb70
Russ Cam <russ.cam@elastic.co>
2018-04-30 12:02:51 +1000

    Add GenericProperty.Indexed to support bool? values
    This commit adds GenericProperty.Indexed to allow a
    bool? value to be specified for the field's "index" value,
    in line with other properties.
    
    Deprecate the usage of GenericProperty.Index and point users to users
    GenericProperty.Indexed. If a user does continue to use GenericProperty.Index
    however, set a value of true for FieldIndexOption.Analyzed and
    FieldIndexOption.NotAnalyzed, false for FieldIndexOption.No, and null for null.
    
    GenericProperty.Index will be updated to type bool? in 7.x
    
    Fixes #3103

--- src/Nest/Mapping/Types/Specialized/Generic/GenericProperty.cs
+++ src/Nest/Mapping/Types/Specialized/Generic/GenericProperty.cs
@@ -11,8 +11,12 @@ namespace Nest
 	[JsonObject(MemberSerialization.OptIn)]
 	public interface IGenericProperty : IDocValuesProperty
 	{
-		[JsonProperty("index")]
+		[JsonIgnore]
+		[Obsolete("Please use Indexed. Will be fixed in NEST 7.x")]
 		FieldIndexOption? Index { get; set; }
+
+		[JsonProperty("index")]
+		bool? Indexed { get; set; }
 
 		[JsonProperty("term_vector")]
 		TermVectorOption? TermVector { get; set; }
@@ -52,6 +56,8 @@ namespace Nest
 	[DebuggerDisplay("{DebugDisplay}")]
 	public class GenericProperty : DocValuesPropertyBase, IGenericProperty
 	{
+		private FieldIndexOption? _index;
+
 		public GenericProperty() : base(FieldType.Object) => this.TypeOverride = null;
 
 		public TermVectorOption? TermVector { get; set; }
@@ -60,7 +66,31 @@ namespace Nest
 		public int? IgnoreAbove { get; set; }
 		public int? PositionIncrementGap { get; set; }
 		public IStringFielddata Fielddata { get; set; }
-		public FieldIndexOption? Index { get; set; }
+
+		[Obsolete("Please use Indexed. Will be fixed in NEST 7.x")]
+		public FieldIndexOption? Index
+		{
+			get => _index;
+			set
+			{
+				_index = value;
+				switch (_index)
+				{
+					case FieldIndexOption.Analyzed:
+					case FieldIndexOption.NotAnalyzed:
+						Indexed = true;
+						break;
+					case FieldIndexOption.No:
+						Indexed = false;
+						break;
+					default:
+						Indexed = null;
+						break;
+				}
+			}
+		}
+
+		public bool? Indexed { get; set; }
 		public string NullValue { get; set; }
 		public bool? Norms { get; set; }
 		public IndexOptions? IndexOptions { get; set; }
@@ -82,7 +112,31 @@ namespace Nest
 		: DocValuesPropertyDescriptorBase<GenericPropertyDescriptor<T>, IGenericProperty, T>, IGenericProperty
 		where T : class
 	{
-		FieldIndexOption? IGenericProperty.Index { get; set; }
+		private FieldIndexOption? _index;
+
+		FieldIndexOption? IGenericProperty.Index
+		{
+			get => _index;
+			set
+			{
+				_index = value;
+				switch (_index)
+				{
+					case FieldIndexOption.Analyzed:
+					case FieldIndexOption.NotAnalyzed:
+						Self.Indexed = true;
+						break;
+					case FieldIndexOption.No:
+						Self.Indexed = false;
+						break;
+					default:
+						Self.Indexed = null;
+						break;
+				}
+			}
+		}
+
+		bool? IGenericProperty.Indexed { get; set; }
 		TermVectorOption? IGenericProperty.TermVector { get; set; }
 		double? IGenericProperty.Boost { get; set; }
 		string IGenericProperty.NullValue { get; set; }
@@ -98,12 +152,16 @@ namespace Nest
 
 		public GenericPropertyDescriptor<T> Type(string type) => Assign(a => this.TypeOverride = type);
 
+		[Obsolete("Please use the overload that accepts bool?. Will be fixed in NEST 7.x")]
 		public GenericPropertyDescriptor<T> Index(FieldIndexOption? index = FieldIndexOption.NotAnalyzed) => Assign(a => a.Index = index);
+
+		public GenericPropertyDescriptor<T> Index(bool? index = true) => Assign(a => a.Indexed = index);
 
 		public GenericPropertyDescriptor<T> Boost(double? boost) => Assign(a => a.Boost = boost);
 
 		public GenericPropertyDescriptor<T> NullValue(string nullValue) => Assign(a => a.NullValue = nullValue);
 
+		[Obsolete("Deprecated. Will be removed in NEST 7.x")]
 		public GenericPropertyDescriptor<T> NotAnalyzed() => Index(FieldIndexOption.NotAnalyzed);
 
 		public GenericPropertyDescriptor<T> TermVector(TermVectorOption? termVector) => Assign(a => a.TermVector = termVector);
