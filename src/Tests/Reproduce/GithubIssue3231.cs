a930fe2f946859759d0ad390f9aa80dd4aae1e78
ivan-sam <ivan.sam@gmail.com>
2018-05-07 10:50:58 +0200

    Ported change related to #3231 to 6.x

---
+++ src/Tests/Reproduce/GithubIssue3231.cs
@@ -0,0 +1,27 @@
+﻿using Elasticsearch.Net;
+using FluentAssertions;
+using System;
+using Tests.Framework;
+using Tests.Framework.ManagedElasticsearch.Clusters;
+using Xunit;
+
+namespace Tests.Reproduce
+{
+	public class GithubIssue3231 : IClusterFixture<ReadOnlyCluster>
+	{
+		private readonly ReadOnlyCluster _cluster;
+
+		public GithubIssue3231(ReadOnlyCluster cluster)
+		{
+			_cluster = cluster;
+		}
+
+		[I]
+		public void CatIndicesUsesThrowExceptionsFromConnectionSettingsWhenRequestSettingsAreNotSetTest()
+		{
+			var client = TestClient.GetClient(connectionSettings => connectionSettings.ThrowExceptions());
+			Action catIndicesRequest = () => client.LowLevel.CatIndices<StringResponse>("non-existing-index");
+			catIndicesRequest.ShouldThrow<ElasticsearchClientException>();
+		}
+	}
+}
