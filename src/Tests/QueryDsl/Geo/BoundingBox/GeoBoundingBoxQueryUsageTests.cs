68cc74b9a23da5279ab30c7f0ef9f62df35a035c
Stuart Cam <stuart.cam@elastic.co>
2018-06-22 12:51:12 +1000

    Add support for well known text (wkt) to geo bounding box queries

--- src/Tests/QueryDsl/Geo/BoundingBox/GeoBoundingBoxQueryUsageTests.cs
+++ src/Tests/QueryDsl/Geo/BoundingBox/GeoBoundingBoxQueryUsageTests.cs
@@ -67,4 +67,56 @@ namespace Tests.QueryDsl.Geo.BoundingBox
 			q =>  q.Field = null
 		};
 	}
+
+	public class GeoBoundingBoxWKTQueryUsageTests : QueryDslUsageTestsBase
+	{
+		public GeoBoundingBoxWKTQueryUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
+
+		protected override object QueryJson => new
+		{
+			geo_bounding_box = new
+			{
+				type = "indexed",
+				validation_method = "strict",
+				_name = "named_query",
+				boost = 1.1,
+				location = new
+				{
+					wkt = "BBOX (34, -34, -34, 34)"
+				}
+			}
+		};
+
+		protected override QueryContainer QueryInitializer => new GeoBoundingBoxQuery
+		{
+			Boost = 1.1,
+			Name = "named_query",
+			Field = Infer.Field<Project>(p => p.Location),
+			BoundingBox = new Nest.BoundingBox
+			{
+				WellKnownText = "BBOX (34, -34, -34, 34)"
+			},
+			Type = GeoExecution.Indexed,
+			ValidationMethod = GeoValidationMethod.Strict
+		};
+
+		protected override QueryContainer QueryFluent(QueryContainerDescriptor<Project> q) => q
+			.GeoBoundingBox(g=>g
+				.Boost(1.1)
+				.Name("named_query")
+				.Field(p=>p.Location)
+				.BoundingBox(b=>b
+					.WellKnownText("BBOX (34, -34, -34, 34)")
+				)
+				.ValidationMethod(GeoValidationMethod.Strict)
+				.Type(GeoExecution.Indexed)
+			);
+
+		protected override ConditionlessWhen ConditionlessWhen => new ConditionlessWhen<IGeoBoundingBoxQuery>(a => a.GeoBoundingBox)
+		{
+			q => q.BoundingBox = null,
+			q => q.BoundingBox = new Nest.BoundingBox { } ,
+			q => q.Field = null
+		};
+	}
 }
