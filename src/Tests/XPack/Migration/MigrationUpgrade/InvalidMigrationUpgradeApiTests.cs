79b5bdee2e838fb5d017ff380a4b37348982d3ab
Stuart Cam <stuart.cam@elastic.co>
2018-07-02 21:18:59 +1000

    Add skipversion and change to XPack cluster

--- src/Tests/XPack/Migration/MigrationUpgrade/InvalidMigrationUpgradeApiTests.cs
+++ src/Tests/XPack/Migration/MigrationUpgrade/InvalidMigrationUpgradeApiTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using Elastic.Xunit.XunitPlumbing;
 using Elasticsearch.Net;
 using FluentAssertions;
 using Nest;
@@ -10,33 +11,34 @@ using Tests.Framework.ManagedElasticsearch.Clusters;
 
 namespace Tests.XPack.Migration.MigrationUpgrade
 {
+	[SkipVersion(">=6.3.0", "Not running on latest version")]
 	public class InvalidMigrationUpgradeApiTests :
-		ApiIntegrationTestBase<ReadOnlyCluster, IMigrationUpgradeResponse, IMigrationUpgradeRequest, MigrationUpgradeDescriptor, MigrationUpgradeRequest>
+		ApiIntegrationTestBase<XPackCluster, IMigrationUpgradeResponse, IMigrationUpgradeRequest, MigrationUpgradeDescriptor, MigrationUpgradeRequest>
 	{
-		public InvalidMigrationUpgradeApiTests(ReadOnlyCluster cluster, EndpointUsage usage)
+		public InvalidMigrationUpgradeApiTests(XPackCluster cluster, EndpointUsage usage)
 			: base(cluster, usage) { }
 
 		protected override LazyResponses ClientUsage() => Calls(
-			fluent: (c, f) => c.MigrationUpgrade(index, f),
-			fluentAsync: (c, f) => c.MigrationUpgradeAsync(index, f),
+			fluent: (c, f) => c.MigrationUpgrade(Index, f),
+			fluentAsync: (c, f) => c.MigrationUpgradeAsync(Index, f),
 			request: (c, r) => c.MigrationUpgrade(r),
 			requestAsync: (c, r) => c.MigrationUpgradeAsync(r)
 		);
 
-		private const string index = ".watches";
+		private const string Index = ".watches";
 
-		protected override string UrlPath => $"/_xpack/migration/upgrade/{index}";
+		protected override string UrlPath => $"/_xpack/migration/upgrade/{Index}";
 		protected override HttpMethod HttpMethod => HttpMethod.POST;
 		protected override int ExpectStatusCode => 400;
 		protected override bool ExpectIsValid => false;
 
 		protected override object ExpectJson => null;
 
-		protected override MigrationUpgradeDescriptor NewDescriptor() => new MigrationUpgradeDescriptor(index);
+		protected override MigrationUpgradeDescriptor NewDescriptor() => new MigrationUpgradeDescriptor(Index);
 
 		protected override Func<MigrationUpgradeDescriptor, IMigrationUpgradeRequest> Fluent => f => f;
 
-		protected override MigrationUpgradeRequest Initializer => new MigrationUpgradeRequest(index);
+		protected override MigrationUpgradeRequest Initializer => new MigrationUpgradeRequest(Index);
 
 		protected override void ExpectResponse(IMigrationUpgradeResponse response)
 		{
