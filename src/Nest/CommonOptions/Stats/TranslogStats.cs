c72508d72502c44e79bcef157383860a1df35f1c
Mpdreamz <mpdreamz@gmail.com>
2018-08-12 15:43:49 +0200

    add additional translog properties as per elastic/elasticsearch#28613 and add properties we weren't mapping

--- src/Nest/CommonOptions/Stats/TranslogStats.cs
+++ src/Nest/CommonOptions/Stats/TranslogStats.cs
@@ -8,11 +8,23 @@ namespace Nest
 		[JsonProperty("operations")]
 		public long Operations  { get; set; }
 
+		[JsonProperty("uncommitted_operations")]
+		public long UncommittedOperations  { get; set; }
+
 		[JsonProperty("size")]
 		public string Size { get; set; }
 
 		[JsonProperty("size_in_bytes")]
 		public long SizeInBytes  { get; set; }
+
+		[JsonProperty("uncommitted_size")]
+		public string UncommittedSize { get; set; }
+
+		[JsonProperty("uncommitted_size_in_bytes")]
+		public long UncommittedSizeInBytes  { get; set; }
+
+		[JsonProperty("earliest_last_modified_age")]
+		public long EarliestLastModifiedAge  { get; set; }
 
 	}
 }
