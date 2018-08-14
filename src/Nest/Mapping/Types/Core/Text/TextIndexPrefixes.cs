a3d2005b45282bb2b7b3b0f7a1119be95754d80b
Martijn Laarman <Mpdreamz@gmail.com>
2018-08-14 14:00:44 +0200

    fix #3326 Add index_prefixes to Text property mapping (#3362)

---
+++ src/Nest/Mapping/Types/Core/Text/TextIndexPrefixes.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace Nest
+{
+	[JsonObject(MemberSerialization.OptIn)]
+	[JsonConverter(typeof(ReadAsTypeJsonConverter<TextIndexPrefixes>))]
+	public interface ITextIndexPrefixes
+	{
+		[JsonProperty("min_chars")]
+		int? MinCharacters { get; set; }
+
+		[JsonProperty("max_chars")]
+		int? MaxCharacters { get; set; }
+	}
+
+	public class TextIndexPrefixes : ITextIndexPrefixes
+	{
+		public int? MinCharacters { get; set; }
+		public int? MaxCharacters { get; set; }
+	}
+
+	public class TextIndexPrefixesDescriptor
+		: DescriptorBase<TextIndexPrefixesDescriptor, ITextIndexPrefixes>, ITextIndexPrefixes
+	{
+		int? ITextIndexPrefixes.MinCharacters { get; set; }
+		int? ITextIndexPrefixes.MaxCharacters { get; set; }
+
+		public TextIndexPrefixesDescriptor MinCharacters(int? min) => Assign(a => a.MinCharacters = min);
+
+		public TextIndexPrefixesDescriptor MaxCharacters(int? max) => Assign(a => a.MaxCharacters = max);
+	}
+}
