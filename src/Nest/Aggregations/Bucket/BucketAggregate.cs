f3a5ef1442a8a98c0bd14f5fd395f7654bdbbced
Martijn Laarman <Mpdreamz@gmail.com>
2018-08-20 06:49:49 +0200

    Fix #3317 and #3322 add after_key support to composite aggregation re… (#3367)
    Fix #3317 and #3322 add after_key support to composite aggregation result and allow format to be set for date histogram composite source
    
    Introduce CompositeBucketAggregate derived from  MultiBucketAggregate<CompositeBucket> to introduce AfterKey property.
    Bump dotnet-xunit .NET tool to official release
    
    (cherry picked from commit 69775d6a22b6036e886ef21b407b4d51a3925d34)

--- src/Nest/Aggregations/Bucket/BucketAggregate.cs
+++ src/Nest/Aggregations/Bucket/BucketAggregate.cs
@@ -14,17 +14,42 @@ namespace Nest
 	{
 		public SingleBucketAggregate(IReadOnlyDictionary<string, IAggregate> aggregations) : base(aggregations) { }
 
+		/// <summary>
+		/// Count of documents in the bucket
+		/// </summary>
 		public long DocCount { get; internal set; }
 	}
 
+	/// <summary>
+	/// Aggregation response for a bucket aggregation
+	/// </summary>
+	/// <typeparam name="TBucket"></typeparam>
 	public class MultiBucketAggregate<TBucket> : IAggregate
 		where TBucket : IBucket
 	{
+		/// <inheritdoc />
 		public IReadOnlyDictionary<string, object> Meta { get; set; }
 
+		/// <summary>
+		/// The buckets into which results are grouped
+		/// </summary>
 		public IReadOnlyCollection<TBucket> Buckets { get; set; } = EmptyReadOnly<TBucket>.Collection;
 	}
 
+	/// <summary>
+	/// Aggregation response of <see cref="CompositeAggregation"/>
+	/// </summary>
+	public class CompositeBucketAggregate : MultiBucketAggregate<CompositeBucket>
+	{
+		/// <summary>
+		/// The composite key of the last bucket returned
+		/// in the response before any filtering by pipeline aggregations.
+		/// If all buckets are filtered/removed by pipeline aggregations,
+		/// <see cref="AfterKey"/> will contain the composite key of the last bucket before filtering.
+		/// </summary>
+		/// <remarks> Valid for Elasticsearch 6.3.0+ </remarks>
+		public CompositeKey AfterKey { get; set; }
+	}
 
 	// Intermediate object used for deserialization
 	public class BucketAggregate : IAggregate
@@ -35,5 +60,6 @@ namespace Nest
 		public IReadOnlyDictionary<string, object> Meta { get; set; } = EmptyReadOnly<string, object>.Dictionary;
 		public long DocCount { get; set; }
 		public long BgCount { get; set; }
+		public IReadOnlyDictionary<string, object> AfterKey { get; set; } = EmptyReadOnly<string, object>.Dictionary;
 	}
 }
