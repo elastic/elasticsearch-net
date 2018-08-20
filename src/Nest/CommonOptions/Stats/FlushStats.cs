524f141fe271f112e263a54040125d20b24f5f2d
Martijn Laarman <Mpdreamz@gmail.com>
2018-08-20 02:29:36 +0200

    Fix #3328 Add periodic flush count to FlushStats (#3360)
    This commit adds periodic flush count to FlushStats

--- src/Nest/CommonOptions/Stats/FlushStats.cs
+++ src/Nest/CommonOptions/Stats/FlushStats.cs
@@ -9,8 +9,21 @@ namespace Nest
 		[JsonProperty("total")]
 		public long Total { get; set; }
 
+		/// <summary>
+		/// The number of flushes that were periodically triggered when translog exceeded the flush threshold.
+		/// </summary>
+		[JsonProperty("periodic")]
+		public long Periodic { get; set; }
+
+		/// <summary>
+		/// The total time merges have been executed.
+		/// </summary>
 		[JsonProperty("total_time")]
 		public string TotalTime { get; set; }
+
+		/// <summary>
+		/// The total time merges have been executed (in milliseconds).
+		/// </summary>
 		[JsonProperty("total_time_in_millis")]
 		public long TotalTimeInMilliseconds { get; set; }
 
