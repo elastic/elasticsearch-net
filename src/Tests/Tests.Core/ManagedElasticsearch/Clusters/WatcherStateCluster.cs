3168d6288f9140836d82afdfd28edcca8919ce22
Martijn Laarman <Mpdreamz@gmail.com>
2018-08-27 16:25:24 +0200

    Bump to latest Elastic.Xunit and Proc (#3381)
    Clusters can now quite easily provide an expected shutdown period.
    
    Proc waits for the streamreaders to emit EOL after the process has
    exitted and if this does not happen before a timeout OnError on the
    observable is called.
    
    In Elastic.Xunit we call `elasticsearch.bat` which spawns
    `elasticsearch.jar` and so `ElasticsearchNode` tracks both process id's.
    
    So while Proc was shutting down the host OK as that triggers the read
    wait for EOL on the console out streams these pipes (coming from
    `elasticsearch.jar`) can stay active quite a while. When we give up the
    jar is kept running (in shutdown mode).
    
    This manifested itself later on when `EphemeralClusterComposer` wanted
    to run its clean up routines and the ephemeral directories were still in
    use.
    
    NodeConfiguration can now provide `WaitForShutDown` which will set
    `Proc`'s `WaitForExit` and `WaitForStreamReaders`. `ClusterBase` also
    provided an overridable `ModifyNodeConfiguration` so that this can be
    set per cluster with relatively low ceremony.
    
    This `WaitForShutdown` does not have to be accurate if its too short
    `ElasticsearchNode` now does a hard kill on both the host and and the
    java pid in a new hook provided by `ObservableProcessBase`
    `OnBeforeWaitForEndOfStreamsError`.
    (cherry picked from commit b0536b9a9ecf618698c8a6faa8358ca94e313ee1)
    
    Conflicts:
    	src/Tests/Tests.Core/Tests.Core.csproj
    	src/Tests/Tests.Domain/Tests.Domain.csproj

--- src/Tests/Tests.Core/ManagedElasticsearch/Clusters/WatcherStateCluster.cs
+++ src/Tests/Tests.Core/ManagedElasticsearch/Clusters/WatcherStateCluster.cs
@@ -1,7 +1,17 @@
-﻿namespace Tests.Core.ManagedElasticsearch.Clusters
+﻿using System;
+using System.Threading;
+using Elastic.Managed.Configuration;
+
+namespace Tests.Core.ManagedElasticsearch.Clusters
 {
 	/// <summary>
 	/// Cluster that modifies the state of the Watcher Service
 	/// </summary>
-	public class WatcherStateCluster : XPackCluster { }
+	public class WatcherStateCluster : XPackCluster
+	{
+		protected override void ModifyNodeConfiguration(NodeConfiguration n, int port)
+		{
+			n.WaitForShutdown = TimeSpan.FromSeconds(30);
+		}
+	}
 }
