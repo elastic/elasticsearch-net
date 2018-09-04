707bbe895880271843dd0ead0ed7f26f231e0f88
Martijn Laarman <Mpdreamz@gmail.com>
2018-09-04 10:56:16 +0200

    Split all the BulkAll and ScrollAll related tests into their own files and classes making it easier to read what they are doing

---
+++ src/Tests/Tests/Document/Multiple/BulkAll/BulkAllCancellationTokenApiTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using FluentAssertions;
+using Nest;
+using Tests.Core.ManagedElasticsearch.Clusters;
+using Tests.Core.Xunit;
+using Tests.Framework.Integration;
+
+namespace Tests.Document.Multiple.BulkAll
+{
+	public class BulkAllCancellationTokenApiTests : BulkAllApiTestsBase
+	{
+		public BulkAllCancellationTokenApiTests(IntrusiveOperationCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		[I, SkipOnTeamCity]
+		public void CancelBulkAll()
+		{
+			var index = CreateIndexName();
+			var handle = new ManualResetEvent(false);
+
+			var size = 1000;
+			var pages = 1000;
+			var seenPages = 0;
+			var numberOfDocuments = size * pages;
+			var documents = this.CreateLazyStreamOfDocuments(numberOfDocuments);
+
+			//first we setup our cold observable
+			var tokenSource = new CancellationTokenSource();
+			var observableBulk = this.Client.BulkAll(documents, f => f
+				.MaxDegreeOfParallelism(8)
+				.BackOffTime(TimeSpan.FromSeconds(10))
+				.BackOffRetries(2)
+				.Size(size)
+				.RefreshOnCompleted()
+				.Index(index)
+			, tokenSource.Token);
+
+			//we set up an observer
+			Exception ex = null;
+			var bulkObserver = new BulkAllObserver(
+				onError: (e) => OnError(ref ex, e, handle),
+				onNext: (b) => Interlocked.Increment(ref seenPages)
+			);
+			//when we subscribe the observable becomes hot
+			observableBulk.Subscribe(bulkObserver);
+
+			//we wait Nseconds to see some bulks
+			handle.WaitOne(TimeSpan.FromSeconds(3));
+			tokenSource.Cancel();
+			//we wait Nseconds to give in flight request a chance to cancel
+			handle.WaitOne(TimeSpan.FromSeconds(3));
+			if (ex != null && !(ex is TaskCanceledException) && !(ex is OperationCanceledException)) throw ex;
+
+			seenPages.Should().BeLessThan(pages).And.BeGreaterThan(0);
+			var count = this.Client.Count<SmallObject>(f => f.Index(index));
+			count.Count.Should().BeLessThan(numberOfDocuments).And.BeGreaterThan(0);
+			bulkObserver.TotalNumberOfFailedBuffers.Should().Be(0);
+			bulkObserver.TotalNumberOfRetries.Should().Be(0);
+		}
+	}
+}
---
+++ src/Tests/Tests/Document/Multiple/BulkAll/BulkAndScrollApiTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using FluentAssertions;
+using Nest;
+using Tests.Core.ManagedElasticsearch.Clusters;
+using Tests.Framework.Integration;
+
+namespace Tests.Document.Multiple.BulkAll
+{
+	public class BulkAndScrollApiTests : BulkAllApiTestsBase
+	{
+		public BulkAndScrollApiTests(IntrusiveOperationCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		[I] public async Task BulkAllAndScrollAll()
+		{
+			var index = CreateIndexName();
+
+			const int size = 1000, pages = 100, numberOfDocuments = size * pages, numberOfShards = 10;
+			var documents = this.CreateLazyStreamOfDocuments(numberOfDocuments);
+
+			await this.CreateIndexAsync(index, numberOfShards);
+
+			this.BulkAll(index, documents, size, pages, numberOfDocuments);
+			this.ScrollAll(index, size, numberOfShards, numberOfDocuments);
+		}
+
+		private void ScrollAll(string index, int size, int numberOfShards, int numberOfDocuments)
+		{
+			var seenDocuments = 0;
+			var seenSlices = new ConcurrentBag<int>();
+			var scrollObserver = this.Client.ScrollAll<SmallObject>("1m", numberOfShards, s => s
+				.MaxDegreeOfParallelism(numberOfShards / 2)
+				.Search(search => search
+					.Size(size / 2)
+					.Index(index)
+					.AllTypes()
+					.MatchAll()
+				)
+			).Wait(TimeSpan.FromMinutes(5), r =>
+			{
+				seenSlices.Add(r.Slice);
+				Interlocked.Add(ref seenDocuments, r.SearchResponse.Hits.Count);
+			});
+
+			seenDocuments.Should().Be(numberOfDocuments);
+			var groups = seenSlices.GroupBy(s => s).ToList();
+			groups.Count().Should().Be(numberOfShards);
+			groups.Should().OnlyContain(g => g.Count() > 1);
+		}
+
+		private void BulkAll(string index, IEnumerable<SmallObject> documents, int size,  int pages, int numberOfDocuments)
+		{
+			var seenPages = 0;
+			//first we setup our cold observable
+			var observableBulk = this.Client.BulkAll(documents, f => f
+				.MaxDegreeOfParallelism(8)
+				.BackOffTime(TimeSpan.FromSeconds(10))
+				.BackOffRetries(2)
+				.Size(size)
+				.RefreshOnCompleted()
+				.Index(index)
+			);
+			//we set up an observer
+			var bulkObserver = observableBulk.Wait(TimeSpan.FromMinutes(5), b => Interlocked.Increment(ref seenPages));
+
+			seenPages.Should().Be(pages);
+			var count = this.Client.Count<SmallObject>(f => f.Index(index));
+			count.Count.Should().Be(numberOfDocuments);
+			bulkObserver.TotalNumberOfFailedBuffers.Should().Be(0);
+		}
+	}
+}
