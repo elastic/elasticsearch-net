302a09c8c90a5336fdb58c71bc2773e17fd7f7e8
Martijn Laarman <Mpdreamz@gmail.com>
2018-09-06 17:10:30 +0200

    cleanup namespaces on full .net HttpConnection alias class

--- src/Elasticsearch.Net/Connection/HttpConnection.cs
+++ src/Elasticsearch.Net/Connection/HttpConnection.cs
@@ -1,20 +1,9 @@
 #if !DOTNETCORE
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
-using System.Linq;
 using System.Net;
-using System.Net.Security;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Elasticsearch.Net
 {
 	/// <summary> The default IConnection implementation. Uses <see cref="HttpWebRequest"/> on the current .NET desktop framework.</summary>
-	public class HttpConnection : HttpWebRequestConnection
-	{
-	}
+	public class HttpConnection : HttpWebRequestConnection { }
 }
 #endif
