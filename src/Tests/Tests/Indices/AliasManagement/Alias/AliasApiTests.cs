1cd431c59f905294d6812734131a41cd943b45b1
Martijn Laarman <Mpdreamz@gmail.com>
2018-10-01 19:56:59 +0200

    Add support for is_write_index on put alias and add alias operation

--- src/Tests/Tests/Indices/AliasManagement/Alias/AliasApiTests.cs
+++ src/Tests/Tests/Indices/AliasManagement/Alias/AliasApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Elastic.Xunit.XunitPlumbing;
 using Elasticsearch.Net;
 using Nest;
 using Tests.Core.ManagedElasticsearch.Clusters;
@@ -47,7 +48,56 @@ namespace Tests.Indices.AliasManagement.Alias
 			Actions = new List<IAliasAction>
 			{
 				new AliasAddAction { Add = new AliasAddOperation {Alias = "alias", Index = CallIsolatedValue, IndexRouting = "x", SearchRouting = "y"} },
-				new AliasRemoveAction {Remove = new AliasRemoveOperation {Alias = "alias", Index = Infer.Index(CallIsolatedValue) }},
+				new AliasRemoveAction {Remove = new AliasRemoveOperation {Alias = "alias", Index = CallIsolatedValue }},
+			}
+		};
+	}
+
+	[SkipVersion("<6.4.0", "is_write_index is a new feature")]
+	public class AliasIsWriteIndexApiTests : ApiIntegrationAgainstNewIndexTestBase<WritableCluster, IBulkAliasResponse, IBulkAliasRequest, BulkAliasDescriptor, BulkAliasRequest>
+	{
+		public AliasIsWriteIndexApiTests(WritableCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.Alias(f),
+			fluentAsync: (client, f) => client.AliasAsync(f),
+			request: (client, r) => client.Alias(r),
+			requestAsync: (client, r) => client.AliasAsync(r)
+		);
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 200;
+		protected override HttpMethod HttpMethod => HttpMethod.POST;
+		protected override string UrlPath => $"/_aliases";
+		private string Index => this.CallIsolatedValue;
+		private string Alias(int i) => $"alias-{this.CallIsolatedValue}-{i}";
+
+		protected override bool SupportsDeserialization => false;
+
+		protected override object ExpectJson => new
+		{
+			actions = new object[]
+			{
+				new Dictionary<string, object> { { "add",
+					new { alias = this.Alias(1), index = this.Index, index_routing="x", search_routing="y", is_write_index = false} } },
+				new Dictionary<string, object> { { "add",
+					new { alias = this.Alias(2), index = this.Index, index_routing="x", search_routing="y", is_write_index = true} } },
+			}
+		};
+
+		protected override Func<BulkAliasDescriptor, IBulkAliasRequest> Fluent => d => d
+			.Add(a=>a.Alias(this.Alias(1)).Index(this.Index).IndexRouting("x").SearchRouting("y").IsWriteIndex(false))
+			.Add(a=>a.Alias(this.Alias(2)).Index(this.Index).IndexRouting("x").SearchRouting("y").IsWriteIndex())
+		;
+
+		protected override BulkAliasRequest Initializer => new BulkAliasRequest
+		{
+			Actions = new List<IAliasAction>
+			{
+				new AliasAddAction { Add =
+					new AliasAddOperation {Alias = this.Alias(1), Index = this.Index, IndexRouting = "x", SearchRouting = "y", IsWriteIndex = false} },
+				new AliasAddAction { Add =
+					new AliasAddOperation {Alias = this.Alias(2), Index = this.Index, IndexRouting = "x", SearchRouting = "y", IsWriteIndex = true} },
 			}
 		};
 	}
