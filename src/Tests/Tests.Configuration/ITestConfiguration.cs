47997f34318805414fbdc16f53a468144b3cbfe4
Martijn Laarman <Mpdreamz@gmail.com>
2018-10-02 10:56:12 +0200

    build.bat cluster now picks up the seed from test(.default).yaml, this prevents unexpected results when running tests from the IDE which is the primary usecase for

--- src/Tests/Tests.Configuration/ITestConfiguration.cs
+++ src/Tests/Tests.Configuration/ITestConfiguration.cs
@@ -1,4 +1,7 @@
-﻿namespace Tests.Configuration
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Configuration
 {
 	public interface ITestConfiguration
 	{
@@ -22,5 +25,31 @@
 	{
 		public bool SourceSerializer { get; set; }
 		public bool TypedKeys { get; set; }
+	}
+
+	public static class TestConfigurationExtensions
+	{
+		public static void DumpConfiguration(this ITestConfiguration config)
+		{
+			Console.WriteLine(new string('-', 20));
+			Console.WriteLine("Starting tests using config:");
+			Console.WriteLine($" - {nameof(config.TestAgainstAlreadyRunningElasticsearch)}: {config.TestAgainstAlreadyRunningElasticsearch}");
+			Console.WriteLine($" - {nameof(config.ElasticsearchVersion)}: {config.ElasticsearchVersion}");
+			Console.WriteLine($" - {nameof(config.ForceReseed)}: {config.ForceReseed}");
+			Console.WriteLine($" - {nameof(config.Mode)}: {config.Mode}");
+			Console.WriteLine($" - {nameof(config.Seed)}: {config.Seed}");
+			if (config.Mode == TestMode.Integration)
+			{
+				Console.WriteLine($" - {nameof(config.ClusterFilter)}: {config.ClusterFilter}");
+				Console.WriteLine($" - {nameof(config.TestFilter)}: {config.TestFilter}");
+
+			}
+			Console.WriteLine($" - {nameof(config.RunIntegrationTests)}: {config.RunIntegrationTests}");
+			Console.WriteLine($" - {nameof(config.RunUnitTests)}: {config.RunUnitTests}");
+			Console.WriteLine($" - Random:");
+			Console.WriteLine($" \t- {nameof(config.Random.SourceSerializer)}: {config.Random.SourceSerializer}");
+			Console.WriteLine($" \t- {nameof(config.Random.TypedKeys)}: {config.Random.TypedKeys}");
+			Console.WriteLine(new string('-', 20));
+		}
 	}
 }
