3d0e6c08de9a620b447a69b48e1ba1513d634919
Russ Cam <russ.cam@forloop.co.uk>
2018-10-17 09:19:26 +1000

    Implement ValueType as an enum

--- src/Nest/Aggregations/Metric/WeightedAverage/WeightedAverageAggregation.cs
+++ src/Nest/Aggregations/Metric/WeightedAverage/WeightedAverageAggregation.cs
@@ -9,20 +9,18 @@ namespace Nest
 	[ContractJsonConverter(typeof(AggregationJsonConverter<WeightedAverageAggregation>))]
 	public interface IWeightedAverageAggregation : IAggregation
 	{
-		/// <summary> The configuration for the field or script that provides the values </summary>
+		/// <summary> The configuration for the field or script that provides the values</summary>
 		[JsonProperty("value")]
 		IWeightedAverageValue Value { get; set; }
-		/// <summary> The configuration for the field or script that provides the weights /// </summary>
+		/// <summary> The configuration for the field or script that provides the weights</summary>
 		[JsonProperty("weight")]
 		IWeightedAverageValue Weight { get; set; }
-		/// <summary> The optional numeric response formatter </summary>
+		/// <summary> The optional numeric response formatter</summary>
 		[JsonProperty("format")]
 		string Format { get; set; }
 		/// <summary> A hint about the values for pure scripts or unmapped fields </summary>
 		[JsonProperty("value_type")]
-		// TODO map as on server enum ?
-		// https://github.com/elastic/elasticsearch/blob/master/server/src/main/java/org/elasticsearch/search/aggregations/support/ValueType.java
-		string ValueType { get; set; }
+		ValueType? ValueType { get; set; }
 	}
 
 	public class WeightedAverageAggregation : AggregationBase, IWeightedAverageAggregation
@@ -32,14 +30,14 @@ namespace Nest
 
 		internal override void WrapInContainer(AggregationContainer c) => c.WeightedAverage = this;
 
-		/// <inheritdoc cref="IWeightedAverageAggregation.Value"/>>
+		/// <inheritdoc cref="IWeightedAverageAggregation.Value"/>
 		public IWeightedAverageValue Value { get; set; }
-		/// <inheritdoc cref="IWeightedAverageAggregation.Weight"/>>
+		/// <inheritdoc cref="IWeightedAverageAggregation.Weight"/>
 		public IWeightedAverageValue Weight { get; set; }
-		/// <inheritdoc cref="IWeightedAverageAggregation.Format"/>>
+		/// <inheritdoc cref="IWeightedAverageAggregation.Format"/>
 		public string Format { get; set; }
-		/// <inheritdoc cref="IWeightedAverageAggregation.ValueType"/>>
-		public string ValueType { get; set; }
+		/// <inheritdoc cref="IWeightedAverageAggregation.ValueType"/>
+		public ValueType? ValueType { get; set; }
 	}
 
 	public class WeightedAverageAggregationDescriptor<T>
@@ -50,29 +48,26 @@ namespace Nest
 		IWeightedAverageValue IWeightedAverageAggregation.Value { get; set; }
 		IWeightedAverageValue IWeightedAverageAggregation.Weight { get; set; }
 		string IWeightedAverageAggregation.Format { get; set; }
-		string IWeightedAverageAggregation.ValueType { get; set; }
-
+		ValueType? IWeightedAverageAggregation.ValueType { get; set; }
 		string IAggregation.Name { get; set; }
 		IDictionary<string, object> IAggregation.Meta { get; set; }
-		/// <inheritdoc cref="IAggregation.Meta"/>>
+
+		/// <inheritdoc cref="IAggregation.Meta"/>
 		public WeightedAverageAggregationDescriptor<T> Meta(Func<FluentDictionary<string, object>, FluentDictionary<string, object>> selector) =>
 			Assign(a => a.Meta = selector?.Invoke(new FluentDictionary<string, object>()));
 
-		/// <inheritdoc cref="IWeightedAverageAggregation.Value"/>>
+		/// <inheritdoc cref="IWeightedAverageAggregation.Value"/>
 		public WeightedAverageAggregationDescriptor<T> Value(Func<WeightedAverageValueDescriptor<T>, IWeightedAverageValue> selector) =>
 			Assign(a => a.Value = selector?.Invoke(new WeightedAverageValueDescriptor<T>()));
 
-		/// <inheritdoc cref="IWeightedAverageAggregation.Weight"/>>
+		/// <inheritdoc cref="IWeightedAverageAggregation.Weight"/>
 		public WeightedAverageAggregationDescriptor<T> Weight(Func<WeightedAverageValueDescriptor<T>, IWeightedAverageValue> selector) =>
 			Assign(a => a.Weight = selector?.Invoke(new WeightedAverageValueDescriptor<T>()));
 
-		/// <inheritdoc cref="IWeightedAverageAggregation.Format"/>>
+		/// <inheritdoc cref="IWeightedAverageAggregation.Format"/>
 		public WeightedAverageAggregationDescriptor<T> Format(string format) => Assign(a => a.Format = format);
 
-		/// <inheritdoc cref="IWeightedAverageAggregation.ValueType"/>>
-		public WeightedAverageAggregationDescriptor<T> ValueType(string valueType) => Assign(a => a.ValueType = valueType);
-
-
+		/// <inheritdoc cref="IWeightedAverageAggregation.ValueType"/>
+		public WeightedAverageAggregationDescriptor<T> ValueType(ValueType? valueType) => Assign(a => a.ValueType = valueType);
 	}
-
 }
--- src/Tests/Tests/Aggregations/Metric/WeightedAverage/WeightedAverageAggregationUsageTests.cs
+++ src/Tests/Tests/Aggregations/Metric/WeightedAverage/WeightedAverageAggregationUsageTests.cs
@@ -6,14 +6,22 @@ using static Nest.Infer;
 using Tests.Core.Extensions;
 using Tests.Core.ManagedElasticsearch.Clusters;
 using Tests.Domain;
+using ValueType = Nest.ValueType;
 
 namespace Tests.Aggregations.Metric.WeightedAverage
 {
+	/**
+	 * A single-value metrics aggregation that computes the weighted average of numeric values that are extracted
+	 * from the aggregated documents. These values can be extracted either from specific numeric fields in the documents.
+	 * When calculating a regular average, each datapoint has an equal "weight" i.e. it contributes equally to the final
+	 * value. Weighted averages, on the other hand, weight each datapoint differently. The amount that each
+	 * datapoint contributes to the final value is extracted from the document, or provided by a script.
+	 *
+	 * Be sure to read the Elasticsearch documentation on {ref_current}/search-aggregations-metrics-weight-avg-aggregation.html[Weighted Avg Aggregation]
+	 */
 	public class WeightedAverageAggregationUsageTests : AggregationUsageTestBase
 	{
 		public WeightedAverageAggregationUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
-
-		private const string AggregationName = "weighted_avg_commits";
 
 		protected override object AggregationJson => new
 		{
@@ -28,32 +36,35 @@ namespace Tests.Aggregations.Metric.WeightedAverage
 					},
 					weight = new
 					{
-						field = "numberOfContributors"
-					}
+						script = "doc.numberOfContributors.value + 1"
+					},
+					value_type = "long"
 				}
 			}
 		};
 
 		protected override Func<AggregationContainerDescriptor<Project>, IAggregationContainer> FluentAggs => a => a
-			.WeightedAverage(AggregationName, avg => avg
+			.WeightedAverage("weighted_avg_commits", avg => avg
 				.Value(v => v.Field(p => p.NumberOfCommits).Missing(0))
-				.Weight(v => v.Field(p => p.NumberOfContributors))
+				.Weight(w => w.Script("doc.numberOfContributors.value + 1"))
+				.ValueType(ValueType.Long)
 			);
 
 		protected override AggregationDictionary InitializerAggs =>
-			new WeightedAverageAggregation(AggregationName)
+			new WeightedAverageAggregation("weighted_avg_commits")
 			{
 				Value = new WeightedAverageValue(Field<Project>(p => p.NumberOfCommits))
 				{
 					Missing = 0
 				},
-				Weight = new WeightedAverageValue(Field<Project>(p => p.NumberOfContributors))
+				Weight = new WeightedAverageValue(new InlineScript("doc.numberOfContributors.value + 1")),
+				ValueType = ValueType.Long
 			};
 
 		protected override void ExpectResponse(ISearchResponse<Project> response)
 		{
 			response.ShouldBeValid();
-			var commitsAvg = response.Aggregations.WeightedAverage(AggregationName);
+			var commitsAvg = response.Aggregations.WeightedAverage("weighted_avg_commits");
 			commitsAvg.Should().NotBeNull();
 			commitsAvg.Value.Should().BeGreaterThan(0);
 		}
