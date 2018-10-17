31bb3c698b4c604aec469626bd9c547d7d4743fd
Martijn Laarman <Mpdreamz@gmail.com>
2018-10-17 07:21:25 +0200

    add mode to keep_types token filter (#3429)

--- src/Tests/Tests/Mapping/Types/Core/Keyword/KeywordPropertyTests.cs
+++ src/Tests/Tests/Mapping/Types/Core/Keyword/KeywordPropertyTests.cs
@@ -2,6 +2,7 @@
 using Elastic.Xunit.XunitPlumbing;
 using Nest;
 using Tests.Analysis;
+using Tests.Analysis.Tokenizers;
 using Tests.Core.ManagedElasticsearch.Clusters;
 using Tests.Domain;
 using Tests.Framework.Integration;
