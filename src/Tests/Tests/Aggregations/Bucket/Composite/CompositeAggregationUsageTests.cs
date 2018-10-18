f7712ff804afba998aafa9458d90028c006b7f1e
Martijn Laarman <Mpdreamz@gmail.com>
2018-10-18 02:27:20 +0200

    add support for missing_bucket on Composite Aggregation (#3420)
    per https://github.com/elastic/elasticsearch/pull/29465

--- src/Tests/Tests/Aggregations/Bucket/Composite/CompositeAggregationUsageTests.cs
+++ src/Tests/Tests/Aggregations/Bucket/Composite/CompositeAggregationUsageTests.cs
@@ -155,7 +155,7 @@ namespace Tests.Aggregations.Bucket.Composite
 			};
 
 		/**==== Handling Responses
-		 * Each Composite aggregation bucket key is an `CompositeKey`, a specialized
+		 * Each Composite aggregation bucket key is a `CompositeKey` type, a specialized
 		 * `IReadOnlyDictionary<string, object>` type with methods to convert values to supported types
 		 */
 		protected override void ExpectResponse(ISearchResponse<Project> response)
@@ -196,6 +196,130 @@ namespace Tests.Aggregations.Bucket.Composite
 		}
 	}
 
+	/**[float]
+	* == Missing buckets
+	* By default documents without a value for a given source are ignored.
+	* It is possible to include them in the response by setting missing_bucket to `true` (defaults to `false`):
+	*
+	* NOTE: Only available in Elasticsearch 6.4.0+
+	*/
+	[SkipVersion("<6.4.0", "Missing buckets added to Composite Aggregation Elasticsearch 6.4.0+")]
+	public class CompositeAggregationMissingBucketUsageTests : ProjectsOnlyAggregationUsageTestBase
+	{
+		public CompositeAggregationMissingBucketUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
+
+		protected override object AggregationJson => new
+		{
+			my_buckets = new
+			{
+				composite = new
+				{
+					sources = new object[]
+					{
+						new
+						{
+							branches = new
+							{
+								terms = new
+								{
+									field = "branches.keyword",
+									order = "asc",
+									missing_bucket = true
+								}
+							}
+						},
+					}
+				},
+				aggs = new
+				{
+					project_tags = new
+					{
+						nested = new { path = "tags" },
+						aggs = new
+						{
+							tags = new { terms = new {field = "tags.name"} }
+						}
+					}
+				}
+			}
+		};
+
+		protected override Func<AggregationContainerDescriptor<Project>, IAggregationContainer> FluentAggs => a => a
+			.Composite("my_buckets", date => date
+				.Sources(s => s
+					.Terms("branches", t => t
+						.Field(f => f.Branches.Suffix("keyword"))
+						.MissingBucket()
+						.Order(SortOrder.Ascending)
+					)
+				)
+				.Aggregations(childAggs => childAggs
+					.Nested("project_tags", n => n
+						.Path(p => p.Tags)
+						.Aggregations(nestedAggs => nestedAggs
+							.Terms("tags", avg => avg.Field(p => p.Tags.First().Name))
+						)
+					)
+				)
+			);
+
+		protected override AggregationDictionary InitializerAggs =>
+			new CompositeAggregation("my_buckets")
+			{
+				Sources = new List<ICompositeAggregationSource>
+				{
+					new TermsCompositeAggregationSource("branches")
+					{
+						Field = Infer.Field<Project>(f => f.Branches.Suffix("keyword")),
+						MissingBucket = true,
+						Order = SortOrder.Ascending
+					}
+				},
+				Aggregations = new NestedAggregation("project_tags")
+				{
+					Path = Field<Project>(p => p.Tags),
+					Aggregations = new TermsAggregation("tags")
+					{
+						Field = Field<Project>(p => p.Tags.First().Name)
+					}
+				}
+			};
+
+		/**==== Handling Responses
+		 * Each Composite aggregation bucket key is an `CompositeKey`, a specialized
+		 * `IReadOnlyDictionary<string, object>` type with methods to convert values to supported types
+		 */
+		protected override void ExpectResponse(ISearchResponse<Project> response)
+		{
+			response.ShouldBeValid();
+
+			var composite = response.Aggregations.Composite("my_buckets");
+			composite.Should().NotBeNull();
+			composite.Buckets.Should().NotBeNullOrEmpty();
+			composite.AfterKey.Should().NotBeNull();
+
+			if (TestConfiguration.Instance.InRange(">=6.3.0"))
+				composite.AfterKey.Should().HaveCount(1).And.ContainKeys("branches");
+
+			var i = 0;
+			foreach (var item in composite.Buckets)
+			{
+				var key = item.Key;
+				key.Should().NotBeNull();
+
+				key.TryGetValue("branches", out string branches).Should().BeTrue("expected to find 'branches' in composite bucket");
+				if (i == 0) branches.Should().BeNull("First key should be null as we expect to have some projects with no branches");
+				else branches.Should().NotBeNullOrEmpty();
+
+				var nested = item.Nested("project_tags");
+				nested.Should().NotBeNull();
+
+				var nestedTerms = nested.Terms("tags");
+				nestedTerms.Buckets.Count.Should().BeGreaterThan(0);
+				i++;
+			}
+		}
+	}
 
 	//hide
 	[SkipVersion("<6.3.0", "Date histogram source only supports format starting from Elasticsearch 6.3.0+")]
@@ -286,10 +410,6 @@ namespace Tests.Aggregations.Bucket.Composite
 				}
 			};
 
-		/**==== Handling Responses
-		 * Each Composite aggregation bucket key is an `CompositeKey`, a specialized
-		 * `IReadOnlyDictionary<string, object>` type with methods to convert values to supported types
-		 */
 		protected override void ExpectResponse(ISearchResponse<Project> response)
 		{
 			response.ShouldBeValid();
