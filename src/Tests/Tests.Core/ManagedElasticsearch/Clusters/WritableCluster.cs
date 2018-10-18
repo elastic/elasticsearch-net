40d86d24df9fe1c77b2fc90b5da5ab646faff87a
Russ Cam <russ.cam@forloop.co.uk>
2018-10-18 18:08:32 +1000

    Move ClientTestClusterConfiguration into static method
    Only add analysis-nori plugin to install when version is >=6.4.0.
    
    (cherry picked from commit c7b5136e0fa9889d5735e3f558a01f20edf8bdfc)

--- src/Tests/Tests.Core/ManagedElasticsearch/Clusters/WritableCluster.cs
+++ src/Tests/Tests.Core/ManagedElasticsearch/Clusters/WritableCluster.cs
@@ -1,25 +1,41 @@
+using System.Collections.Generic;
+using Elastic.Managed.Configuration;
 using Elastic.Managed.Ephemeral.Plugins;
+using Tests.Core.Client;
 using Tests.Core.ManagedElasticsearch.NodeSeeders;
 using static Elastic.Managed.Ephemeral.Plugins.ElasticsearchPlugin;
 
 namespace Tests.Core.ManagedElasticsearch.Clusters
 {
-	/// <summary> Use this cluster for api's that do writes. If they are however intrusive or long running consider IntrusiveOperationCluster instead. </summary>
+	/// <summary> Use this cluster for APIs that do writes. If they are however intrusive or long running consider IntrusiveOperationCluster instead. </summary>
 	public class WritableCluster : ClientTestClusterBase
 	{
-		public WritableCluster() : base(new ClientTestClusterConfiguration(
-			IngestGeoIp,
-			IngestAttachment,
-			AnalysisKuromoji,
-			AnalysisIcu,
-			AnalysisPhonetic,
-			MapperMurmur3,
-			//TODO move this to elasticsearch-net abstractions
-			new ElasticsearchPlugin("analysis-nori", v => v >= "6.4.0")
-		)
+		public WritableCluster() : base(CreateConfiguration()) { }
+
+		private static ClientTestClusterConfiguration CreateConfiguration()
 		{
-			MaxConcurrency = 4
-		}) { }
+			var plugins = new List<ElasticsearchPlugin>
+			{
+				IngestGeoIp,
+				IngestAttachment,
+				AnalysisKuromoji,
+				AnalysisIcu,
+				AnalysisPhonetic,
+				MapperMurmur3,
+			};
+
+			// TODO: temporary until https://github.com/elastic/elasticsearch-net-abstractions/commit/3977ccb6449870fb4f1e6059be960e12ec5e5125 is released
+			if (ElasticsearchVersion.From(TestClient.Configuration.ElasticsearchVersion) >= "6.4.0")
+			{
+				//TODO move this to elasticsearch-net abstractions
+				plugins.Add(new ElasticsearchPlugin("analysis-nori", v => v >= "6.4.0"));
+			}
+
+			return new ClientTestClusterConfiguration(plugins.ToArray())
+			{
+				MaxConcurrency = 4
+			};
+		}
 
 		protected override void SeedCluster()
 		{
