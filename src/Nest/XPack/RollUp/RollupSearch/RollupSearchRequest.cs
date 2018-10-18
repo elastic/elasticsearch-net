9c6a36f9c8c2cf926a6fa3f4a65eead3a65482af
Russ Cam <russ.cam@forloop.co.uk>
2018-10-18 10:25:00 +1000

    Move XML documentation to interface and inherit

--- src/Nest/XPack/RollUp/RollupSearch/RollupSearchRequest.cs
+++ src/Nest/XPack/RollUp/RollupSearch/RollupSearchRequest.cs
@@ -6,20 +6,26 @@ namespace Nest
 	[MapsApi("xpack.rollup.rollup_search.json")]
 	public partial interface IRollupSearchRequest
 	{
+		/// <summary>When doing rollup searches against rolled up and live indices size needs to be set to 0 explicitly </summary>
 		[JsonProperty("size")]
 		int? Size { get; set; }
 
+		/// <summary> Describe the query to perform using a query descriptor lambda</summary>
 		[JsonProperty("query")]
 		QueryContainer Query { get; set; }
 
+		/// <summary> Describe the aggregations to perform</summary>
 		[JsonProperty("aggs")]
 		AggregationDictionary Aggregations { get; set; }
 	}
 
 	public partial class RollupSearchRequest
 	{
+		/// <inheritdoc />
 		public int? Size { get; set; }
+		/// <inheritdoc />
 		public QueryContainer Query { get; set; }
+		/// <inheritdoc />
 		public AggregationDictionary Aggregations { get; set; }
 	}
 
@@ -29,16 +35,18 @@ namespace Nest
 		AggregationDictionary IRollupSearchRequest.Aggregations { get; set; }
 		int? IRollupSearchRequest.Size { get; set; }
 
-		/// <summary>When doing rollup searches against rolled up and live indices size needs to be set to 0 explicitly </summary>
+		/// <inheritdoc cref="IRollupSearchRequest.Size" />
 		public RollupSearchDescriptor<T> Size(int? size) => Assign(a => a.Size = size);
 
+		/// <inheritdoc cref="IRollupSearchRequest.Aggregations" />
 		public RollupSearchDescriptor<T> Aggregations(Func<AggregationContainerDescriptor<T>, IAggregationContainer> aggregationsSelector) =>
 			Assign(a => a.Aggregations = aggregationsSelector(new AggregationContainerDescriptor<T>())?.Aggregations);
 
+		/// <inheritdoc cref="IRollupSearchRequest.Aggregations" />
 		public RollupSearchDescriptor<T> Aggregations(AggregationDictionary aggregations) =>
 			Assign(a => a.Aggregations = aggregations);
 
-		/// <summary> Describe the query to perform using a query descriptor lambda</summary>
+		/// <inheritdoc cref="IRollupSearchRequest.Query" />
 		public RollupSearchDescriptor<T> Query(Func<QueryContainerDescriptor<T>, QueryContainer> query) =>
 			Assign(a => a.Query = query?.Invoke(new QueryContainerDescriptor<T>()));
 	}
