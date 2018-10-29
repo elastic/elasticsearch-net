08c8466d2c28c91753bce796652e0cef0f32ed7c
Russ Cam <russ.cam@forloop.co.uk>
2018-10-29 12:54:09 +1000

    Allow null refresh_interval to be passed
    This commit allows a null value to be passed for refresh_interval, which
    allows a user to remove a transient value to fallback to the configured
    persistent value.
    
    A couple of different approaches were considered
    
    1. Introducing an internal field on concrete implementations of IDynamicIndexSettings
    that could be checked in IndexSettingsConverter to determine whether to serialize
    the refresh_interval value, allowing a user to pass null. This has the downside of
    needing to cast to concrete implementations in IndexSettingsConverter, rendering the
    interface less useful.
    2. Introducing a Time 'null' value with a Null Object pattern implementation, and
    setting a Time.Null value in the event that null is passed to the property setter.
    This would mean that IndexSettingsConverter would not need to change, but now introduces
    much more complexity within the Time type to handle a new special value for equality and
    comparisons.
    
    In the end, the least intrusive approach of setting the key and value in the underlying
    dictionary, and handling whether the key/value should be skipped in IndexSettingsConverter
    was considered the cleanest.
    
    Closes #3402

--- src/Tests/Tests/Indices/IndexSettings/UpdateIndicesSettings/UpdateIndexSettingsApiTests.cs
+++ src/Tests/Tests/Indices/IndexSettings/UpdateIndicesSettings/UpdateIndexSettingsApiTests.cs
@@ -65,4 +65,52 @@ namespace Tests.Indices.IndexSettings.UpdateIndicesSettings
 			}
 		};
 	}
+
+	public class UpdateIndexSettingsRefreshIntervalNullApiTests : ApiIntegrationTestBase<WritableCluster, IUpdateIndexSettingsResponse, IUpdateIndexSettingsRequest, UpdateIndexSettingsDescriptor, UpdateIndexSettingsRequest>
+	{
+		public UpdateIndexSettingsRefreshIntervalNullApiTests(WritableCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		protected override void IntegrationSetup(IElasticClient client, CallUniqueValues values)
+		{
+			foreach (var value in values)
+			{
+				var index = value.Value;
+				var createIndexResponse = client.CreateIndex(index);
+
+				if (!createIndexResponse.IsValid)
+					throw new Exception($"Invalid response when setting up index for integration test {this.GetType().Name}");
+			}
+		}
+
+		protected override LazyResponses ClientUsage() => Calls(
+			fluent: (client, f) => client.UpdateIndexSettings(CallIsolatedValue, f),
+			fluentAsync: (client, f) => client.UpdateIndexSettingsAsync(CallIsolatedValue, f),
+			request: (client, r) => client.UpdateIndexSettings(r),
+			requestAsync: (client, r) => client.UpdateIndexSettingsAsync(r)
+		);
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 200;
+		protected override HttpMethod HttpMethod => HttpMethod.PUT;
+		protected override string UrlPath => $"{CallIsolatedValue}/_settings";
+
+		protected override object ExpectJson { get; } = new Dictionary<string, object>
+		{
+			{ "index.refresh_interval", null },
+		};
+
+		protected override Func<UpdateIndexSettingsDescriptor, IUpdateIndexSettingsRequest> Fluent => d => d
+			.Index(CallIsolatedValue)
+			.IndexSettings(i => i
+				.RefreshInterval(null)
+			);
+
+		protected override UpdateIndexSettingsRequest Initializer => new UpdateIndexSettingsRequest(CallIsolatedValue)
+		{
+			IndexSettings = new Nest.IndexSettings
+			{
+				RefreshInterval = null
+			}
+		};
+	}
 }
