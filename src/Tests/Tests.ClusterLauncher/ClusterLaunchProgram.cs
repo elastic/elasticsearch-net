b532584aef4e29f2dfce15f7298c075ea8511b32
Martijn Laarman <Mpdreamz@gmail.com>
2018-11-01 15:44:33 +0100

    Code Cleanup: Tests.ClusterLauncher

--- src/Tests/Tests.ClusterLauncher/ClusterLaunchProgram.cs
+++ src/Tests/Tests.ClusterLauncher/ClusterLaunchProgram.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using Elastic.Managed;
 using Elastic.Managed.Ephemeral;
 using FluentAssertions.Common;
@@ -13,6 +12,8 @@ namespace Tests.ClusterLauncher
 {
 	public static class ClusterLaunchProgram
 	{
+		private static ICluster<EphemeralClusterConfiguration> Instance { get; set; }
+
 		public static int Main(string[] arguments)
 		{
 			if (arguments.Length < 1)
@@ -22,7 +23,7 @@ namespace Tests.ClusterLauncher
 			}
 
 			var clusterName = arguments[0];
-			if 	(arguments.Length > 1)
+			if (arguments.Length > 1)
 				Environment.SetEnvironmentVariable("NEST_INTEGRATION_VERSION", arguments[1], EnvironmentVariableTarget.Process);
 			Environment.SetEnvironmentVariable("NEST_INTEGRATION_SHOW_OUTPUT_AFTER_START", "1", EnvironmentVariableTarget.Process);
 
@@ -45,41 +46,8 @@ namespace Tests.ClusterLauncher
 				Console.Error.WriteLine($"Could not create an instance of '{cluster.FullName}");
 				return 1;
 			}
+
 			return 0;
-		}
-
-		private static ICluster<EphemeralClusterConfiguration> Instance { get; set; }
-
-		private static bool TryStartXPackClusterImplementation(Type cluster)
-		{
-			if (!(Activator.CreateInstance(cluster) is XPackCluster instance)) return false;
-			Instance = instance;
-			using (instance)
-				return Run(instance);
-		}
-
-
-		private static bool TryStartClientTestClusterBaseImplementation(Type cluster)
-		{
-			if (!(Activator.CreateInstance(cluster) is ClientTestClusterBase instance)) return false;
-			Instance = instance;
-			using (instance)
-				return Run(instance);
-		}
-
-		private static bool Run(ICluster<EphemeralClusterConfiguration> instance)
-		{
-			TestConfiguration.Instance.DumpConfiguration();
-			instance.Start();
-			if (!instance.Started)
-			{
-				Console.Error.WriteLine($"Failed to start cluster: '{instance.GetType().FullName}");
-				return false;
-			}
-			Console.WriteLine("Press any key to shutdown the running cluster");
-			var c = default(ConsoleKeyInfo);
-			while (c.Key != ConsoleKey.Q) c = Console.ReadKey();
-			return true;
 		}
 
 		private static Type[] GetClusters()
@@ -98,6 +66,41 @@ namespace Tests.ClusterLauncher
 			return types
 				.Where(t => t.Implements(typeof(IEphemeralCluster)))
 				.ToArray();
+		}
+
+		private static bool Run(ICluster<EphemeralClusterConfiguration> instance)
+		{
+			TestConfiguration.Instance.DumpConfiguration();
+			instance.Start();
+			if (!instance.Started)
+			{
+				Console.Error.WriteLine($"Failed to start cluster: '{instance.GetType().FullName}");
+				return false;
+			}
+
+			Console.WriteLine("Press any key to shutdown the running cluster");
+			var c = default(ConsoleKeyInfo);
+			while (c.Key != ConsoleKey.Q) c = Console.ReadKey();
+			return true;
+		}
+
+
+		private static bool TryStartClientTestClusterBaseImplementation(Type cluster)
+		{
+			if (!(Activator.CreateInstance(cluster) is ClientTestClusterBase instance)) return false;
+
+			Instance = instance;
+			using (instance)
+				return Run(instance);
+		}
+
+		private static bool TryStartXPackClusterImplementation(Type cluster)
+		{
+			if (!(Activator.CreateInstance(cluster) is XPackCluster instance)) return false;
+
+			Instance = instance;
+			using (instance)
+				return Run(instance);
 		}
 	}
 }
