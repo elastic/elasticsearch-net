2b213edd8ac3d40ee982d3f7157d0847861d83ce
Martijn Laarman <Mpdreamz@gmail.com>
2018-11-01 15:33:17 +0100

    Code Cleanup: Tests.Profiling

--- src/Tests/Tests.Profiling/DummyProgram.cs
+++ src/Tests/Tests.Profiling/DummyProgram.cs
@@ -1,12 +1,9 @@
 ﻿public static class Program
 {
 	/// <summary>
-	/// This file is only included because the Jetbrains profiling self hosting SDK has not yet been pulled down
-	/// by the build.bat or build.sh root. You are probably seeing this file because you did a fresh checkout and
-	/// are building this project in the IDE.
+	///     This file is only included because the Jetbrains profiling self hosting SDK has not yet been pulled down
+	///     by the build.bat or build.sh root. You are probably seeing this file because you did a fresh checkout and
+	///     are building this project in the IDE.
 	/// </summary>
-	public static void Main()
-	{
-
-	}
+	public static void Main() { }
 }
