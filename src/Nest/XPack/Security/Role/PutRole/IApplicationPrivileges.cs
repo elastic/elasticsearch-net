25605910377e4f464f0aaee489f5eaae7a69b04d
Russ Cam <russ.cam@elastic.co>
2018-11-01 19:10:18 +1000

    Add application privileges to Put Role API (#3462)
    Closes #3409
    
    (cherry picked from commit f8be99c2ad3978990dfb68b9577c08865f9125ec)

---
+++ src/Nest/XPack/Security/Role/PutRole/IApplicationPrivileges.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nest
+{
+	/// <summary>
+	/// Privileges for an application
+	/// </summary>
+	[JsonConverter(typeof(ReadAsTypeJsonConverter<ApplicationPrivileges>))]
+	public interface IApplicationPrivileges
+	{
+		/// <summary>
+		/// The name of the application to which this entry applies
+		/// </summary>
+		[JsonProperty("application")]
+		string Application { get; set; }
+
+		/// <summary>
+		/// A list of strings, where each element is the name of an application privilege or action.
+		/// </summary>
+		[JsonProperty("privileges")]
+		IEnumerable<string> Privileges { get; set; }
+
+		/// <summary>
+		/// A list resources to which the privileges are applied.
+		/// </summary>
+		[JsonProperty("resources")]
+		IEnumerable<string> Resources { get; set; }
+	}
+
+	/// <inheritdoc />
+	public class ApplicationPrivileges : IApplicationPrivileges
+	{
+		/// <inheritdoc />
+		public string Application { get; set; }
+		/// <inheritdoc />
+		public IEnumerable<string> Privileges { get; set; }
+		/// <inheritdoc />
+		public IEnumerable<string> Resources { get; set; }
+	}
+
+	public class ApplicationPrivilegesDescriptor : DescriptorPromiseBase<ApplicationPrivilegesDescriptor, IList<IApplicationPrivileges>>
+	{
+		public ApplicationPrivilegesDescriptor() : base(new List<IApplicationPrivileges>()) { }
+
+		/// <summary>
+		/// Adds an application privilege
+		/// </summary>
+		public ApplicationPrivilegesDescriptor Add<T>(Func<ApplicationPrivilegesDescriptor<T>, IApplicationPrivileges> selector) where T : class  =>
+			Assign(a => a.AddIfNotNull(selector?.Invoke(new ApplicationPrivilegesDescriptor<T>())));
+	}
+
+	/// <inheritdoc cref="IApplicationPrivileges" />
+	public class ApplicationPrivilegesDescriptor<T>: DescriptorBase<ApplicationPrivilegesDescriptor<T>, IApplicationPrivileges>, IApplicationPrivileges
+		where T :class
+	{
+		/// <inheritdoc />
+		string IApplicationPrivileges.Application { get; set; }
+		/// <inheritdoc />
+		IEnumerable<string> IApplicationPrivileges.Privileges { get; set; }
+		/// <inheritdoc />
+		IEnumerable<string> IApplicationPrivileges.Resources { get; set; }
+
+		/// <inheritdoc cref="IApplicationPrivileges.Application" />
+		public ApplicationPrivilegesDescriptor<T> Application(string application) => Assign(a => a.Application = application);
+
+		/// <inheritdoc cref="IApplicationPrivileges.Privileges" />
+		public ApplicationPrivilegesDescriptor<T> Privileges(params string[] privileges) => Assign(a => a.Privileges = privileges);
+		/// <inheritdoc cref="IApplicationPrivileges.Privileges" />
+		public ApplicationPrivilegesDescriptor<T> Privileges(IEnumerable<string> privileges) => Assign(a => a.Privileges = privileges);
+
+		/// <inheritdoc cref="IApplicationPrivileges.Resources" />
+		public ApplicationPrivilegesDescriptor<T> Resources(params string[] resources) => Assign(a => a.Resources = resources);
+		/// <inheritdoc cref="IApplicationPrivileges.Resources" />
+		public ApplicationPrivilegesDescriptor<T> Resources(IEnumerable<string> resources) => Assign(a => a.Resources = resources);
+	}
+}
