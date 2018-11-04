dfe38db1615527300771cac4d63a365e8528cace
Martijn Laarman <Mpdreamz@gmail.com>
2018-11-04 13:38:40 +0100

    Code Cleanup: Tests.Core, Tests.Configuration

--- src/Tests/Tests.Configuration/YamlConfiguration.cs
+++ src/Tests/Tests.Configuration/YamlConfiguration.cs
@@ -8,14 +8,6 @@ namespace Tests.Configuration
 	public class YamlConfiguration : TestConfigurationBase
 	{
 		private readonly Dictionary<string, string> _config;
-		public sealed override bool TestAgainstAlreadyRunningElasticsearch { get; protected set; } = true;
-		public sealed override string ElasticsearchVersion { get; protected set; }
-		public sealed override bool ForceReseed { get; protected set; } = true;
-		public sealed override TestMode Mode { get; protected set; } = TestMode.Unit;
-		public sealed override string ClusterFilter { get; protected set; }
-		public sealed override string TestFilter { get; protected set; }
-		public sealed override int Seed { get; protected set; }
-		public sealed override bool ShowElasticsearchOutputAfterStarted { get; protected set; }
 
 		public YamlConfiguration(string configurationFile)
 		{
@@ -25,24 +17,33 @@ namespace Tests.Configuration
 				.Where(l => !l.Trim().StartsWith("#") && !string.IsNullOrWhiteSpace(l))
 				.ToDictionary(ConfigName, ConfigValue);
 
-			this.Mode = GetTestMode(_config["mode"]);
+			Mode = GetTestMode(_config["mode"]);
 			var version = _config["elasticsearch_version"];
-			this.ElasticsearchVersion = string.IsNullOrWhiteSpace(version) ? DefaultVersion : version;
-			this.ForceReseed = this.BoolConfig("force_reseed", false);
-			this.TestAgainstAlreadyRunningElasticsearch = this.BoolConfig("test_against_already_running_elasticsearch", false);
-			this.ShowElasticsearchOutputAfterStarted = this.BoolConfig("elasticsearch_out_after_started", false);
-			this.ClusterFilter = _config.ContainsKey("cluster_filter") ? _config["cluster_filter"] : null;
-			this.TestFilter = _config.ContainsKey("test_filter") ? _config["test_filter"] : null;
+			ElasticsearchVersion = string.IsNullOrWhiteSpace(version) ? DefaultVersion : version;
+			ForceReseed = BoolConfig("force_reseed", false);
+			TestAgainstAlreadyRunningElasticsearch = BoolConfig("test_against_already_running_elasticsearch", false);
+			ShowElasticsearchOutputAfterStarted = BoolConfig("elasticsearch_out_after_started", false);
+			ClusterFilter = _config.ContainsKey("cluster_filter") ? _config["cluster_filter"] : null;
+			TestFilter = _config.ContainsKey("test_filter") ? _config["test_filter"] : null;
 
 			var newRandom = new Random().Next(1, 100000);
-			this.Seed = _config.TryGetValue("seed", out var seed) ? int.Parse(seed) : newRandom;
-			var randomizer = new Random(this.Seed);
-			this.Random = new RandomConfiguration
+			Seed = _config.TryGetValue("seed", out var seed) ? int.Parse(seed) : newRandom;
+			var randomizer = new Random(Seed);
+			Random = new RandomConfiguration
 			{
-				SourceSerializer = this.RandomBool("source_serializer", randomizer),
-				TypedKeys = this.RandomBool("typed_keys", randomizer),
+				SourceSerializer = RandomBool("source_serializer", randomizer),
+				TypedKeys = RandomBool("typed_keys", randomizer),
 			};
 		}
+
+		public sealed override string ClusterFilter { get; protected set; }
+		public sealed override string ElasticsearchVersion { get; protected set; }
+		public sealed override bool ForceReseed { get; protected set; } = true;
+		public sealed override TestMode Mode { get; protected set; } = TestMode.Unit;
+		public sealed override int Seed { get; protected set; }
+		public sealed override bool ShowElasticsearchOutputAfterStarted { get; protected set; }
+		public sealed override bool TestAgainstAlreadyRunningElasticsearch { get; protected set; } = true;
+		public sealed override string TestFilter { get; protected set; }
 
 		private bool BoolConfig(string key, bool @default) => _config.TryGetValue(key, out var v) ? bool.Parse(v) : @default;
 
@@ -50,7 +51,9 @@ namespace Tests.Configuration
 			_config.TryGetValue($"random_{key}", out var v) ? bool.Parse(v) : random.NextDouble() >= 0.5;
 
 		private static string ConfigName(string configLine) => Parse(configLine, 0);
+
 		private static string ConfigValue(string configLine) => Parse(configLine, 1);
+
 		private static string Parse(string configLine, int index) => configLine.Split(':')[index].Trim(' ');
 
 		private static TestMode GetTestMode(string mode)
