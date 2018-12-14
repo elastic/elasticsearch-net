a2d07d760b508da9e8cc3edae20c7295fe5954f3
Russ Cam <russ.cam@elastic.co>
2018-12-14 16:14:45 +1000

    Fix DateMath serialization

--- src/Nest/CommonOptions/DateMath/DateMathExpression.cs
+++ src/Nest/CommonOptions/DateMath/DateMathExpression.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using Utf8Json;
+using Utf8Json.Formatters;
 
 namespace Nest
 {
+	[JsonFormatter(typeof(DateMathExpressionFormatter))]
 	public class DateMathExpression : DateMath
 	{
 		public DateMathExpression(string anchor) => Anchor = anchor;
@@ -40,5 +43,33 @@ namespace Nest
 			Round = round;
 			return this;
 		}
+	}
+
+	internal class DateMathExpressionFormatter : IJsonFormatter<DateMathExpression>
+	{
+		public DateMathExpression Deserialize(ref JsonReader reader, IJsonFormatterResolver formatterResolver)
+		{
+			var token = reader.GetCurrentJsonToken();
+			if (token != JsonToken.String)
+				return null;
+
+			var segment = reader.ReadStringSegmentUnsafe();
+			var segmentReader = new JsonReader(segment.Array, segment.Offset - 1); // include opening "
+
+			try
+			{
+				// TODO: possibly nicer way of doing this than brute try?
+				var dateTime = ISO8601DateTimeFormatter.Default.Deserialize(ref segmentReader, formatterResolver);
+				return new DateMathExpression(dateTime);
+			}
+			catch (InvalidOperationException)
+			{
+				var value = segment.Utf8String();
+				return new DateMathExpression(value);
+			}
+		}
+
+		public void Serialize(ref JsonWriter writer, DateMathExpression value, IJsonFormatterResolver formatterResolver) =>
+			writer.WriteString(value.ToString());
 	}
 }
