8b78cd381b9796616e81a6c33a344635036d8364
Russ Cam <russ.cam@elastic.co>
2019-01-09 14:55:51 +1000

    Fix MatrixStats aggregation unit tests

--- src/Nest/CommonAbstractions/Infer/Field/FieldFormatter.cs
+++ src/Nest/CommonAbstractions/Infer/Field/FieldFormatter.cs
@@ -2,7 +2,7 @@
 
 namespace Nest
 {
-	internal class FieldFormatter : IJsonFormatter<Field>
+	internal class FieldFormatter : IJsonFormatter<Field>, IObjectPropertyNameFormatter<Field>
 	{
 		public Field Deserialize(ref JsonReader reader, IJsonFormatterResolver formatterResolver)
 		{
@@ -24,5 +24,11 @@ namespace Nest
 			var settings = formatterResolver.GetConnectionSettings();
 			writer.WriteString(settings.Inferrer.Field(value));
 		}
+
+		public void SerializeToPropertyName(ref JsonWriter writer, Field value, IJsonFormatterResolver formatterResolver) =>
+			Serialize(ref writer, value, formatterResolver);
+
+		public Field DeserializeFromPropertyName(ref JsonReader reader, IJsonFormatterResolver formatterResolver) =>
+			Deserialize(ref reader, formatterResolver);
 	}
 }
