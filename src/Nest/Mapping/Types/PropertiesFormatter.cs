dfc4165898e5cfe8c3440efd4de23da1ca9e0c97
Russ Cam <russ.cam@elastic.co>
2019-01-11 17:20:54 +1000

    Fix PutMapping unit tests
    Deduplicate properties in formatter Serialize method. Find a better
    way of doing this

--- src/Nest/Mapping/Types/PropertiesFormatter.cs
+++ src/Nest/Mapping/Types/PropertiesFormatter.cs
@@ -1,9 +1,13 @@
 using Utf8Json;
+using Utf8Json.Formatters;
 
 namespace Nest
 {
 	internal class PropertiesFormatter : IJsonFormatter<IProperties>
 	{
+		private static readonly InterfaceDictionaryFormatter<PropertyName, IProperty> Formatter =
+			new InterfaceDictionaryFormatter<PropertyName, IProperty>();
+
 		public IProperties Deserialize(ref JsonReader reader, IJsonFormatterResolver formatterResolver)
 		{
 			var settings = formatterResolver.GetConnectionSettings();
@@ -15,7 +19,7 @@ namespace Nest
 				var name = reader.ReadPropertyName();
 				if (reader.GetCurrentJsonToken() != JsonToken.BeginObject)
 				{
-					reader.ReadNext();
+					reader.ReadNextBlock();
 					continue;
 				}
 
@@ -36,11 +40,10 @@ namespace Nest
 			}
 
 			var settings = formatterResolver.GetConnectionSettings();
-			var propertyNameFormatter = formatterResolver.GetFormatter<PropertyName>();
-			var propertyFormatter = formatterResolver.GetFormatter<IProperty>();
-			var written = false;
 
-			writer.WriteBeginObject();
+			// HACK: Deduplicate property mappings with an instance of Properties that has access to ConnectionSettings to sanitize PropertyName keys
+			// TODO: Find a way to deduplicate outside of serialization
+			var properties = new Properties(settings);
 
 			foreach (var kv in value)
 			{
@@ -48,13 +51,7 @@ namespace Nest
 				var propertyInfo = clrOrigin?.ClrOrigin;
 				if (propertyInfo == null)
 				{
-					if (written)
-						writer.WriteValueSeparator();
-
-					propertyNameFormatter.Serialize(ref writer, kv.Key, formatterResolver);
-					writer.WriteNameSeparator();
-					propertyFormatter.Serialize(ref writer, kv.Value, formatterResolver);
-					written = true;
+					properties[kv.Key] = kv.Value;
 					continue;
 				}
 				// Check against connection settings mappings
@@ -63,30 +60,17 @@ namespace Nest
 					if (propertyMapping.Ignore)
 						continue;
 
-					if (written)
-						writer.WriteValueSeparator();
-
-					writer.WritePropertyName(propertyMapping.Name);
-					propertyFormatter.Serialize(ref writer, kv.Value, formatterResolver);
-					written = true;
+					properties[propertyMapping.Name] = kv.Value;
 					continue;
 				}
 				// Check against attribute mapping, CreatePropertyMapping caches.
 				// We do not have to take .Name into account from serializer PropertyName (kv.Key) already handles this
 				propertyMapping = settings.PropertyMappingProvider?.CreatePropertyMapping(propertyInfo);
 				if (propertyMapping == null || !propertyMapping.Ignore)
-				{
-					if (written)
-						writer.WriteValueSeparator();
-
-					propertyNameFormatter.Serialize(ref writer, kv.Key, formatterResolver);
-					writer.WriteNameSeparator();
-					propertyFormatter.Serialize(ref writer, kv.Value, formatterResolver);
-					written = true;
-				}
+					properties[kv.Key] = kv.Value;
 			}
 
-			writer.WriteEndObject();
+			Formatter.Serialize(ref writer, properties, formatterResolver);
 		}
 	}
 }
