a5594c618b99b0269654d59d9c5cfa1d4f12424c
Russ Cam <russ.cam@elastic.co>
2019-01-16 17:41:33 +1000

    Rerun API generator

--- src/Nest/_Generated/_Requests.generated.cs
+++ src/Nest/_Generated/_Requests.generated.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using Elasticsearch.Net;
+using System.Runtime.Serialization;
 using Utf8Json;
 
 //This file contains all the typed querystring parameters that are generated of the client spec.
