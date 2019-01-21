00bad609888adce8d0cd775c9d5d61863b637e03
Russ Cam <russ.cam@elastic.co>
2019-01-21 11:12:20 +1000

    Introduce IJsonProperty interface for serializing MemberInfo
    This commit introduces an IJsonProperty to Utf8Json, to allow
    attributes within NEST to implement to control serialization of
    MemberInfo. This is used for PropertyNameAttribute and specialized
    property attributes such as DateAttribute

--- src/Nest/Mapping/AttributeBased/ElasticsearchPropertyAttributeBase.cs
+++ src/Nest/Mapping/AttributeBased/ElasticsearchPropertyAttributeBase.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Reflection;
 using Elasticsearch.Net;
 using System.Runtime.Serialization;
+using Utf8Json;
 
 namespace Nest
 {
 	[AttributeUsage(AttributeTargets.Property)]
 	[DataContract]
-	public abstract class ElasticsearchPropertyAttributeBase : Attribute, IProperty, IPropertyMapping
+	public abstract class ElasticsearchPropertyAttributeBase : Attribute, IProperty, IPropertyMapping, IJsonProperty
 	{
 		protected ElasticsearchPropertyAttributeBase(FieldType type) => Self.Type = type.GetStringValue();
 
 		public bool Ignore { get; set; }
 
 		public string Name { get; set; }
+
+		public int Order { get; } = -2;
+
 		IDictionary<string, object> IProperty.LocalMetadata { get; set; }
 
 		PropertyName IProperty.Name { get; set; }
