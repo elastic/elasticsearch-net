00c3833ee676c1da18333f9f127ecd4a3cafed05
Martijn Laarman <Mpdreamz@gmail.com>
2019-01-24 13:54:32 +0100

    deprecate standard token filter as per https://github.com/elastic/elasticsearch/pull/33468 (#3520)

--- src/Nest/Analysis/TokenFilters/StandardTokenFilter.cs
+++ src/Nest/Analysis/TokenFilters/StandardTokenFilter.cs
@@ -1,17 +1,22 @@
-﻿namespace Nest
+﻿using System;
+
+namespace Nest
 {
 	/// <summary>
 	/// A token filter of type standard that normalizes tokens extracted with the Standard Tokenizer.
 	/// </summary>
+	[Obsolete(" The `standard` token filter has been deprecated because it doesn't change anything in the stream. It will be removed in 7.0.")]
 	public interface IStandardTokenFilter : ITokenFilter { }
 
 	/// <inheritdoc />
+	[Obsolete(" The `standard` token filter has been deprecated because it doesn't change anything in the stream. It will be removed in 7.0.")]
 	public class StandardTokenFilter : TokenFilterBase, IStandardTokenFilter
 	{
 		public StandardTokenFilter() : base("standard") { }
 	}
 
 	/// <inheritdoc />
+	[Obsolete(" The `standard` token filter has been deprecated because it doesn't change anything in the stream. It will be removed in 7.0.")]
 	public class StandardTokenFilterDescriptor
 		: TokenFilterDescriptorBase<StandardTokenFilterDescriptor, IStandardTokenFilter>, IStandardTokenFilter
 	{
