5e7e3918afb45b4dac5ce5e7a386d4b5cc60573d
Martijn Laarman <Mpdreamz@gmail.com>
2019-01-24 14:00:06 +0100

    V65/processors (#3528)
    * Add support for the dissect ingest processor
    
    * Add support for if/tag and ignore_failure on all processors fixes #3506
    
    * Add support for the drop processor
    
    * Add support for the set_security_user processor
    
    * Add support for the pipeline processor
    
    * addressed PR comments regarding xmldocs and TODO statements

---
+++ src/Nest/Ingest/Processors/DropProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+using Newtonsoft.Json;
+
+namespace Nest
+{
+	/// <summary>
+	/// Drops the document without raising any errors. This is useful to prevent the document from getting indexed based on some condition.
+	/// </summary>
+	[JsonObject(MemberSerialization.OptIn)]
+	[JsonConverter(typeof(ProcessorJsonConverter<DropProcessor>))]
+	public interface IDropProcessor : IProcessor
+	{
+	}
+
+	/// <inheritdoc cref="IDropProcessor"/>
+	public class DropProcessor : ProcessorBase, IDropProcessor
+	{
+		protected override string Name => "drop";
+	}
+
+	/// <inheritdoc cref="IDropProcessor"/>
+	public class DropProcessorDescriptor : ProcessorDescriptorBase<DropProcessorDescriptor, IDropProcessor>, IDropProcessor
+	{
+		protected override string Name => "drop";
+	}
+}
---
+++ src/Nest/Ingest/Processors/PipelineProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq.Expressions;
+using Newtonsoft.Json;
+
+namespace Nest
+{
+	/// <summary> Executes another pipeline.</summary>
+	[JsonObject(MemberSerialization.OptIn)]
+	[JsonConverter(typeof(ProcessorJsonConverter<PipelineProcessor>))]
+	public interface IPipelineProcessor : IProcessor
+	{
+		//TODO 7.x: this property clashes with the Name property on the IProcessor, need to rename base in master
+		/// <summary>The name of the pipeline to execute. </summary>
+		[JsonProperty("name")]
+		string ProcessorName { get; set; }
+	}
+
+	/// <inheritdoc cref="IPipelineProcessor" />
+	public class PipelineProcessor : ProcessorBase, IPipelineProcessor
+	{
+		/// <inheritdoc cref="IPipelineProcessor.ProcessorName"/>
+		[JsonProperty("name")]
+		public string ProcessorName { get; set; }
+
+		internal const string ProcessorTypeName = "pipeline";
+		protected override string Name => ProcessorTypeName;
+	}
+
+	/// <inheritdoc cref="IPipelineProcessor" />
+	public class PipelineProcessorDescriptor
+		: ProcessorDescriptorBase<PipelineProcessorDescriptor, IPipelineProcessor>, IPipelineProcessor
+	{
+		protected override string Name => PipelineProcessor.ProcessorTypeName;
+		string IPipelineProcessor.ProcessorName { get; set; }
+
+		/// <inheritdoc cref="IPipelineProcessor.ProcessorName"/>
+		public PipelineProcessorDescriptor ProcessorName(string processorName) => Assign(a => a.ProcessorName = processorName);
+	}
+}
