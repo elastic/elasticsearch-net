523ba0745b6abfc82563705d3e47e4a296a0dfff
Mpdreamz <Mpdreamz@gmail.com>
2019-01-25 15:39:54 +0100

    fix invalid upgrade api tests
    (cherry picked from commit d59113a20c6c1d5a017eb01c1905ceb467a13822)

--- src/Tests/Tests/XPack/Migration/MigrationUpgrade/InvalidMigrationUpgradeApiTests.cs
+++ src/Tests/Tests/XPack/Migration/MigrationUpgrade/InvalidMigrationUpgradeApiTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Elastic.Xunit.XunitPlumbing;
 using Elasticsearch.Net;
 using FluentAssertions;
 using Nest;
@@ -9,40 +10,51 @@ using Tests.Framework.Integration;
 
 namespace Tests.XPack.Migration.MigrationUpgrade
 {
+	[SkipVersion("<6.3.0", "Not running on latest version")]
 	public class InvalidMigrationUpgradeApiTests
 		: ApiIntegrationTestBase<XPackCluster, IMigrationUpgradeResponse, IMigrationUpgradeRequest, MigrationUpgradeDescriptor,
 			MigrationUpgradeRequest>
 	{
-		private const string index = ".watches";
-
 		public InvalidMigrationUpgradeApiTests(XPackCluster cluster, EndpointUsage usage)
 			: base(cluster, usage) { }
 
 		protected override bool ExpectIsValid => false;
 
 		protected override object ExpectJson => null;
-		protected override int ExpectStatusCode => 400;
+		protected override int ExpectStatusCode => 500;
 
 		protected override Func<MigrationUpgradeDescriptor, IMigrationUpgradeRequest> Fluent => f => f;
 		protected override HttpMethod HttpMethod => HttpMethod.POST;
 
-		protected override MigrationUpgradeRequest Initializer => new MigrationUpgradeRequest(index);
+		protected override MigrationUpgradeRequest Initializer => new MigrationUpgradeRequest(CallIsolatedValue);
 
-		protected override string UrlPath => $"/_xpack/migration/upgrade/{index}";
+		protected override string UrlPath => $"/_xpack/migration/upgrade/{CallIsolatedValue}";
 
 		protected override LazyResponses ClientUsage() => Calls(
-			(c, f) => c.MigrationUpgrade(index, f),
-			(c, f) => c.MigrationUpgradeAsync(index, f),
+			(c, f) => c.MigrationUpgrade(CallIsolatedValue, f),
+			(c, f) => c.MigrationUpgradeAsync(CallIsolatedValue, f),
 			(c, r) => c.MigrationUpgrade(r),
 			(c, r) => c.MigrationUpgradeAsync(r)
 		);
 
-		protected override MigrationUpgradeDescriptor NewDescriptor() => new MigrationUpgradeDescriptor(index);
+		protected override MigrationUpgradeDescriptor NewDescriptor() => new MigrationUpgradeDescriptor(CallIsolatedValue);
+
+		protected override void IntegrationSetup(IElasticClient client, CallUniqueValues values)
+		{
+			foreach (var (k, v) in values)
+				client.CreateIndex(v);
+		}
+		protected override void IntegrationTeardown(IElasticClient client, CallUniqueValues values)
+		{
+			foreach (var (k, v) in values)
+				client.DeleteIndex(v);
+		}
 
 		protected override void ExpectResponse(IMigrationUpgradeResponse response)
 		{
 			response.ShouldNotBeValid();
-			response.ServerError.Should().BeNull();
+			response.ServerError.Should().NotBeNull();
+			response.ServerError.Status.Should().Be(500);
 		}
 	}
 }
