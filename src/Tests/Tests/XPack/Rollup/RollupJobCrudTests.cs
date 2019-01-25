6d8e06ff27bfd7b576a32bea4db2e87b4630d7e4
Mpdreamz <Mpdreamz@gmail.com>
2019-01-25 15:09:10 +0100

    Rollup job was only ignoring 6.5.0 not <6.5.0
    (cherry picked from commit f632ae768ef8d7ec620a84d73819d6f493224430)

--- src/Tests/Tests/XPack/Rollup/RollupJobCrudTests.cs
+++ src/Tests/Tests/XPack/Rollup/RollupJobCrudTests.cs
@@ -13,7 +13,7 @@ using Tests.Framework.Integration;
 
 namespace Tests.XPack.Rollup
 {
-	[SkipVersion("6.5.0", "")]
+	[SkipVersion("<6.5.0", "")]
 	public class RollupJobCrudTests
 		: CrudTestBase<TimeSeriesCluster, ICreateRollupJobResponse, IGetRollupJobResponse, ICreateRollupJobResponse, IDeleteRollupJobResponse>
 	{
