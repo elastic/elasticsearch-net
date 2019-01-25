a839bd84f0646b6e4a269436c32b30b91715e4ec
Martijn Laarman <Mpdreamz@gmail.com>
2019-01-25 10:45:56 +0100

    add support for the get ssl certificates API (#3537)
    * add support for the get ssl certificates API
    
    * removed superfluous folder reference in Nest.csproj
    
    * internal setters on GetCertificatesResponse

---
+++ src/Nest/XPack/Ssl/GetCertificates/GetCertificatesRequest.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace Nest
+{
+	[MapsApi("xpack.ssl.certificates.json")]
+	public partial interface IGetCertificatesRequest { }
+
+	public partial class GetCertificatesRequest { }
+
+	public partial class GetCertificatesDescriptor { }
+}
