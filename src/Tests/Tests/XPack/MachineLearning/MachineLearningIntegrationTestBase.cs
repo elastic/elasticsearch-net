12435d3dc82af4dc09cea1e62307526486d513b9
Mpdreamz <Mpdreamz@gmail.com>
2019-01-25 16:40:17 +0100

    Skip machine learning api tests on TC as the agents are underpowered
    (cherry picked from commit 4ba6465baf821c2ac66c675306db8c534825c5d0)

--- src/Tests/Tests/XPack/MachineLearning/MachineLearningIntegrationTestBase.cs
+++ src/Tests/Tests/XPack/MachineLearning/MachineLearningIntegrationTestBase.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Elastic.Xunit.XunitPlumbing;
 using Elasticsearch.Net;
 using Nest;
+using Tests.Core.Xunit;
 using Tests.Domain;
 using Tests.Framework;
 using Tests.Framework.Integration;
@@ -11,6 +12,7 @@ using Tests.Framework.ManagedElasticsearch.Clusters;
 namespace Tests.XPack.MachineLearning
 {
 	[SkipVersion("<5.5.0", "Machine Learning does not exist in previous versions")]
+	[SkipOnTeamCity]
 	public abstract class MachineLearningIntegrationTestBase<TResponse, TInterface, TDescriptor, TInitializer>
 		: ApiIntegrationTestBase<MachineLearningCluster, TResponse, TInterface, TDescriptor, TInitializer>
 		where TResponse : class, IResponse
