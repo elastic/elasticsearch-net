a7b71bda3f2aae4cfbda0c7b2a58ce90ef7cd6af
Martijn Laarman <Mpdreamz@gmail.com>
2019-01-25 10:56:20 +0100

    add support for the reload secure settings API (#3536)

---
+++ src/Nest/Cluster/ReloadSecureSettings/ReloadSecureSettingsResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nest
+{
+	public interface IReloadSecureSettingsResponse : INodesResponse
+	{
+		[JsonProperty("cluster_name")]
+		string ClusterName { get; }
+
+		[JsonProperty("nodes")]
+		[JsonConverter(typeof(VerbatimDictionaryKeysJsonConverter<string, NodeStats>))]
+		IReadOnlyDictionary<string, NodeStats> Nodes { get; }
+	}
+
+	public class ReloadSecureSettingsResponse : NodesResponseBase, IReloadSecureSettingsResponse
+	{
+		public string ClusterName { get; internal set; }
+
+		public IReadOnlyDictionary<string, NodeStats> Nodes { get; internal set; } = EmptyReadOnly<string, NodeStats>.Dictionary;
+	}
+}
