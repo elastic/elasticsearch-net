99b9d3651fee480ebeee5403fe0538ac5459e757
Russ Cam <russ.cam@elastic.co>
2019-02-26 15:50:50 +1000

    Delete SimpleJson

--- src/Elasticsearch.Net/Serialization/JsonFormatterResolverExtensions.cs
+++ src/Elasticsearch.Net/Serialization/JsonFormatterResolverExtensions.cs
@@ -6,6 +6,7 @@ namespace Elasticsearch.Net
 	{
 		internal static T ReserializeAndDeserialize<T>(this IJsonFormatterResolver formatterResolver, object value)
 		{
+			// TODO: Use ArrayPool or MemoryStreamFactory.Create() for byte array / stream
 			var bytes = JsonSerializer.Serialize(value);
 			var formatter = formatterResolver.GetFormatter<T>();
 			var reader = new JsonReader(bytes);
