ea05c82747a5e241fc2bbff88a36573ce100f43f
Russ Cam <russ.cam@elastic.co>
2019-03-01 17:37:18 +1000

    Deserialize to Hit<TDocument> using IJsonFormatterResolver
    This commit changes the lazy deserialization of LazyDocument to Hit<Document> to use
    IJsonFormatterResolver, because Hit<> must be deserialized by the internal serializer

--- src/Nest/Aggregations/Metric/TopHits/TopHitsAggregate.cs
+++ src/Nest/Aggregations/Metric/TopHits/TopHitsAggregate.cs
@@ -1,22 +1,36 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Elasticsearch.Net;
 
 namespace Nest
 {
 	public class TopHitsAggregate : MetricAggregateBase
 	{
 		private readonly IList<LazyDocument> _hits;
+		private readonly IJsonFormatterResolver _formatterResolver;
 
 		public TopHitsAggregate() { }
 
-		internal TopHitsAggregate(IList<LazyDocument> hits) => _hits = hits;
+		internal TopHitsAggregate(IList<LazyDocument> hits, IJsonFormatterResolver formatterResolver)
+		{
+			_hits = hits;
+			_formatterResolver = formatterResolver;
+		}
 
 		public double? MaxScore { get; set; }
 
 		public HitsTotal Total { get; set; }
 
 		private IEnumerable<Hit<TDocument>> ConvertHits<TDocument>()
-			where TDocument : class => _hits.Select(h => h.As<Hit<TDocument>>());
+			where TDocument : class
+		{
+			var formatter = _formatterResolver.GetFormatter<Hit<TDocument>>();		
+			return _hits.Select(h =>
+			{
+				var reader = new JsonReader(h.Bytes);
+				return formatter.Deserialize(ref reader, _formatterResolver);
+			});
+		}
 
 		public IReadOnlyCollection<Hit<TDocument>> Hits<TDocument>()
 			where TDocument : class =>
