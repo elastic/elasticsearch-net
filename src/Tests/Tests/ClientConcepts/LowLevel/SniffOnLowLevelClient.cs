a94947e54181d15a50807b4dc80e766d84b78f17
Martijn Laarman <Mpdreamz@gmail.com>
2019-03-06 08:41:33 +0100

    fix #3582 low level client sniffing (#3587)
    * fix #3582 #3483 introduced SIMPLE_JSON_TYPEDEF to be defined which caused netstandard and netfx exceptions when deserializing sniff responses, but would light up in different places as well
    
    (cherry picked from commit c49736a3bef8f1f8bdd96f4233e90535b3223259)
    
    * update assertion to be 5.x specific

---
+++ src/Tests/Tests/ClientConcepts/LowLevel/SniffOnLowLevelClient.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using Elastic.Xunit.XunitPlumbing;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Tests.Core.Client.Settings;
+using Tests.Core.ManagedElasticsearch.Clusters;
+using Tests.Framework;
+
+namespace Tests.ClientConcepts.LowLevel
+{
+	public class SniffOnLowLevelClient : IClusterFixture<ReadOnlyCluster>
+	{
+		private readonly ReadOnlyCluster _cluster;
+
+		public SniffOnLowLevelClient(ReadOnlyCluster cluster) => _cluster = cluster;
+
+		[I] public void CanSniffUsingJustTheLowLevelClient()
+		{
+			var uri = TestConnectionSettings.CreateUri(_cluster.Nodes.First().Port ?? 9200);
+			var sniffingConnectionPool = new SniffingConnectionPool(new[] { uri });
+			var elasticClient = new ElasticLowLevelClient(new ConnectionConfiguration(sniffingConnectionPool));
+
+			Func<ElasticsearchResponse<DynamicResponse>> act = () => elasticClient.ClusterHealth<DynamicResponse>();
+			act.Invoking(s => s.Invoke()).ShouldNotThrow();
+		}
+
+	}
+}
