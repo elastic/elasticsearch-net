66382b22f0e619e1df9e3aee5afba93a4b6288f1
Russ Cam <russ.cam@elastic.co>
2019-03-15 17:25:36 +1000

    Include cluster statistics on search response (#3601)
    * Include cluster statistics on search response
    
    This commit includes cluster statistics returned on a search response
    under the key _clusters when a cross cluster search is performed.
    
    Closes #3512

--- src/Nest/CommonOptions/Hit/ShardStatistics.cs
+++ src/Nest/CommonOptions/Hit/ShardStatistics.cs
@@ -7,16 +7,29 @@ namespace Nest
 	[JsonObject]
 	public class ShardStatistics
 	{
-		[JsonProperty]
+		[JsonProperty("failed")]
 		public int Failed { get; internal set; }
 
 		[JsonProperty("failures")]
 		public IReadOnlyCollection<ShardFailure> Failures { get; internal set; } = EmptyReadOnly<ShardFailure>.Collection;
 
-		[JsonProperty]
+		[JsonProperty("successful")]
 		public int Successful { get; internal set; }
 
-		[JsonProperty]
+		[JsonProperty("total")]
+		public int Total { get; internal set; }
+	}
+
+	[JsonObject]
+	public class ClusterStatistics
+	{
+		[JsonProperty("skipped")]
+		public int Skipped { get; internal set; }
+
+		[JsonProperty("successful")]
+		public int Successful { get; internal set; }
+
+		[JsonProperty("total")]
 		public int Total { get; internal set; }
 	}
 }
