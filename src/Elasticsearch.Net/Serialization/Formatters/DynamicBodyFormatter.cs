9ca7039d8816f3a1adbf6250a68ba8e090c6cad4
Russ Cam <russ.cam@elastic.co>
2019-03-25 14:21:54 +1000

    Write value separators when serializing dynamic body

--- src/Elasticsearch.Net/Serialization/Formatters/DynamicBodyFormatter.cs
+++ src/Elasticsearch.Net/Serialization/Formatters/DynamicBodyFormatter.cs
@@ -17,10 +17,14 @@ namespace Elasticsearch.Net
 
 			writer.WriteBeginObject();
 			var formatter = formatterResolver.GetFormatter<object>();
+			var count = 0;
 			foreach (var kv in (IDictionary<string, object>)value)
 			{
+				if (count > 0)
+					writer.WriteValueSeparator();
 				writer.WritePropertyName(kv.Key);
 				formatter.Serialize(ref writer, kv.Value, formatterResolver);
+				count++;
 			}
 			writer.WriteEndObject();
 		}
