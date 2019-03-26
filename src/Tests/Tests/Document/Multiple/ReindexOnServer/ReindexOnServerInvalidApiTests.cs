50448c886d0903e3589a294dcd30c338300610ed
Russ Cam <russ.cam@elastic.co>
2019-03-26 12:26:09 +1000

    Script compile error is now a 400 response
    This commit updates the ReindexOnServerInvalidApiTests to assert a 400 response
    instead of 500 response.

--- src/Tests/Tests/Document/Multiple/ReindexOnServer/ReindexOnServerInvalidApiTests.cs
+++ src/Tests/Tests/Document/Multiple/ReindexOnServer/ReindexOnServerInvalidApiTests.cs
@@ -14,19 +14,18 @@ namespace Tests.Document.Multiple.ReindexOnServer
 		public ReindexOnServerInvalidApiTests(IntrusiveOperationCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
 
 		protected override bool ExpectIsValid => false;
-		protected override int ExpectStatusCode => 500;
+		protected override int ExpectStatusCode => 400;
 
-		//bad painless script
+		//bad painless script - missing opening ( in front of ctx.
 		protected override string PainlessScript { get; } = "if ctx._source.flag == 'bar') {ctx._source.remove('flag')}";
 
 		protected override void ExpectResponse(IReindexOnServerResponse response)
 		{
-			// hello
 			response.ServerError.Should().NotBeNull();
-			response.ServerError.Status.Should().Be(500);
+			response.ServerError.Status.Should().Be(400);
 			response.ServerError.Error.Should().NotBeNull();
 			response.ServerError.Error.RootCause.Should().NotBeNullOrEmpty();
-			response.ServerError.Error.RootCause.First().Reason.Should().Contain("compil");
+			response.ServerError.Error.RootCause.First().Reason.Should().Contain("compile");
 			response.ServerError.Error.RootCause.First().Type.Should().Be("script_exception");
 		}
 
