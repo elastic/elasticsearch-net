d43fca809fa6e7cf1c4ecebdd5c7140b687dee5d
Mpdreamz <Mpdreamz@gmail.com>
2019-03-28 20:41:16 +0100

    created combined tasks
    (cherry picked from commit 3022fef3eb896d3052e998364572f06c801a7c00)

--- build/scripts/Targets.fs
+++ build/scripts/Targets.fs
@@ -15,8 +15,66 @@ open InheritDoc
 open Commandline
 open Fake.IO
 open Tooling
+open Bullseye
+open Bullseye
 
 module Main =
+
+    let deps x = 
+        // Dependencies
+        "Start"
+          =?> ("Clean", Commandline.needsClean )
+          =?> ("Version", hasBuildParam "version")
+          ==> "Restore"
+          =?> ("FullBuild", Commandline.needsFullBuild)
+          =?> ("Test", (not Commandline.skipTests && Commandline.target <> "canary"))
+          =?> ("InternalizeDependencies", (not isMono))
+          ==> "InheritDoc"
+          =?> ("Documentation", (not Commandline.skipDocs))
+          ==> "Build"
+          |> ignore
+
+        "Start"
+          =?> ("Clean", Commandline.needsClean )
+          =?> ("FullBuild", Commandline.needsFullBuild)
+          ==> "Benchmark"
+          |> ignore
+
+        "Version"
+          ==> "Release"
+          =?> ("TestNugetPackage", (not isMono && not Commandline.skipTests))
+          ==> "Canary"
+          |> ignore
+
+        "Start"
+          =?> ("Clean", Commandline.needsClean )
+          ==> "Restore"
+          =?> ("FullBuild", Commandline.needsFullBuild)
+          ==> "Integrate"
+          |> ignore
+
+        "Build"
+          ==> "NugetPack"
+          =?> ("NugetPackVersioned", Commandline.target = "canary")
+          ==> "ValidateArtifacts"
+          =?> ("GenerateReleaseNotes", Commandline.target <> "canary")
+          ==> "Release"
+          |> ignore
+          
+        "Touch" |> ignore
+        "Start"
+          ==> "Clean"
+          ==> "Diff"
+          |> ignore
+          
+        "Start"
+          ==> "Restore"
+          ==> "FullBuild"
+          ==> "Cluster"
+          |> ignore
+
+        ignore()
+          
 
     let [<EntryPoint>] main args = 
     
@@ -24,50 +82,56 @@ module Main =
 
         Commandline.parse()
 
-        Target "Touch" <| fun _ -> traceHeader "Touching build"
-        Target "Build" <| fun _ -> traceHeader "STARTING BUILD"
-        Target "Start" <| fun _ -> 
+        Targets.Target("Touch", fun _ -> traceHeader "Touching build")
+
+        Targets.Target("Build", fun _ -> traceHeader "STARTING BUILD")
+
+        Targets.Target("Start", fun _ -> 
             match (isMono, Commandline.validMonoTarget) with
             | (true, false) -> failwithf "%s is not a valid target on mono because it can not call ILRepack" (Commandline.target)
             | _ -> traceHeader "STARTING BUILD"
+        )
 
-        Target "Clean" Clean
+        Targets.Target("Clean", Clean)
 
-        Target "Restore" Restore
+        Targets.Target("Restore", Restore)
 
-        Target "FullBuild" <| fun _ -> Compile Commandline.needsFullBuild
+        Targets.Target("FullBuild", ["Restore"], fun _ -> Compile Commandline.needsFullBuild)
             
-        Target "Test" Tests.RunUnitTests
+        Targets.Target("Test", Tests.RunUnitTests)
 
-        Target "Integrate" Tests.RunIntegrationTests
+        Targets.Target("Integrate", Tests.RunIntegrationTests)
 
-        Target "Benchmark" Benchmarker.Run
+        Targets.Target("Benchmark", Benchmarker.Run)
 
-        Target "InternalizeDependencies" <| fun _ -> ILRepack |> ignore
+        Targets.Target("InternalizeDependencies", fun _ -> ILRepack |> ignore)
 
-        Target "InheritDoc" InheritDoc.PatchInheritDocs
+        Targets.Target("InheritDoc", InheritDoc.PatchInheritDocs)
 
-        Target "Documentation" Documentation.Generate
+        Targets.Target("Documentation", Documentation.Generate)
 
-        Target "Version" <| fun _ -> 
+        Targets.Target("Version", fun _ -> 
             tracefn "Current Version: %s" (Versioning.CurrentVersion.ToString())
+        )
 
-        Target "TestNugetPackage" <| fun _ -> 
+        Targets.Target("TestNugetPackage", fun _ -> 
             //RunReleaseUnitTests restores the canary nugetpackages in tests, since these end up being cached
             //its too evasive to run on development machines or TC, Run only on AppVeyor containers.
             if buildServer <> AppVeyor then Tests.RunUnitTests()
             else Tests.RunReleaseUnitTests()
+        )
             
-        Target "Canary" <| fun _ -> tracefn "Finished Release Build %O" Versioning.CurrentVersion
+        Targets.Target("Canary", fun _ -> tracefn "Finished Release Build %O" Versioning.CurrentVersion)
             
-        Target "Diff" <| fun _ ->
+        Targets.Target("Diff", fun _ ->
             let differ = Paths.PaketDotNetGlobalTool "assembly-differ" @"tools\netcoreapp2.1\any\assembly-differ.dll"
             let args = Commandline.arguments |> List.skip 1 |> String.concat " "
             let command = sprintf @"%s %s" differ args
             setProcessEnvironVar "NUGET" Tooling.nugetFile
             DotNetCli.RunCommand (fun p -> { p with TimeOut = TimeSpan.FromMinutes(3.) }) command |> ignore
+        )
 
-        Target "Cluster" <| fun _ -> 
+        Targets.Target("Cluster", fun _ -> 
             let clusterName = getBuildParam "clusterName"
             let clusterVersion = getBuildParam "clusterVersion"
             let testsProjectDirectory = Path.Combine(Path.GetFullPath(Paths.Output("Tests.ClusterLauncher")), "netcoreapp2.1")
@@ -91,62 +155,35 @@ module Main =
                 }) (sprintf "%s %s" (Path.Combine(tempDir, "Tests.ClusterLauncher.dll")) command)
             
             Shell.deleteDir tempDir
+        )
 
-        Target "Release" <| fun _ -> traceHeader (sprintf "Finished Release Build %O" Versioning.CurrentVersion)
+        Targets.Target("Release", fun _ -> traceHeader (sprintf "Finished Release Build %O" Versioning.CurrentVersion))
 
-        Target "NugetPack" Release.NugetPack
+        Targets.Target("NugetPack", Release.NugetPack)
 
-        Target "NugetPackVersioned" Release.NugetPackVersioned
+        Targets.Target("NugetPackVersioned", Release.NugetPackVersioned)
 
-        Target "ValidateArtifacts" Versioning.ValidateArtifacts
+        Targets.Target("ValidateArtifacts", Versioning.ValidateArtifacts)
 
-        Target "GenerateReleaseNotes" Release.GenerateNotes
+        Targets.Target("GenerateReleaseNotes", Release.GenerateNotes)
+        
+        Targets.Target("Build", 
+          [
+            "Clean"; "Version"; "Restore"; "FullBuild"; "Test"; 
+            "InternalizeDependencies"; "InheritDoc"; "Documentation"; 
+          ]
+        )
 
+        Targets.Target("Benchmark", [ "Clean"; "FullBuild"; ])
+        Targets.Target("Canary", [ "Version"; "Release"; "TestNugetPackage";])
+        Targets.Target("Integrate", [ "Clean"; "Restore"; "FullBuild";])
+        Targets.Target("Release", [ 
+           "Build"; "NugetPack"; "NugetPackVersioned"; "ValidateArtifacts"; "GenerateReleaseNotes"
+        ])
+        Targets.Target("Diff", [ "Clean"; ])
+        Targets.Target("Cluster", [ "Restore"; "FullBuild" ])
 
-        // Dependencies
-        "Start"
-          =?> ("Clean", Commandline.needsClean )
-          =?> ("Version", hasBuildParam "version")
-          ==> "Restore"
-          =?> ("FullBuild", Commandline.needsFullBuild)
-          =?> ("Test", (not Commandline.skipTests && Commandline.target <> "canary"))
-          =?> ("InternalizeDependencies", (not isMono))
-          ==> "InheritDoc"
-          =?> ("Documentation", (not Commandline.skipDocs))
-          ==> "Build"
+        Targets.RunTargetsAndExit(args)
 
-        "Start"
-          =?> ("Clean", Commandline.needsClean )
-          =?> ("FullBuild", Commandline.needsFullBuild)
-          ==> "Benchmark"
-
-        "Version"
-          ==> "Release"
-          =?> ("TestNugetPackage", (not isMono && not Commandline.skipTests))
-          ==> "Canary"
-
-        "Start"
-          =?> ("Clean", Commandline.needsClean )
-          ==> "Restore"
-          =?> ("FullBuild", Commandline.needsFullBuild)
-          ==> "Integrate"
-
-        "Build"
-          ==> "NugetPack"
-          =?> ("NugetPackVersioned", Commandline.target = "canary")
-          ==> "ValidateArtifacts"
-          =?> ("GenerateReleaseNotes", Commandline.target <> "canary")
-          ==> "Release"
-          
-        "Touch"
-        "Start"
-          ==> "Clean"
-          ==> "Diff"
-          
-        "Start"
-          ==> "Restore"
-          ==> "FullBuild"
-          ==> "Cluster"
-          
         0
 
