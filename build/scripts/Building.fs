b779490d5faaf2c87cef02b319310e12504a23e0
Martijn Laarman <Mpdreamz@gmail.com>
2019-04-03 12:33:48 +0200

    Refactor/build scripts (#3636)
    * Move to BullsEye
    
    (cherry picked from commit 90f82a30ff4519b92cc4f499e9a2e22252820a51)
    
    * scripts now a netcoreapp project and seems to compile, used hacks to get typeproviders happy
    
    (cherry picked from commit bfbb6e2dc67f662e9570d32487cab7093532672b)
    
    * created combined tasks
    
    (cherry picked from commit 3022fef3eb896d3052e998364572f06c801a7c00)
    
    * played around with optional targets
    
    (cherry picked from commit ec6dc726257d3dc3be2a6e6ead00462a12c51ba3)
    
    * Versioning refactor and args are typed and not passed around with getBuildParam
    
    (cherry picked from commit a815714e8f1a0f8727df9f478bc964842aee26e4)
    
    * Remove FAKE4, selectively depend on handy FAKE5 modules
    
    (cherry picked from commit 0dfc3d41bc2e3444a3207ae376d9a950b19539a4)
    
    * update proc and prefer exec over start
    
    (cherry picked from commit c5eff444f8d618ed817f560a28c0b4ffdde8a438)
    
    * Differ and Cluster to separate files
    
    (cherry picked from commit 30c88fdc7ecf2f21e4a564283ee02328410943cf)
    
    * Differ and Cluster separate, move remaining paket items over
    
    (cherry picked from commit c379a4b3cbcedc807291397c7e2e9096d5fb93a1)
    
    * remove paket, now relying on the tools as DotNetCliTools
    
    (cherry picked from commit 4f46084bc9b582b9614ee421ae0af7b2b22c336b)
    
    * update cluster command, handover from yaml to environment config now more explicit
    
    (cherry picked from commit d3ad1c887f2147b95b56e6e17116ba43fb80b11a)
    
    * Pull ILRepack in and make sure its avabilable in ouput path thanks to new GeneratePathProperty
    
    (cherry picked from commit 189b2562623cfba5f92427d0cc18c3e171a7ad13)
    
    * nuget.exe now pulled in though NuGet
    
    (cherry picked from commit bcc314871bed1b025554ed64dc51414a154cdda0)
    
    * try to remove sn.exe and resolve it from SDK
    
    (cherry picked from commit 1254c8a0815b3296fd890fb21190df713f68b9fa)
    
    * DocGenerator run by referencing dll
    
    (cherry picked from commit 78f50b967513fd4fa9d7416e475de4052f851dae)
    
    * make sure canary target works locally again
    
    (cherry picked from commit f8c110d9b5dd5e1084ab683ad033a739be6f76e5)
    
    * make sure release command works
    
    (cherry picked from commit ffaa06b50cd2ff0716a5469ddb05db4a04f854e1)
    
    * remove sn.exe from source control
    
    (cherry picked from commit 9c909d08ca2aeb2994ef832953472730f73b0154)
    
    * fix IDE build on linux
    
    * update build.sh to use our build scripts location
    
    * update latest mono and sdk on build server, local build works so wondering if updating CI does the trick
    
    * update to latest mono and sdk fixed typeproviders yay, however took 16 minutes to pull down on azure pipelines. Rewrote global json handling without type providers
    
    * update path to global.json, json.net does not like private record types
    
    * Tests.Benchmarking needs conditional net461 TFM
    
    * update casing issues in the build
    
    * Working diff target
    
    (cherry picked from commit 5e858c5275672c55d6e41db3405dbc3794b42639)

---
+++ build/scripts/Building.fs
@@ -0,0 +1,44 @@
+﻿namespace Scripts
+
+open System 
+open System.IO
+
+open FSharp.Data
+
+open Paths
+open Projects
+open Tooling
+open Versioning
+open Fake.Core
+open Fake.IO
+open Commandline
+
+module Build =
+
+    let Restore() = DotNet.Exec ["restore"; Solution; ] |> ignore
+        
+    let Compile args (ArtifactsVersion(version)) = 
+        let sourceLink = if args.DoSourceLink then "1" else ""
+        let props = 
+            [ 
+                "CurrentVersion", (version.Full.ToString());
+                "CurrentAssemblyVersion", (version.Assembly.ToString());
+                "CurrentAssemblyFileVersion", (version.AssemblyFile.ToString());
+                "DoSourceLink", sourceLink;
+                "FakeBuild", "1";
+                "OutputPathBaseDir", Path.GetFullPath Paths.BuildOutput;
+            ] 
+            |> List.map (fun (p,v) -> sprintf "%s=%s" p v)
+            |> String.concat ";"
+            |> sprintf "/property:%s"
+            
+        DotNet.Exec ["build"; Solution; "-c"; "Release"; props] |> ignore
+
+
+    let Clean () =
+        printfn "Cleaning known output folders"
+        Shell.cleanDir Paths.BuildOutput
+        DotNet.Exec ["clean"; Solution; "-c"; "Release"] |> ignore 
+        DotNetProject.All |> Seq.iter(fun p -> Shell.cleanDir (Paths.BinFolder p.Name))
+        
+         
--- build/scripts/Documentation.fs
+++ build/scripts/Documentation.fs
@@ -1,13 +1,6 @@
-﻿#I @"../../packages/build/FAKE/tools"
-#r @"FakeLib.dll"
-#nowarn "0044" //TODO sort out FAKE 5
-
-#load @"Paths.fsx"
-#load @"Commandline.fsx"
+﻿namespace Scripts
 
 open System.IO
-
-open Fake 
 
 open Paths
 open Projects
@@ -15,22 +8,19 @@ open Commandline
 
 module Documentation = 
 
-    let Generate() = 
+    let Generate args = 
         let docGenerator = PrivateProject(DocGenerator)
         let path = Paths.ProjectOutputFolder docGenerator DotNetFramework.NetCoreApp2_1
-        let generator = sprintf "%s.dll %s" docGenerator.Name Commandline.docsBranch
+        let generator = sprintf "%s.dll" docGenerator.Name 
         
-        DotNetCli.RunCommand(fun p ->
-            { p with
-                WorkingDir = path
-            }) generator
+        Tooling.DotNet.ExecIn path [generator; args.DocsBranch] |> ignore
 
     // TODO: hook documentation validation into the process
     let Validate() = 
         let elasticDocsDir = "../elasticsearch-docs"
-        if (directoryExists elasticDocsDir = false) then
-            let fullPath = combinePaths currentDirectory elasticDocsDir |> Path.GetFullPath
-            traceFAKE "No elasticsearch docs repo found at %s. Cannot validate generated documentation" fullPath
+        if (Directory.Exists elasticDocsDir = false) then
+            let fullPath = Path.Combine(Directory.GetCurrentDirectory(), elasticDocsDir) |> Path.GetFullPath
+            printfn "No elasticsearch docs repo found at %s. Cannot validate generated documentation" fullPath
         //else
             // Needs to be able to run the build_docs.pl perl script. The best options on Windows for this
             // are Cygwin or Linux Bash for Windows.
