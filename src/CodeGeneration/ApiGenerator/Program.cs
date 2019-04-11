2edffd312f569536d6cb39ad49bd6e6b12df3c5e
Mpdreamz <Mpdreamz@gmail.com>
2019-04-11 14:28:04 +0200

    ask to generate code again as well, allows you to only download the specs without regenerating all the code

--- src/CodeGeneration/ApiGenerator/Program.cs
+++ src/CodeGeneration/ApiGenerator/Program.cs
@@ -10,15 +10,15 @@ namespace ApiGenerator
 		private static void Main(string[] args)
 		{
 			var redownloadCoreSpecification = false;
+			var generateCode = false;
 			var downloadBranch = DownloadBranch;
 
 			var answer = "invalid";
 			while (answer != "y" && answer != "n" && answer != "")
 			{
 				Console.Write("Download online rest specifications? [Y/N] (default N): ");
-				//answer = Console.ReadLine()?.Trim().ToLowerInvariant();
-				answer = "n";
-				redownloadCoreSpecification = answer == "y";
+				answer = Console.ReadLine()?.Trim().ToLowerInvariant();
+				redownloadCoreSpecification = answer == "y" || answer == "";
 			}
 
 			if (redownloadCoreSpecification)
@@ -40,7 +40,15 @@ namespace ApiGenerator
 			if (redownloadCoreSpecification)
 				RestSpecDownloader.Download(downloadBranch);
 
-			ApiGenerator.Generate(downloadBranch, "Core", "XPack");
+			answer = "invalid";
+			while (answer != "y" && answer != "n" && answer != "")
+			{
+				Console.Write("Generate code from the specification files on disk? [Y/N] (default Y): ");
+				answer = Console.ReadLine()?.Trim().ToLowerInvariant();
+				generateCode = answer == "y" || answer == "";
+			}
+			if (generateCode)
+				ApiGenerator.Generate(downloadBranch, "Core", "XPack");
 		}
 	}
 }
