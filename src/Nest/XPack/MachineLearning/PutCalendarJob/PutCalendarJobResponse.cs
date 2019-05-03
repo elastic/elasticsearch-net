57ebae31b9b23fc8ba9e861f74eb6fea7051fbe2
Russ Cam <russ.cam@elastic.co>
2019-05-03 10:50:43 +1000

    Fix serialization attributes

--- src/Nest/XPack/MachineLearning/PutCalendarJob/PutCalendarJobResponse.cs
+++ src/Nest/XPack/MachineLearning/PutCalendarJob/PutCalendarJobResponse.cs
@@ -9,10 +9,10 @@ namespace Nest
 		[DataMember(Name = "calendar_id")]
 		public string CalendarId { get; internal set; }
 
-		[DataMember(Name = "job_ids")]
+		[DataMember(Name = "description")]
 		public string Description { get; internal set; }
 
-		[DataMember(Name = "description")]
+		[DataMember(Name = "job_ids")]
 		public IReadOnlyCollection<string> JobIds { get; internal set; } = EmptyReadOnly<string>.Collection;
 	}
 }
