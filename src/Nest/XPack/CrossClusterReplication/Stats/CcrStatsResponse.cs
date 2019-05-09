8d51f6ad93792f36bf0ceeb762cd82964202ef18
Russ Cam <russ.cam@elastic.co>
2019-05-09 13:45:26 +1000

    Add time since last auto follow fetch to auto follow stats (#3727)
    Add time since last auto follow fetch to auto follow stats (#3727)

--- src/Nest/XPack/CrossClusterReplication/Stats/CcrStatsResponse.cs
+++ src/Nest/XPack/CrossClusterReplication/Stats/CcrStatsResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Elasticsearch.Net;
 
@@ -15,7 +16,6 @@ namespace Nest
 		public CcrFollowStats FollowStats { get; internal set; }
 	}
 
-
 	public class CcrFollowStats
 	{
 		/// <inheritdoc cref="FollowIndexStatsResponse.Indices" />
@@ -25,28 +25,55 @@ namespace Nest
 
 	public class CcrAutoFollowStats
 	{
-
 		/// <summary>
-		/// the number of indices that the auto-follow coordinator failed to automatically follow; the causes of recent failures are
-		/// captured in the logs of the elected master node, and in the auto_follow_stats.recent_auto_follow_errors field
+		/// The number of indices that the auto-follow coordinator failed to automatically follow; the causes of recent failures are
+		/// captured in the logs of the elected master node, and in the <see cref="RecentAutoFollowErrors"/> field.
 		/// </summary>
 		[DataMember(Name = "number_of_failed_follow_indices")]
 		public long NumberOfFailedFollowIndices { get; internal set; }
 
 		/// <summary>
-		/// the number of times that the auto-follow coordinator failed to retrieve the cluster state from a
-		/// remote cluster registered in a collection of auto-follow patterns
+		/// The number of times that the auto-follow coordinator failed to retrieve the cluster state from a
+		/// remote cluster registered in a collection of auto-follow patterns.
 		/// </summary>
 		[DataMember(Name = "number_of_failed_remote_cluster_state_requests")]
 		public long NumberOfFailedRemoteClusterStateRequests { get; internal set; }
 
-		/// <summary> the number of indices that the auto-follow coordinator successfully followed </summary>
+		/// <summary>The number of indices that the auto-follow coordinator successfully followed.</summary>
 		[DataMember(Name = "number_of_successful_follow_indices")]
 		public long NumberOfSuccessfulFollowIndices { get; internal set; }
 
-		/// <summary> an array of objects representing failures by the auto-follow coordinator </summary>
+		/// <summary>An array of objects representing failures by the auto-follow coordinator.</summary>
 		[DataMember(Name = "recent_auto_follow_errors")]
 		public IReadOnlyCollection<ErrorCause> RecentAutoFollowErrors { get; internal set; } = EmptyReadOnly<ErrorCause>.Collection;
 
+		/// <summary>
+		///  An array of auto followed clusters.
+		/// </summary>
+		[DataMember(Name = "auto_followed_clusters")]
+		public IReadOnlyCollection< AutoFollowedCluster> AutoFollowedClusters { get; internal set; } = EmptyReadOnly<AutoFollowedCluster>.Collection;
+
+	}
+
+	public class AutoFollowedCluster
+	{
+		/// <summary>
+		/// The cluster name.
+		/// </summary>
+		[DataMember(Name = "cluster_name")]
+		public string ClusterName { get; internal set; }
+
+		/// <summary>
+		/// Time since last check.
+		/// </summary>
+		[DataMember(Name = "time_since_last_check_millis")]
+		[JsonFormatter(typeof(DateTimeOffsetEpochMillisecondsFormatter))]
+		public DateTimeOffset TimeSinceLastCheck { get; internal set; }
+
+		/// <summary>
+		/// Last seen metadata version.
+		/// </summary>
+		[DataMember(Name = "last_seen_metadata_version")]
+		public long LastSeenMetadataVersion { get; internal set; }
 	}
 }
