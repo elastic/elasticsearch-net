63a55a61cb25cc8aadb779779a6b4cdc72a2f1bf
Russ Cam <russ.cam@elastic.co>
2019-05-09 16:34:47 +1000

    Patch in ctors and methods needed for binary compatibility
    This commit patches in ctors and methods that are removed between 6.6.0 and 6.7.0 rest api specs, and marks then as obsolete

--- src/Nest/XPack/Security/User/EnableUser/EnableUserRequest.cs
+++ src/Nest/XPack/Security/User/EnableUser/EnableUserRequest.cs
@@ -1,9 +1,19 @@
-﻿namespace Nest
+﻿using System;
+
+namespace Nest
 {
 	public partial interface IEnableUserRequest { }
 
 	public partial class EnableUserRequest { }
 
 	[DescriptorFor("XpackSecurityEnableUser")]
-	public partial class EnableUserDescriptor { }
+	public partial class EnableUserDescriptor
+	{
+		[Obsolete("Use the constructor that accepts Name. Will be removed in the next major release")]
+		public EnableUserDescriptor() { }
+
+		[Obsolete("Use the constructor that accepts Name. Will be removed in the next major release")]
+		public EnableUserDescriptor Username(Name username) =>
+			Assign(username, (a , v) => a.RouteValues.Required("username", username));
+	}
 }
