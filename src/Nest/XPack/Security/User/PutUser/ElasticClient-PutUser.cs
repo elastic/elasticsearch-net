370e2d2b5b9c12b3995239d833dae21c264fcc47
Russ Cam <russ.cam@elastic.co>
2019-05-09 13:42:58 +1000

    Add password_hash parameter to PutUser (#3723)
    Add password_hash parameter to PutUser (#3723)

--- src/Nest/XPack/Security/User/PutUser/ElasticClient-PutUser.cs
+++ src/Nest/XPack/Security/User/PutUser/ElasticClient-PutUser.cs
@@ -7,39 +7,41 @@ namespace Nest
 {
 	public partial interface IElasticClient
 	{
-		/// <inheritdoc />
+		/// <summary>
+		/// Adds and updates users in the native realm. These users are commonly referred to as native users.
+		/// </summary>
 		PutUserResponse PutUser(Name username, Func<PutUserDescriptor, IPutUserRequest> selector = null);
 
-		/// <inheritdoc />
+		/// <inheritdoc cref="PutUser(Nest.Name,System.Func{Nest.PutUserDescriptor,Nest.IPutUserRequest})" />
 		PutUserResponse PutUser(IPutUserRequest request);
 
-		/// <inheritdoc />
+		/// <inheritdoc cref="PutUser(Nest.Name,System.Func{Nest.PutUserDescriptor,Nest.IPutUserRequest})" />
 		Task<PutUserResponse> PutUserAsync(Name username, Func<PutUserDescriptor, IPutUserRequest> selector = null,
 			CancellationToken ct = default
 		);
 
-		/// <inheritdoc />
+		/// <inheritdoc cref="PutUser(Nest.Name,System.Func{Nest.PutUserDescriptor,Nest.IPutUserRequest})" />
 		Task<PutUserResponse> PutUserAsync(IPutUserRequest request, CancellationToken ct = default);
 	}
 
 	public partial class ElasticClient
 	{
-		/// <inheritdoc />
+		/// <inheritdoc cref="PutUser(Nest.Name,System.Func{Nest.PutUserDescriptor,Nest.IPutUserRequest})" />
 		public PutUserResponse PutUser(Name username, Func<PutUserDescriptor, IPutUserRequest> selector = null) =>
 			PutUser(selector.InvokeOrDefault(new PutUserDescriptor(username)));
 
-		/// <inheritdoc />
+		/// <inheritdoc cref="PutUser(Nest.Name,System.Func{Nest.PutUserDescriptor,Nest.IPutUserRequest})" />
 		public PutUserResponse PutUser(IPutUserRequest request) =>
 			DoRequest<IPutUserRequest, PutUserResponse>(request, request.RequestParameters);
 
-		/// <inheritdoc />
+		/// <inheritdoc cref="PutUser(Nest.Name,System.Func{Nest.PutUserDescriptor,Nest.IPutUserRequest})" />
 		public Task<PutUserResponse> PutUserAsync(
 			Name username,
 			Func<PutUserDescriptor, IPutUserRequest> selector = null,
 			CancellationToken ct = default
 		) => PutUserAsync(selector.InvokeOrDefault(new PutUserDescriptor(username)), ct);
 
-		/// <inheritdoc />
+		/// <inheritdoc cref="PutUser(Nest.Name,System.Func{Nest.PutUserDescriptor,Nest.IPutUserRequest})" />
 		public Task<PutUserResponse> PutUserAsync(IPutUserRequest request, CancellationToken ct = default) =>
 			DoRequestAsync<IPutUserRequest, PutUserResponse>
 				(request, request.RequestParameters, ct);
