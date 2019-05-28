4de53eb2887189bda1693089891148b56ad48470
Stuart Cam <stuart.cam@elastic.co>
2019-05-28 18:26:18 +1000

    Use a custom convertor for GeoOrientation to tolerate alternative options in Elasticsearch

--- src/Nest/Mapping/Types/Geo/GeoShape/GeoOrientation.cs
+++ src/Nest/Mapping/Types/Geo/GeoShape/GeoOrientation.cs
@@ -1,16 +1,45 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace Nest
 {
-	[JsonConverter(typeof(StringEnumConverter))]
+	[JsonConverter(typeof(GeoOrientationConverter))]
 	public enum GeoOrientation
 	{
-		[EnumMember(Value = "cw")]
 		ClockWise,
-
-		[EnumMember(Value = "ccw")]
 		CounterClockWise
+	}
+
+	public class GeoOrientationConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			var geoOrientation = (GeoOrientation)value;
+			switch (geoOrientation)
+			{
+				case GeoOrientation.ClockWise:
+					writer.WriteValue("cw");
+					break;
+				case GeoOrientation.CounterClockWise:
+					writer.WriteValue("ccw");
+					break;
+			}
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			var enumString = (string)reader.Value;
+			switch (enumString.ToLower())
+			{
+				case "left":
+				case "cw":
+				case "clockwise":
+					return GeoOrientation.ClockWise;
+			}
+			// Default, complies with the OGC standard
+			return GeoOrientation.CounterClockWise;
+		}
+
+		public override bool CanConvert(Type objectType) => true;
 	}
 }
