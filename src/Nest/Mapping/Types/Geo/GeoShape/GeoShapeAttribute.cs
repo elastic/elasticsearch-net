a15f3b8b399808020fd3b5293c267d03bd95123f
Martijn Laarman <Mpdreamz@gmail.com>
2019-05-31 15:17:57 +0200

    fix obsolete warning from forward port of #3770
    (cherry picked from commit 7c4996f8d889791d3cf16eb17a5d4f42310f67db)

--- src/Nest/Mapping/Types/Geo/GeoShape/GeoShapeAttribute.cs
+++ src/Nest/Mapping/Types/Geo/GeoShape/GeoShapeAttribute.cs
@@ -4,15 +4,13 @@
 	public class GeoShapeAttribute : ElasticsearchDocValuesPropertyAttributeBase, IGeoShapeProperty
 	{
 		public GeoShapeAttribute() : base(FieldType.GeoShape) { }
-
-		/// <inheritdoc cref="IGeoShapeProperty.DistanceErrorPercentage" />
-		public double DistanceErrorPercentage
-		{
-			get => (Self.Precision != null) | (Self.TreeLevels != null)
-				? Self.DistanceErrorPercentage.GetValueOrDefault(0)
-				: Self.DistanceErrorPercentage.GetValueOrDefault(0.025);
-			set => Self.DistanceErrorPercentage = value;
-		}
+		
+		bool? IGeoShapeProperty.IgnoreMalformed { get; set; }
+		bool? IGeoShapeProperty.IgnoreZValue { get; set; }
+		GeoOrientation? IGeoShapeProperty.Orientation { get; set; }
+		private IGeoShapeProperty Self => this;
+		GeoStrategy? IGeoShapeProperty.Strategy { get; set; }
+		bool? IGeoShapeProperty.Coerce { get; set; }
 
 		/// <inheritdoc cref="IGeoShapeProperty.IgnoreMalformed" />
 		public bool IgnoreMalformed
@@ -35,32 +33,11 @@
 			set => Self.Orientation = value;
 		}
 
-		/// <inheritdoc cref="IGeoShapeProperty.PointsOnly" />
-		public bool PointsOnly
-		{
-			get => Self.PointsOnly.GetValueOrDefault(false);
-			set => Self.PointsOnly = value;
-		}
-
 		/// <inheritdoc cref="IGeoShapeProperty.Strategy" />
 		public GeoStrategy Strategy
 		{
 			get => Self.Strategy.GetValueOrDefault(GeoStrategy.Recursive);
 			set => Self.Strategy = value;
-		}
-
-		/// <inheritdoc cref="IGeoShapeProperty.Tree" />
-		public GeoTree Tree
-		{
-			get => Self.Tree.GetValueOrDefault(GeoTree.Geohash);
-			set => Self.Tree = value;
-		}
-
-		/// <inheritdoc cref="IGeoShapeProperty.TreeLevels" />
-		public int TreeLevels
-		{
-			get => Self.TreeLevels.GetValueOrDefault(50);
-			set => Self.TreeLevels = value;
 		}
 
 		/// <inheritdoc cref="IGeoShapeProperty.Coerce" />
@@ -70,18 +47,5 @@
 			set => Self.Coerce = value;
 		}
 
-		double? IGeoShapeProperty.DistanceErrorPercentage { get; set; }
-		bool? IGeoShapeProperty.IgnoreMalformed { get; set; }
-		bool? IGeoShapeProperty.IgnoreZValue { get; set; }
-		GeoOrientation? IGeoShapeProperty.Orientation { get; set; }
-		bool? IGeoShapeProperty.PointsOnly { get; set; }
-		Distance IGeoShapeProperty.Precision { get; set; }
-		private IGeoShapeProperty Self => this;
-		GeoStrategy? IGeoShapeProperty.Strategy { get; set; }
-
-		GeoTree? IGeoShapeProperty.Tree { get; set; }
-		int? IGeoShapeProperty.TreeLevels { get; set; }
-
-		bool? IGeoShapeProperty.Coerce { get; set; }
 	}
 }
--- src/Tests/Tests/Mapping/Types/Geo/GeoShape/GeoShapeAttributeTests.cs
+++ src/Tests/Tests/Mapping/Types/Geo/GeoShape/GeoShapeAttributeTests.cs
@@ -5,12 +5,8 @@ namespace Tests.Mapping.Types.Geo.GeoShape
 	public class GeoShapeTest
 	{
 		[GeoShape(
-			Tree = GeoTree.Quadtree,
 			Orientation = GeoOrientation.ClockWise,
 			Strategy = GeoStrategy.Recursive,
-			TreeLevels = 3,
-			PointsOnly = true,
-			DistanceErrorPercentage = 1.0,
 			Coerce = true)]
 		public object Full { get; set; }
 
@@ -27,12 +23,8 @@ namespace Tests.Mapping.Types.Geo.GeoShape
 				full = new
 				{
 					type = "geo_shape",
-					tree = "quadtree",
 					orientation = "cw",
 					strategy = "recursive",
-					tree_levels = 3,
-					points_only = true,
-					distance_error_pct = 1.0,
 					coerce = true
 				},
 				minimal = new
