eb055a24a5c053980a8548161d1e7d9f2681be89
Martijn Laarman <Mpdreamz@gmail.com>
2019-06-03 18:02:02 +0200

    remove unused namespaces

--- src/Nest/ElasticClient.cs
+++ src/Nest/ElasticClient.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Elasticsearch.Net;
