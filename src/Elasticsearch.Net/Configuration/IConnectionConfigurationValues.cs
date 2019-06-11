2a9fac4786661cf73e2e2e00adda7dbf4d5397fb
Russ Cam <russ.cam@elastic.co>
2019-06-11 12:18:14 +1000

    Remove unneeded using directive

--- src/Elasticsearch.Net/Configuration/IConnectionConfigurationValues.cs
+++ src/Elasticsearch.Net/Configuration/IConnectionConfigurationValues.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Net.Security;
-using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
