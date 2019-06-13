1414aab5c261e897fd1acd110ad7e25a85f2cc33
Stuart Cam <stuart.cam@elastic.co>
2019-06-13 20:52:00 +1000

    Make TResult generic argument on PreviewDatafeed contrained to class

--- src/Nest/XPack/MachineLearning/PreviewDatafeed/PreviewDatafeedResponse.cs
+++ src/Nest/XPack/MachineLearning/PreviewDatafeed/PreviewDatafeedResponse.cs
@@ -2,13 +2,12 @@
 
 namespace Nest
 {
-	//TODO T can be anything?
-	public interface IPreviewDatafeedResponse<out TResult> : IResponse
+	public interface IPreviewDatafeedResponse<out TResult> : IResponse where TResult : class
 	{
 		IReadOnlyCollection<TResult> Data { get; }
 	}
 
-	public class PreviewDatafeedResponse<TResult> : ResponseBase, IPreviewDatafeedResponse<TResult>
+	public class PreviewDatafeedResponse<TResult> : ResponseBase, IPreviewDatafeedResponse<TResult> where TResult : class
 	{
 		public IReadOnlyCollection<TResult> Data { get; internal set; } = EmptyReadOnly<TResult>.Collection;
 	}
