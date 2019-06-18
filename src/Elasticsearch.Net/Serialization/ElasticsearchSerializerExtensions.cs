5f10120eee4a8b954337aee6ba0ae873d83d0476
Mpdreamz <Mpdreamz@gmail.com>
2019-06-18 11:25:58 +0200

    XmlDocs for the json serializer extensions

--- src/Elasticsearch.Net/Serialization/ElasticsearchSerializerExtensions.cs
+++ src/Elasticsearch.Net/Serialization/ElasticsearchSerializerExtensions.cs
@@ -1,16 +1,25 @@
 using Elasticsearch.Net.Extensions;
-using Elasticsearch.Net.Utf8Json;
 
 namespace Elasticsearch.Net
 {
 	public static class ElasticsearchSerializerExtensions
 	{
+		/// <summary>
+		/// Extension method that serializes an instance of <typeparamref name="T"/> to a byte array.
+		/// </summary>
 		public static byte[] SerializeToBytes<T>(
 			this IElasticsearchSerializer serializer,
 			T data,
 			SerializationFormatting formatting = SerializationFormatting.None) =>
 			SerializeToBytes(serializer, data, RecyclableMemoryStreamFactory.Default, formatting);
 
+		/// <summary>
+		/// Extension method that serializes an instance of <typeparamref name="T"/> to a byte array.
+		/// </summary>
+		/// <param name="memoryStreamFactory">
+		/// A factory yielding MemoryStream instances, defaults to <see cref="RecyclableMemoryStreamFactory"/>
+		/// that yields memory streams backed by pooled byte arrays.
+		/// </param>
 		public static byte[] SerializeToBytes<T>(
 			this IElasticsearchSerializer serializer,
 			T data,
@@ -26,12 +35,22 @@ namespace Elasticsearch.Net
 			}
 		}
 
+		/// <summary>
+		/// Extension method that serializes an instance of <typeparamref name="T"/> to a string.
+		/// </summary>
 		public static string SerializeToString<T>(
 			this IElasticsearchSerializer serializer,
 			T data,
 			SerializationFormatting formatting = SerializationFormatting.None) =>
 			SerializeToString(serializer, data, RecyclableMemoryStreamFactory.Default, formatting);
 
+		/// <summary>
+		/// Extension method that serializes an instance of <typeparamref name="T"/> to a string.
+		/// </summary>
+		/// <param name="memoryStreamFactory">
+		/// A factory yielding MemoryStream instances, defaults to <see cref="RecyclableMemoryStreamFactory"/>
+		/// that yields memory streams backed by pooled byte arrays.
+		/// </param>
 		public static string SerializeToString<T>(
 			this IElasticsearchSerializer serializer,
 			T data,
