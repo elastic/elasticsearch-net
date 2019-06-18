cf9167fa47c95f4c63524abf0d1f34f3e7e996f0
Mpdreamz <Mpdreamz@gmail.com>
2019-06-18 12:59:17 +0200

    removed todo to rename IProxyRequest, no longer only deals with sources

--- src/Nest/CommonAbstractions/Request/IProxyRequest.cs
+++ src/Nest/CommonAbstractions/Request/IProxyRequest.cs
@@ -3,7 +3,6 @@ using Elasticsearch.Net;
 
 namespace Nest
 {
-	//TODO: since this only deals with source serializer rename this to ISourceProxyRequest
 	/// <summary> A request that that does not necessarily (de)serializes itself </summary>
 	public interface IProxyRequest : IRequest
 	{
