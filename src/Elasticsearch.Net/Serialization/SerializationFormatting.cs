469c36272a53d60dac835f0cbee6ca930fca316a
Martijn Laarman <Mpdreamz@gmail.com>
2019-06-19 05:20:41 +0200

    Add documentation for serialization formatting (#3826)
    * Documented that formatting is a hint and added some xmldocs on IElasticsearchSerializer
    * XmlDocs for the json serializer extensions

--- src/Elasticsearch.Net/Serialization/SerializationFormatting.cs
+++ src/Elasticsearch.Net/Serialization/SerializationFormatting.cs
@@ -1,5 +1,9 @@
 namespace Elasticsearch.Net
 {
+	/// <summary>
+	/// A hint to <see cref="IElasticsearchSerializer"/> how to format the json.
+	/// Implementation of <see cref="IElasticsearchSerializer"/> might choose to ignore this hint though.
+	/// </summary>
 	public enum SerializationFormatting
 	{
 		None,
