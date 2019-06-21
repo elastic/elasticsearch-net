c8eadf1f7dd906e1cb429021c36c3ae0bf3cda0b
Russ Cam <russ.cam@elastic.co>
2019-06-21 20:34:42 +1000

    Add index.percolator.map_unmapped_fields_as_text index setting (#3850)
    Relates: https://github.com/elastic/elasticsearch/pull/28060

--- src/Nest/IndexModules/IndexSettings/Settings/FixedIndexSettings.cs
+++ src/Nest/IndexModules/IndexSettings/Settings/FixedIndexSettings.cs
@@ -9,5 +9,12 @@ namespace Nest
 		public const string NumberOfRoutingShards = "index.number_of_routing_shards";
 		public const string NumberOfShards = "index.number_of_shards";
 		public const string RoutingPartitionSize = "index.routing_partition_size";
+
+		/// <summary>
+		/// If a field referred to in a percolator query does not exist,
+		/// it will be handled as a default text field so that adding the percolator query doesn't fail.
+		/// Defaults to <c>false</c>
+		/// </summary>
+		public const string PercolatorMapUnmappedFieldsAsText = "index.percolator.map_unmapped_fields_as_text";
 	}
 }
