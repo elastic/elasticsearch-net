ab9f6f1879a4d2ea78e0ee4e120f74695bbb88a8
Martijn Laarman <Mpdreamz@gmail.com>
2019-06-25 21:59:39 +0200

    GetAliasResponse should take base.IsValid into account before validation Indices see #3828

--- src/Nest/Indices/AliasManagement/GetAlias/GetAliasResponse.cs
+++ src/Nest/Indices/AliasManagement/GetAlias/GetAliasResponse.cs
@@ -11,7 +11,7 @@ namespace Nest
 		[IgnoreDataMember]
 		public IReadOnlyDictionary<IndexName, IndexAliases> Indices => Self.BackingDictionary;
 
-		public override bool IsValid => Indices.Count > 0;
+		public override bool IsValid => base.IsValid || Indices.Count > 0;
 	}
 
 	public class IndexAliases
