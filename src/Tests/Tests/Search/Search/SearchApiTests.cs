10125f79ed50096175274bdcf24bf03ffb535450
Russ Cam <russ.cam@elastic.co>
2019-06-27 19:26:48 +1000

    Remove use_field_mapping from integration test (#3885)
    * Remove use_field_mapping from integration test
    
    Relates: https://github.com/elastic/elasticsearch/pull/30831
    
    * Update expected serialized JSON

--- src/Tests/Tests/Search/Search/SearchApiTests.cs
+++ src/Tests/Tests/Search/Search/SearchApiTests.cs
@@ -277,11 +277,7 @@ namespace Tests.Search.Search
 			},
 			docvalue_fields = new object[]
 			{
-				new
-				{
-					field = "name",
-					format = "use_field_mapping"
-				},
+				"name",
 				new
 				{
 					field = "lastActivity",
@@ -305,7 +301,7 @@ namespace Tests.Search.Search
 				.Term(p => p.State, StateOfBeing.Stable)
 			)
 			.DocValueFields(fs => fs
-				.Field(p => p.Name, format: "use_field_mapping")
+				.Field(p => p.Name)
 				.Field(p => p.LastActivity, format: "weekyear")
 			);
 
@@ -323,7 +319,7 @@ namespace Tests.Search.Search
 				Field = "state",
 				Value = "Stable"
 			}),
-			DocValueFields = Infer.Field<Project>(p => p.Name, format: "use_field_mapping")
+			DocValueFields = Infer.Field<Project>(p => p.Name)
 				.And<Project>(p => p.LastActivity, format: "weekyear")
 		};
 
