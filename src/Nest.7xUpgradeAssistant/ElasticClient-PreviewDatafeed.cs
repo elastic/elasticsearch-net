9b5a34ca5922ef1c205df310aa8f672be2f93522
Martijn Laarman <Mpdreamz@gmail.com>
2019-06-27 09:03:16 +0200

    move previewdatafeed to TDOcument: class

--- src/Nest.7xUpgradeAssistant/ElasticClient-PreviewDatafeed.cs
+++ src/Nest.7xUpgradeAssistant/ElasticClient-PreviewDatafeed.cs
@@ -7,25 +7,23 @@ namespace Nest
 	public static partial class ElasticClientExtensions
 	{
 		[Obsolete("Moved to client.MachineLearning.PreviewDatafeed(), please update this usage.")]
-		public static PreviewDatafeedResponse<T> PreviewDatafeed<T>(this IElasticClient client, Id datafeedId,
+		public static PreviewDatafeedResponse<TDocument> PreviewDatafeed<TDocument>(this IElasticClient client, Id datafeedId,
 			Func<PreviewDatafeedDescriptor, IPreviewDatafeedRequest> selector = null
-		)
-			=> client.MachineLearning.PreviewDatafeed<T>(datafeedId, selector);
+		) where TDocument : class => client.MachineLearning.PreviewDatafeed<TDocument>(datafeedId, selector);
 
 		[Obsolete("Moved to client.MachineLearning.PreviewDatafeed(), please update this usage.")]
-		public static PreviewDatafeedResponse<T> PreviewDatafeed<T>(this IElasticClient client, IPreviewDatafeedRequest request)
-			=> client.MachineLearning.PreviewDatafeed<T>(request);
+		public static PreviewDatafeedResponse<TDocument> PreviewDatafeed<TDocument>(this IElasticClient client, IPreviewDatafeedRequest request)
+			where TDocument : class => client.MachineLearning.PreviewDatafeed<TDocument>(request);
 
 		[Obsolete("Moved to client.MachineLearning.PreviewDatafeedAsync(), please update this usage.")]
-		public static Task<PreviewDatafeedResponse<T>> PreviewDatafeedAsync<T>(this IElasticClient client, Id datafeedId,
+		public static Task<PreviewDatafeedResponse<TDocument>> PreviewDatafeedAsync<TDocument>(this IElasticClient client, Id datafeedId,
 			Func<PreviewDatafeedDescriptor, IPreviewDatafeedRequest> selector = null, CancellationToken ct = default
-		)
-			=> client.MachineLearning.PreviewDatafeedAsync<T>(datafeedId, selector, ct);
+		) where TDocument : class => client.MachineLearning.PreviewDatafeedAsync<TDocument>(datafeedId, selector, ct);
 
 		[Obsolete("Moved to client.MachineLearning.PreviewDatafeedAsync(), please update this usage.")]
-		public static Task<PreviewDatafeedResponse<T>> PreviewDatafeedAsync<T>(this IElasticClient client, IPreviewDatafeedRequest request,
+		public static Task<PreviewDatafeedResponse<TDocument>> PreviewDatafeedAsync<TDocument>(this IElasticClient client,
+			IPreviewDatafeedRequest request,
 			CancellationToken ct = default
-		)
-			=> client.MachineLearning.PreviewDatafeedAsync<T>(request, ct);
+		) where TDocument : class => client.MachineLearning.PreviewDatafeedAsync<TDocument>(request, ct);
 	}
 }
