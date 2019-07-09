dd6c997820dd62f460d7d7483a5dcc8b3769cac9
Russ Cam <russ.cam@elastic.co>
2019-07-09 13:58:49 +1000

    Add script_score query support (#3919)
    This commit adds script_score query support to the high level client
    
    Closes #3843
    
    (cherry picked from commit a4741949bd50da2210d7699f0485961cf09fecd8)

---
+++ src/Nest/QueryDsl/Specialized/ScriptScore/ScriptScoreQuery.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Elasticsearch.Net.Utf8Json;
+
+namespace Nest
+{
+	/// <summary>
+	/// A query allowing you to modify the score of documents that are retrieved by a query.
+	/// This can be useful if, for example, a score function is computationally expensive and it is sufficient to
+	/// compute the score on a filtered set of documents.
+	/// </summary>
+	[ReadAs(typeof(ScriptScoreQuery))]
+	[InterfaceDataContract]
+	public interface IScriptScoreQuery : IQuery
+	{
+		/// <summary>
+		/// The query to execute
+		/// </summary>
+		[DataMember(Name = "query")]
+		QueryContainer Query { get; set; }
+
+		/// <summary>
+		/// The script to execute
+		/// </summary>
+		[DataMember(Name = "script")]
+		IScript Script { get; set; }
+	}
+
+	/// <inheritdoc cref="IScriptScoreQuery" />
+	public class ScriptScoreQuery : QueryBase, IScriptScoreQuery
+	{
+		/// <inheritdoc />
+		public QueryContainer Query { get; set; }
+
+		/// <inheritdoc />
+		public IScript Script { get; set; }
+
+		protected override bool Conditionless => IsConditionless(this);
+
+		internal override void InternalWrapInContainer(IQueryContainer c) => c.ScriptScore = this;
+
+		internal static bool IsConditionless(IScriptScoreQuery q)
+		{
+			if (q.Script == null || q.Query.IsConditionless())
+				return true;
+
+			switch (q.Script)
+			{
+				case IInlineScript inlineScript:
+					return inlineScript.Source.IsNullOrEmpty();
+				case IIndexedScript indexedScript:
+					return indexedScript.Id.IsNullOrEmpty();
+			}
+
+			return false;
+		}
+	}
+
+	/// <inheritdoc cref="IScriptScoreQuery" />
+	public class ScriptScoreQueryDescriptor<T>
+		: QueryDescriptorBase<ScriptScoreQueryDescriptor<T>, IScriptScoreQuery>
+			, IScriptScoreQuery where T : class
+	{
+		protected override bool Conditionless => ScriptScoreQuery.IsConditionless(this);
+		QueryContainer IScriptScoreQuery.Query { get; set; }
+
+		IScript IScriptScoreQuery.Script { get; set; }
+
+		/// <inheritdoc cref="IScriptScoreQuery.Query" />
+		public ScriptScoreQueryDescriptor<T> Query(Func<QueryContainerDescriptor<T>, QueryContainer> selector) =>
+			Assign(selector, (a, v) => a.Query = v?.Invoke(new QueryContainerDescriptor<T>()));
+
+		/// <inheritdoc cref="IScriptScoreQuery.Script" />
+		public ScriptScoreQueryDescriptor<T> Script(Func<ScriptDescriptor, IScript> selector) =>
+			Assign(selector, (a, v) => a.Script = v?.Invoke(new ScriptDescriptor()));
+	}
+}
---
+++ src/Tests/Tests/QueryDsl/Specialized/ScriptScore/ScriptScoreQueryUsageTests.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using Nest;
+using Tests.Core.ManagedElasticsearch.Clusters;
+using Tests.Domain;
+using Tests.Framework.EndpointTests.TestState;
+
+namespace Tests.QueryDsl.Specialized.ScriptScore
+{
+	/**
+	* A query allowing you to modify the score of documents that are retrieved by a query.
+	* This can be useful if, for example, a score function is computationally expensive and
+	* it is sufficient to compute the score on a filtered set of documents.
+	*
+	* See the Elasticsearch documentation on {ref_current}/query-dsl-script-score-query.html[script_score query] for more details.
+	*/
+	public class ScriptScoreQueryUsageTests : QueryDslUsageTestsBase
+	{
+		private static readonly string _scriptScoreSource = "decayNumericLinear(params.origin, params.scale, params.offset, params.decay, doc['numberOfCommits'].value)";
+
+		public ScriptScoreQueryUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
+
+		protected override ConditionlessWhen ConditionlessWhen => new ConditionlessWhen<IScriptScoreQuery>(a => a.ScriptScore)
+		{
+			q =>
+			{
+				q.Query = null;
+			},
+			q =>
+			{
+				q.Script = null;
+			},
+			q =>
+			{
+				q.Script = new InlineScript(null);
+			},
+			q =>
+			{
+				q.Script = new InlineScript("");
+			},
+			q =>
+			{
+				q.Script = new IndexedScript(null);
+			},
+			q =>
+			{
+				q.Script = new IndexedScript("");
+			}
+		};
+
+		protected override QueryContainer QueryInitializer => new ScriptScoreQuery
+		{
+			Name = "named_query",
+			Boost = 1.1,
+			Query = new NumericRangeQuery
+			{
+				Field = Infer.Field<Project>(f => f.NumberOfCommits),
+				GreaterThan = 50
+			},
+			Script = new InlineScript(_scriptScoreSource)
+			{
+				Params = new Dictionary<string, object>
+				{
+					{ "origin", 100 },
+					{ "scale", 10 },
+					{ "decay", 0.5 },
+					{ "offset", 0 }
+				}
+			},
+		};
+
+		protected override object QueryJson => new
+		{
+			script_score = new
+			{
+				_name = "named_query",
+				boost = 1.1,
+				query = new
+				{
+					range = new
+					{
+						numberOfCommits = new
+						{
+							gt = 50.0
+						}
+					}
+				},
+				script = new
+				{
+					source = _scriptScoreSource,
+					@params = new
+					{
+						origin = 100,
+						scale = 10,
+						decay = 0.5,
+						offset = 0
+					}
+				}
+			}
+		};
+
+		protected override QueryContainer QueryFluent(QueryContainerDescriptor<Project> q) => q
+			.ScriptScore(sn => sn
+				.Name("named_query")
+				.Boost(1.1)
+				.Query(qq => qq
+					.Range(r => r
+						.Field(f => f.NumberOfCommits)
+						.GreaterThan(50)
+					)
+				)
+				.Script(s => s
+					.Source(_scriptScoreSource)
+					.Params(p => p
+						.Add("origin", 100)
+						.Add("scale", 10)
+						.Add("decay", 0.5)
+						.Add("offset", 0)
+					)
+				)
+			);
+	}
+}
