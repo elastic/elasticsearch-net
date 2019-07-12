d73fbfce79dc236a9a3fae4fb1ae5101b33b111f
Stuart Cam <stuart.cam@elastic.co>
2019-07-12 14:46:13 +1000

    Fix typo and XML comment

--- src/Nest/Aggregations/Bucket/GeoTileGrid/GeoTilePrecision.cs
+++ src/Nest/Aggregations/Bucket/GeoTileGrid/GeoTilePrecision.cs
@@ -14,7 +14,7 @@
 		Precision2 = 2,
 
 		/// <summary>
-		/// Largets Country
+		/// Largest Country
 		/// </summary>
 		Precision3 = 3,
 		Precision4 = 4,
@@ -66,7 +66,9 @@
 		/// </summary>
 		Precision15 = 15,
 
-		// Street
+		/// <summary>
+		/// Street
+		/// </summary>
 		Precision16 = 16,
 
 		/// <summary>
