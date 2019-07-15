7cd8613fa2a738dd56726d6728f0898a9ed9872b
Stuart Cam <stuart.cam@elastic.co>
2019-07-15 14:23:09 +1000

    Implement Freeze and Unfreeze API (#3940)
    This commit implements the index freeze and unfreeze APIs

---
+++ src/Tests/Tests/Indices/IndexManagement/UnfreezeIndex/UnfreezeIndexUrlTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using Nest;
+using Tests.Domain;
+using Tests.Framework.EndpointTests;
+using static Nest.Indices;
+using static Tests.Framework.EndpointTests.UrlTester;
+
+namespace Tests.Indices.IndexManagement.UnfreezeIndex
+{
+	public class UnfreezeIndexUrlTests
+	{
+		[U] public async Task Urls()
+		{
+			var index = "project";
+			await POST($"/{index}/_unfreeze")
+					.Fluent(c => c.Indices.Unfreeze(index, s => s))
+					.Request(c => c.Indices.Unfreeze(new UnfreezeIndexRequest(index)))
+					.FluentAsync(c => c.Indices.UnfreezeAsync(index))
+					.RequestAsync(c => c.Indices.UnfreezeAsync(new UnfreezeIndexRequest(index)))
+				;
+		}
+	}
+}
