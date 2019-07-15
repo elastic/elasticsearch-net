47167fa61a2a917caf756f0ab2be3d007a078f87
Stuart Cam <stuart.cam@elastic.co>
2019-07-15 13:56:06 +1000

    Additions to Invalidate Token API (#3937)
    * Implement additional fields on invalidate user token requests
    * Add error details to response
    * Update src/Nest/XPack/Security/User/InvalidateUserAccessToken/InvalidateUserAccessTokenRequest.cs
    * Fix inherit doc references
    
    Co-Authored-By: Russ Cam <russ.cam@elastic.co>

--- src/Nest/XPack/Security/User/InvalidateUserAccessToken/InvalidateUserAccessTokenResponse.cs
+++ src/Nest/XPack/Security/User/InvalidateUserAccessToken/InvalidateUserAccessTokenResponse.cs
@@ -1,16 +1,33 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Elasticsearch.Net;
 
 namespace Nest
 {
 	public class InvalidateUserAccessTokenResponse : ResponseBase
 	{
+		/// <summary>
+		/// The number of the tokens that were invalidated as part of this request.
+		/// </summary>
 		[DataMember(Name = "invalidated_tokens")]
 		public long InvalidatedTokens { get; internal set;  }
 
+		/// <summary>
+		/// The number of tokens that were already invalidated.
+		/// </summary>
 		[DataMember(Name = "previously_invalidated_tokens")]
 		public long PreviouslyInvalidatedTokens { get; internal set;  }
 
+		/// <summary>
+		/// The number of errors that were encountered when invalidating the tokens.
+		/// </summary>
 		[DataMember(Name = "error_count")]
 		public long ErrorCount { get; internal set;  }
+
+		/// <summary>
+		/// Details about these errors. This field is not present in the response when there are no errors.
+		/// </summary>
+		[DataMember(Name = "error_details")]
+		public IReadOnlyCollection<ErrorCause> ErrorDetails { get; internal set; } = EmptyReadOnly<ErrorCause>.Collection;
 	}
 }
