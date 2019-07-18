7759807fe9056b50486325c6266508506edfc2fd
Russ Cam <russ.cam@elastic.co>
2019-07-18 12:17:14 +1000

    Fix default chunked transfer encoding and expose as configurable (#3958)
    This commit reverts the behaviour of the client to not use chunked tranfer encoding by default, and exposes chunked transfer encoding as a configuration value on IConnectionConfigurationValues and RequestConfiguration.
    
    With the introduction of the pull based streaming model in RequestDataContent, a ContentLength value cannot be computed for what will be written to the request stream; a ContentLength value is attempted to be determined, and TryComputeLength() executed, _before_ PostData is serialized to the request stream, making it not possible to provide a ContentLength value in the implementation, and falling into the HttpClient behaviour of using chunked transfer encoding.
    
    A potential fix could have been applied in RequestDataContent, to serialize PostData ahead of time and store in a byte array or stream field, and the logical place to do this would have been the ctor. It was determined however that this would be less than optimal to do because
    
    1. It starts to overcomplicate the RequestDataContent implementation
    2. An implementation in the RequestDataContent ctor would not have been able to use aynchronous methods for the RequestAsync<T> asynchronous path.
    
    Instead, the serialization of PostData happens in sync and async paths in HttpConnection, using ByteArrayContent and the written request bytes when DisabledDirectStreaming is enabled to avoid keeping two copies of the request bytes around.
    
    Additional integration tests added for HttpConnection and HttpWebRequestConnection to assert behaviour.
    
    Fixes #3952
    
    * Update default value
    
    * Update test name
    
    * Update test name
    
    (cherry picked from commit 3dcbd45b08abebd8fe6e1c079d63ccb43ecf3775)

---
+++ src/Tests/Tests/ClientConcepts/Connection/HttpWebRequestConnectionTests.cs
@@ -0,0 +1,106 @@
+﻿#if !DOTNETCORE
+using System.Linq;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Tests.Core.ManagedElasticsearch;
+using Tests.Core.ManagedElasticsearch.Clusters;
+using HttpMethod = Elasticsearch.Net.HttpMethod;
+
+namespace Tests.ClientConcepts.Connection
+{
+	public class HttpWebRequestConnectionTests : ClusterTestClassBase<ReadOnlyCluster>
+	{
+		public HttpWebRequestConnectionTests(ReadOnlyCluster cluster) : base(cluster) { }
+
+		private RequestData CreateRequestData(
+			TimeSpan requestTimeout = default,
+			Uri proxyAddress = null,
+			bool disableAutomaticProxyDetection = false,
+			bool httpCompression = false,
+			bool transferEncodingChunked = false
+		)
+		{
+			if (requestTimeout == default) requestTimeout = TimeSpan.FromSeconds(10);
+
+			var node = Client.ConnectionSettings.ConnectionPool.Nodes.First();
+			var connectionSettings = new ConnectionSettings(node.Uri)
+				.RequestTimeout(requestTimeout)
+				.DisableAutomaticProxyDetection(disableAutomaticProxyDetection)
+				.TransferEncodingChunked(transferEncodingChunked)
+				.EnableHttpCompression(httpCompression);
+
+			if (proxyAddress != null)
+				connectionSettings.Proxy(proxyAddress, null, (string)null);
+
+			var requestData = new RequestData(HttpMethod.POST, "/_search", "{ \"query\": { \"match_all\" : { } } }", connectionSettings,
+				new SearchRequestParameters(),
+				new MemoryStreamFactory()) { Node = node };
+
+			return requestData;
+		}
+
+		[I] public async Task HttpWebRequestUseTransferEncodingChunkedWhenTransferEncodingChunkedTrue()
+		{
+			var connection = new TestableHttpWebRequestConnection();
+			var requestData = CreateRequestData(transferEncodingChunked: true);
+
+			connection.Request<StringResponse>(requestData);
+			connection.LastRequest.SendChunked.Should().BeTrue();
+			connection.LastRequest.ContentLength.Should().Be(-1);
+
+			await connection.RequestAsync<StringResponse>(requestData, CancellationToken.None).ConfigureAwait(false);
+			connection.LastRequest.SendChunked.Should().BeTrue();
+			connection.LastRequest.ContentLength.Should().Be(-1);
+		}
+
+		[I] public async Task HttpWebRequestSetsContentLengthWhenTransferEncodingChunkedFalse()
+		{
+			var connection = new TestableHttpWebRequestConnection();
+			var requestData = CreateRequestData(transferEncodingChunked: false);
+
+			connection.Request<StringResponse>(requestData);
+			connection.LastRequest.SendChunked.Should().BeFalse();
+			connection.LastRequest.ContentLength.Should().BeGreaterThan(0);
+
+			await connection.RequestAsync<StringResponse>(requestData, CancellationToken.None).ConfigureAwait(false);
+			connection.LastRequest.SendChunked.Should().BeFalse();
+			connection.LastRequest.ContentLength.Should().BeGreaterThan(0);
+		}
+
+		public class TestableHttpWebRequestConnection : HttpWebRequestConnection
+		{
+			public int CallCount { get; private set; }
+
+			public HttpWebRequest LastRequest { get; private set; }
+
+			public HttpWebResponse LastResponse => LastRequest != null && LastRequest.HaveResponse
+				? (HttpWebResponse)LastRequest?.GetResponse()
+				: null;
+
+			public override TResponse Request<TResponse>(RequestData requestData)
+			{
+				CallCount++;
+				return base.Request<TResponse>(requestData);
+			}
+
+			public override Task<TResponse> RequestAsync<TResponse>(RequestData requestData, CancellationToken cancellationToken)
+			{
+				CallCount++;
+				return base.RequestAsync<TResponse>(requestData, cancellationToken);
+			}
+
+			protected override HttpWebRequest CreateHttpWebRequest(RequestData requestData)
+			{
+				LastRequest = base.CreateHttpWebRequest(requestData);
+				return LastRequest;
+			}
+		}
+	}
+}
+#endif
