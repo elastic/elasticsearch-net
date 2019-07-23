99d60c1cdae41a2dd75aa5eee3077a1e17deac9b
Russ Cam <russ.cam@elastic.co>
2019-07-23 11:00:37 +1000

    Allow outputting literal expressions as source blocks

--- src/CodeGeneration/DocGenerator/SyntaxNodeExtensions.cs
+++ src/CodeGeneration/DocGenerator/SyntaxNodeExtensions.cs
@@ -71,12 +71,11 @@ namespace DocGenerator
 		{
 			json = null;
 
-			// find the first anonymous object or new object expression
-			var creationExpressionSyntax = node.DescendantNodes()
-				.FirstOrDefault(n => n is AnonymousObjectCreationExpressionSyntax || n is ObjectCreationExpressionSyntax);
+			// find the first anonymous object, new object expression, or literal expression
+			var syntax = node.DescendantNodes()
+				.FirstOrDefault(n => n is AnonymousObjectCreationExpressionSyntax || n is ObjectCreationExpressionSyntax || n is LiteralExpressionSyntax);
 
-			return creationExpressionSyntax != null &&
-				creationExpressionSyntax.ToFullString().TryGetJsonForAnonymousType(out json);
+			return syntax != null && syntax.ToFullString().TryGetJsonForExpressionSyntax(out json);
 		}
 
 		/// <summary>
