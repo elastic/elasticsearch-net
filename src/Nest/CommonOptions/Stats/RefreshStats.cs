ff99321ad6ad581296edd91f406dd506b4e66055
Stuart Cam <stuart.cam@elastic.co>
2019-08-08 15:58:24 +1000

    Expose external refreshes through the stats API (#3992)
    Expose external refreshes through the stats API
    
    (cherry picked from commit 92ee114221b8f9f1e2bd0d7224a3f8380a244aec)

--- src/Nest/CommonOptions/Stats/RefreshStats.cs
+++ src/Nest/CommonOptions/Stats/RefreshStats.cs
@@ -13,5 +13,17 @@ namespace Nest
 
 		[DataMember(Name ="total_time_in_millis")]
 		public long TotalTimeInMilliseconds { get; set; }
+
+		/// <summary>
+		/// Only valid for Elasticsearch 7.2.0 and above.
+		/// </summary>
+		[DataMember(Name ="external_total")]
+		public long ExternalTotal { get; set; }
+
+		/// <summary>
+		/// Only valid for Elasticsearch 7.2.0 and above.
+		/// </summary>
+		[DataMember(Name ="external_total_time_in_millis")]
+		public long ExternalTotalTimeInMilliseconds { get; set; }
 	}
 }
