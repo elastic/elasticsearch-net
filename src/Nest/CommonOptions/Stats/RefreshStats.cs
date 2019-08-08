852558669598694d1daad7d7ec1e8d538277bb25
Stuart Cam <stuart.cam@elastic.co>
2019-08-08 15:57:56 +1000

    Address PR comments and add XML summary

--- src/Nest/CommonOptions/Stats/RefreshStats.cs
+++ src/Nest/CommonOptions/Stats/RefreshStats.cs
@@ -14,9 +14,15 @@ namespace Nest
 		[DataMember(Name ="total_time_in_millis")]
 		public long TotalTimeInMilliseconds { get; set; }
 
+		/// <summary>
+		/// Only valid for Elasticsearch 7.2.0 and above.
+		/// </summary>
 		[DataMember(Name ="external_total")]
 		public long ExternalTotal { get; set; }
 
+		/// <summary>
+		/// Only valid for Elasticsearch 7.2.0 and above.
+		/// </summary>
 		[DataMember(Name ="external_total_time_in_millis")]
 		public long ExternalTotalTimeInMilliseconds { get; set; }
 	}
