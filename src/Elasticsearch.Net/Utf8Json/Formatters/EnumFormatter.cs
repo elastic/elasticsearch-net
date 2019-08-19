0c84c5970ddd3cbe48e8bc200319286010e4d271
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-08-19 16:03:09 +0200

    fix #4029: Use enum item.Name instead of Enum.GetName to get enum value
    This is the propsed fix from the original repo of the code: https://github.com/neuecc/Utf8Json/pull/129

--- src/Elasticsearch.Net/Utf8Json/Formatters/EnumFormatter.cs
+++ src/Elasticsearch.Net/Utf8Json/Formatters/EnumFormatter.cs
@@ -166,7 +166,7 @@ namespace Elasticsearch.Net.Utf8Json.Formatters
 			foreach (var item in type.GetFields().Where(fi => fi.FieldType == type))
 			{
 				var value = item.GetValue(null);
-				var name = Enum.GetName(type, value);
+				var name = item.Name;
 				var dataMember = item.GetCustomAttributes(typeof(DataMemberAttribute), true)
 					.OfType<DataMemberAttribute>()
 					.FirstOrDefault();
