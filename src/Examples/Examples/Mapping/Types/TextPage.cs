80df656b46a667d8f97f0d99f3df1aaa99cd98fe
Rickard Lyrenius <rly@combination.se>
2019-08-20 09:13:23 +0200

    Added unit test for text data type example as reference

--- src/Examples/Examples/Mapping/Types/TextPage.cs
+++ src/Examples/Examples/Mapping/Types/TextPage.cs
@@ -5,14 +5,18 @@ namespace Examples.Mapping.Types
 {
 	public class TextPage : ExampleBase
 	{
-		[U(Skip = "Example not implemented")]
+		[U]
 		public void Line22()
 		{
 			// tag::24ea1c6cdf10165228951e562b7ec0ef[]
-			var response0 = new SearchResponse<object>();
+			var createIndexResponse = client.Indices.Create("my_index", c => c
+				.Map(m => m
+					.Properties(p => p.Text(t => t.Name("full_name")))
+				)
+			);
 			// end::24ea1c6cdf10165228951e562b7ec0ef[]
 
-			response0.MatchesExample(@"PUT my_index
+			createIndexResponse.MatchesExample(@"PUT my_index
 			{
 			  ""mappings"": {
 			    ""properties"": {
