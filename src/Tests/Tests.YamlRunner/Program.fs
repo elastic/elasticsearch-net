3c92c6c91c5708fc1aaf89b1831b3109312651cc
Martijn Laarman <Mpdreamz@gmail.com>
2019-08-21 20:37:35 +0200

    stage

--- src/Tests/Tests.YamlRunner/Program.fs
+++ src/Tests/Tests.YamlRunner/Program.fs
@@ -9,15 +9,13 @@ let barOptions =
        ForegroundColor = ConsoleColor.Cyan,
        ForegroundColorDone = Nullable ConsoleColor.DarkGreen,
        ProgressCharacter = '─',
-       BackgroundColor = Nullable ConsoleColor.DarkGray,
-       CollapseWhenFinished = true
+       BackgroundColor = Nullable ConsoleColor.DarkGray
     )
 let subBarOptions = 
     ProgressBarOptions(
-       ForegroundColor = ConsoleColor.DarkYellow,
+       ForegroundColor = ConsoleColor.Yellow,
        ForegroundColorDone = Nullable ConsoleColor.DarkGreen,
-       ProgressCharacter = '─',
-       CollapseWhenFinished = true
+       ProgressCharacter = '─'
     )
 let branchOrTag = "master"
 let folders branch = 
@@ -28,41 +26,74 @@ let folders branch =
         |> List.filter(fun f -> not <| f.EndsWith(".asciidoc"))
         
 let downloadYamlFile url (progress:ChildProgressBar) = async {
-    let! yaml = Http.AsyncRequestString url
+    //let! yaml = Http.AsyncRequestString url
+    let! x = Async.Sleep 2000
     progress.Tick(url)
-    return yaml
+    return ""
 }
-let folderFiles branch folder (progress:ProgressBar) = async { 
+let folderFiles branch folder = async { 
     let url = sprintf "https://github.com/elastic/elasticsearch/tree/%s/rest-api-spec/src/main/resources/rest-api-spec/test/%s" branch folder
     let! doc = HtmlDocument.AsyncLoad(url)
-    progress.Tick()
     let url file = sprintf "https://raw.githubusercontent.com/elastic/elasticsearch/%s/rest-api-spec/src/main/resources/rest-api-spec/test/%s/%s" branch folder file
     let yamlFiles =
         doc.CssSelect("td.content a.js-navigation-open")
         |> List.map(fun a -> a.InnerText())
         |> List.filter(fun f -> f.EndsWith(".yml"))
         |> List.map url
+    return (folder, yamlFiles)
+}
+
+let f (yamlFiles:Async<string * list<string>>) (progress: IProgressBar) = async {
+    let! token = Async.StartChild yamlFiles
+    let! (folder, yamlFiles) = token
     
     let filesProgress = progress.Spawn(yamlFiles.Length, sprintf "Downloading %i files in %s" yamlFiles.Length folder, subBarOptions)
     let actions =
         yamlFiles
-        |> List.map (fun url -> downloadYamlFile url filesProgress)
+        |> Seq.indexed
+        |> Seq.groupBy (fun (i, _) -> i % yamlFiles.Length / 4)
+        |> Seq.map (fun (_, indexed) -> indexed |> Seq.map (fun (_, url) -> url))
+        //|> Seq.map (fun url -> downloadYamlFile url filesProgress)
+        
+    for files in actions do
+        let tasks = files |> Seq.map (fun f -> downloadYamlFile f filesProgress)
+        let! token = Async.StartChild <| Async.Parallel tasks
+        let! result = token
+        ignore()
+            
+    progress.Tick()
+}
+
+let h = async {
+    let folders = folders branchOrTag
     
-    let result = Async.RunSynchronously <| Async.Parallel (actions, 4)
-    return result
+    
+    use pbar = new ProgressBar(folders.Length, "Listing folders", barOptions)
+    let folderDownloads =
+        folders
+        |> Seq.map(fun folder -> folderFiles branchOrTag folder)
+        |> Seq.indexed
+        |> Seq.groupBy (fun (i, _) -> i % folders.Length / 4)
+        |> Seq.map (fun (_, indexed) -> indexed |> Seq.map (fun (_, folder) -> folder))
+        |> Seq.toList
+        
+    for group in folderDownloads do
+        pbar.WriteLine( sprintf "length %i" (group |> Seq.length) )
+        let tasks = group |> Seq.map (fun files -> f files pbar)
+        let! token = Async.StartChild <| Async.Parallel tasks
+        let! result = token
+        ignore()
+        
+    pbar.Dispose()
+    
+    printfn "Hello World" 
 }
 
 [<EntryPoint>]
 let main argv =
-    // load the live package stats for FSharp.Data
-    let folders = folders branchOrTag
-    use pbar = new ProgressBar(folders.Length, "Listing folders", barOptions)
-    
-    let x = folders |> List.map(fun folder -> folderFiles branchOrTag folder pbar)
-    
-    let result = Async.RunSynchronously <| Async.Parallel (x, 2)
-    pbar.Dispose()
-    
-    printfn "Hello World %O" result 
-    0 // return an integer exit code
+    async {
+        do! Async.SwitchToThreadPool ()
+        let! exitCode = h
+        return 0
+    } |> Async.RunSynchronously
     
