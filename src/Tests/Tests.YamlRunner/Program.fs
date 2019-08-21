881c730983c02cbd560abce0d82f684486b383c4
Martijn Laarman <Mpdreamz@gmail.com>
2019-08-21 21:57:06 +0200

    stage

--- src/Tests/Tests.YamlRunner/Program.fs
+++ src/Tests/Tests.YamlRunner/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.Threading
 open System.Threading.Tasks
 open FSharp.Data
 open ShellProgressBar
@@ -25,10 +26,9 @@ let folders branch =
         |> List.map(fun a -> a.InnerText())
         |> List.filter(fun f -> not <| f.EndsWith(".asciidoc"))
         
-let downloadYamlFile url (progress:ChildProgressBar) = async {
+let downloadYamlFile url = async {
     //let! yaml = Http.AsyncRequestString url
-    let! x = Async.Sleep 2000
-    progress.Tick(url)
+    let! x = Async.Sleep 500
     return ""
 }
 let folderFiles branch folder = async { 
@@ -42,25 +42,52 @@ let folderFiles branch folder = async {
         |> List.map url
     return (folder, yamlFiles)
 }
+let foreach<'a> maxDegreeOfParallelism (asyncs:seq<Async<'a>>) = async {
+    let tasks = new System.Collections.Generic.List<Task<'a>>(4)
+    for async in asyncs do
+        let! x = Async.StartChildAsTask async
+        tasks.Add <| x
+        if (tasks.Count >= maxDegreeOfParallelism) then
+            let! task = Async.AwaitTask <| Task.WhenAny(tasks)
+            if (task.IsFaulted) then System.Runtime.ExceptionServices.ExceptionDispatchInfo.Capture(task.Exception).Throw();
+            let removed = tasks.Remove <| task
+            ignore()
+    let! completed = Async.AwaitTask <| Task.WhenAll tasks
+    ignore()
+}
+
+let any asyncs =
+    async {
+        let! t = 
+            asyncs
+            |> Seq.map Async.StartAsTask
+            |> System.Threading.Tasks.Task.WhenAny
+            |> Async.AwaitTask
+
+        return t.Result }
+
 
 let f (yamlFiles:Async<string * list<string>>) (progress: IProgressBar) = async {
     let! token = Async.StartChild yamlFiles
     let! (folder, yamlFiles) = token
     
-    let filesProgress = progress.Spawn(yamlFiles.Length, sprintf "Downloading %i files in %s" yamlFiles.Length folder, subBarOptions)
+    let mutable seenFiles = 0;
+    let filesProgress = progress.Spawn(yamlFiles.Length, sprintf "Downloading [0/%i] files in %s" yamlFiles.Length folder, subBarOptions)
     let actions =
         yamlFiles
-        |> Seq.indexed
-        |> Seq.groupBy (fun (i, _) -> i % yamlFiles.Length / 4)
-        |> Seq.map (fun (_, indexed) -> indexed |> Seq.map (fun (_, url) -> url))
-        //|> Seq.map (fun url -> downloadYamlFile url filesProgress)
+        |> Seq.map (fun url -> async {
+            let! result = downloadYamlFile url
+            let i = Interlocked.Increment (&seenFiles)
+            let message = sprintf "Downloaded [%i/%i] files in %s" i yamlFiles.Length folder
+            filesProgress.Tick(message)
+            return result
+        })
+//        |> Seq.indexed
+//        |> Seq.groupBy (fun (i, _) -> i % yamlFiles.Length / 4)
+//        |> Seq.map (fun (_, indexed) -> indexed |> Seq.map (fun (_, url) -> url))
         
-    for files in actions do
-        let tasks = files |> Seq.map (fun f -> downloadYamlFile f filesProgress)
-        let! token = Async.StartChild <| Async.Parallel tasks
-        let! result = token
-        ignore()
-            
+        
+    let! completed = foreach 1 actions
     progress.Tick()
 }
 
@@ -71,19 +98,19 @@ let h = async {
     use pbar = new ProgressBar(folders.Length, "Listing folders", barOptions)
     let folderDownloads =
         folders
-        |> Seq.map(fun folder -> folderFiles branchOrTag folder)
-        |> Seq.indexed
-        |> Seq.groupBy (fun (i, _) -> i % folders.Length / 2)
-        |> Seq.map (fun (_, indexed) -> indexed |> Seq.map (fun (_, folder) -> folder))
-        |> Seq.toList
+        |> Seq.map(fun folder -> f (folderFiles branchOrTag folder) pbar)
+//        |> Seq.indexed
+//        |> Seq.groupBy (fun (i, _) -> i % folders.Length / 2)
+//        |> Seq.map (fun (_, indexed) -> indexed |> Seq.map (fun (_, folder) -> folder))
+//        |> Seq.toList
+
+    let! completed = foreach 4 folderDownloads
         
-        
-    for group in folderDownloads do
-        pbar.WriteLine( sprintf "length %i" (group |> Seq.length) )
-        let tasks = group |> Seq.map (fun files -> f files pbar)
-        let! token = Async.StartChild <| Async.Parallel (tasks , 4)
-        let! result = token
-        ignore()
+//    for group in folderDownloads do
+//        let tasks = group |> Seq.map (fun files -> f files pbar)
+//        let! token = Async.StartChild <| Async.Parallel (tasks , 4)
+//        let! result = token
+//        ignore()
         
     pbar.Dispose()
     
