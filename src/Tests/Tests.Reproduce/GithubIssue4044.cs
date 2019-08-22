3eb233453fe67743b3d0b0662e92673d072ee575
Rickard <rickardp@users.noreply.github.com>
2019-08-22 09:13:46 +0200

    Serialize Distance and Percentiles aggregate using an invariant locale (#4042)
    * Serialize Distance to JSON using invariant culture
    * Use invariant culture for double.Parse in PercentileItem
    * Added test case and fix for similar issue
    
    Closes #4044
    Closes #4041
    
    (cherry picked from commit 5839da06a2e11e020bce5cbb16f938336641f5fd)
    (cherry picked from commit 9fe3e4769c544e047fe180f260112ca08293f0d1)

---
+++ src/Tests/Tests.Reproduce/GithubIssue4044.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Elastic.Xunit.XunitPlumbing;
+using FluentAssertions;
+using Nest;
+using static Tests.Core.Serialization.SerializationTestHelper;
+
+namespace Tests.Reproduce
+{
+	public class GithubIssue4044
+	{
+		[U]
+		[UseCulture("sv-SE")]
+		public void PercentilesAggregateSerializesWithInvariantCulture()
+		{
+			const string percentilesString = @"{
+				""test_percentiles#pp"": {
+					""values"": {
+						""42.42"": 42.42
+					}
+				}
+			}";
+
+			Expect(percentilesString).NoRoundTrip().DeserializesTo<AggregateDictionary>((s, val) =>
+			{
+				val.Count.Should().Be(1);
+			});
+		}
+	}
+}
