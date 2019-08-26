ab1171c1fdb59ec31b0d61a466310e6ba09a2d3d
Martijn Laarman <Mpdreamz@gmail.com>
2019-08-26 12:02:37 +0200

    stage

--- src/Tests/Tests.YamlRunner/Models.fs
+++ src/Tests/Tests.YamlRunner/Models.fs
@@ -1,4 +1,101 @@
 module Tests.YamlRunner.Models
 
+open System
+open System.IO
+
 type TestSuite = OpenSource | XPack
+
+type Skip = { Version:string; Reason:string option }
+
+type DoCatch =
+    | BadRequest // bad_request, 400 response from ES
+    | Unauthorized //unauthorized a 401 response from ES
+    | Forbidden //forbidden a 403 response from ES
+    | Missing //missing a 404 response from ES
+    | RequestTimeout //request_timeout a 408 response from ES
+    | Conflict //conflict a 409 response from ES
+    | Unavailable//unavailable a 503 response from ES
+    | UnknownParameter //param a client-side error indicating an unknown parameter has been passed to the method
+    | OtherBadResponse //request 4xx-5xx error response from ES, not equal to any named response above
+    | Regex // /foo bar/ the text of the error message matches this regular expression
+    
+let (|IsDoCatch|_|) (s:string) =
+    match s with
+    | "bad_request" -> Some BadRequest 
+    | "unauthorized " -> Some Unauthorized 
+    | "forbidden " -> Some Forbidden 
+    | "missing " -> Some Missing 
+    | "request_timeout " -> Some RequestTimeout 
+    | "conflict " -> Some Conflict 
+    | "unavailable " -> Some Unavailable
+    | "param " -> Some UnknownParameter
+    | "request " -> Some OtherBadResponse 
+    | "regex" -> Some Regex
+    | _ -> None
+    
+type NodeSelector =
+    | NodeVersionSelector of string
+    | NodeAttributeSelector of string * string
+    | NodeSelector of string * string * string
+
+type ResponseProperty = ResponseProperty of string
+
+type StashedId = private StashedId of string
+    with static member Create s = StashedId <| sprintf "$%s" s
+    
+type SetTransformation = private SetTransformation of string
+    with static member Create s = SetTransformation <| sprintf "$%s" s
+type AssertPath = AssertPath of string
+
+type Set = Map<ResponseProperty, StashedId>
+type TransformAndSet = Map<StashedId, SetTransformation>
+type Match = Map<AssertPath, Object>
+type NumericMatch = Map<AssertPath, int64>
+    
+type Do = {
+    ApiCall: string * Object
+    Catch:DoCatch option
+    Warnings:option<string list>
+    NodeSelector:NodeSelector option
+}
+
+type NumericAssert = 
+    | LowerThan 
+    | GreaterThan 
+    | GreaterThanOrEqual 
+    | LowerThanOrEqual 
+    | Equal 
+    | Length
+
+let (|IsNumericAssert|_|) (s:string) =
+    match s with
+    | "length" -> Some Length
+    | "eq" -> Some Equal
+    | "gte" -> Some GreaterThanOrEqual
+    | "lte" -> Some LowerThanOrEqual
+    | "gt" -> Some GreaterThan
+    | "lt" -> Some LowerThan
+    | _ -> None
+    
+type Assert = 
+    | IsTrue of AssertPath
+    | IsFalse of AssertPath
+    | Match of Match
+    | NumericAssert of NumericAssert * NumericMatch
+
+type Operation =
+    | Unknown of string
+    | Skip of Skip
+    | Do of Do
+    | Set of Set
+    | TransformAndSet of TransformAndSet
+    | Assert of Assert
+type Operations = Operation list
+
+type YamlTest = {
+    Name:string
+    Operations: Operations
+}
+    
+type YamlTestSection = | Setup of Operations | Teardown of Operations | YamlTest of YamlTest
 
--- src/Tests/Tests.YamlRunner/TestsReader.fs
+++ src/Tests/Tests.YamlRunner/TestsReader.fs
@@ -1,141 +1,51 @@
 module Tests.YamlRunner.TestsReader
 
 open System
-open System
 open System.Collections.Generic
 open System.Text.RegularExpressions
 open System.Linq
 
-
-type Skip = { Version:string; Reason:string option }
-
-type DoCatch =
-    | BadRequest // bad_request, 400 response from ES
-    | Unauthorized //unauthorized a 401 response from ES
-    | Forbidden //forbidden a 403 response from ES
-    | Missing //missing a 404 response from ES
-    | RequestTimeout //request_timeout a 408 response from ES
-    | Conflict //conflict a 409 response from ES
-    | Unavailable//unavailable a 503 response from ES
-    | UnknownParameter //param a client-side error indicating an unknown parameter has been passed to the method
-    | OtherBadResponse //request 4xx-5xx error response from ES, not equal to any named response above
-    | Regex // /foo bar/ the text of the error message matches this regular expression
-    
-let (|IsDoCatch|_|) (s:string) =
-    match s with
-    | "bad_request" -> Some BadRequest 
-    | "unauthorized " -> Some Unauthorized 
-    | "forbidden " -> Some Forbidden 
-    | "missing " -> Some Missing 
-    | "request_timeout " -> Some RequestTimeout 
-    | "conflict " -> Some Conflict 
-    | "unavailable " -> Some Unavailable
-    | "param " -> Some UnknownParameter
-    | "request " -> Some OtherBadResponse 
-    | "regex" -> Some Regex
-    | _ -> None
-    
-type NodeSelector =
-    | NodeVersionSelector of string
-    | NodeAttributeSelector of string * string
-    | NodeSelector of string * string * string
-
-type ResponseProperty = private ResponseProperty of string
-type StashedId = private StashedId of string
-type SetTransformation = private SetTransformation of string
-type AssertPath = private AssertPath of string
-
-type Set = Map<ResponseProperty, StashedId>
-type TransformAndSet = Map<StashedId, SetTransformation>
-type Match = Map<AssertPath, Object>
-type NumericMatch = Map<AssertPath, int64>
-    
-type Do = {
-    ApiCall: string * Object
-    Catch:DoCatch option
-    Warnings:option<string list>
-    NodeSelector:NodeSelector option
-}
-
-type NumericAssert = 
-    | LowerThan 
-    | GreaterThan 
-    | GreaterThanOrEqual 
-    | LowerThanOrEqual 
-    | Equal 
-    | Length
-
-let (|IsNumericAssert|_|) (s:string) =
-    match s with
-    | "length" -> Some Length
-    | "eq" -> Some Equal
-    | "gte" -> Some GreaterThanOrEqual
-    | "lte" -> Some LowerThanOrEqual
-    | "gt" -> Some GreaterThan
-    | "lt" -> Some LowerThan
-    | _ -> None
-    
-type Assert = 
-    | IsTrue of AssertPath
-    | IsFalse of AssertPath
-    | Match of Match
-    | NumericAssert of NumericAssert * NumericMatch
-
-type Operation =
-    | Unknown of string
-    | Skip of Skip
-    | Do of Do
-    | Set of Set
-    | TransformAndSet of TransformAndSet
-    | Assert of Assert
-type Operations = Operation list
-
-type YamlTest = {
-    Name:string;
-    Actions: Operations;
-}
-    
-type TestType = | Setup of Operations | Teardown of Operations | Suite of YamlTest
+open Tests.YamlRunner.Models
 
 type YamlMap = Dictionary<Object,Object>
 
-let tryPick<'a> (map:YamlMap) key =
+let private tryPick<'a> (map:YamlMap) key =
     let found, value =  map.TryGetValue key
     if (found) then Some (value :?> 'a) else None
     
-let pick<'a> (map:YamlMap) key =
+let private pick<'a> (map:YamlMap) key =
     let found, value =  map.TryGetValue key
     if (found) then (value :?> 'a)
     else failwithf "expected to find %s of type %s" key typeof<'a>.Name
 
-let mapSkip (operation:YamlMap) =
+let private mapSkip (operation:YamlMap) =
     let version = pick<string> operation "version" 
     let reason = tryPick<string> operation "reason"
     Skip { Version=version; Reason=reason }
     
-let mapNumericAssert (operation:YamlMap) =
+let private mapNumericAssert (operation:YamlMap) =
     operation
         |> Seq.map (fun (kv) -> AssertPath (kv.Key :?> string), kv.Value :?> int64)
         |> Map.ofSeq
         
-let firstValueAsPath (operation:YamlMap) = AssertPath (operation.Values.First() :?> string)
+let private firstValueAsPath (operation:YamlMap) = AssertPath (operation.Values.First() :?> string)
 
-let mapMatch (operation:YamlMap) =
+let private mapMatch (operation:YamlMap) =
     operation
         |> Seq.map (fun (kv) -> AssertPath (kv.Key :?> string), kv.Value)
         |> Map.ofSeq
         
-let mapTransformAndSet (operation:YamlMap) =
+let private mapTransformAndSet (operation:YamlMap) =
     operation
-        |> Seq.map (fun (kv) -> StashedId (kv.Key :?> string), SetTransformation (kv.Value :?> string))
+        |> Seq.map (fun (kv) -> StashedId.Create  (kv.Key :?> string), SetTransformation.Create (kv.Value :?> string))
         |> Map.ofSeq
         
-let mapSet (operation:YamlMap) =
+let private mapSet (operation:YamlMap) =
     operation
-        |> Seq.map (fun (kv) -> ResponseProperty (kv.Key :?> string), StashedId (kv.Value :?> string))
+        |> Seq.map (fun (kv) -> ResponseProperty (kv.Key :?> string), StashedId.Create (kv.Value :?> string))
         |> Map.ofSeq
         
-let mapNodeSelector (operation:YamlMap) =
+let private mapNodeSelector (operation:YamlMap) =
     let version = tryPick<string> operation "version" 
     let attribute = tryPick<YamlMap> operation "attribute"
     match (version, attribute) with
@@ -150,7 +60,7 @@ let mapNodeSelector (operation:YamlMap) =
     | _ -> None
             
     
-let mapDo (operation:YamlMap) =
+let private mapDo (operation:YamlMap) =
     
     let last = operation.Last()
     let lastKey = last.Key :?> string
@@ -175,7 +85,7 @@ let mapDo (operation:YamlMap) =
         NodeSelector = nodeSelector
     }
     
-let mapOperation (operation:YamlMap) =
+let private mapOperation (operation:YamlMap) =
     let kv = operation.First();
     let key = kv.Key :?> string
     let yamlMap = kv.Value :?> YamlMap
@@ -191,30 +101,32 @@ let mapOperation (operation:YamlMap) =
     | IsNumericAssert n -> Assert <| NumericAssert (n, mapNumericAssert yamlMap)
     | unknownOperation -> Unknown unknownOperation
     
-let mapOperations (operations:YamlMap list) =
+let private mapOperations (operations:YamlMap list) =
     operations |> List.map mapOperation
     
-let mapDocument (document:Dictionary<string, Object>) =
+let private mapDocument (document:Dictionary<string, Object>) =
     
     let kv = document.First();
+    let key = kv.Key
+    let values = kv.Value :?> List<Object>
     
-    let list = (kv.Value :?> List<Object>) |> Enumerable.Cast<YamlMap> |> Seq.toList |> mapOperations
+    let operations = values |> Enumerable.Cast<YamlMap> |> Seq.toList |> mapOperations
     
-    document
+    match key with
+    | "setup" -> Setup operations
+    | "teardown" -> Teardown operations
+    | name -> YamlTest { Name=name; Operations=operations }
 
-let test () = 
-    let document = "/tmp/elastic/tests-master/cat.aliases/10_basic.yml"
+let ReadYamlFile yamlString = 
 
-    let x = SharpYaml.Serialization.Serializer()
-    let contents = System.IO.File.ReadAllText document
-    let tests =
-        Regex.Split(contents, @"--- ?\r?\n")
+    let serializer = SharpYaml.Serialization.Serializer()
+    let sections =
+        Regex.Split(yamlString, @"--- ?\r?\n")
         |> Seq.filter (fun s -> not <| String.IsNullOrEmpty s)
-        |> Seq.map (fun document -> x.Deserialize<Dictionary<string, Object>> document)
+        |> Seq.map (fun document -> serializer.Deserialize<Dictionary<string, Object>> document)
         |> Seq.filter (fun s -> s <> null)
         |> Seq.map mapDocument
         |> Seq.toList
         
-    
-    tests
+    sections
 
