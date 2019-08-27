4d38f3fe0bac79e3360666d5827610ff24cf125a
Martijn Laarman <Mpdreamz@gmail.com>
2019-08-27 15:58:57 +0200

    stage

--- src/Tests/Tests.YamlRunner/TestsRunner.fs
+++ src/Tests/Tests.YamlRunner/TestsRunner.fs
@@ -14,25 +14,18 @@ let RunOperation (progress:IProgressBar) m operation = async {
     return true
 }
 
-let RunOperations (progress:IProgressBar) (barOptions:ProgressBarOptions) m (ops:Operations) =
-    let mutable seen = 0;
-    let l = ops.Length
-    let message = sprintf "%s [0/%i] operations" m l
-    //progress.WriteLine <| message
-    let actions =
-        let p = progress.Spawn(l, message, barOptions)
+let RunOperations (progress:IProgressBar) (barOptions:ProgressBarOptions) m (ops:Operations) = async {
+    let executedOperations =
         ops
         |> List.map (fun op -> async {
-            let i = Interlocked.Increment (&seen)
-            let message = sprintf "%s [%i/%i] operations" m i l
-            p.Tick(message)
             let! pass = RunOperation progress m op
             
             let! x = Async.Sleep <| randomTime.Next(50, 200)
             return pass
         })
     
-    actions
+    return (m, executedOperations)
+}
     
 
 let RunTestFile (progress:IProgressBar) (barOptions:ProgressBarOptions) (file:YamlTestDocument) = async {
@@ -48,20 +41,37 @@ let RunTestFile (progress:IProgressBar) (barOptions:ProgressBarOptions) (file:Ya
     
     let teardown =  file.Teardown |> Option.map (f "Teardown") |> Option.toList //|> Option.map Async.RunSynchronously
     
-    let suites = (setup @ passed @ teardown)
+    let sections = (setup @ passed @ teardown)
     //let combined  = suites |> List.concat
     
-    let l = suites.Length
+    let l = sections.Length
     p.MaxTicks <- l
     
     let actions =
-        suites
+        sections
         |> List.map (fun suite -> async {
             let! x = Async.Sleep <| randomTime.Next(50, 200)
             let i = Interlocked.Increment (&seen)
             let message = sprintf "[%i/%i] sections" i l
             p.Tick(message)
-            let result = suite |> List.map Async.RunSynchronously
+            
+            
+            let! (m, ops) = suite
+            let lOps = ops.Length
+            let mutable seenL = 0;
+            let messageL = sprintf "%s [0/%i] operations" m l
+            progress.WriteLine <| message
+            let lp = p.Spawn(lOps, messageL, barOptions)
+            
+            let result =
+                ops
+                |> List.map (fun op -> async {
+                    let i = Interlocked.Increment (&seenL)
+                    let message = sprintf "%s [%i/%i] operations" m i l
+                    lp.Tick(message)
+                    return! op
+                })
+                |> List.map Async.RunSynchronously
             return result
         })
         |> List.map Async.RunSynchronously
