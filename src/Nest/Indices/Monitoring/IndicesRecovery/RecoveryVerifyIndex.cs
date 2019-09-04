412b83212d48abec631135e839679095d0f5eec5
Stuart Cam <stuart.cam@elastic.co>
2019-09-04 13:59:06 +1000

    Map additional Recovery API response properties. (#4067)
    This commit
    
    - Map additonal Recovery API response properties.
    - Add NullableDateTimeEpochMillisecondsFormatter

---
+++ src/Nest/Indices/Monitoring/IndicesRecovery/RecoveryVerifyIndex.cs
@@ -0,0 +1,13 @@
+using System.Runtime.Serialization;
+
+namespace Nest
+{
+	public class RecoveryVerifyIndex
+	{
+		[DataMember(Name ="check_index_time_in_millis")]
+		public long CheckIndexTimeInMilliseconds { get; internal set; }
+
+		[DataMember(Name ="total_time_in_millis")]
+		public long TotalTimeInMilliseconds { get; internal set; }
+	}
+}
