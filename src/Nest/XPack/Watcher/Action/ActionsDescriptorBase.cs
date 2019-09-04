8acde7ebc45e22ff431e1fec301b2a7729f85a03
Russ Cam <russ.cam@elastic.co>
2019-09-04 14:16:32 +1000

    Allow execution of each Watcher action in array (#4068)
    * Allow execution of each Watcher action in array
    
    Relates: #4001
    
    This commit introduces Foreach on Watcher actions, to allow execution of the Watcher
    action on each element in an array specified by the path assigned to foreach.
    
    Add condition to Watcher actions and ensure condition and transform are serialized.
    Add integration test for Action foreach, transform and condition.

--- src/Nest/XPack/Watcher/Action/ActionsDescriptorBase.cs
+++ src/Nest/XPack/Watcher/Action/ActionsDescriptorBase.cs
@@ -2,6 +2,7 @@
 
 namespace Nest
 {
+	/// <inheritdoc cref="IAction"/>
 	public abstract class ActionsDescriptorBase<TDescriptor, TInterface>
 		: DescriptorBase<TDescriptor, TInterface>, IAction
 		where TDescriptor : DescriptorBase<TDescriptor, TInterface>, TInterface
@@ -29,10 +30,21 @@ namespace Nest
 
 		Time IAction.ThrottlePeriod { get; set; }
 		TransformContainer IAction.Transform { get; set; }
+		ConditionContainer IAction.Condition { get; set; }
+		string IAction.Foreach { get; set; }
 
+		/// <inheritdoc cref="IAction.Transform"/>
 		public TDescriptor Transform(Func<TransformDescriptor, TransformContainer> selector) =>
 			Assign(selector.InvokeOrDefault(new TransformDescriptor()), (a, v) => a.Transform = v);
 
+		/// <inheritdoc cref="IAction.Condition"/>
+		public TDescriptor Condition(Func<ConditionDescriptor, ConditionContainer> selector) =>
+			Assign(selector.InvokeOrDefault(new ConditionDescriptor()), (a, v) => a.Condition = v);
+
+		/// <inheritdoc cref="IAction.ThrottlePeriod"/>
 		public TDescriptor ThrottlePeriod(Time throttlePeriod) => Assign(throttlePeriod, (a, v) => a.ThrottlePeriod = v);
+
+		/// <inheritdoc cref="IAction.Foreach"/>
+		public TDescriptor Foreach(string @foreach) => Assign(@foreach, (a, v) => a.Foreach = v);
 	}
 }
