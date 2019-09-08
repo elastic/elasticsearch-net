655009eb0406a211959cff29e50aecb384a0a145
Andrey Borisko <borisko.andrey@gmail.com>
2019-09-08 02:07:04 -0400

    Fix fluent serialization of if_primary_term (#4078)
    Fixes fluent serialization of if_primary_term for BulkIndexDescriptor and BulkUpdateDescriptor

---
+++ src/Tests/Tests.Reproduce/GithubIssue4078.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Elastic.Xunit.XunitPlumbing;
+using FluentAssertions;
+using Nest;
+using Tests.Core.Client;
+using Tests.Core.Extensions;
+using Tests.Core.Serialization;
+using Tests.Domain;
+
+namespace Tests.Reproduce
+{
+	public class GithubIssue4078
+	{
+		[U]
+		public void BulkIndexOperationIssue() {
+
+			var indexResponse = TestClient.InMemoryWithJsonNetSerializer.Bulk(d =>
+				d.Index<Project>(i => i.IfSequenceNumber(12345).IfPrimaryTerm(67890).Document(Project.Instance))
+			);
+			Encoding.UTF8.GetString(indexResponse.ApiCall.RequestBodyInBytes).Should().Contain("\"if_seq_no\"").And.Contain("12345")
+				.And.Contain("\"if_primary_term\"").And.Contain("67890");
+		}
+
+		[U]
+		public void BulkUpdateOperationIssue()
+		{
+
+			var indexResponse = TestClient.InMemoryWithJsonNetSerializer.Bulk(d =>
+				d.Update<Project, object>(i => i.IfSequenceNumber(12345).IfPrimaryTerm(67890).Doc(Project.Instance))
+			);
+			Encoding.UTF8.GetString(indexResponse.ApiCall.RequestBodyInBytes).Should().Contain("\"if_seq_no\"").And.Contain("12345")
+				.And.Contain("\"if_primary_term\"").And.Contain("67890");
+		}
+	}
+}
