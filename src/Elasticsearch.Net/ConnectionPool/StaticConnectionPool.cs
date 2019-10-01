d3fa5b46bd2bdf3f3fef295328c22d335f634a91
Russ Cam <russ.cam@elastic.co>
2019-10-01 11:29:18 +1000

    Fix initial randomization of nodes (#4112)
    This commit fixes the initial randomization of nodes in a StaticConnectionPool. Instead of calling the protected ctor,
    the initialization logic has been pulled out into a separate method that is now called from the ctor that accepts a randomize parameter.
    
    Fixes #4055

--- src/Elasticsearch.Net/ConnectionPool/StaticConnectionPool.cs
+++ src/Elasticsearch.Net/ConnectionPool/StaticConnectionPool.cs
@@ -15,24 +15,29 @@ namespace Elasticsearch.Net
 			: this(uris.Select(uri => new Node(uri)), randomize, dateTimeProvider) { }
 
 		public StaticConnectionPool(IEnumerable<Node> nodes, bool randomize = true, IDateTimeProvider dateTimeProvider = null)
-			: this(nodes, null, dateTimeProvider) => Randomize = randomize;
+			: this(nodes, null, randomize, dateTimeProvider) { }
 
 		//this constructor is protected because nodeScorer only makes sense on subclasses that support reseeding
 		//otherwise just manually sort `nodes` before instantiating.
-		protected StaticConnectionPool(IEnumerable<Node> nodes, Func<Node, float> nodeScorer, IDateTimeProvider dateTimeProvider = null)
+		protected StaticConnectionPool(IEnumerable<Node> nodes, Func<Node, float> nodeScorer = null, bool randomize = true, IDateTimeProvider dateTimeProvider = null)
 		{
 			nodes.ThrowIfEmpty(nameof(nodes));
+			string scheme = null;
+			foreach (var node in nodes)
+			{
+				if (scheme == null)
+				{
+					scheme = node.Uri.Scheme;
+					UsingSsl = scheme == "https";
+				}
+				else if (scheme != node.Uri.Scheme)
+					throw new ArgumentException("Trying to instantiate a connection pool with mixed URI Schemes");
+			}
+
 			DateTimeProvider = dateTimeProvider ?? Net.DateTimeProvider.Default;
-
-			var nn = nodes.ToList();
-			var uris = nn.Select(n => n.Uri).ToList();
-			if (uris.Select(u => u.Scheme).Distinct().Count() > 1)
-				throw new ArgumentException("Trying to instantiate a connection pool with mixed URI Schemes");
-
-			UsingSsl = uris.Any(uri => uri.Scheme == "https");
-
+			Randomize = randomize;
 			_nodeScorer = nodeScorer;
-			InternalNodes = SortNodes(nn)
+			InternalNodes = SortNodes(nodes)
 				.DistinctBy(n => n.Uri)
 				.ToList();
 			LastUpdate = DateTimeProvider.Now();
