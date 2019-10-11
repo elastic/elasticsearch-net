2919a66cc98625d4c07462f925aef29359935440
Russ Cam <russ.cam@elastic.co>
2019-10-11 11:28:44 +1000

    Add support for clone index API
    This commit adds support for the clone index API, introduced in Elasticsearch 7.4.0

---
+++ src/Nest/Indices/IndexManagement/CloneIndex/CloneIndexResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Nest
+{
+	public class CloneIndexResponse : AcknowledgedResponseBase
+	{
+		[DataMember(Name = "shards_acknowledged")]
+		public bool ShardsAcknowledged { get; set; }
+
+		/// <summary>
+		/// The target index created
+		/// </summary>
+		[DataMember(Name = "index")]
+		public string Index { get; set; }
+	}
+}
