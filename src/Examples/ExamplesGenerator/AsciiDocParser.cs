dbd7e25920fb488adb19c6d9bcb9229cbc1b82fb
Russ Cam <russ.cam@elastic.co>
2019-10-21 10:48:57 +1000

    Use JSON examples reference
    This commit updates the examples generation to use the JSON reference file.

--- src/Examples/ExamplesGenerator/AsciiDocParser.cs
+++ src/Examples/ExamplesGenerator/AsciiDocParser.cs
@@ -4,63 +4,52 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace ExamplesGenerator
 {
 	internal static class AsciiDocParser
 	{
-		private static readonly Regex TitleRegex =
-			new Regex(@"=== (?<name>.*?)\.a(?:scii)?doc: line (?<lineNumber>\d+): (?<hash>.*?)(?:\.a(?:scii)?doc)?$");
+		private static readonly Regex NameRegex =
+			new Regex(@"(?<name>.*?)\.a(?:scii)?doc$");
 
 		public static List<ReferencePage> ParsePages(string path)
 		{
-			var file = GetLinesFromPath(path);
 			var pages = new Dictionary<string, ReferencePage>();
 
-			for (var index = 0; index < file.Length; index++)
+			using (var stream = GetStreamFromPath(path))
+			using (var document = JsonDocument.Parse(stream))
 			{
-				var line = file[index];
-				if (line.StartsWith("=== "))
+				var i = 0;
+				foreach (var element in document.RootElement.EnumerateArray())
 				{
-					var match = TitleRegex.Match(line);
+					var lang = element.GetProperty("lang").GetString();
+
+					// skip anything other than console examples for now, such
+					// as console-result.
+					if (lang != "console")
+						continue;
+
+					var sourceLocation = element.GetProperty("source_location");
+					var file = sourceLocation.GetProperty("file").GetString();
+					var match = NameRegex.Match(file);
 					if (!match.Success)
 					{
-						Console.WriteLine($"Could not find title match, line: {index}, input: {line}");
+						Console.WriteLine($"Could not find title match, index {i}");
 						continue;
 					}
 
 					var name = match.Groups["name"].Value;
-					var lineNumber = int.Parse(match.Groups["lineNumber"].Value);
-					var hash = match.Groups["hash"].Value;
+					var lineNumber = sourceLocation.GetProperty("line").GetInt32();
+					var hash = element.GetProperty("digest").GetString();
 
-					// skip to start of body
-					index += 3;
-					line = file[index];
-					var builder = new StringBuilder();
-
-					while (line != "----")
-					{
-						builder.AppendLine(line);
-						index++;
-						line = file[index];
-					}
-
-					var content = builder.ToString();
-
-					index += 2;
-					line = file[index];
-
-					var languages = line.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-					index += 2;
-					line = file[index];
-					var implemented = line.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-
-					var exampleLanguages = languages
-						.Zip(implemented, (l, i) => new Language { Name = l.Trim(), Implemented = i.Trim() == "&check;" })
+					var languages = element.GetProperty("found")
+						.EnumerateArray()
+						.Select(s => new Language { Name = s.GetString(), Implemented = true })
 						.ToList();
 
-					index++;
+					var content = element.GetProperty("source").GetString();
 
 					if (!pages.TryGetValue(name, out var page))
 					{
@@ -69,40 +58,34 @@ namespace ExamplesGenerator
 					}
 
 					var example = new ReferenceExample(hash, lineNumber, content);
-					example.Languages.AddRange(exampleLanguages);
+					example.Languages.AddRange(languages);
 
 					if (!page.Examples.Contains(example))
 						page.Examples.Add(example);
+
+					i++;
 				}
 			}
 
 			return pages.Values.ToList();
 		}
 
-		private static string[] GetLinesFromPath(string path)
+		private static Stream GetStreamFromPath(string path)
 		{
 			var uri = new Uri(path);
 
 			if (uri.IsFile)
-				return File.ReadAllLines(path);
+				return File.OpenRead(path);
 
 			using (var client = new WebClient())
 			{
 				try
 				{
-					var lines = new List<string>();
-					using (var stream = client.OpenRead(path))
-					using (var streamReader = new StreamReader(stream, Encoding.UTF8))
-					{
-						string str;
-						while ((str = streamReader.ReadLine()) != null)
-							lines.Add(str);
-					}
-					return lines.ToArray();
+					return client.OpenRead(path);
 				}
 				catch (Exception e)
 				{
-					throw new Exception($"Could not download master reference file from {path}", e);
+					throw new Exception($"Could not download reference file from {path}", e);
 				}
 			}
 		}
