33b0fcbd15dbe738b171dd89e7813850e7df39ef
Martijn Laarman <Mpdreamz@gmail.com>
2019-11-12 13:44:22 +0100

    add comment explaining the check for possible fast path in sourceformatter
    (cherry picked from commit ffc4e46322da16bc40c91848537272678f891fea)

--- src/Nest/CommonAbstractions/SerializationBehavior/JsonFormatters/SourceFormatter.cs
+++ src/Nest/CommonAbstractions/SerializationBehavior/JsonFormatters/SourceFormatter.cs
@@ -30,8 +30,13 @@ namespace Nest
 			var settings = formatterResolver.GetConnectionSettings();
 
 			// avoid serialization to bytes when not using custom source serializer
-			if (ReferenceEquals(settings.SourceSerializer, settings.RequestResponseSerializer)
-				|| settings.SourceSerializer is IInternalSerializerWithFormatter s && s.FormatterResolver != null)
+			// This used to check for reference of the source serializer and the request response serializer
+			// However each now gets wrapped in a new `DiagnosticsSerializerProxy` so this check no longer works
+			// therefor we now check if the SourceSerializer is internal with a formatter.
+			// DiagnosticsSerializerProxy implements this interface, it simply proxies to whatever it wraps so
+			// we need to assert the resolver is not actually null here since it can wrap something that is not
+			// `IInternalSerializerWithFormatter`
+			if (settings.SourceSerializer is IInternalSerializerWithFormatter s && s.FormatterResolver != null)
 			{
 				formatterResolver.GetFormatter<T>().Serialize(ref writer, value, formatterResolver);
 				return;
