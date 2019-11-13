e837dd07ede5e747fccf5ecd5384d5fb65157cfb
Russ Cam <russ.cam@elastic.co>
2019-11-13 15:24:32 +1000

    Serialize own IProperty implementations (#4219)
    This commit fixes the ability to serialize own IProperty types within PropertyFormatter, using utf8json. Initially, this feature was not originally available in utf8json due to how enums with the same underlying values but different field names were serialized, and how this interacts with internal enums of attribute runtime types. Since this is now fixed in #elastic/elasticsearch-net/4032, custom IProperty implementations will now serialize correctly.

--- src/Tests/Tests/ClientConcepts/HighLevel/Serialization/ExtendingNestTypes.doc.cs
+++ src/Tests/Tests/ClientConcepts/HighLevel/Serialization/ExtendingNestTypes.doc.cs
@@ -16,19 +16,22 @@ namespace Tests.ClientConcepts.HighLevel.Serialization
 	/**[[extending-nest-types]]
 	 * === Extending NEST types
 	 *
-	 * Sometimes you might want to provide a custom implementation of a type perhaps to work around an issue or because
+	 * Sometimes you might want to provide a custom implementation of a type, perhaps to work around an issue or because
 	 * you're using a third-party plugin that extends the features of Elasticsearch, and NEST does not provide support out of the box.
+	 *
+	 * NEST allows extending its types in some scenarios, discussed here.
+	 *
+	 * ==== Creating your own property mapping
+	 *
+	 * As an example, let's imagine we're using a third party plugin that provides support for additional data type
+	 * for field mapping. We can implement a custom `IProperty` implementation so that we can use the field mapping
+	 * type with NEST.
 	 */
 	public class ExtendingNestTypes
 	{
-		private readonly IElasticClient _client = TestClient.DisabledStreaming;
+		// keep field name as client, for documentation purposes
+		private readonly IElasticClient client = TestClient.DisabledStreaming;
 
-		/* ==== Creating your own property mapping
-		 *
-		 * As an example, let's imagine we're using a third party plugin that provides support for additional data type
-		 * for field mapping. We can implement a custom `IProperty` implementation so that we can use the field mapping
-		 * type with NEST.
-		 */
 		public class MyPluginProperty : IProperty
 		{
 			IDictionary<string, object> IProperty.LocalMetadata { get; set; }
@@ -48,10 +51,9 @@ namespace Tests.ClientConcepts.HighLevel.Serialization
 			[PropertyName("numeric")]
 			public bool Numeric { get; set; }
 		}
-		
 
-		[U (Skip = "TODO: Does not work with utf8json")]
-		// hide
+
+		[U]
 		public void InjectACustomIPropertyImplementation()
 		{
 			/**
@@ -60,7 +62,7 @@ namespace Tests.ClientConcepts.HighLevel.Serialization
 			 *
 			 * Now that we have our own `IProperty` implementation we can add it to our propertes mapping when creating an index
 			 */
-			var createIndexResponse = _client.Indices.Create("myindex", c => c
+			var createIndexResponse = client.Indices.Create("myindex", c => c
 				.Map<Project>(m => m
 					.Properties(props => props
 						.Custom(new MyPluginProperty("fieldName", "dutch"))
@@ -90,7 +92,7 @@ namespace Tests.ClientConcepts.HighLevel.Serialization
 
 			/**
 			 * Whilst NEST can _serialize_ our `my_plugin_property`, it does not know how to _deserialize_ it;
-			 * We plan to make this more pluggable in the future
+			 * We plan to make this more pluggable in the future.
 			 */
 			// hide
 			Expect(expected).FromRequest(createIndexResponse);
