780f52f4cb678470d2615f9a76ad2c2318c6c5be
Russ Cam <russ.cam@elastic.co>
2019-11-13 10:14:37 +1000

    Deserialize anonymous bucket with only doc_count (#4214)
    This commit deserializes an anonymous bucket that contains only a doc_count. Existing tests cover the case where the bucket has sub-aggregations, but did not handle the case where there are none.
    
    Fixes #4197

---
+++ src/Tests/Tests.Reproduce/GithubIssue4197.cs
@@ -0,0 +1,50 @@
+using System;
+using Elastic.Xunit.XunitPlumbing;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Tests.Core.ManagedElasticsearch.Clusters;
+
+namespace Tests.Reproduce
+{
+	public class GithubIssue4197 : IClusterFixture<WritableCluster>
+	{
+		private readonly IElasticClient _client;
+
+		public GithubIssue4197(WritableCluster cluster) => _client = cluster.Client;
+
+		[I]
+		public void CanDeserializeAnonymousFiltersAggregation()
+		{
+			const string index = "github-issue-4197";
+
+			_client.Indices.Create(index);
+
+			_client.Index(new Doc { ModificationDate = DateTime.Parse("2019-10-09T10:43:07.8633456+02:00") },
+				i => i.Index(index).Refresh(Refresh.WaitFor));
+
+			var searchResponse = _client.Search<Doc>(s => s
+				.Index(index)
+				.Aggregations(a => a
+					.Filters("Modification date", f => f
+						.AnonymousFilters(q => q
+							.DateRange(dr => dr
+								.Field(d => d.ModificationDate)
+								.GreaterThan(DateMath.Now.Subtract(TimeSpan.FromDays(120)))
+							)
+						)
+					)
+				)
+			);
+
+			var filtersAggregate = searchResponse.Aggregations.Filters("Modification date");
+			filtersAggregate.AnonymousBuckets().Count.Should().Be(1);
+			filtersAggregate.AnonymousBuckets()[0].DocCount.Should().Be(1);
+		}
+
+		private class Doc
+		{
+			public DateTime ModificationDate { get; set; }
+		}
+	}
+}
