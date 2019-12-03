019da7d87292133d7c61b977b0779740bdbba0eb
Stuart Cam <stuart.cam@elastic.co>
2019-12-03 17:15:32 +1100

    Url tests

---
+++ src/Tests/Tests/XPack/Slm/StartLifecycle/StartLifecycleUrlTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using Nest;
+using Tests.Framework.EndpointTests;
+using static Tests.Framework.EndpointTests.UrlTester;
+
+namespace Tests.XPack.Slm.StartLifecycle
+{
+	public class StartLifecycleUrlTests : UrlTestsBase
+	{
+		[U] public override async Task Urls() =>
+			await POST("/_slm/start")
+				.Fluent(c => c.SnapshotLifecycleManagement.Start())
+				.Request(c => c.SnapshotLifecycleManagement.Start(new StartSlmRequest()))
+				.FluentAsync(c => c.SnapshotLifecycleManagement.StartAsync())
+				.RequestAsync(c => c.SnapshotLifecycleManagement.StartAsync(new StartSlmRequest()));
+	}
+}
---
+++ src/Tests/Tests/XPack/Slm/StopLifecycle/StopLifecycleUrlTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using Nest;
+using Tests.Framework.EndpointTests;
+using static Tests.Framework.EndpointTests.UrlTester;
+
+namespace Tests.XPack.Slm.StopLifecycle
+{
+	public class StopLifecycleUrlTests : UrlTestsBase
+	{
+		[U] public override async Task Urls() =>
+			await POST("/_slm/stop")
+				.Fluent(c => c.SnapshotLifecycleManagement.Stop())
+				.Request(c => c.SnapshotLifecycleManagement.Stop(new StopSlmRequest()))
+				.FluentAsync(c => c.SnapshotLifecycleManagement.StopAsync())
+				.RequestAsync(c => c.SnapshotLifecycleManagement.StopAsync(new StopSlmRequest()));
+	}
+}
