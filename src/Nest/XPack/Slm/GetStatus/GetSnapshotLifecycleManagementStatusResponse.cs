fae4565a62463ec11a183642a6f78595d5092c73
Stuart Cam <stuart.cam@elastic.co>
2019-12-13 18:42:18 +1100

    Add SLM Start/Stop/Status APIs (#4245)
    (cherry picked from commit d4c6b802a15d74e9d964210cb4c5af126d5f3a5b)

---
+++ src/Nest/XPack/Slm/GetStatus/GetSnapshotLifecycleManagementStatusResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.Serialization;
+
+namespace Nest
+{
+	public class GetSnapshotLifecycleManagementStatusResponse : ResponseBase
+	{
+		[DataMember(Name = "operation_mode")]
+		public LifecycleOperationMode OperationMode { get; internal set; }
+	}
+}
---
+++ src/Nest/XPack/Slm/Stop/StopSnapshotLifecycleManagementRequest.cs
@@ -0,0 +1,9 @@
+namespace Nest
+{
+	[MapsApi("slm.stop")]
+	public partial interface IStopSnapshotLifecycleManagementRequest { }
+
+	public partial class StopSnapshotLifecycleManagementRequest { }
+
+	public partial class StopSnapshotLifecycleManagementDescriptor { }
+}
