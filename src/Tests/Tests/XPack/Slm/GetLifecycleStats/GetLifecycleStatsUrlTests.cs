18b89848142b1f2a284ba3a59f1246809d910b0c
Stuart Cam <stuart.cam@elastic.co>
2019-12-13 15:51:44 +1100

    Add /_slm/stats Url tests

---
+++ src/Tests/Tests/XPack/Slm/GetLifecycleStats/GetLifecycleStatsUrlTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using Nest;
+using Tests.Framework.EndpointTests;
+using static Tests.Framework.EndpointTests.UrlTester;
+
+namespace Tests.XPack.Slm.StartLifecycle
+{
+	public class GetLifecycleStatsUrlTests : UrlTestsBase
+	{
+		[U] public override async Task Urls() =>
+			await GET("/_slm/stats")
+				.Fluent(c => c.SnapshotLifecycleManagement.GetSnapshotLifecycleStats())
+				.Request(c => c.SnapshotLifecycleManagement.GetSnapshotLifecycleStats(new GetSnapshotLifecycleStatsRequest()))
+				.FluentAsync(c => c.SnapshotLifecycleManagement.GetSnapshotLifecycleStatsAsync())
+				.RequestAsync(c => c.SnapshotLifecycleManagement.GetSnapshotLifecycleStatsAsync(new GetSnapshotLifecycleStatsRequest()));
+	}
+}
