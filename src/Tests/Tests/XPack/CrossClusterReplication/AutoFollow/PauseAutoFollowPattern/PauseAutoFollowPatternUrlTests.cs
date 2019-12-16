17b8558691904b1eec34c68a06d27a4a62effbca
Stuart Cam <stuart.cam@elastic.co>
2019-12-16 18:08:28 +1100

    Add url tests

---
+++ src/Tests/Tests/XPack/CrossClusterReplication/AutoFollow/PauseAutoFollowPattern/PauseAutoFollowPatternUrlTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using Nest;
+using Tests.Framework.EndpointTests;
+
+namespace Tests.XPack.CrossClusterReplication.AutoFollow.PauseAutoFollowPattern
+{
+	public class PauseAutoFollowPatternUrlTests : UrlTestsBase
+	{
+		[U] public override async Task Urls()
+		{
+			var name = "x";
+			await UrlTester.POST($"/_ccr/auto_follow/{name}/pause")
+				.Fluent(c => c.CrossClusterReplication.PauseAutoFollowPattern(name))
+				.Request(c => c.CrossClusterReplication.PauseAutoFollowPattern(new PauseAutoFollowPatternRequest(name)))
+				.FluentAsync(c => c.CrossClusterReplication.PauseAutoFollowPatternAsync(name))
+				.RequestAsync(c => c.CrossClusterReplication.PauseAutoFollowPatternAsync(new PauseAutoFollowPatternRequest(name)));
+		}
+	}
+}
