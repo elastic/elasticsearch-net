6b1575d58c0eed503fc8f7b649ad2342757c161e
Martijn Laarman <Mpdreamz@gmail.com>
2019-12-17 22:54:10 +0100

    simplify our props and targets files

--- tests/Tests.Domain/Metric.cs
+++ tests/Tests.Domain/Metric.cs
@@ -1,6 +1,4 @@
 using System;
-using Elasticsearch.Net.Utf8Json;
-using Elasticsearch.Net.Utf8Json.Formatters;
 using Nest;
 
 namespace Tests.Domain
@@ -19,45 +17,8 @@ namespace Tests.Domain
 
 		[Date(Name = "@timestamp")]
 		[MachineLearningDateTime]
-		[JsonFormatter(typeof(MachineLearningDateTimeFormatter))]
 		public DateTime Timestamp { get; set; }
 
 		public long Total { get; set; }
-	}
-
-	// Required as PreviewDatafeed API returns Timestamp values as epoch milliseconds, irrespective
-	// of the format in which it was originally indexed.
-	internal class MachineLearningDateTimeFormatter : IJsonFormatter<DateTime>
-	{
-		private static readonly DateTimeOffset Epoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
-
-		public DateTime Deserialize(ref JsonReader reader, IJsonFormatterResolver formatterResolver)
-		{
-			var token = reader.GetCurrentJsonToken();
-
-			if (token == JsonToken.String)
-			{
-				var formatter = formatterResolver.GetFormatter<DateTime>();
-				return formatter.Deserialize(ref reader, formatterResolver);
-			}
-
-			if (token == JsonToken.Null)
-			{
-				reader.ReadNext();
-				return default;
-			}
-
-			if (token == JsonToken.Number)
-			{
-				var millisecondsSinceEpoch = reader.ReadDouble();
-				var dateTimeOffset = Epoch.AddMilliseconds(millisecondsSinceEpoch);
-				return dateTimeOffset.DateTime;
-			}
-
-			throw new Exception($"Cannot deserialize {nameof(DateTimeOffset)} from token {token}");
-		}
-
-		public void Serialize(ref JsonWriter writer, DateTime value, IJsonFormatterResolver formatterResolver) =>
-			ISO8601DateTimeFormatter.Default.Serialize(ref writer, value, formatterResolver);
 	}
 }
