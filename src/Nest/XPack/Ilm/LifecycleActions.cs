4f64bc407a084fd9dc0954128e482b89905ad147
Russ Cam <russ.cam@elastic.co>
2020-02-06 15:10:12 +1000

    Add WaitForSnapshot ILM lifecycle action
    Relates: #4341
    
    This commit adds a wait_for_snapshot ILM lifecycle action

--- src/Nest/XPack/Ilm/LifecycleActions.cs
+++ src/Nest/XPack/Ilm/LifecycleActions.cs
@@ -15,23 +15,35 @@ namespace Nest
 
 		public LifecycleActions(IDictionary<string, ILifecycleAction> container) : base(container) { }
 
+		/// <inheritdoc cref="IAllocateLifecycleAction"/>
 		public void Add(IAllocateLifecycleAction action) => BackingDictionary.Add("allocate", action);
 
+		/// <inheritdoc cref="IDeleteLifecycleAction"/>
 		public void Add(IDeleteLifecycleAction action) => BackingDictionary.Add("delete", action);
 
+		/// <inheritdoc cref="IForceMergeLifecycleAction"/>
 		public void Add(IForceMergeLifecycleAction action) => BackingDictionary.Add("forcemerge", action);
 
+		/// <inheritdoc cref="IFreezeLifecycleAction"/>
 		public void Add(IFreezeLifecycleAction action) => BackingDictionary.Add("freeze", action);
 
+		/// <inheritdoc cref="IReadOnlyLifecycleAction"/>
 		public void Add(IReadOnlyLifecycleAction action) => BackingDictionary.Add("readonly", action);
 
+		/// <inheritdoc cref="IRolloverLifecycleAction"/>
 		public void Add(IRolloverLifecycleAction action) => BackingDictionary.Add("rollover", action);
 
+		/// <inheritdoc cref="ISetPriorityLifecycleAction"/>
 		public void Add(ISetPriorityLifecycleAction action) => BackingDictionary.Add("set_priority", action);
 
+		/// <inheritdoc cref="IShrinkLifecycleAction"/>
 		public void Add(IShrinkLifecycleAction action) => BackingDictionary.Add("shrink", action);
 
+		/// <inheritdoc cref="IUnfollowLifecycleAction"/>
 		public void Add(IUnfollowLifecycleAction action) => BackingDictionary.Add("unfollow", action);
+
+		/// <inheritdoc cref="IWaitForSnapshotLifecycleAction"/>
+		public void Add(IWaitForSnapshotLifecycleAction action) => BackingDictionary.Add("wait_for_snapshot", action);
 	}
 
 	internal class LifecycleActionsJsonFormatter : IJsonFormatter<ILifecycleActions>
@@ -47,6 +59,7 @@ namespace Nest
 			{ "set_priority", 6 },
 			{ "shrink", 7 },
 			{ "unfollow", 8 },
+			{ "wait_for_snapshot", 9 },
 		};
 
 		public ILifecycleActions Deserialize(ref JsonReader reader, IJsonFormatterResolver formatterResolver)
@@ -102,6 +115,10 @@ namespace Nest
 							break;
 						case 8:
 							lifecycleAction = formatterResolver.GetFormatter<UnfollowLifecycleAction>()
+								.Deserialize(ref reader, formatterResolver);
+							break;
+						case 9:
+							lifecycleAction = formatterResolver.GetFormatter<WaitForSnapshotLifecycleAction>()
 								.Deserialize(ref reader, formatterResolver);
 							break;
 					}
@@ -161,6 +178,9 @@ namespace Nest
 					case "unfollow":
 						Serialize<IUnfollowLifecycleAction>(ref writer, action.Value, formatterResolver);
 						break;
+					case "wait_for_snapshot":
+						Serialize<IWaitForSnapshotLifecycleAction>(ref writer, action.Value, formatterResolver);
+						break;
 					default:
 						DynamicObjectResolver.ExcludeNullCamelCase.GetFormatter<ILifecycleAction>()
 							.Serialize(ref writer, action.Value, formatterResolver);
@@ -181,31 +201,44 @@ namespace Nest
 	{
 		public LifecycleActionsDescriptor() : base(new LifecycleActions()) { }
 
+		/// <inheritdoc cref="IAllocateLifecycleAction"/>
 		public LifecycleActionsDescriptor Allocate(Func<AllocateLifecycleActionDescriptor, IAllocateLifecycleAction> selector) =>
 			Assign("allocate", selector.InvokeOrDefault(new AllocateLifecycleActionDescriptor()));
 
+		/// <inheritdoc cref="IDeleteLifecycleAction"/>
 		public LifecycleActionsDescriptor Delete(Func<DeleteLifecycleActionDescriptor, IDeleteLifecycleAction> selector) =>
 			Assign("delete", selector.InvokeOrDefault(new DeleteLifecycleActionDescriptor()));
 
+		/// <inheritdoc cref="IForceMergeLifecycleAction"/>
 		public LifecycleActionsDescriptor ForceMerge(Func<ForceMergeLifecycleActionDescriptor, IForceMergeLifecycleAction> selector) =>
 			Assign("forcemerge", selector.InvokeOrDefault(new ForceMergeLifecycleActionDescriptor()));
 
+		/// <inheritdoc cref="IFreezeLifecycleAction"/>
 		public LifecycleActionsDescriptor Freeze(Func<FreezeLifecycleActionDescriptor, IFreezeLifecycleAction> selector) =>
 			Assign("freeze", selector.InvokeOrDefault(new FreezeLifecycleActionDescriptor()));
 
+		/// <inheritdoc cref="IReadOnlyLifecycleAction"/>
 		public LifecycleActionsDescriptor ReadOnly(Func<ReadOnlyLifecycleActionDescriptor, IReadOnlyLifecycleAction> selector) =>
 			Assign("readonly", selector.InvokeOrDefault(new ReadOnlyLifecycleActionDescriptor()));
 
+		/// <inheritdoc cref="IRolloverLifecycleAction"/>
 		public LifecycleActionsDescriptor Rollover(Func<RolloverLifecycleActionDescriptor, IRolloverLifecycleAction> selector) =>
 			Assign("rollover", selector.InvokeOrDefault(new RolloverLifecycleActionDescriptor()));
 
+		/// <inheritdoc cref="ISetPriorityLifecycleAction"/>
 		public LifecycleActionsDescriptor SetPriority(Func<SetPriorityLifecycleActionDescriptor, ISetPriorityLifecycleAction> selector) =>
 			Assign("set_priority", selector.InvokeOrDefault(new SetPriorityLifecycleActionDescriptor()));
 
+		/// <inheritdoc cref="IShrinkLifecycleAction"/>
 		public LifecycleActionsDescriptor Shrink(Func<ShrinkLifecycleActionDescriptor, IShrinkLifecycleAction> selector) =>
 			Assign("shrink", selector.InvokeOrDefault(new ShrinkLifecycleActionDescriptor()));
 
+		/// <inheritdoc cref="IUnfollowLifecycleAction"/>
 		public LifecycleActionsDescriptor Unfollow(Func<UnfollowLifecycleActionDescriptor, IUnfollowLifecycleAction> selector) =>
 			Assign("unfollow", selector.InvokeOrDefault(new UnfollowLifecycleActionDescriptor()));
+
+		/// <inheritdoc cref="IWaitForSnapshotLifecycleAction"/>
+		public LifecycleActionsDescriptor WaitForSnapshot(Func<WaitForSnapshotLifecycleActionDescriptor, IWaitForSnapshotLifecycleAction> selector) =>
+			Assign("wait_for_snapshot", selector.InvokeOrDefault(new WaitForSnapshotLifecycleActionDescriptor()));
 	}
 }
