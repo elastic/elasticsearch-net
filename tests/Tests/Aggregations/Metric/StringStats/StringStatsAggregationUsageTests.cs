cc45fb10fccff7195ac33760b8acbd87511ec28d
Russ Cam <russ.cam@elastic.co>
2020-02-10 11:10:27 +1000

    Add StringStats aggregation (#4370)
    This commit adds support for the string_stats aggregation introduced in
    Elasticsearch 7.6.0. It is a metric aggregation but does not implement
    IMetricAggregation because the type of Missing field is a string and not a
    double value. Missing is implemented as object as the hierarchy of
    metric aggregations will be changed as part of #4332, and Missing
    will be an object.
    
    The documentation for StringStats indicates that the distribution is returned
    in descending probability order, but are modelled as a JSON object. Following
    internal discussion, this is modelled as a dictionary on the response as it is
    considered this modelling will not diminish functionality.
    
    Closes #4369

---
+++ tests/Tests/Aggregations/Metric/StringStats/StringStatsAggregationUsageTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Elastic.Xunit.XunitPlumbing;
+using FluentAssertions;
+using Nest;
+using Tests.Core.Extensions;
+using Tests.Core.ManagedElasticsearch.Clusters;
+using Tests.Domain;
+using Tests.Framework.EndpointTests.TestState;
+using static Nest.Infer;
+
+namespace Tests.Aggregations.Metric.StringStats
+{
+	[SkipVersion("<7.6.0", "Available in 7.6.0 with at least basic license level")]
+	public class StringStatsAggregationUsageTests : AggregationUsageTestBase
+	{
+		public StringStatsAggregationUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
+
+		protected override object AggregationJson => new
+		{
+			name_stats = new
+			{
+				string_stats = new
+				{
+					field = "name"
+				}
+			}
+		};
+
+		protected override Func<AggregationContainerDescriptor<Project>, IAggregationContainer> FluentAggs => a => a
+			.StringStats("name_stats", st => st
+				.Field(p => p.Name)
+			);
+
+		protected override AggregationDictionary InitializerAggs =>
+			new StringStatsAggregation("name_stats", Field<Project>(p => p.Name));
+
+		protected override void ExpectResponse(ISearchResponse<Project> response)
+		{
+			response.ShouldBeValid();
+			var commitStats = response.Aggregations.StringStats("name_stats");
+			commitStats.Should().NotBeNull();
+			commitStats.AverageLength.Should().BeGreaterThan(0);
+			commitStats.MaxLength.Should().BeGreaterThan(0);
+			commitStats.MinLength.Should().BeGreaterThan(0);
+			commitStats.Count.Should().BeGreaterThan(0);
+			commitStats.Distribution.Should().NotBeNull().And.BeEmpty();
+		}
+	}
+
+	// hide
+	[SkipVersion("<7.6.0", "Available in 7.6.0 with at least basic license level")]
+	public class StringStatsWithDistributionAggregationUsageTests : AggregationUsageTestBase
+	{
+		public StringStatsWithDistributionAggregationUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
+
+		protected override object AggregationJson => new
+		{
+			name_stats = new
+			{
+				string_stats = new
+				{
+					field = "name",
+					show_distribution = true
+				}
+			}
+		};
+
+		protected override Func<AggregationContainerDescriptor<Project>, IAggregationContainer> FluentAggs => a => a
+			.StringStats("name_stats", st => st
+				.Field(p => p.Name)
+				.ShowDistribution()
+			);
+
+		protected override AggregationDictionary InitializerAggs =>
+			new StringStatsAggregation("name_stats", Field<Project>(p => p.Name))
+			{
+				ShowDistribution = true
+			};
+
+		protected override void ExpectResponse(ISearchResponse<Project> response)
+		{
+			response.ShouldBeValid();
+			var commitStats = response.Aggregations.StringStats("name_stats");
+			commitStats.Should().NotBeNull();
+			commitStats.AverageLength.Should().BeGreaterThan(0);
+			commitStats.MaxLength.Should().BeGreaterThan(0);
+			commitStats.MinLength.Should().BeGreaterThan(0);
+			commitStats.Count.Should().BeGreaterThan(0);
+			commitStats.Distribution.Should().NotBeNull().And.NotBeEmpty();
+		}
+	}
+}
