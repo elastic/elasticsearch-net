d439a8b87d368e6dbbf2b07fe8b958d63d60907d
Russ Cam <russ.cam@elastic.co>
2020-02-20 15:15:19 +1000

    Add xml comment to GeoShapeRelation.Contains
    This commit adds a comment to GeoShapeRelation.Contains to
    indicate where it is supported.
    
    (cherry picked from commit 916214e7b699002d291e098356cc7bc45a5adab0)

--- src/Nest/CommonOptions/Geo/GeoShapeRelation.cs
+++ src/Nest/CommonOptions/Geo/GeoShapeRelation.cs
@@ -6,15 +6,29 @@ namespace Nest
 	[StringEnum]
 	public enum GeoShapeRelation
 	{
+		/// <summary>
+		/// INTERSECTS relation.
+		/// </summary>
 		[EnumMember(Value = "intersects")]
 		Intersects,
 
+		/// <summary>
+		/// DISJOINT relation.
+		/// </summary>
 		[EnumMember(Value = "disjoint")]
 		Disjoint,
 
+		/// <summary>
+		/// WITHIN relation.
+		/// </summary>
 		[EnumMember(Value = "within")]
 		Within,
 
+		/// <summary>
+		/// CONTAINS relation.
+		/// <para />
+		/// Supported for indices created with Elasticsearch 7.5.0+
+		/// </summary>
 		[EnumMember(Value = "contains")]
 		Contains
 	}
