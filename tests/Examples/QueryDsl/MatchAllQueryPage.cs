bed4b0e45a71c167341bb35fa149dddda631234f
Stuart Cam <stuart.cam@elastic.co>
2020-03-03 20:34:17 +1100

    Implement https://www.elastic.co/guide/en/elasticsearch/reference/master/query-dsl-match-all-query.html

--- tests/Examples/QueryDsl/MatchAllQueryPage.cs
+++ tests/Examples/QueryDsl/MatchAllQueryPage.cs
@@ -5,14 +5,16 @@ namespace Examples.QueryDsl
 {
 	public class MatchAllQueryPage : ExampleBase
 	{
-		[U(Skip = "Example not implemented")]
+		[U]
 		public void Line11()
 		{
 			// tag::09d617863a103c82fb4101e6165ea7fe[]
-			var response0 = new SearchResponse<object>();
+			var searchResponse = client.Search<object>(s => s
+				.AllIndices()
+				.MatchAll(m => m));
 			// end::09d617863a103c82fb4101e6165ea7fe[]
 
-			response0.MatchesExample(@"GET /_search
+			searchResponse.MatchesExample(@"GET /_search
 			{
 			    ""query"": {
 			        ""match_all"": {}
@@ -20,14 +22,16 @@ namespace Examples.QueryDsl
 			}");
 		}
 
-		[U(Skip = "Example not implemented")]
+		[U]
 		public void Line23()
 		{
 			// tag::75330ec1305d2beb0e2f34d2195464e2[]
-			var response0 = new SearchResponse<object>();
+			var searchResponse = client.Search<object>(s => s
+				.AllIndices()
+				.MatchAll(m => m.Boost(1.2)));
 			// end::75330ec1305d2beb0e2f34d2195464e2[]
 
-			response0.MatchesExample(@"GET /_search
+			searchResponse.MatchesExample(@"GET /_search
 			{
 			    ""query"": {
 			        ""match_all"": { ""boost"" : 1.2 }
@@ -35,14 +39,17 @@ namespace Examples.QueryDsl
 			}");
 		}
 
-		[U(Skip = "Example not implemented")]
+		[U]
 		public void Line39()
 		{
 			// tag::81c9aa2678d6166a9662ddf2c011a6a5[]
-			var response0 = new SearchResponse<object>();
+			var searchResponse = client.Search<object>(s => s
+				.AllIndices()
+				.Query(q => q.MatchNone())
+			);
 			// end::81c9aa2678d6166a9662ddf2c011a6a5[]
 
-			response0.MatchesExample(@"GET /_search
+			searchResponse.MatchesExample(@"GET /_search
 			{
 			    ""query"": {
 			        ""match_none"": {}
