c74a8a841fc27847782da23f835bf14207c4e2f5
Martijn Laarman <Mpdreamz@gmail.com>
2020-03-05 11:26:36 +0100

    Update .ci folder (#4484)
    * update wait-for-container.sh
    
    * update certs and make sure run-repository can accept external elasticsearch url
    
    * remove pem format
    
    (cherry picked from commit 44530ae18b535fe001861acf28d6f95b02b87cbf)

--- .ci/run-repository.sh
+++ .ci/run-repository.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 # parameters are available to this script
 
-# ELASTICSEARCH_VERSION -- version e.g Major.Minor.Patch(-Prelease)
-# ELASTICSEARCH_CONTAINER -- the docker moniker as a reference to know which docker image distribution is used
-# ELASTICSEARCH_URL -- The url at which elasticsearch is reachable
-# NETWORK_NAME -- The docker network name
-# NODE_NAME -- The docker container name also used as Elasticsearch node name
+# STACK_VERSION -- version e.g Major.Minor.Patch(-Prelease)
+# TEST_SUITE -- which test suite to run: oss or xpack
+# ELASTICSEARCH_URL -- The url at which elasticsearch is reachable, a default is composed based on STACK_VERSION and TEST_SUITE
 # DOTNET_VERSION -- SDK version (defined in test-matrix.yml, a default is hardcoded here)
 script_path=$(dirname $(realpath -s $0))
 source $script_path/functions/imports.sh
 set -euo pipefail
 
 DOTNET_VERSION=${DOTNET_VERSION-3.0.100}
-
+ELASTICSEARCH_URL=${ELASTICSEARCH_URL-"$elasticsearch_url"}
+elasticsearch_container=${elasticsearch_container-}
+ 
 echo -e "\033[34;1mINFO:\033[0m VERSION ${STACK_VERSION}\033[0m"
 echo -e "\033[34;1mINFO:\033[0m TEST_SUITE ${TEST_SUITE}\033[0m"
-echo -e "\033[34;1mINFO:\033[0m DOTNET_VERSION ${DOTNET_VERSION}\033[0m"
-echo -e "\033[34;1mINFO:\033[0m URL ${elasticsearch_url}\033[0m"
+echo -e "\033[34;1mINFO:\033[0m URL ${ELASTICSEARCH_URL}\033[0m"
 echo -e "\033[34;1mINFO:\033[0m CONTAINER ${elasticsearch_container}\033[0m"
-
+echo -e "\033[34;1mINFO:\033[0m DOTNET_VERSION ${DOTNET_VERSION}\033[0m"
+ 
 echo -e "\033[1m>>>>> Build [elastic/elasticsearch-net container] >>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m"
 
 docker build --file .ci/DockerFile --tag elastic/elasticsearch-net .
@@ -36,4 +36,4 @@ docker run \
   --volume ${repo}/build/output:/sln/build/output \
   --rm \
   elastic/elasticsearch-net \
-  ./build.sh rest-spec-tests $TEST_SUITE -e ${elasticsearch_url} -o /sln/build/output/rest-spec-junit.xml
+  ./build.sh rest-spec-tests $TEST_SUITE -e ${ELASTICSEARCH_URL} -o /sln/build/output/rest-spec-junit.xml
