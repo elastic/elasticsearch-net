e0990c4e37da9b0c5d24f75b01c766e4566af41e
Stuart Cam <stuart.cam@elastic.co>
2020-04-02 19:37:13 +1100

    This commit adds some missing roles to NodeRole and some additional properties to NodeInfo.
    Fixes broken integration test against 7.7.0-SNAPSHOT

--- src/Nest/Cluster/NodesInfo/NodeInfo.cs
+++ src/Nest/Cluster/NodesInfo/NodeInfo.cs
@@ -8,35 +8,32 @@ namespace Nest
 	[DataContract]
 	public class NodeInfo
 	{
-		[DataMember(Name = "build_hash")]
-		public string BuildHash { get; internal set; }
+		[DataMember(Name = "name")]
+		public string Name { get; internal set; }
+
+		[DataMember(Name = "transport_address")]
+		public string TransportAddress { get; internal set; }
 
 		[DataMember(Name = "host")]
 		public string Host { get; internal set; }
 
-		[DataMember(Name = "http")]
-		public NodeInfoHttp Http { get; internal set; }
-
 		[DataMember(Name = "ip")]
 		public string Ip { get; internal set; }
 
-		[DataMember(Name = "jvm")]
-		public NodeJvmInfo Jvm { get; internal set; }
+		[DataMember(Name = "version")]
+		public string Version { get; internal set; }
 
-		[DataMember(Name = "name")]
-		public string Name { get; internal set; }
+		[DataMember(Name = "build_flavor")]
+		public string BuildFlavor { get; internal set; }
 
-		[DataMember(Name = "network")]
-		public NodeInfoNetwork Network { get; internal set; }
+		[DataMember(Name = "build_type")]
+		public string BuildType { get; internal set; }
 
-		[DataMember(Name = "os")]
-		public NodeOperatingSystemInfo OperatingSystem { get; internal set; }
+		[DataMember(Name = "build_hash")]
+		public string BuildHash { get; internal set; }
 
-		[DataMember(Name = "plugins")]
-		public List<PluginStats> Plugins { get; internal set; }
-
-		[DataMember(Name = "process")]
-		public NodeProcessInfo Process { get; internal set; }
+		[DataMember(Name = "total_indexing_buffer")]
+		public long TotalIndexingBuffer { get; internal set; }
 
 		/// <summary>
 		/// All of the different roles that the node fulfills. An empty
@@ -45,8 +42,29 @@ namespace Nest
 		[DataMember(Name = "roles")]
 		public List<NodeRole> Roles { get; internal set; }
 
+		[DataMember(Name ="attributes")]
+		public IReadOnlyDictionary<string, string> Attributes { get; internal set; } = EmptyReadOnly<string, string>.Dictionary;
+
 		[DataMember(Name = "settings")]
 		public DynamicDictionary Settings { get; internal set; }
+
+		[DataMember(Name = "os")]
+		public NodeOperatingSystemInfo OperatingSystem { get; internal set; }
+
+		[DataMember(Name = "process")]
+		public NodeProcessInfo Process { get; internal set; }
+
+		[DataMember(Name = "jvm")]
+		public NodeJvmInfo Jvm { get; internal set; }
+
+		[DataMember(Name = "http")]
+		public NodeInfoHttp Http { get; internal set; }
+
+		[DataMember(Name = "network")]
+		public NodeInfoNetwork Network { get; internal set; }
+
+		[DataMember(Name = "plugins")]
+		public List<PluginStats> Plugins { get; internal set; }
 
 		[DataMember(Name = "thread_pool")]
 		[JsonFormatter(typeof(VerbatimInterfaceReadOnlyDictionaryKeysFormatter<string, NodeThreadPoolInfo>))]
@@ -54,12 +72,6 @@ namespace Nest
 
 		[DataMember(Name = "transport")]
 		public NodeInfoTransport Transport { get; internal set; }
-
-		[DataMember(Name = "transport_address")]
-		public string TransportAddress { get; internal set; }
-
-		[DataMember(Name = "version")]
-		public string Version { get; internal set; }
 	}
 
 	[DataContract]
