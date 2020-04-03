7311747758ebf778b5703b20ae0a2ccf68be2c12
Stuart Cam <stuart.cam@elastic.co>
2020-04-03 15:39:09 +1100

    Fixes integration tests against latest 7.7.0-SNAPSHOT (#4545)
    - Change date histogram aggregations to use minimum document = 0 in order to pass server validation.
    - Changes terms query to use a string value to pass server validation
    - Change search template API test to not tie to a specific server message substring
    
    Fixes #4521
    Fixes #4508

--- tests/Tests/Aggregations/Pipeline/MovingFunction/MovingFunctionAggregationUsageTests.cs
+++ tests/Tests/Aggregations/Pipeline/MovingFunction/MovingFunctionAggregationUsageTests.cs
@@ -34,6 +34,7 @@ namespace Tests.Aggregations.Pipeline.MovingFunction
 				{
 					field = "startedOn",
 					interval = "month",
+					min_doc_count = 0
 				},
 				aggs = new
 				{
@@ -63,6 +64,7 @@ namespace Tests.Aggregations.Pipeline.MovingFunction
 			.DateHistogram("projects_started_per_month", dh => dh
 				.Field(p => p.StartedOn)
 				.Interval(DateInterval.Month)
+				.MinimumDocumentCount(0)
 				.Aggregations(aa => aa
 					.Sum("commits", sm => sm
 						.Field(p => p.NumberOfCommits)
@@ -81,6 +83,7 @@ namespace Tests.Aggregations.Pipeline.MovingFunction
 			{
 				Field = "startedOn",
 				Interval = DateInterval.Month,
+				MinimumDocumentCount = 0,
 				Aggregations =
 					new SumAggregation("commits", "numberOfCommits")
 					&& new MovingFunctionAggregation("commits_moving_avg", "commits")
