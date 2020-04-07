a657a889b8bf8d9435da2e440369ef5a1d1f67e5
Martijn Laarman <Mpdreamz@gmail.com>
2020-04-07 12:20:52 +0200

    allow low level config to influence API names too
    (cherry picked from commit 2ee1a4eb6b71eff9a9969bb897d037deadadeaf8)

--- src/ApiGenerator/Configuration/CodeConfiguration.cs
+++ src/ApiGenerator/Configuration/CodeConfiguration.cs
@@ -74,10 +74,20 @@ namespace ApiGenerator.Configuration
 
 
 		/// <summary>
+		/// Map API default names for API's we are only supporting on the low level client first
+		/// </summary>
+		private static readonly Dictionary<string, string> LowLevelApiNameMapping = new Dictionary<string, string>
+		{
+			{ "indices.delete_index_template", "DeleteIndexTemplateV2" },
+			{ "indices.get_index_template", "GetIndexTemplateV2" },
+			{ "indices.put_index_template", "PutIndexTemplateV2" }
+		};
+
+		/// <summary>
 		/// Scan all nest source code files for Requests and look for the [MapsApi(filename)] attribute.
 		/// The class name minus Request is used as the canonical .NET name for the API.
 		/// </summary>
-		public static readonly Dictionary<string, string> ApiNameMapping =
+		private static readonly Dictionary<string, string> HighLevelApiNameMapping =
 			(from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*.cs", SearchOption.AllDirectories)
 				let contents = File.ReadAllText(f.FullName)
 				let c = Regex.Replace(contents, @"^.+\[MapsApi\(""([^ \r\n]+)""\)\].*$", "$1", RegexOptions.Singleline)
@@ -86,7 +96,28 @@ namespace ApiGenerator.Configuration
 			.DistinctBy(v => v.Key)
 			.ToDictionary(k => k.Key, v => v.Value.Replace(".cs", ""));
 
+		private static Dictionary<string, string> _apiNameMapping;
+
+		public static Dictionary<string, string> ApiNameMapping
+		{
+			get
+			{
+				if (_apiNameMapping != null) return _apiNameMapping;
+				lock (LowLevelApiNameMapping)
+				{
+					if (_apiNameMapping != null) return _apiNameMapping;
+
+					var mapping = HighLevelApiNameMapping;
+					foreach (var (k, v) in LowLevelApiNameMapping)
+						mapping[k] = v;
+					_apiNameMapping = mapping;
+					return _apiNameMapping;
+				}
+			}
+		}
+
 		private static readonly string ResponseBuilderAttributeRegex = @"^.+\[ResponseBuilderWithGeneric\(""([^ \r\n]+)""\)\].*$";
+
 		/// <summary>
 		/// Scan all nest source code files for Requests and look for the [MapsApi(filename)] attribute.
 		/// The class name minus Request is used as the canonical .NET name for the API.
@@ -113,19 +144,19 @@ namespace ApiGenerator.Configuration
 
 		/// <summary> Scan all NEST files for request interfaces and note any generics declared on them </summary>
 		private static readonly List<Tuple<string, string>> AllKnownRequestInterfaces = (
-			// find all files in NEST ending with Request.cs
-			from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*Request.cs", SearchOption.AllDirectories)
-			from l in File.ReadLines(f.FullName)
-			// attempt to locate all Request interfaces lines
-			where Regex.IsMatch(l, @"^.+interface [^ \r\n]+Request")
-			//grab the interface name including any generics declared on it
-			let c = Regex.Replace(l, @"^.+interface ([^ \r\n]+Request(?:<[^>\r\n]+>)?[^ \r\n]*).*$", "$1", RegexOptions.Singleline)
-			where c.StartsWith("I") && c.Contains("Request")
-			let request = Regex.Replace(c, "<.*$", "")
-			let generics = Regex.Replace(c, @"^.*?(?:(\<.+>).*?)?$", "$1")
-			select Tuple.Create(request,  generics)
+				// find all files in NEST ending with Request.cs
+				from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*Request.cs", SearchOption.AllDirectories)
+				from l in File.ReadLines(f.FullName)
+				// attempt to locate all Request interfaces lines
+				where Regex.IsMatch(l, @"^.+interface [^ \r\n]+Request")
+				//grab the interface name including any generics declared on it
+				let c = Regex.Replace(l, @"^.+interface ([^ \r\n]+Request(?:<[^>\r\n]+>)?[^ \r\n]*).*$", "$1", RegexOptions.Singleline)
+				where c.StartsWith("I") && c.Contains("Request")
+				let request = Regex.Replace(c, "<.*$", "")
+				let generics = Regex.Replace(c, @"^.*?(?:(\<.+>).*?)?$", "$1")
+				select Tuple.Create(request, generics)
 			)
-			.OrderBy(v=>v.Item1)
+			.OrderBy(v => v.Item1)
 			.ToList();
 
 		public static readonly HashSet<string> GenericOnlyInterfaces = new HashSet<string>(AllKnownRequestInterfaces
@@ -135,69 +166,67 @@ namespace ApiGenerator.Configuration
 			.ToList());
 
 		public static readonly HashSet<string> DocumentRequests = new HashSet<string>((
-			// find all files in NEST ending with Request.cs
-			from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*Request.cs", SearchOption.AllDirectories)
-			from l in File.ReadLines(f.FullName)
-			// attempt to locate all Request interfaces lines
-			where Regex.IsMatch(l, @"^.+interface [^ \r\n]+Request")
-			where l.Contains("IDocumentRequest")
-			let c = Regex.Replace(l, @"^.+interface ([^ \r\n]+Request(?:<[^>\r\n]+>)?[^ \r\n]*).*$", "$1", RegexOptions.Singleline)
-			//grab the interface name including any generics declared on it
-			let request = Regex.Replace(c, "<.*$", "")
-			select request
+				// find all files in NEST ending with Request.cs
+				from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*Request.cs", SearchOption.AllDirectories)
+				from l in File.ReadLines(f.FullName)
+				// attempt to locate all Request interfaces lines
+				where Regex.IsMatch(l, @"^.+interface [^ \r\n]+Request")
+				where l.Contains("IDocumentRequest")
+				let c = Regex.Replace(l, @"^.+interface ([^ \r\n]+Request(?:<[^>\r\n]+>)?[^ \r\n]*).*$", "$1", RegexOptions.Singleline)
+				//grab the interface name including any generics declared on it
+				let request = Regex.Replace(c, "<.*$", "")
+				select request
 			)
 			.ToList());
 
 		public static readonly Dictionary<string, string> DescriptorConstructors = (
-			// find all files in NEST ending with Request.cs
-			from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*Request.cs", SearchOption.AllDirectories)
-			let descriptor = Path.GetFileNameWithoutExtension(f.Name).Replace("Request", "Descriptor")
-			let re = $@"^.+public {descriptor}\(([^\r\n\)]+?)\).*$"
-			from l in File.ReadLines(f.FullName)
-			where Regex.IsMatch(l, re)
-			let args = Regex.Replace(l, re, "$1", RegexOptions.Singleline)
-			where !string.IsNullOrWhiteSpace(args) && !args.Contains(": base")
-			select (Descriptor: descriptor, Args: args)
+				// find all files in NEST ending with Request.cs
+				from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*Request.cs", SearchOption.AllDirectories)
+				let descriptor = Path.GetFileNameWithoutExtension(f.Name).Replace("Request", "Descriptor")
+				let re = $@"^.+public {descriptor}\(([^\r\n\)]+?)\).*$"
+				from l in File.ReadLines(f.FullName)
+				where Regex.IsMatch(l, re)
+				let args = Regex.Replace(l, re, "$1", RegexOptions.Singleline)
+				where !string.IsNullOrWhiteSpace(args) && !args.Contains(": base")
+				select (Descriptor: descriptor, Args: args)
 			)
 			.ToDictionary(r => r.Descriptor, r => r.Args);
 
 		public static readonly Dictionary<string, string> RequestInterfaceGenericsLookup =
 			AllKnownRequestInterfaces
-			.GroupBy(v=>v.Item1)
-			.Select(g=>g.Last())
-			.ToDictionary(k => k.Item1, v => v.Item2);
+				.GroupBy(v => v.Item1)
+				.Select(g => g.Last())
+				.ToDictionary(k => k.Item1, v => v.Item2);
 
 		/// <summary>
 		/// Some API's reuse response this is a hardcoded map of these cases
 		/// </summary>
 		private static Dictionary<string, (string, string)> ResponseReroute = new Dictionary<string, (string, string)>
 		{
-			{"UpdateByQueryRethrottleResponse", ("ListTasksResponse", "")},
-			{"DeleteByQueryRethrottleResponse", ("ListTasksResponse", "")},
-			{"MultiSearchTemplateResponse", ("MultiSearchResponse", "")},
-			{"ScrollResponse", ("SearchResponse", "<TDocument>")},
-			{"SearchTemplateResponse", ("SearchResponse", "<TDocument>")},
-
+			{ "UpdateByQueryRethrottleResponse", ("ListTasksResponse", "") },
+			{ "DeleteByQueryRethrottleResponse", ("ListTasksResponse", "") },
+			{ "MultiSearchTemplateResponse", ("MultiSearchResponse", "") },
+			{ "ScrollResponse", ("SearchResponse", "<TDocument>") },
+			{ "SearchTemplateResponse", ("SearchResponse", "<TDocument>") },
 		};
 
 
 		/// <summary> Create a dictionary lookup of all responses and their generics </summary>
 		public static readonly SortedDictionary<string, (string, string)> ResponseLookup = new SortedDictionary<string, (string, string)>(
-		(
-			// find all files in NEST ending with Request.cs
-			from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*Response.cs", SearchOption.AllDirectories)
-			from l in File.ReadLines(f.FullName)
-			// attempt to locate all Response class lines
-			where Regex.IsMatch(l, @"^.+public class [^ \r\n]+Response")
-			//grab the response name including any generics declared on it
-			let c = Regex.Replace(l, @"^.+public class ([^ \r\n]+Response(?:<[^>\r\n]+>)?[^ \r\n]*).*$", "$1", RegexOptions.Singleline)
-			where c.Contains("Response")
-			let response = Regex.Replace(c, "<.*$", "")
-			let generics = Regex.Replace(c, @"^.*?(?:(\<.+>).*?)?$", "$1")
-			select (response,  (response, generics))
-		)
-			.Concat(ResponseReroute.Select(kv=>(kv.Key, (kv.Value.Item1, kv.Value.Item2))))
-			.ToDictionary(t=>t.Item1, t=>t.Item2));
-
+			(
+				// find all files in NEST ending with Request.cs
+				from f in new DirectoryInfo(GeneratorLocations.NestFolder).GetFiles("*Response.cs", SearchOption.AllDirectories)
+				from l in File.ReadLines(f.FullName)
+				// attempt to locate all Response class lines
+				where Regex.IsMatch(l, @"^.+public class [^ \r\n]+Response")
+				//grab the response name including any generics declared on it
+				let c = Regex.Replace(l, @"^.+public class ([^ \r\n]+Response(?:<[^>\r\n]+>)?[^ \r\n]*).*$", "$1", RegexOptions.Singleline)
+				where c.Contains("Response")
+				let response = Regex.Replace(c, "<.*$", "")
+				let generics = Regex.Replace(c, @"^.*?(?:(\<.+>).*?)?$", "$1")
+				select (response, (response, generics))
+			)
+			.Concat(ResponseReroute.Select(kv => (kv.Key, (kv.Value.Item1, kv.Value.Item2))))
+			.ToDictionary(t => t.Item1, t => t.Item2));
 	}
 }
