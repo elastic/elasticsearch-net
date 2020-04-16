8e1f5d0c9230b0614d2abd627a41af72ca975e42
Stuart Cam <stuart.cam@elastic.co>
2020-04-16 18:49:59 +1000

    Implement indices.reload_search_analyzers (#4529)
    Implement indices.reload_search_analyzers

---
+++ src/Nest/Indices/ReloadSearchAnalyzers/ReloadSearchAnalyzersResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Elasticsearch.Net;
+
+namespace Nest
+{
+	public class ReloadSearchAnalyzersResponse : ResponseBase
+	{
+		[DataMember(Name ="_shards")]
+		public ShardStatistics Shards { get; internal set; }
+
+		[DataMember(Name ="reload_details")]
+		public IReadOnlyCollection<ReloadDetails> ReloadDetails { get; internal set; }  = EmptyReadOnly<ReloadDetails>.Collection;
+	}
+}
