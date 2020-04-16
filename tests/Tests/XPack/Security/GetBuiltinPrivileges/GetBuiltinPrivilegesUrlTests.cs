1ae442e26f05bd06f4bc746881aaa228ad871dd5
Stuart Cam <stuart.cam@elastic.co>
2020-04-16 20:29:28 +1000

    Implement security.get_builtin_privileges (#4536)
    Implement security.get_builtin_privileges

---
+++ tests/Tests/XPack/Security/GetBuiltinPrivileges/GetBuiltinPrivilegesUrlTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Elastic.Xunit.XunitPlumbing;
+using Nest;
+using Tests.Framework.EndpointTests;
+using static Tests.Framework.EndpointTests.UrlTester;
+
+namespace Tests.XPack.Security.GetBuiltinPrivileges
+{
+	public class GetBuiltinPrivilegesUrlTests : UrlTestsBase
+	{
+		[U] public override async Task Urls() =>
+			await GET("/_security/privilege/_builtin")
+				.Fluent(c => c.Security.GetBuiltinPrivileges())
+				.Request(c => c.Security.GetBuiltinPrivileges(new GetBuiltinPrivilegesRequest()))
+				.FluentAsync(c => c.Security.GetBuiltinPrivilegesAsync())
+				.RequestAsync(c => c.Security.GetBuiltinPrivilegesAsync(new GetBuiltinPrivilegesRequest()));
+	}
+}
