fab0de5606d38869d010685399b8e7bc6b6d511e
Stuart Cam <stuart.cam@elastic.co>
2020-04-16 20:21:49 +1000

    Implement ml.set_upgrade_mode (#4535)
    Implement ml.set_upgrade_mode

---
+++ src/Nest/XPack/MachineLearning/SetUpgradeMode/SetUpgradeModeRequest.cs
@@ -0,0 +1,9 @@
+namespace Nest
+{
+	[MapsApi("ml.set_upgrade_mode.json")]
+	public partial interface ISetUpgradeModeRequest { }
+
+	public partial class SetUpgradeModeRequest { }
+
+	public partial class SetUpgradeModeDescriptor { }
+}
