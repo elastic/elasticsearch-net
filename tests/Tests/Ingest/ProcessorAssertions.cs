c681ae28c546dda1626d9e34dd3e659a2bd6a7cd
Stuart Cam <stuart.cam@elastic.co>
2020-04-20 18:15:34 +1000

    Ignore CSV assertion in 7.8.0-SNAPSHOT
    (cherry picked from commit 14d13ec6b4c91398bb17d7bd43a66e5dceff68ae)

--- tests/Tests/Ingest/ProcessorAssertions.cs
+++ tests/Tests/Ingest/ProcessorAssertions.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using Elastic.Xunit.XunitPlumbing;
 using Nest;
 using Tests.Core.Client;
+using Tests.Core.Extensions;
 using Tests.Domain;
 
 namespace Tests.Ingest
@@ -34,7 +35,7 @@ namespace Tests.Ingest
 			from t in typeof(ProcessorAssertions).GetNestedTypes()
 			where typeof(IProcessorAssertion).IsAssignableFrom(t) && t.IsClass
 			let a = t.GetCustomAttributes(typeof(SkipVersionAttribute)).FirstOrDefault() as SkipVersionAttribute
-			where a == null || !a.Ranges.Any(r => r.IsSatisfied(TestClient.Configuration.ElasticsearchVersion))
+			where a == null || !a.Ranges.Any(r => TestClient.Configuration.InRange(r.ToString()))
 			select (IProcessorAssertion)Activator.CreateInstance(t);
 
 		public static IProcessor[] Initializers => All.Select(a => a.Initializer).ToArray();
@@ -70,7 +71,7 @@ namespace Tests.Ingest
 			public override string Key => "append";
 		}
 
-		[SkipVersion("<7.7.0", "Empty Value introduced in Elasticsearch 7.7.0+, CSV introduced in 7.6.0+")]
+		[SkipVersion("<7.7.0,>=7.8.0", "Empty Value introduced in Elasticsearch 7.7.0+, CSV introduced in 7.6.0+")]
 		public class Csv : ProcessorAssertion
 		{
 			public override Func<ProcessorsDescriptor, IPromise<IList<IProcessor>>> Fluent => d => d
