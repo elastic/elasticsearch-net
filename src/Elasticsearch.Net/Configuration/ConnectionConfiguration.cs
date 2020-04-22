1cacf375a4fe9c2bcad9397280d9c7410a484308
Martijn Laarman <Mpdreamz@gmail.com>
2020-04-22 14:19:15 +0200

    remove unused reference

--- src/Elasticsearch.Net/Configuration/ConnectionConfiguration.cs
+++ src/Elasticsearch.Net/Configuration/ConnectionConfiguration.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Net;
 #if DOTNETCORE
 using System.Net.Http;
 using System.Runtime.InteropServices;
