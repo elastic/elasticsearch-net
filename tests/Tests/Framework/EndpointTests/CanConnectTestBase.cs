4aff9e50655eb9baa2cfb2097a070eb4e64c5bf1
Martijn Laarman <Mpdreamz@gmail.com>
2020-05-25 13:52:37 +0200

    update usings inside Tests
    (cherry picked from commit 61f6444b5831c96b549d36d1db527cdc0bc2d1e4)

--- tests/Tests/Framework/EndpointTests/CanConnectTestBase.cs
+++ tests/Tests/Framework/EndpointTests/CanConnectTestBase.cs
@@ -3,8 +3,8 @@
 // See the LICENSE file in the project root for more information
 
 using System.Threading.Tasks;
-using Elastic.Managed.Ephemeral;
-using Elastic.Xunit.XunitPlumbing;
+using Elastic.Elasticsearch.Ephemeral;
+using Elastic.Elasticsearch.Xunit.XunitPlumbing;
 using Elasticsearch.Net;
 using FluentAssertions;
 using Nest;
