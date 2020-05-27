ddc277224611877e36d217768e527e8b47950966
Russ Cam <russ.cam@forloop.co.uk>
2020-05-27 11:42:58 +1000

    update CatAllocationRecord (#4705)
    This commit updates the fields available on CatAllocationRecord
    to match what is returned from Elasticsearch.
    
    Fixes #4699
    
    (cherry picked from commit f3b329f94717e30f3c766ed64f8ad1099d2424a6)

--- tests/Tests/Cat/CatAllocation/CatAllocationApiTests.cs
+++ tests/Tests/Cat/CatAllocation/CatAllocationApiTests.cs
@@ -1,4 +1,9 @@
-﻿using Elasticsearch.Net;
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.Linq;
+using Elasticsearch.Net;
 using FluentAssertions;
 using Nest;
 using Tests.Core.ManagedElasticsearch.Clusters;
@@ -25,7 +30,22 @@ namespace Tests.Cat.CatAllocation
 			(client, r) => client.Cat.AllocationAsync(r)
 		);
 
-		protected override void ExpectResponse(CatResponse<CatAllocationRecord> response) =>
-			response.Records.Should().NotBeEmpty().And.Contain(a => !string.IsNullOrEmpty(a.Node));
+		protected override void ExpectResponse(CatResponse<CatAllocationRecord> response)
+		{
+			var records = response.Records;
+			records.Should().NotBeEmpty().And.Contain(a => !string.IsNullOrEmpty(a.Node));
+
+			foreach (var record in records.Where(r => !string.IsNullOrEmpty(r.Ip)))
+			{
+				record.Shards.Should().NotBeNullOrEmpty();
+				record.DiskIndices.Should().NotBeNullOrEmpty();
+				record.DiskUsed.Should().NotBeNullOrEmpty();
+				record.DiskAvailable.Should().NotBeNullOrEmpty();
+				record.DiskTotal.Should().NotBeNullOrEmpty();
+				record.DiskPercent.Should().NotBeNullOrEmpty();
+				record.Host.Should().NotBeNullOrEmpty();
+				record.Node.Should().NotBeNullOrEmpty();
+			}
+		}
 	}
 }
