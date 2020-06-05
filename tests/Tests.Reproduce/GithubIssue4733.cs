b916e23fca3f3dd6aad83848971f39abb8f59e23
Russ Cam <russ.cam@forloop.co.uk>
2020-06-05 11:30:48 +1000

    Fix Ids overloads on PinnedQuery
    This commit fixes the Ids overloads on PinnedQuery by
    performing the conversion to IEnumerable<Id> on each
    overload and assigning to the Ids value.
    
    Fixes #4733

---
+++ tests/Tests.Reproduce/GithubIssue4733.cs
@@ -0,0 +1,34 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System;
+using Elastic.Elasticsearch.Xunit.XunitPlumbing;
+using FluentAssertions;
+using Nest;
+using Tests.Core.Client;
+
+namespace Tests.Reproduce
+{
+	public class GithubIssue4733
+	{
+		[U]
+		public void CanSerializeIdsQueryWithIEnumerableString()
+		{
+			var client = TestClient.DefaultInMemoryClient;
+
+			Func<ISearchResponse<object>> func = () => client.Search<object>(s => s
+				.From(0)
+				.Size(25)
+				.Source(s => s.Excludes(e => e.Field("events")))
+				.Query(q => q.Pinned(p => p
+						.Organic(o => o.MatchAll())
+						.Ids(new [] { "387c2c78-95b1-42f8-a965-e09a73f7cff6" })
+					)
+				)
+			);
+
+			func.Should().NotThrow();
+		}
+	}
+}
