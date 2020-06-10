2f556aa59819181128dd60752cf86546c04d2df0
Russ Cam <russ.cam@elastic.co>
2020-06-10 13:18:50 +1000

    Add Voting Config Exclusions APIs (#4738)
    Relates: #4718, elastic/elasticsearch#55760
    
    This commit adds the voting config exclusions APIs to the high level client.
    Custom response builder types are required because the APIs return
    \n as the response, which the client will attempt to deserialize to a
    response object.

---
+++ tests/Tests/Cluster/VotingConfigExclusions/DeleteVotingConfigExclusions/DeleteVotingConfigExclusionsApiTests.cs
@@ -0,0 +1,44 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System;
+using System.Threading;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Tests.Cluster.TaskManagement.GetTask;
+using Tests.Core.Extensions;
+using Tests.Core.ManagedElasticsearch.Clusters;
+using Tests.Core.ManagedElasticsearch.NodeSeeders;
+using Tests.Domain;
+using Tests.Framework.EndpointTests;
+using Tests.Framework.EndpointTests.TestState;
+
+namespace Tests.Cluster.VotingConfigExclusions.DeleteVotingConfigExclusions
+{
+	public class DeleteVotingConfigExclusionsApiTests : ApiIntegrationTestBase<WritableCluster, DeleteVotingConfigExclusionsResponse, IDeleteVotingConfigExclusionsRequest, DeleteVotingConfigExclusionsDescriptor, DeleteVotingConfigExclusionsRequest>
+	{
+		public DeleteVotingConfigExclusionsApiTests(WritableCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		protected override bool ExpectIsValid => true;
+		protected override int ExpectStatusCode => 200;
+
+		protected override Func<DeleteVotingConfigExclusionsDescriptor, IDeleteVotingConfigExclusionsRequest> Fluent => s => s
+			.WaitForRemoval();
+
+		protected override HttpMethod HttpMethod => HttpMethod.DELETE;
+
+		protected override DeleteVotingConfigExclusionsRequest Initializer => new DeleteVotingConfigExclusionsRequest { WaitForRemoval = true };
+		protected override string UrlPath => $"/_cluster/voting_config_exclusions?wait_for_removal=true";
+
+		protected override LazyResponses ClientUsage() => Calls(
+			(client, f) => client.Cluster.DeleteVotingConfigExclusions(f),
+			(client, f) => client.Cluster.DeleteVotingConfigExclusionsAsync(f),
+			(client, r) => client.Cluster.DeleteVotingConfigExclusions(r),
+			(client, r) => client.Cluster.DeleteVotingConfigExclusionsAsync(r)
+		);
+
+		protected override void ExpectResponse(DeleteVotingConfigExclusionsResponse response) => response.ShouldBeValid();
+	}
+}
