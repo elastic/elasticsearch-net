379ee45edfc4081996b66115303a41647becc95f
Russ Cam <russ.cam@forloop.co.uk>
2020-06-10 15:26:08 +1000

    Generate Voting config exclusion APIs

--- src/Nest/_Generated/ApiUrlsLookup.generated.cs
+++ src/Nest/_Generated/ApiUrlsLookup.generated.cs
@@ -62,9 +62,11 @@ namespace Nest
 		internal static ApiUrls CrossClusterReplicationUnfollowIndex = new ApiUrls(new[]{"{index}/_ccr/unfollow"});
 		internal static ApiUrls NoNamespaceClearScroll = new ApiUrls(new[]{"_search/scroll"});
 		internal static ApiUrls ClusterAllocationExplain = new ApiUrls(new[]{"_cluster/allocation/explain"});
+		internal static ApiUrls ClusterDeleteVotingConfigExclusions = new ApiUrls(new[]{"_cluster/voting_config_exclusions"});
 		internal static ApiUrls ClusterGetSettings = new ApiUrls(new[]{"_cluster/settings"});
 		internal static ApiUrls ClusterHealth = new ApiUrls(new[]{"_cluster/health", "_cluster/health/{index}"});
 		internal static ApiUrls ClusterPendingTasks = new ApiUrls(new[]{"_cluster/pending_tasks"});
+		internal static ApiUrls ClusterPostVotingConfigExclusions = new ApiUrls(new[]{"_cluster/voting_config_exclusions"});
 		internal static ApiUrls ClusterPutSettings = new ApiUrls(new[]{"_cluster/settings"});
 		internal static ApiUrls ClusterRemoteInfo = new ApiUrls(new[]{"_remote/info"});
 		internal static ApiUrls ClusterReroute = new ApiUrls(new[]{"_cluster/reroute"});
