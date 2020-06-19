428a3acd7ce5063917acf59db9861c07ce331ce4
Russ Cam <russ.cam@forloop.co.uk>
2020-06-19 11:20:07 +1000

    Add DeleteExpiredData overloads without body
    Relates: https://github.com/elastic/elasticsearch/pull/56895
    
    This commit patches in DeleteExpiredData that don't accept
    a body.
    
    (cherry picked from commit 8ebd663ebdf8a87003a77e9e50bd80f792882f2e)

---
+++ src/Elasticsearch.Net/Patch.ElasticLowLevelClient.MachineLearning.cs
@@ -0,0 +1,24 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.Threading;
+using System.Threading.Tasks;
+using static Elasticsearch.Net.HttpMethod;
+
+namespace Elasticsearch.Net.Specification.MachineLearningApi
+{
+	// Introduced as workaround for the introduction of a body in https://github.com/elastic/elasticsearch/pull/56895
+	public partial class LowLevelMachineLearningNamespace
+	{
+		///<summary>DELETE on /_ml/_delete_expired_data <para>https://www.elastic.co/guide/en/elasticsearch/reference/current/ml-delete-expired-data.html</para></summary>
+		///<param name = "requestParameters">Request specific configuration such as querystring parameters &amp; request specific connection settings.</param>
+		public TResponse DeleteExpiredData<TResponse>(DeleteExpiredDataRequestParameters requestParameters = null)
+			where TResponse : class, IElasticsearchResponse, new() => DoRequest<TResponse>(DELETE, "_ml/_delete_expired_data", null, RequestParams(requestParameters));
+		///<summary>DELETE on /_ml/_delete_expired_data <para>https://www.elastic.co/guide/en/elasticsearch/reference/current/ml-delete-expired-data.html</para></summary>
+		///<param name = "requestParameters">Request specific configuration such as querystring parameters &amp; request specific connection settings.</param>
+		[MapsApi("ml.delete_expired_data")]
+		public Task<TResponse> DeleteExpiredDataAsync<TResponse>(DeleteExpiredDataRequestParameters requestParameters = null, CancellationToken ctx = default)
+			where TResponse : class, IElasticsearchResponse, new() => DoRequestAsync<TResponse>(DELETE, "_ml/_delete_expired_data", ctx, null, RequestParams(requestParameters));
+	}
+}
