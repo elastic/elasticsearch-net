dcd04d1e41805f40f0e1b6b926ccf2359726f219
Russ Cam <russ.cam@elastic.co>
2020-06-30 09:53:55 +1000

    Do not wrap null ISourceFilter in Union (#4807)
    This commit updates the .Source() method
    to not wrap a null ISourceFilter in Union<bool,ISourceFilter>, so
    that it is not serialized in the request.
    
    Fixes #4787

---
+++ tests/Tests.Reproduce/GithubIssue4787.cs
@@ -0,0 +1,38 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System;
+using System.Text;
+using Elastic.Elasticsearch.Xunit.XunitPlumbing;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Nest;
+using Tests.Core.Client;
+
+namespace Tests.Reproduce
+{
+	public class GithubIssue4787
+	{
+		[U]
+		public void DoNotSerializeNullSourceFilter()
+		{
+			var connectionSettings = new ConnectionSettings(new InMemoryConnection()).DisableDirectStreaming();
+			var client = new ElasticClient(connectionSettings);
+
+			Func<ISearchResponse<object>> action = () =>
+				client.Search<object>(s => s
+					.Query(q => q
+						.MatchAll()
+					)
+					.Index("index")
+					.Source(sfd => null)
+				);
+
+			var response = action.Should().NotThrow().Subject;
+
+			var json = Encoding.UTF8.GetString(response.ApiCall.RequestBodyInBytes);
+			json.Should().Be(@"{""query"":{""match_all"":{}}}");
+		}
+	}
+}
