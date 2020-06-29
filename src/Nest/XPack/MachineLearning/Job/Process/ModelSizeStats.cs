25e6da08f00513516ea869e8f3128247187a941c
Russ Cam <russ.cam@elastic.co>
2020-06-30 09:57:44 +1000

    Add FailedCategoryCount to ModelSizeStats (#4813)
    Relates: elastic/elasticsearch#55716, #4803
    
    This commit adds FailedCategoryCount to ModelSizeStats
    and adds other missing fields to JobStats

--- src/Nest/XPack/MachineLearning/Job/Process/ModelSizeStats.cs
+++ src/Nest/XPack/MachineLearning/Job/Process/ModelSizeStats.cs
@@ -22,6 +22,42 @@ namespace Nest
 		public long BucketAllocationFailuresCount { get; internal set; }
 
 		/// <summary>
+		/// The number of documents that have had a field categorized.
+		/// </summary>
+		[DataMember(Name = "categorized_doc_count")]
+		public long CategorizedDocCount { get; internal set; }
+
+		/// <summary>
+		/// The status of categorization for the job.
+		/// </summary>
+		[DataMember(Name = "categorization_status")]
+		public ModelCategorizationStatus CategorizationStatus { get; internal set; }
+
+		/// <summary>
+		/// The number of categories created by categorization that will never be assigned again because another
+		/// category's definition makes it a superset of the dead category.
+		/// (Dead categories are a side effect of the way categorization has no prior training.)
+		/// </summary>
+		[DataMember(Name = "dead_category_count")]
+		public long DeadCategoryCount { get; internal set; }
+
+		/// <summary>
+		/// The number of times that categorization wanted to create a new category but couldn't because the job had hit its model_memory_limit.
+		/// This count does not track which specific categories failed to be created. Therefore you cannot use this value to determine
+		/// the number of unique categories that were missed.
+		/// <para />
+		/// Available in Elasticsearch 7.8.0+
+		/// </summary>
+		[DataMember(Name = "failed_category_count")]
+		public long FailedCategoryCount { get; internal set; }
+
+		/// <summary>
+		/// The number of categories that match more than 1% of categorized documents.
+		/// </summary>
+		[DataMember(Name = "frequent_category_count")]
+		public long FrequentCategoryCount { get; internal set; }
+
+		/// <summary>
 		///  A unique identifier for the job.
 		/// </summary>
 		[DataMember(Name = "job_id")]
@@ -48,6 +84,24 @@ namespace Nest
 		public long ModelBytes { get; internal set; }
 
 		/// <summary>
+		/// The number of bytes over the high limit for memory usage at the last allocation failure.
+		/// </summary>
+		[DataMember(Name = "model_bytes_exceeded")]
+		public long ModelBytesExceeded { get; internal set; }
+
+		/// <summary>
+		/// The upper limit for model memory usage, checked on increasing values.
+		/// </summary>
+		[DataMember(Name = "model_bytes_memory_limit")]
+		public long ModelBytesMemoryLimit { get; internal set; }
+
+		/// <summary>
+		/// The number of categories that match just one categorized document.
+		/// </summary>
+		[DataMember(Name = "rare_category_count")]
+		public long RareCategoryCount { get; internal set; }
+
+		/// <summary>
 		/// For internal use. The type of result.
 		/// </summary>
 		[DataMember(Name = "result_type")]
@@ -65,6 +119,12 @@ namespace Nest
 		/// </summary>
 		[DataMember(Name = "total_by_field_count")]
 		public long TotalByFieldCount { get; internal set; }
+
+		/// <summary>
+		/// The number of categories created by categorization.
+		/// </summary>
+		[DataMember(Name = "total_category_count")]
+		public long TotalCategoryCount { get; internal set; }
 
 		/// <summary>
 		/// The number of over field values that were analyzed by the models.
