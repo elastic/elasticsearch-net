75f0d7bdbbf7840b715ec3ad3c5cb5e746291309
Russ Cam <russ.cam@elastic.co>
2020-06-30 10:45:47 +1000

    Add Indices and Aliases in BulkAlias API (#4816)
    * Add Indices and Aliases in BulkAlias API
    
    This commit adds the aliases and indices
    options to add and remove alias operations
    for the bulk alias API.
    
    Closes #4721
    
    (cherry picked from commit 126106c8cefdb7c095831b436803e4e9ab9f6fe3)

--- src/Nest/Indices/AliasManagement/Alias/Actions/AliasRemove.cs
+++ src/Nest/Indices/AliasManagement/Alias/Actions/AliasRemove.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information
 
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Elasticsearch.Net.Utf8Json;
 
@@ -26,27 +27,52 @@ namespace Nest
 
 		AliasRemoveOperation IAliasRemoveAction.Remove { get; set; }
 
+		/// <inheritdoc cref="AliasRemoveOperation.Index"/>
 		public AliasRemoveDescriptor Index(string index)
 		{
 			Self.Remove.Index = index;
 			return this;
 		}
 
+		/// <inheritdoc cref="AliasRemoveOperation.Index"/>
 		public AliasRemoveDescriptor Index(Type index)
 		{
 			Self.Remove.Index = index;
 			return this;
 		}
 
+		/// <inheritdoc cref="AliasRemoveOperation.Index"/>
 		public AliasRemoveDescriptor Index<T>() where T : class
 		{
 			Self.Remove.Index = typeof(T);
 			return this;
 		}
 
+		/// <inheritdoc cref="AliasRemoveOperation.Indices"/>
+		public AliasRemoveDescriptor Indices(Indices indices)
+		{
+			Self.Remove.Indices = indices;
+			return this;
+		}
+
+		/// <inheritdoc cref="AliasRemoveOperation.Alias"/>
 		public AliasRemoveDescriptor Alias(string alias)
 		{
 			Self.Remove.Alias = alias;
+			return this;
+		}
+
+		/// <inheritdoc cref="AliasRemoveOperation.Aliases"/>
+		public AliasRemoveDescriptor Aliases(IEnumerable<string> aliases)
+		{
+			Self.Remove.Aliases = aliases;
+			return this;
+		}
+
+		/// <inheritdoc cref="AliasRemoveOperation.Aliases"/>
+		public AliasRemoveDescriptor Aliases(params string[] aliases)
+		{
+			Self.Remove.Aliases = aliases;
 			return this;
 		}
 	}
