186e825976ce984ccac21008ae983c6ba0b1d43e
Russ Cam <russ.cam@elastic.co>
2020-07-31 11:09:06 +1000

    Add index filter to field_caps API (#4888)
    Relates: elastic/elasticsearch#57276

--- src/Nest/Search/FieldCapabilities/FieldCapabilitiesRequest.cs
+++ src/Nest/Search/FieldCapabilities/FieldCapabilitiesRequest.cs
@@ -2,20 +2,33 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-﻿using Elasticsearch.Net;
+using System;
+using System.Runtime.Serialization;
+using Elasticsearch.Net;
 
 namespace Nest
 {
 	[MapsApi("field_caps.json")]
-	public partial interface IFieldCapabilitiesRequest { }
+	public partial interface IFieldCapabilitiesRequest
+	{
+		[DataMember(Name = "index_filter")]
+		QueryContainer IndexFilter { get; set; }
+	}
 
 	public partial class FieldCapabilitiesRequest
 	{
-		protected override HttpMethod HttpMethod => HttpMethod.GET;
+		protected override HttpMethod HttpMethod => IndexFilter != null? HttpMethod.POST : HttpMethod.GET;
+
+		public QueryContainer IndexFilter { get; set; }
 	}
 
 	public partial class FieldCapabilitiesDescriptor
 	{
-		protected override HttpMethod HttpMethod => HttpMethod.GET;
+		QueryContainer IFieldCapabilitiesRequest.IndexFilter { get; set; }
+
+		protected override HttpMethod HttpMethod => Self.IndexFilter != null? HttpMethod.POST : HttpMethod.GET;
+
+		public FieldCapabilitiesDescriptor IndexFilter<T>(Func<QueryContainerDescriptor<T>, QueryContainer> query) where T : class =>
+			Assign(query, (a, v) => a.IndexFilter = v?.Invoke(new QueryContainerDescriptor<T>()));
 	}
 }
