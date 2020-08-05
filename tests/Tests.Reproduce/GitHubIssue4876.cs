f1766534eabc32c9f6dc3447d3484d4e402cb295
Russ Cam <russ.cam@forloop.co.uk>
2020-08-05 11:59:48 +1000

    Fix Iso8601 format with varying fractional values
    Relates: #4910
    
    The PR introduced a fix to support basic offset formats
    when deserializing Iso8601, but the fix would only work with
    7 fractional second values. This commit fixes the implementation
    to support any number of fractional second values.

--- tests/Tests.Reproduce/GitHubIssue4876.cs
+++ tests/Tests.Reproduce/GitHubIssue4876.cs
@@ -19,58 +19,167 @@ namespace Tests.Reproduce
 
 		[U]
 		public void CanDeserializeExtendedFormatOffsetIso8601DateTime() =>
-			AssertDateTime("{\"timestamp\":\"2020-07-31T12:29:29.4425068+10:00\"}");
+			AssertDateTime("2020-07-31T12:29:29.4425068+10:00", 637317593694425068);
 
 		[U]
 		public void CanDeserializeBasicFormatOffsetWithMinutesIso8601DateTime() =>
-			AssertDateTime("{\"timestamp\":\"2020-07-31T12:29:29.4425068+1000\"}");
+			AssertDateTime("2020-07-31T12:29:29.4425068+1000", 637317593694425068);
 
 		[U]
 		public void CanDeserializeBasicFormatOffsetIso8601DateTime() =>
-			AssertDateTime("{\"timestamp\":\"2020-07-31T12:29:29.4425068+10\"}");
+			AssertDateTime("2020-07-31T12:29:29.4425068+10", 637317593694425068);
 
 		[U]
 		public void ThrowExceptionWhenInvalidBasicFormatOffset()
 		{
-			Action action = () => AssertDateTime("{\"timestamp\":\"2020-07-31T12:29:29.4425068+100\"}");
+			Action action = () => AssertDateTime("2020-07-31T12:29:29.4425068+100", 637317593694425068);
 			action.Should().Throw<InvalidOperationException>();
 		}
 
 		[U]
 		public void ThrowExceptionWhenInvalidBasicFormatOffset2()
 		{
-			Action action = () => AssertDateTime("{\"timestamp\":\"2020-07-31T12:29:29.4425068-10000\"}");
+			Action action = () => AssertDateTime("2020-07-31T12:29:29.4425068-10000", 637317593694425068);
 			action.Should().Throw<InvalidOperationException>();
 		}
 
 		[U]
 		public void CanDeserializeExtendedFormatOffsetIso8601DateTimeOffset() =>
-			AssertDateTimeOffset("{\"timestamp\":\"2020-07-31T12:29:29.4425068+10:00\"}");
+			AssertDateTimeOffset("2020-07-31T12:29:29.4425068+10:00", 637317593694425068);
 
 		[U]
 		public void CanDeserializeBasicFormatOffsetWithMinutesIso8601DateTimeOffset() =>
-			AssertDateTimeOffset("{\"timestamp\":\"2020-07-31T12:29:29.4425068+1000\"}");
+			AssertDateTimeOffset("2020-07-31T12:29:29.4425068+1000", 637317593694425068);
 
 		[U]
 		public void CanDeserializeBasicFormatOffsetIso8601DateTimeOffset() =>
-			AssertDateTimeOffset("{\"timestamp\":\"2020-07-31T12:29:29.4425068+10\"}");
+			AssertDateTimeOffset("2020-07-31T12:29:29.4425068+10", 637317593694425068);
 
-		private void AssertDateTime(string json)
+		[U]
+		public void CanDeserializeNoFractionalSeconds()
 		{
-			var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+			var ticks = 637321081960000000;
+			AssertDateTime("2020-08-04T13:23:16+10:00", ticks);
+			AssertDateTime("2020-08-04T13:23:16+1000", ticks);
+			AssertDateTime("2020-08-04T13:23:16+10", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16+10:00", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16+1000", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16+10", ticks);
+		}
+
+		[U]
+		public void CanDeserializeOneFractionalSecond()
+		{
+			var ticks = 637321081967000000;
+			AssertDateTime("2020-08-04T13:23:16.7+10:00", ticks);
+			AssertDateTime("2020-08-04T13:23:16.7+1000", ticks);
+			AssertDateTime("2020-08-04T13:23:16.7+10", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7+10:00", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7+1000", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7+10", ticks);
+		}
+
+		[U]
+		public void CanDeserializeTwoFractionalSeconds()
+		{
+			var ticks = 637321081967200000;
+			AssertDateTime("2020-08-04T13:23:16.72+10:00", ticks);
+			AssertDateTime("2020-08-04T13:23:16.72+1000", ticks);
+			AssertDateTime("2020-08-04T13:23:16.72+10", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.72+10:00", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.72+1000", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.72+10", ticks);
+		}
+
+		[U]
+		public void CanDeserializeThreeFractionalSeconds()
+		{
+			var ticks = 637321081967220000;
+			AssertDateTime("2020-08-04T13:23:16.722+10:00", ticks);
+			AssertDateTime("2020-08-04T13:23:16.722+1000", ticks);
+			AssertDateTime("2020-08-04T13:23:16.722+10", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.722+10:00", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.722+1000", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.722+10", ticks);
+		}
+
+		[U]
+		public void CanDeserializeFourFractionalSeconds()
+		{
+			var ticks = 637321081967220000;
+			AssertDateTime("2020-08-04T13:23:16.7220+10:00", ticks);
+			AssertDateTime("2020-08-04T13:23:16.7220+1000", ticks);
+			AssertDateTime("2020-08-04T13:23:16.7220+10", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7220+10:00", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7220+1000", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7220+10", ticks);
+		}
+
+		[U]
+		public void CanDeserializeFiveFractionalSeconds()
+		{
+			var ticks = 637321081967220100;
+			AssertDateTime("2020-08-04T13:23:16.72201+10:00", ticks);
+			AssertDateTime("2020-08-04T13:23:16.72201+1000", ticks);
+			AssertDateTime("2020-08-04T13:23:16.72201+10", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.72201+10:00", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.72201+1000", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.72201+10", ticks);
+		}
+
+		[U]
+		public void CanDeserializeSixFractionalSeconds()
+		{
+			var ticks = 637321081967220110;
+			AssertDateTime("2020-08-04T13:23:16.722011+10:00", ticks);
+			AssertDateTime("2020-08-04T13:23:16.722011+1000", ticks);
+			AssertDateTime("2020-08-04T13:23:16.722011+10", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.722011+10:00", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.722011+1000", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.722011+10", ticks);
+		}
+
+		[U]
+		public void CanDeserializeSevenFractionalSeconds()
+		{
+			var ticks = 637321081967220111;
+			AssertDateTime("2020-08-04T13:23:16.7220111+10:00", ticks);
+			AssertDateTime("2020-08-04T13:23:16.7220111+1000", ticks);
+			AssertDateTime("2020-08-04T13:23:16.7220111+10", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7220111+10:00", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7220111+1000", ticks);
+			AssertDateTimeOffset("2020-08-04T13:23:16.7220111+10", ticks);
+		}
+
+		[U]
+		public void CanDeserializeMoreThanSevenFractionalSeconds()
+		{
+			var ticks = 637321081967220111;
+			var digits = new Random().Next();
+			AssertDateTime($"2020-08-04T13:23:16.7220111{digits}+10:00", ticks);
+			AssertDateTime($"2020-08-04T13:23:16.7220111{digits}+1000", ticks);
+			AssertDateTime($"2020-08-04T13:23:16.7220111{digits}+10", ticks);
+			AssertDateTimeOffset($"2020-08-04T13:23:16.7220111{digits}+10:00", ticks);
+			AssertDateTimeOffset($"2020-08-04T13:23:16.7220111{digits}+1000", ticks);
+			AssertDateTimeOffset($"2020-08-04T13:23:16.7220111{digits}+10", ticks);
+		}
+
+		private void AssertDateTime(string json, long expectedTicks)
+		{
+			var stream = new MemoryStream(Encoding.UTF8.GetBytes($"{{\"timestamp\":\"{json}\"}}"));
 			var document = _client.SourceSerializer.Deserialize<Document>(stream);
 
 			document.Timestamp.Should().Be(
-				new DateTime(637317593694425068, DateTimeKind.Utc).ToLocalTime());
+				new DateTime(expectedTicks, DateTimeKind.Utc).ToLocalTime());
 		}
 
-		private void AssertDateTimeOffset(string json)
+		private void AssertDateTimeOffset(string json, long expectedTicks)
 		{
-			var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+			var stream = new MemoryStream(Encoding.UTF8.GetBytes($"{{\"timestamp\":\"{json}\"}}"));
 			var document = _client.SourceSerializer.Deserialize<Document2>(stream);
 
 			document.Timestamp.Should().Be(
-				new DateTimeOffset(637317593694425068, TimeSpan.Zero));
+				new DateTimeOffset(expectedTicks, TimeSpan.Zero));
 		}
 
 		public class Document
