c397b6312b8bbb8a279f0cd173e3a2021746c6a1
Russ Cam <russ.cam@forloop.co.uk>
2020-09-17 15:37:36 +1000

    Mark members of ElasticsearchResponseBase with JsonIgnore

--- tests/Tests/CodeStandards/NamingConventions.doc.cs
+++ tests/Tests/CodeStandards/NamingConventions.doc.cs
@@ -2,7 +2,7 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-?using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
