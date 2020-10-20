2b7324bd9c066f9f96913b1e606f4f71340c4472
mandiscombination <50206413+mandiscombination@users.noreply.github.com>
2020-10-20 10:29:55 +0200

    Updated sampler aggregation (#5032)

--- src/Nest/Aggregations/Bucket/Sampler/SamplerAggregation.cs
+++ src/Nest/Aggregations/Bucket/Sampler/SamplerAggregation.cs
@@ -12,15 +12,6 @@ namespace Nest
 	[ReadAs(typeof(SamplerAggregation))]
 	public interface ISamplerAggregation : IBucketAggregation
 	{
-		[DataMember(Name ="execution_hint")]
-		SamplerAggregationExecutionHint? ExecutionHint { get; set; }
-
-		[DataMember(Name ="max_docs_per_value")]
-		int? MaxDocsPerValue { get; set; }
-
-		[DataMember(Name ="script")]
-		IScript Script { get; set; }
-
 		[DataMember(Name ="shard_size")]
 		int? ShardSize { get; set; }
 	}
@@ -31,9 +22,6 @@ namespace Nest
 
 		public SamplerAggregation(string name) : base(name) { }
 
-		public SamplerAggregationExecutionHint? ExecutionHint { get; set; }
-		public int? MaxDocsPerValue { get; set; }
-		public IScript Script { get; set; }
 		public int? ShardSize { get; set; }
 
 		internal override void WrapInContainer(AggregationContainer c) => c.Sampler = this;
@@ -43,20 +31,7 @@ namespace Nest
 		: BucketAggregationDescriptorBase<SamplerAggregationDescriptor<T>, ISamplerAggregation, T>, ISamplerAggregation
 		where T : class
 	{
-		SamplerAggregationExecutionHint? ISamplerAggregation.ExecutionHint { get; set; }
-		int? ISamplerAggregation.MaxDocsPerValue { get; set; }
-		IScript ISamplerAggregation.Script { get; set; }
 		int? ISamplerAggregation.ShardSize { get; set; }
-
-		public SamplerAggregationDescriptor<T> ExecutionHint(SamplerAggregationExecutionHint? executionHint) =>
-			Assign(executionHint, (a, v) => a.ExecutionHint = v);
-
-		public SamplerAggregationDescriptor<T> MaxDocsPerValue(int? maxDocs) => Assign(maxDocs, (a, v) => a.MaxDocsPerValue = v);
-
-		public SamplerAggregationDescriptor<T> Script(string script) => Assign((InlineScript)script, (a, v) => a.Script = v);
-
-		public SamplerAggregationDescriptor<T> Script(Func<ScriptDescriptor, IScript> scriptSelector) =>
-			Assign(scriptSelector, (a, v) => a.Script = v?.Invoke(new ScriptDescriptor()));
 
 		public SamplerAggregationDescriptor<T> ShardSize(int? shardSize) => Assign(shardSize, (a, v) => a.ShardSize = v);
 	}
