f1df2e42cf6b23e19b0230e634ef0901938fac4d
Martijn Laarman <Mpdreamz@gmail.com>
2020-10-21 12:40:44 +0200

    remove Elastic.Transport and related projects

--- tests/Elastic.Transport.Tests/Plumbing/TestResponse.cs
+++
@@ -1,8 +0,0 @@
-// Licensed to Elasticsearch B.V under one or more agreements.
-// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
-// See the LICENSE file in the project root for more information
-
-namespace Elastic.Transport.Tests.Plumbing
-{
-	public class TestResponse : TransportResponseBase { }
-}
