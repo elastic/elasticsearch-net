e40df69bd6187e3633882674ecc6143c004d6f91
Steve Gordon <sgordon@hotmail.co.uk>
2020-11-19 14:37:52 +0000

    Add hard_bounds for histograms (#5098)
    * Add hard_bounds for histograms
    * Apply date_optional_time format for hard_bounds
    * Add new hard_bounds tests
    
    As with extended_bounds, we append the date_optional_time format
    to the format starting on the aggregation. This avoids parsing errors on
    the server.
    
    Includes an update to the documentation which we be generated and 
    added in a subsequent PR.

--- tests/Tests/Aggregations/Bucket/Histogram/HistogramAggregationUsageTests.cs
+++ tests/Tests/Aggregations/Bucket/Histogram/HistogramAggregationUsageTests.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information
 
 using System;
+using Elastic.Elasticsearch.Xunit.XunitPlumbing;
 using FluentAssertions;
 using Nest;
 using Tests.Core.Extensions;
@@ -31,6 +32,7 @@ namespace Tests.Aggregations.Bucket.Histogram
 						_key = "desc"
 					},
 					offset = 1.1
+
 				}
 			}
 		};
@@ -63,6 +65,68 @@ namespace Tests.Aggregations.Bucket.Histogram
 			commits.Buckets.Count.Should().BeGreaterThan(0);
 			foreach (var item in commits.Buckets)
 				item.DocCount.Should().BeGreaterThan(0);
+		}
+	}
+
+	// hide
+	[SkipVersion("<7.10.0", "hard_bounds introduced in 7.10.0")]
+	public class HistogramAggregationWithHardBoundsUsageTests : AggregationUsageTestBase
+	{
+		private const double HardBoundsMinimum = 100;
+		private const double HardBoundsMaximum = 300;
+
+		public HistogramAggregationWithHardBoundsUsageTests(ReadOnlyCluster i, EndpointUsage usage) : base(i, usage) { }
+
+		protected override object AggregationJson => new
+		{
+			commits = new
+			{
+				histogram = new
+				{
+					field = "numberOfCommits",
+					hard_bounds = new { min = HardBoundsMinimum, max = HardBoundsMaximum },
+					interval = 100.0,
+					min_doc_count = 1,
+					order = new
+					{
+						_key = "desc"
+					}
+				}
+			}
+		};
+
+		protected override Func<AggregationContainerDescriptor<Project>, IAggregationContainer> FluentAggs => a => a
+			.Histogram("commits", h => h
+				.Field(p => p.NumberOfCommits)
+				.Interval(100)
+				.MinimumDocumentCount(1)
+				.Order(HistogramOrder.KeyDescending)
+				.HardBounds(HardBoundsMinimum, HardBoundsMaximum)
+			);
+
+		protected override AggregationDictionary InitializerAggs =>
+			new HistogramAggregation("commits")
+			{
+				Field = Field<Project>(p => p.NumberOfCommits),
+				Interval = 100,
+				MinimumDocumentCount = 1,
+				Order = HistogramOrder.KeyDescending,
+				HardBounds = new HardBounds<double>
+				{
+					Minimum = HardBoundsMinimum,
+					Maximum = HardBoundsMaximum
+				}
+			};
+
+		protected override void ExpectResponse(ISearchResponse<Project> response)
+		{
+			response.ShouldBeValid();
+			var commits = response.Aggregations.Histogram("commits");
+			commits.Should().NotBeNull();
+			commits.Buckets.Should().NotBeNull();
+
+			foreach (var bucket in commits.Buckets)
+				bucket.Key.Should().BeGreaterOrEqualTo(HardBoundsMinimum).And.BeLessOrEqualTo(HardBoundsMaximum);
 		}
 	}
 }
