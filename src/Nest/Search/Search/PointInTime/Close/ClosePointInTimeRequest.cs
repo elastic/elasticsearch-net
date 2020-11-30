160c8bab7c92bf404d7aee7701e8df2f4fc2b2f9
Steve Gordon <sgordon@hotmail.co.uk>
2020-11-30 11:21:25 +0000

    Add empty request/response for close point in time

---
+++ src/Nest/Search/Search/PointInTime/Close/ClosePointInTimeRequest.cs
@@ -0,0 +1,13 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+namespace Nest
+{
+	[MapsApi("close_point_in_time.json")]
+	public partial interface IClosePointInTimeRequest { }
+
+	public partial class ClosePointInTimeRequest { }
+
+	public partial class ClosePointInTimeDescriptor { }
+}
