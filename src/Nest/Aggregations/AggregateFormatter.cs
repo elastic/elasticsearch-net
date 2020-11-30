3ffcda095f9e154399390ab5ac31246271f1479b
Steve Gordon <sgordon@hotmail.co.uk>
2020-11-30 16:58:54 +0000

    Handle additional JSON properties (#5139)
    * Handle additional JSON properties
    
    * PR feedback - add comment

--- src/Nest/Aggregations/AggregateFormatter.cs
+++ src/Nest/Aggregations/AggregateFormatter.cs
@@ -258,6 +258,24 @@ namespace Nest
 			reader.ReadNext(); // "q3"
 			reader.ReadNext(); // :
 			boxplot.Q3 = reader.ReadDouble();
+
+			// ES 7.11.0 adds these two additional properties on the payload
+			// When present on a 7.11 response, we avoid an exception by
+			// handling (and skipping) over the properties. The 7.11 client
+			// will add support for these new properties on BoxplotAggregate.
+			var token = reader.GetCurrentJsonToken();
+			if (token != JsonToken.EndObject)
+			{
+				reader.ReadNext(); // ,
+				reader.ReadNext(); // "lower"
+				reader.ReadNext(); // :
+				reader.ReadDouble();
+				reader.ReadNext(); // ,
+				reader.ReadNext(); // "upper"
+				reader.ReadNext(); // :
+				reader.ReadDouble();
+			}
+
 			return boxplot;
     }
 
