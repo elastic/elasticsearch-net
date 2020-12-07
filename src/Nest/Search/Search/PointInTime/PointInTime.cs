0e7e4cc376cad6cbd7af831f41b91674971b79e2
Steve Gordon <sgordon@hotmail.co.uk>
2020-12-07 08:28:18 +0000

    Support point in time searches (#5151)
    * Support point in time searches
    * Support override of ResolveUrl
    
    This allows us to special case pit searches to remove the index parameter.
    Includes new usage tests for doc generation.

---
+++ src/Nest/Search/Search/PointInTime/PointInTime.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.Runtime.Serialization;
+using Elasticsearch.Net.Utf8Json;
+
+namespace Nest
+{
+	[InterfaceDataContract]
+	[ReadAs(typeof(PointInTime))]
+	public interface IPointInTime
+	{
+		/// <summary>
+		/// The ID of the point in time.
+		/// </summary>
+		[DataMember(Name = "id")]
+		string Id { get; set; }
+
+		/// <summary>
+		/// How long to extend the TTL of the point in time.
+		/// </summary>
+		[DataMember(Name = "keep_alive")]
+		Time KeepAlive { get; set; }
+	}
+
+	public class PointInTime : IPointInTime
+	{
+		/// <param name="id">The ID of the point in time.</param>
+		public PointInTime(string id) => Id = id;
+
+		/// <param name="id">The ID of the point in time.</param>
+		/// <param name="keepAlive">How long to extend the TTL of the point in time.</param>
+		public PointInTime(string id, Time keepAlive)
+		{
+			Id = id;
+			KeepAlive = keepAlive;
+		}
+
+		/// <inheritdoc />
+		public string Id { get; set; }
+
+		/// <inheritdoc />
+		public Time KeepAlive { get; set; }
+	}
+
+	public class PointInTimeDescriptor : DescriptorBase<PointInTimeDescriptor, IPointInTime>, IPointInTime
+	{
+		public PointInTimeDescriptor(string id) => Self.Id = id;
+		
+		string IPointInTime.Id { get; set; }
+		Time IPointInTime.KeepAlive { get; set; }
+		
+		/// <inheritdoc cref="IPointInTime.KeepAlive" />
+		public PointInTimeDescriptor KeepAlive(Time id) => Assign(id, (a, v) => a.KeepAlive = v);
+	}
+}
