e607d3126d05ec862c4d2a9de37bfcaecc05a249
Steve Gordon <sgordon@hotmail.co.uk>
2020-12-07 12:21:33 +0000

    Support clear API key cache (#5147)
    * Support clear API key cache
    * Patched to support optional ids

---
+++ src/Nest/XPack/Security/ApiKey/ClearApiKeyCache/ClearApiKeyCacheResponse.cs
@@ -0,0 +1,27 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Elasticsearch.Net;
+using Elasticsearch.Net.Utf8Json;
+
+namespace Nest
+{
+	public class ClearApiKeyCacheResponse : NodesResponseBase
+	{
+		/// <summary>
+		/// The cluster name.
+		/// </summary>
+		[DataMember(Name = "cluster_name")]
+		public string ClusterName { get; internal set; }
+
+		/// <summary>
+		/// A dictionary of <see cref="CompactNodeInfo"/> container details of the nodes that cleared the cache.
+		/// </summary>
+		[DataMember(Name = "nodes")]
+		[JsonFormatter(typeof(VerbatimInterfaceReadOnlyDictionaryKeysFormatter<string, CompactNodeInfo>))]
+		public IReadOnlyDictionary<string, CompactNodeInfo> Nodes { get; internal set; } = EmptyReadOnly<string, CompactNodeInfo>.Dictionary;
+	}
+}
