7d0fffccf3024b3ccf5a4290fcc1aae9631365d1
Steve Gordon <sgordon@hotmail.co.uk>
2021-01-15 08:06:32 +0000

    Fix GetRoleMappings when multiple distinguished names returned (#5271)
    * Fix deserialisation of multiple dns
    * Support serialisation

--- src/Nest/XPack/Security/RoleMapping/Rules/Field/FieldRuleBaseFormatter.cs
+++ src/Nest/XPack/Security/RoleMapping/Rules/Field/FieldRuleBaseFormatter.cs
@@ -2,6 +2,7 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using System;
 using System.Collections.Generic;
 using Nest.Utf8Json;
 namespace Nest
@@ -38,6 +39,13 @@ namespace Nest
 							fieldRule = new UsernameRule(username);
 							break;
 						case 1:
+							if (reader.GetCurrentJsonToken() == JsonToken.BeginArray)
+							{
+								var fm = formatterResolver.GetFormatter<IEnumerable<string>>();
+								var dns = fm.Deserialize(ref reader, formatterResolver);
+								fieldRule = new DistinguishedNameRule(dns);
+								break;
+							}
 							var dn = reader.ReadString();
 							fieldRule = new DistinguishedNameRule(dn);
 							break;
