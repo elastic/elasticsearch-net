54a6d5ca9177b473849dd5e80cff3ca5388da1b8
Steve Gordon <sgordon@hotmail.co.uk>
2021-02-10 16:29:42 +0000

    Skip cat template YAML test

--- tests/Tests.YamlRunner/SkipList.fs
+++ tests/Tests.YamlRunner/SkipList.fs
@@ -158,15 +158,11 @@ let SkipList = dict<SkipFile,SkipSection> [
     //These are ignored because they were flagged on a big PR.
     
     //additional enters in regex
-    SkipFile "cat.templates/10_basic.yml", Sections [ "Multiple template"; "Sort templates" ]
+    SkipFile "cat.templates/10_basic.yml", Sections [ "Multiple template"; "Sort templates"; "No templates" ]
     
     //Replace stashed value in body that is passed as string json
     SkipFile "api_key/10_basic.yml", Section "Test get api key"
-    
-    //additional enters in regex
-    SkipFile "cat.templates/10_basic.yml", Sections [ "Multiple template"; "Sort templates" ]
-    
-    
+        
     //new API TODO remove when we regenerate
     SkipFile "cluster.voting_config_exclusions/10_basic.yml", All
     
