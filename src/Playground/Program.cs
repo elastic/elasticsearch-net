e6a864f823d9020445728c43b04ad31e0465682b
Steve Gordon <sgordon@hotmail.co.uk>
2021-03-05 15:30:08 +0000

    Playground update

--- src/Playground/Program.cs
+++ src/Playground/Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Elastic.Transport;
 using Nest;
 
 namespace Playground
 {
 	internal class Program
     {
-	    private static void Main(string[] args)
+	    private static async Task Main(string[] args)
 	    {
-		    var client = new ElasticClient(new Uri("http://localhost:9200"));
-			
+		    var client = new ElasticClient(new Uri("http://localhost:9600"));
+
+		    var request = new ClusterHealthRequest("test")
+		    {
+			    WaitForStatus = WaitForStatus.Green,
+			    Level = Level.Cluster
+		    };
+
+		    var response = await client.Cluster.HealthAsync(request);
 	    }
     }
 }
