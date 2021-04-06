4c126e037e700cdb298361a4af2aed9383718480
Steve Gordon <sgordon@hotmail.co.uk>
2021-04-06 12:42:04 +0100

    Unskip cluster settings tests

--- tests/Tests.YamlRunner/SkipList.fs
+++ tests/Tests.YamlRunner/SkipList.fs
@@ -11,7 +11,7 @@ type SkipFile = SkipFile of string
 let SkipList = dict<SkipFile,SkipSection> [
 
     // Need to implement cluster settings cleanup
-    SkipFile "cluster.put_settings/10_basic.yml", All
+    // SkipFile "cluster.put_settings/10_basic.yml", All
     
     SkipFile "ml/set_upgrade_mode.yml", All
     SkipFile "ml/inference_crud.yml", Section "Test update model alias with model id referring to missing model"
