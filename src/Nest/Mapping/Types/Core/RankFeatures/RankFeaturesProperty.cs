19ffd41a6338f51631c699e8dbdd445b5072e534
Steve Gordon <sgordon@hotmail.co.uk>
2021-04-23 05:54:46 +0100

    Add positive_score_impact to rank_features property (#5607)

--- src/Nest/Mapping/Types/Core/RankFeatures/RankFeaturesProperty.cs
+++ src/Nest/Mapping/Types/Core/RankFeatures/RankFeaturesProperty.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information
 
 using System.Diagnostics;
+using System.Runtime.Serialization;
 using Elasticsearch.Net.Utf8Json;
 
 namespace Nest
@@ -15,20 +16,37 @@ namespace Nest
 	[InterfaceDataContract]
 	public interface IRankFeaturesProperty : IProperty
 	{
+		/// <summary>
+		/// Rank features that correlate negatively with the score should set <see cref="PositiveScoreImpact"/>
+		/// to false (defaults to true). This will be used by the rank_features query to modify the scoring
+		/// formula in such a way that the score decreases with the value of the feature instead of
+		/// increasing.
+		/// </summary>
+		[DataMember(Name = "positive_score_impact")]
+		bool? PositiveScoreImpact { get; set; }
 	}
 
 	/// <inheritdoc cref="IRankFeaturesProperty" />
 	public class RankFeaturesProperty : PropertyBase, IRankFeaturesProperty
 	{
 		public RankFeaturesProperty() : base(FieldType.RankFeatures) { }
+
+		/// <inheritdoc />
+		public bool? PositiveScoreImpact { get; set; }
 	}
 
 	/// <inheritdoc cref="IRankFeaturesProperty" />
-	[DebuggerDisplay("{DebugDisplay}")]
+	[DebuggerDisplay("{" + nameof(DebugDisplay) + "}")]
 	public class RankFeaturesPropertyDescriptor<T>
 		: PropertyDescriptorBase<RankFeaturesPropertyDescriptor<T>, IRankFeaturesProperty, T>, IRankFeaturesProperty
 		where T : class
 	{
 		public RankFeaturesPropertyDescriptor() : base(FieldType.RankFeatures) { }
+		
+		bool? IRankFeaturesProperty.PositiveScoreImpact { get; set; }
+
+		/// <inheritdoc cref="IRankFeaturesProperty.PositiveScoreImpact" />
+		public RankFeaturesPropertyDescriptor<T> PositiveScoreImpact(bool? positiveScoreImpact = true) =>
+			Assign(positiveScoreImpact, (a, v) => a.PositiveScoreImpact = v);
 	}
 }
