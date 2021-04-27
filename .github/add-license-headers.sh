aa92930b823d55ae75c3bd1cb20b9feddf4c5ff9
Steve Gordon <sgordon@hotmail.co.uk>
2021-04-27 11:24:47 +0100

    Tweak license header scripts (#5649)
    * Remove newline from license text
    
    * Tweak add license headers script to add newline

--- .github/add-license-headers.sh
+++ .github/add-license-headers.sh
@@ -5,7 +5,8 @@ function add_license () {
     (find "$script_path" -name $1 | grep -v "/bin/" | grep -v "/obj/" )|while read fname; do
         line=$(sed -n '2p;3q' "$fname")
         if ! [[ "$line" == " * Licensed to Elasticsearch B.V. under one or more contributor" ]] ; then
-            cat "${script_path}.github/license-header.txt" "$fname" > "${fname}.new"
+            # awk joins the header with the existing file, inserting a newline between them
+            awk '(NR>1 && FNR==1){print ""}1' "${script_path}.github/license-header.txt" "$fname" > "${fname}.new"
             mv "${fname}.new" "$fname"
         fi
     done
