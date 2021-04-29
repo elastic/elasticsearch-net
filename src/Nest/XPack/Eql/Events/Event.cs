641a2a0f0fcfecbe3edebd9fa17cc8881624cd5c
Steve Gordon <sgordon@hotmail.co.uk>
2021-04-29 11:17:17 +0100

    Add event type for EQL

--- src/Nest/XPack/Eql/Events/Event.cs
+++ src/Nest/XPack/Eql/Events/Event.cs
@@ -1,4 +1,22 @@
-﻿
+﻿/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 using System.Runtime.Serialization;
 using Elasticsearch.Net.Utf8Json;
 
@@ -9,6 +27,7 @@ namespace Nest.XPack.Eql.Events
 	/// </summary>
 	/// <typeparam name="TEvent">The type of the source document</typeparam>
 	[InterfaceDataContract]
+	[ReadAs(typeof(Event<>))]
 	public interface IEvent<out TEvent> where TEvent : class
 	{
 		/// <summary>
@@ -35,21 +54,14 @@ namespace Nest.XPack.Eql.Events
 		[DataMember(Name = "_source")]
 		[JsonFormatter(typeof(SourceFormatter<>))]
 		TEvent Source { get; }
-
-		/// <summary>
-		/// The type of hit
-		/// </summary>
-		[DataMember(Name = "_type")]
-		string Type { get; }
-
-		/// <summary>
-		/// The version of the hit
-		/// </summary>
-		[DataMember(Name = "_version")]
-		long Version { get; }
 	}
 
-	public class Event
+	public class Event<TEvent> : IEvent<TEvent>
+		where TEvent : class
 	{
+		public FieldValues Fields { get; }
+		public string Id { get; }
+		public string Index { get; }
+		public TEvent Source { get; }
 	}
 }
