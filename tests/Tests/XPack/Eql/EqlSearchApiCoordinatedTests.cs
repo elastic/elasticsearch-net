f93bc8785bdff4250beddbda05d96f17e7929df3
Steve Gordon <sgordon@hotmail.co.uk>
2021-05-04 12:54:59 +0100

    Update license headers (again)!

--- tests/Tests/XPack/Eql/EqlSearchApiCoordinatedTests.cs
+++ tests/Tests/XPack/Eql/EqlSearchApiCoordinatedTests.cs
@@ -1,21 +1,6 @@
-/*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
 
 using System.Linq;
 using System.Threading.Tasks;
@@ -33,57 +18,75 @@ namespace Tests.XPack.Eql
 	[SkipVersion("<7.11.0", "GA in 7.11.0")]
 	public class EqlSearchApiCoordinatedTests : CoordinatedIntegrationTestBase<TimeSeriesCluster>
 	{
-		private const string SubmitStep = nameof(SubmitStep);
-		private const string StatusStep = nameof(StatusStep);
 		private const string GetStep = nameof(GetStep);
+		private const string StatusStep = nameof(StatusStep);
+		private const string SubmitStep = nameof(SubmitStep);
 		private const string WaitStep = nameof(WaitStep);
 		private const string DeleteStep = nameof(DeleteStep);
 
-		public EqlSearchApiCoordinatedTests(TimeSeriesCluster cluster, EndpointUsage usage) : base(new CoordinatedUsage(cluster, usage, testOnlyOne: true)
-		{
-			{SubmitStep, u =>
-				u.Calls<EqlSearchDescriptor<Log>, EqlSearchRequest<Log>, IEqlSearchRequest, EqlSearchResponse<Log>>(
-					v => new EqlSearchRequest<Log>
-					{
-						Query = "any where true",
-						KeepOnCompletion = true,
-						TimestampField = Infer.Field<Log>(f => f.Timestamp),
-						WaitForCompletionTimeout = "1nanos"
-					},
-					(v, d) => d
-						.Query("any where true")
-						.KeepOnCompletion()
-						.TimestampField(Infer.Field<Log>(f => f.Timestamp))
-						.WaitForCompletionTimeout("1nanos"),
-					(v, c, f) => c.Eql.Search(f),
-					(v, c, f) => c.Eql.SearchAsync(f),
-					(v, c, r) => c.Eql.Search<Log>(r),
-					(v, c, r) => c.Eql.SearchAsync<Log>(r),
-					onResponse: (r, values) => values.ExtendedValue("id", r.Id)
-				)
-			},
-			{StatusStep, u =>
-				u.Calls<EqlSearchStatusDescriptor, EqlSearchStatusRequest, IEqlSearchStatusRequest, EqlSearchStatusResponse>(
-					v => new EqlSearchStatusRequest(v),
-					(v, d) => d,
-					(v, c, f) => c.Eql.SearchStatus(v, f),
-					(v, c, f) => c.Eql.SearchStatusAsync(v, f),
-					(v, c, r) => c.Eql.SearchStatus(r),
-					(v, c, r) => c.Eql.SearchStatusAsync(r),
-					uniqueValueSelector: values => values.ExtendedValue<string>("id")
-				)
-			},
-			{WaitStep, u => u.Call(async (v, c) =>
+		public EqlSearchApiCoordinatedTests(TimeSeriesCluster cluster, EndpointUsage usage) : base(
+			new CoordinatedUsage(cluster, usage, testOnlyOne: true)
 			{
-				// wait for the search to complete
-				var complete = false;
-				var count = 0;
-
-				while (!complete && count++ < 10)
 				{
-					await Task.Delay(100);
-					var status = await c.Eql.SearchStatusAsync(u.Usage.CallUniqueValues.ExtendedValue<string>("id"));
-					complete = !status.IsRunning;
+					SubmitStep, u =>
+						u.Calls<EqlSearchDescriptor<Log>, EqlSearchRequest<Log>, IEqlSearchRequest, EqlSearchResponse<Log>>(
+							v => new EqlSearchRequest<Log>
+							{
+								Query = "any where true",
+								KeepOnCompletion = true,
+								TimestampField = Infer.Field<Log>(f => f.Timestamp),
+								WaitForCompletionTimeout = "1nanos"
+							},
+							(v, d) => d
+								.Query("any where true")
+								.KeepOnCompletion()
+								.TimestampField(Infer.Field<Log>(f => f.Timestamp))
+								.WaitForCompletionTimeout("1nanos"),
+							(v, c, f) => c.Eql.Search(f),
+							(v, c, f) => c.Eql.SearchAsync(f),
+							(v, c, r) => c.Eql.Search<Log>(r),
+							(v, c, r) => c.Eql.SearchAsync<Log>(r),
+							(r, values) => values.ExtendedValue("id", r.Id)
+						)
+				},
+				{
+					StatusStep, u =>
+						u.Calls<EqlSearchStatusDescriptor, EqlSearchStatusRequest, IEqlSearchStatusRequest, EqlSearchStatusResponse>(
+							v => new EqlSearchStatusRequest(v),
+							(v, d) => d,
+							(v, c, f) => c.Eql.SearchStatus(v, f),
+							(v, c, f) => c.Eql.SearchStatusAsync(v, f),
+							(v, c, r) => c.Eql.SearchStatus(r),
+							(v, c, r) => c.Eql.SearchStatusAsync(r),
+							uniqueValueSelector: values => values.ExtendedValue<string>("id")
+						)
+				},
+				{
+					WaitStep, u => u.Call(async (v, c) =>
+					{
+						// wait for the search to complete
+						var complete = false;
+						var count = 0;
+
+						while (!complete && count++ < 10)
+						{
+							await Task.Delay(100);
+							var status = await c.Eql.SearchStatusAsync(u.Usage.CallUniqueValues.ExtendedValue<string>("id"));
+							complete = !status.IsRunning;
+						}
+					})
+				}, // allows the search to complete
+				{
+					GetStep, u =>
+						u.Calls<EqlGetDescriptor, EqlGetRequest, IEqlGetRequest, EqlGetResponse<Log>>(
+							v => new EqlGetRequest(v),
+							(v, d) => d,
+							(v, c, f) => c.Eql.Get<Log>(v, f),
+							(v, c, f) => c.Eql.GetAsync<Log>(v, f),
+							(v, c, r) => c.Eql.Get<Log>(r),
+							(v, c, r) => c.Eql.GetAsync<Log>(r),
+							uniqueValueSelector: values => values.ExtendedValue<string>("id")
+						)
 				}
 			})}, // allows the search to complete
 			{GetStep, u =>
@@ -118,7 +121,7 @@ namespace Tests.XPack.Eql
 			r.IsRunning.Should().BeTrue();
 			r.TimedOut.Should().BeFalse();
 		});
-		
+
 		[I] public async Task EqlSearchStatusResponse() => await Assert<EqlSearchStatusResponse>(StatusStep, r =>
 		{
 			r.ShouldBeValid();
