4aa1adbfd8143a26be068080af390b8eac36c3ed
Steve Gordon <sgordon@hotmail.co.uk>
2021-07-06 15:42:00 +0100

    Mark version property on Analyzer as obsolete (#5815)

--- src/Nest/Analysis/Analyzers/AnalyzerBase.cs
+++ src/Nest/Analysis/Analyzers/AnalyzerBase.cs
@@ -2,6 +2,7 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using System;
 using System.Runtime.Serialization;
 using Elasticsearch.Net.Utf8Json;
 
@@ -13,7 +14,12 @@ namespace Nest
 		[DataMember(Name = "type")]
 		string Type { get; }
 
+		/// <summary>
+		/// Setting a version on analysis component has no effect and is deprecated. 
+		/// This setting will be removed in v8.0.
+		/// </summary>
 		[DataMember(Name = "version")]
+		[Obsolete("Setting a version on analysis component has no effect and is deprecated.")]
 		string Version { get; set; }
 	}
 
@@ -26,6 +32,8 @@ namespace Nest
 
 		public virtual string Type { get; protected set; }
 
+		/// <inheritdoc cref="IAnalyzer.Version" />
+		[Obsolete("Setting a version on analysis component has no effect and is deprecated.")]
 		public string Version { get; set; }
 	}
 
@@ -38,6 +46,8 @@ namespace Nest
 		string IAnalyzer.Type => Type;
 		string IAnalyzer.Version { get; set; }
 
+		/// <inheritdoc cref="IAnalyzer.Version" />
+		[Obsolete("Setting a version on analysis component has no effect and is deprecated.")]
 		public TAnalyzer Version(string version) => Assign(version, (a, v) => a.Version = v);
 	}
 }
