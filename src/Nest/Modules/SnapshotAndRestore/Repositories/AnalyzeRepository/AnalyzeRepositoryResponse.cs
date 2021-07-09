beae1ebc2c21c0ac4110dc2a2ef6d531a5b3fd78
Steve Gordon <sgordon@hotmail.co.uk>
2021-07-09 11:58:35 +0100

    Add basic request and response

---
+++ src/Nest/Modules/SnapshotAndRestore/Repositories/AnalyzeRepository/AnalyzeRepositoryResponse.cs
@@ -0,0 +1,25 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.Runtime.Serialization;
+
+namespace Nest
+{
+	[DataContract]
+	public class AnalyzeRepositoryResponse : ResponseBase
+	{
+		[DataMember(Name = "coordinating_node")]
+		public CoordinatingNode CoordinatingNode{ get; internal set; }
+	}
+
+	[DataContract]
+	public class CoordinatingNode
+	{
+		[DataMember(Name = "id")]
+		public string Id { get; internal set; }
+
+		[DataMember(Name = "name")]
+		public string Name { get; internal set; }
+	}
+}
