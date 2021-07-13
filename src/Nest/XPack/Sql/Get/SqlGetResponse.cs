21ed243b4fb9838b16b1b37caa65b11ea4934151
Steve Gordon <sgordon@hotmail.co.uk>
2021-07-13 10:15:20 +0100

    Add get SQL search request and response

---
+++ src/Nest/XPack/Sql/Get/SqlGetResponse.cs
@@ -0,0 +1,13 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+namespace Nest
+{
+	/// <summary>
+	/// A response to an SQL get async search request.
+	/// </summary>
+	public class SqlGetResponse : QuerySqlResponse
+	{
+	}
+}
