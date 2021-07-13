bc53d58051613a073a7622f4a281f9e3cdac3c7d
Steve Gordon <sgordon@hotmail.co.uk>
2021-07-13 10:13:23 +0100

    [master] Support get async SQL search status API (#5859)
    * Add request and response models
    
    * Generate NEST code for SQL search status
    
    * Add SQL search status URL tests

---
+++ src/Nest/XPack/Sql/Status/SqlSearchStatusRequest.cs
@@ -0,0 +1,14 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+namespace Nest
+{
+	[MapsApi("sql.get_async_status")]
+	[ReadAs(typeof(EqlSearchStatusRequest))]
+	public partial interface ISqlSearchStatusRequest { }
+
+	public partial class SqlSearchStatusRequest { }
+
+	public partial class SqlSearchStatusDescriptor { }
+}
