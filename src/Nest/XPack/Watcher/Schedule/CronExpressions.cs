81d8cfd0e707cac120b5e77812411fc2c7cc576a
Steve Gordon <sgordon@hotmail.co.uk>
2021-07-15 09:48:51 +0100

    Fixup

--- src/Nest/XPack/Watcher/Schedule/CronExpressions.cs
+++ src/Nest/XPack/Watcher/Schedule/CronExpressions.cs
@@ -2,11 +2,10 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Elasticsearch.Net.Utf8Json;
+using Nest.Utf8Json;
 
 namespace Nest
 {
