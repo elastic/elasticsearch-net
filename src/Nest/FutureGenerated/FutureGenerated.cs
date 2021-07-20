9243e251ac584ed5018c72f0477f11579df35ee8
Steve Gordon <sgordon@hotmail.co.uk>
2021-07-20 13:48:44 +0100

    Reorg

--- src/Nest/FutureGenerated/FutureGenerated.cs
+++ src/Nest/FutureGenerated/FutureGenerated.cs
@@ -6,6 +6,24 @@ namespace Nest
 {
 	// Stubs until we generate these - Allows the code to compile so we can identify real errors.
 
+	// TODO: Implement properly
+	[JsonConverter(typeof(UnionConverter<EpochMillis>))]
+	public partial class EpochMillis
+	{
+		public EpochMillis(string value) { }
+
+		public EpochMillis(int value) { }
+	}
+
+	// TODO: Implement properly
+	[JsonConverter(typeof(PercentageConverter))]
+	public partial class Percentage
+	{
+		public Percentage(string value) { }
+
+		public Percentage(float value) { }
+	}
+
 	public class DateMath
 	{
 	}
