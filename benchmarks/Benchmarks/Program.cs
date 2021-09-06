76c0192404a05134e82de5dc0e6c41cf553594d5
Steve Gordon <sgordon@hotmail.co.uk>
2021-09-06 15:54:26 +0100

    Misc updates

--- benchmarks/Benchmarks/Program.cs
+++ benchmarks/Benchmarks/Program.cs
@@ -11,70 +11,129 @@ internal class Program
 {
 	private static void Main()
 	{
-		var thing = new IndicesGetString() { NameCount = 3 };
-		thing.Setup();
-		thing.NaiveStringCreate();
+		//var thing = new IndicesGetString() { NameCount = 3 };
+		//thing.Setup();
+		//thing.NaiveStringCreate();
 
-		BenchmarkRunner.Run<IndicesGetString>();
+		//BenchmarkRunner.Run<OneOrMany>();
 	}
 }
 
-[MemoryDiagnoser]
-public class IndicesGetString
-{
-	private readonly List<IndexName> _indices = new();
+//[MemoryDiagnoser]
+//public class IndicesGetString
+//{
+//	private readonly List<IndexName> _indices = new();
 
-	private string _output = string.Empty;
+//	private string _output = string.Empty;
 
-	private readonly ElasticsearchClientSettings _settings = new();
+//	private readonly ElasticsearchClientSettings _settings = new();
 
-	[Params(1,3)]
-	public int NameCount { get; set; }
+//	[Params(1,3)]
+//	public int NameCount { get; set; }
 
-	[GlobalSetup]
-	public void Setup()
-	{
-		for (var i = 0; i < NameCount; i++)
-		{
-			_indices.Add($"item{i}");
-		}
-	}
+//	[GlobalSetup]
+//	public void Setup()
+//	{
+//		for (var i = 0; i < NameCount; i++)
+//		{
+//			_indices.Add($"item{i}");
+//		}
+//	}
 
-	[Benchmark(Baseline = true)]
-	public void StringJoin()
-	{
-		var indices = _indices.Select(i => i.GetString(_settings)).Distinct();
-		_output = string.Join(',', indices);
-	}
+//	[Benchmark(Baseline = true)]
+//	public void StringJoin()
+//	{
+//		var indices = _indices.Select(i => i.GetString(_settings)).Distinct();
+//		_output = string.Join(',', indices);
+//	}
 
-	[Benchmark]
-	public void NaiveStringCreate()
-	{
-		// This implementation does ensure distinct values
-		// Is that really an issue as it's unlikely and would still be a valid request
+//	[Benchmark]
+//	public void NaiveStringCreate()
+//	{
+//		// This implementation doesn't ensure distinct values
+//		// Is that really an issue as it's unlikely and would still be a valid request?
 
-		var length = 0;
+//		// Issue: This doesn't call the Inferrer so is not a fair test since we'd 
 
-		var indices = _indices;
-		for (var i = 0; i < indices.Count; i++)
-		{
-			length += _indices[i].Value.Length + 1;
-		}
+//		var length = 0;
 
-		length = length == 0 ? 0 : length - 1;
+//		var indices = _indices;
+//		for (var i = 0; i < indices.Count; i++)
+//		{
+//			length += _indices[i].Value.Length + 1;
+//		}
 
-		_output = string.Create(length, indices, (span, state) =>
-		{
-			var written = 0;
-			for (var i = 0; i < indices.Count; i++)
-			{
-				var value = state[i].Value.AsSpan();
-				value.CopyTo(span[written..]);
-				written += value.Length;
+//		length = length == 0 ? 0 : length - 1;
 
-				if (i != indices.Count - 1)
-					span[written++] = ',';
-			}
-		});
-	}
-}
+//		_output = string.Create(length, indices, (span, state) =>
+//		{
+//			var written = 0;
+//			for (var i = 0; i < indices.Count; i++)
+//			{
+//				var value = state[i].Value.AsSpan();
+//				value.CopyTo(span[written..]);
+//				written += value.Length;
+
+//				if (i != indices.Count - 1)
+//					span[written++] = ',';
+//			}
+//		});
+//	}
+//}
+
+///// <summary>
+///// Investigate whether it's "more efficient" to use a one or many design for Indices.
+///// This avoids extra allocations in the case of a single item but is not so good for > 1 item.
+///// </summary>
+//[MemoryDiagnoser]
+//public class OneOrMany
+//{
+//	private readonly List<IndexName> _indices = new();
+
+//	private Indices? _finalIndices = null;
+//	private IndicesV2? _finalIndicesV2 = null;
+
+//	private readonly ElasticsearchClientSettings _settings = new();
+
+//	[Params(1, 3)]
+//	public int NameCount { get; set; }
+
+//	[GlobalSetup]
+//	public void Setup()
+//	{
+//		for (var i = 0; i < NameCount; i++)
+//		{
+//			_indices.Add($"item{i}");
+//		}
+//	}
+
+//	[Benchmark(Baseline = true)]
+//	public void PureHashSet() => _finalIndices = new Indices(_indices);
+
+//	[Benchmark]
+//	public void OneOrManyConcept()
+//	{
+//		if (_indices.Count == 1)
+//			_finalIndicesV2 = new IndicesV2(_indices[0]);
+//		else
+//			_finalIndicesV2 = new IndicesV2(_indices);
+//	}
+
+//	internal partial class IndicesV2
+//	{
+//		public static readonly IndicesV2 All = new("_all");
+
+//		private readonly HashSet<IndexName>? _indices;
+//		private readonly IndexName? _index;
+
+//		internal IndicesV2(IndexName index) => _index = index;
+
+//		public IndicesV2(IEnumerable<IndexName> indices)
+//		{
+//			if (_indices is null)
+//				_indices = new HashSet<IndexName>();
+
+//			_indices.UnionWith(indices);
+//		}
+//	}
+//}
---
+++ benchmarks/Profiling/Program.cs
@@ -0,0 +1,54 @@
+using JetBrains.Profiler.Api;
+using Nest;
+using Nest.IndexManagement;
+
+var req1 = new DeleteRequest("test");
+
+//var list = new List<IndexName>();
+//IEnumerable<IndexName> items = new IndexName[] { "a", "b" };
+
+MemoryProfiler.ForceGc();
+
+MemoryProfiler.CollectAllocations(true);
+
+MemoryProfiler.GetSnapshot();
+
+//var req = new DeleteRequest("test");
+
+//var i = Indices.Parse("test");
+
+var i = Indices.Single("test");
+
+MemoryProfiler.GetSnapshot();
+
+//if (req.AllowNoIndices.HasValue)
+//{
+
+//}
+
+_ = i.ToString();
+
+MemoryProfiler.CollectAllocations(false);
+
+//var source = new IndexName[] { "index-01", "index-02" };
+
+//var indices = new Indices(source);
+//var indicesList = new IndicesList(source);
+
+//MemoryProfiler.CollectAllocations(true);
+
+//MemoryProfiler.GetSnapshot();
+
+//var indices2 = new Indices(source);
+
+//MemoryProfiler.GetSnapshot();
+
+//var indicesList2 = new IndicesList(source);
+
+//MemoryProfiler.GetSnapshot();
+
+//MemoryProfiler.CollectAllocations(false);
+
+//// Ensure no GC between snapshots
+//_ = indices2.Values.Count;
+//_ = indicesList2.Values.Count;
