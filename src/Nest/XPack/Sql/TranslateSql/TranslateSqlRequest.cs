af9201ef456a9fbc0dfa6726fbfaa1dac94aa332
Steve Gordon <sgordon@hotmail.co.uk>
2021-10-21 12:37:17 +0100

    Support params on SQL query & translate APIs

--- src/Nest/XPack/Sql/TranslateSql/TranslateSqlRequest.cs
+++ src/Nest/XPack/Sql/TranslateSql/TranslateSqlRequest.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information
 
 using System;
+using System.Collections.Generic;
 using Elasticsearch.Net.Specification.SqlApi;
 
 namespace Nest
@@ -22,6 +23,9 @@ namespace Nest
 		/// <inheritdoc cref="ISqlRequest.Filter" />
 		public QueryContainer Filter { get; set; }
 
+		/// <inheritdoc cref="ISqlRequest.Params" />
+		public IList<object> Params { get; set; }
+
 		/// <inheritdoc cref="ISqlRequest.Query" />
 		public string Query { get; set; }
 
@@ -39,24 +43,30 @@ namespace Nest
 
 		int? ISqlRequest.FetchSize { get; set; }
 		QueryContainer ISqlRequest.Filter { get; set; }
+		IList<object> ISqlRequest.Params { get; set; }
 		string ISqlRequest.Query { get; set; }
 		string ISqlRequest.TimeZone { get; set; }
 		IRuntimeFields ISqlRequest.RuntimeFields { get; set; }
 
+		/// <inheritdoc cref="ISqlRequest.Params" />
+		public TranslateSqlDescriptor Params(IEnumerable<object> parameters) => Assign(parameters, (a, v) => a.Params = v?.ToListOrNullIfEmpty());
+
+		/// <inheritdoc cref="ISqlRequest.Params" />
+		public TranslateSqlDescriptor Params(IList<object> parameters) => Assign(parameters, (a, v) => a.Params = v);
+
+		/// <inheritdoc cref="ISqlRequest.Params" />
+		public TranslateSqlDescriptor Params(params object[] parameters) => Assign(parameters, (a, v) => a.Params = v);
+
 		/// <inheritdoc cref="ISqlRequest.Query" />
-		/// >
 		public TranslateSqlDescriptor Query(string query) => Assign(query, (a, v) => a.Query = v);
 
 		/// <inheritdoc cref="ISqlRequest.TimeZone" />
-		/// >
 		public TranslateSqlDescriptor TimeZone(string timeZone) => Assign(timeZone, (a, v) => a.TimeZone = v);
 
 		/// <inheritdoc cref="ISqlRequest.FetchSize" />
-		/// >
 		public TranslateSqlDescriptor FetchSize(int? fetchSize) => Assign(fetchSize, (a, v) => a.FetchSize = v);
 
 		/// <inheritdoc cref="ISqlRequest.Filter" />
-		/// >
 		public TranslateSqlDescriptor Filter<T>(Func<QueryContainerDescriptor<T>, QueryContainer> querySelector)
 			where T : class => Assign(querySelector, (a, v) => a.Filter = v?.Invoke(new QueryContainerDescriptor<T>()));
 
