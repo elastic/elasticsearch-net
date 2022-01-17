e5d6c4d49e775ead77ebc4a5760c787e6a7c7263
Steve Gordon <sgordon@hotmail.co.uk>
2022-01-17 15:44:56 +0000

    Error serialization handling

--- src/Elastic.Clients.Elasticsearch/Client/NamespacedClientProxy.cs
+++ src/Elastic.Clients.Elasticsearch/Client/NamespacedClientProxy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Elastic.Transport;
+using Elastic.Transport.Products.Elasticsearch.Failures;
 
 namespace Elastic.Clients.Elasticsearch
 {
@@ -21,14 +22,14 @@ namespace Elastic.Clients.Elasticsearch
 			IRequestParameters parameters,
 			Action<IRequestConfiguration>? forceConfiguration = null)
 			where TRequest : class, IRequest
-			where TResponse : class, ITransportResponse, new() =>
+			where TResponse : class, ITransportResponse<ServerError>, new() =>
 			_client.DoRequest<TRequest, TResponse>(request, parameters, forceConfiguration);
 
 		internal TResponse DoRequest<TRequest, TResponse>(
 			TRequest request,
 			Action<IRequestConfiguration>? forceConfiguration = null)
 			where TRequest : class, IRequest
-			where TResponse : class, ITransportResponse, new() =>
+			where TResponse : class, ITransportResponse<ServerError>, new() =>
 				_client.DoRequest<TRequest, TResponse>(request, forceConfiguration);
 
 		internal Task<TResponse> DoRequestAsync<TRequest, TResponse>(
@@ -36,14 +37,14 @@ namespace Elastic.Clients.Elasticsearch
 			IRequestParameters parameters,
 			CancellationToken cancellationToken = default)
 			where TRequest : class, IRequest
-			where TResponse : class, ITransportResponse, new() =>
+			where TResponse : class, ITransportResponse<ServerError>, new() =>
 			_client.DoRequestAsync<TRequest, TResponse>(request, parameters, cancellationToken: cancellationToken);
 
 		internal Task<TResponse> DoRequestAsync<TRequest, TResponse>(
 			TRequest request,
 			CancellationToken cancellationToken = default)
 			where TRequest : class, IRequest
-			where TResponse : class, ITransportResponse, new() =>
+			where TResponse : class, ITransportResponse<ServerError>, new() =>
 			_client.DoRequestAsync<TRequest, TResponse>(request, cancellationToken: cancellationToken);
 
 		internal Task<TResponse> DoRequestAsync<TRequest, TResponse>(
@@ -52,7 +53,7 @@ namespace Elastic.Clients.Elasticsearch
 			Action<IRequestConfiguration>? forceConfiguration,
 			CancellationToken cancellationToken = default)
 			where TRequest : class, IRequest
-			where TResponse : class, ITransportResponse, new() =>
+			where TResponse : class, ITransportResponse<ServerError>, new() =>
 			_client.DoRequestAsync<TRequest, TResponse>(request, parameters, forceConfiguration, cancellationToken);
 	}
 }
