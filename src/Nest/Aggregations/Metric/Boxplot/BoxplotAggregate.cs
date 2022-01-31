40da3a88127b176b504f3e33a7fda9c4bf77829e
Steve Gordon <sgordon@hotmail.co.uk>
2022-01-31 16:21:54 +0000

    Update boxplot to handle non-numeric values (#6095)

--- src/Nest/Aggregations/Metric/Boxplot/BoxplotAggregate.cs
+++ src/Nest/Aggregations/Metric/Boxplot/BoxplotAggregate.cs
@@ -2,22 +2,31 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using Elasticsearch.Net.Utf8Json;
+
 namespace Nest
 {
 	public class BoxplotAggregate : MetricAggregateBase
 	{
+		[JsonFormatter(typeof(StringDoubleFormatter))]
 		public double Min { get; set; }
 
+		[JsonFormatter(typeof(StringDoubleFormatter))]
 		public double Max { get; set; }
 
+		[JsonFormatter(typeof(StringDoubleFormatter))]
 		public double Q1 { get; set; }
 
+		[JsonFormatter(typeof(StringDoubleFormatter))]
 		public double Q2 { get; set; }
 
+		[JsonFormatter(typeof(StringDoubleFormatter))]
 		public double Q3 { get; set; }
 
+		[JsonFormatter(typeof(StringDoubleFormatter))]
 		public double Lower { get; set; }
 
+		[JsonFormatter(typeof(StringDoubleFormatter))]
 		public double Upper { get; set; }
 	}
 }
