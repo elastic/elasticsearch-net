b7c56000b1d9601305779e6dc678667c62fe544e
Steve Gordon <sgordon@hotmail.co.uk>
2022-02-09 08:59:42 +0000

    Fix license headers

--- tests/Tests.Core/Serialization/JsonDocumentExtensions.cs
+++ tests/Tests.Core/Serialization/JsonDocumentExtensions.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Text;
 using System.Text.Json;
 using System.IO;
 
