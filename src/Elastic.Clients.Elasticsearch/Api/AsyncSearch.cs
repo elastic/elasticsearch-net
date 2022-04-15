548b8a24cee832e5b29a80f69c032fba6d4b0290
Steve Gordon <sgordon@hotmail.co.uk>
2022-04-15 10:21:21 +0100

    Add Scroll APIs (#6334)
    * Add Scroll APIs

---
+++ src/Elastic.Clients.Elasticsearch/Api/AsyncSearch.cs
@@ -0,0 +1,21 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace Elastic.Clients.Elasticsearch.AsyncSearch;
+
+public partial class AsyncSearch<TDocument>
+{
+	[JsonIgnore]
+	public IReadOnlyCollection<Hit<TDocument>> Hits => HitsMetadata.Hits;
+
+	[JsonIgnore]
+	public IReadOnlyCollection<TDocument> Documents => HitsMetadata.Hits.Select(s => s.Source).ToReadOnlyCollection();
+
+	[JsonIgnore]
+	public long Total => HitsMetadata?.Total?.Value ?? -1;
+}
