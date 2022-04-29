7521dd4534d5cdac445f13778badd790f8c47728
Steve Gordon <sgordon@hotmail.co.uk>
2022-04-29 17:17:27 +0100

    Complete JoinFieldConverter (#6396)
    * Complete JoinFieldConverter
    
    * Fix license headers

--- src/PlaygroundV7x/Person.cs
+++ src/PlaygroundV7x/Person.cs
@@ -2,13 +2,26 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using Nest;
+
 namespace PlaygroundV7x
 {
 	public class Person
 	{
+		public int Id { get; set; }
+
+		public Guid SecondaryId { get; set; } = Guid.NewGuid();
 		public string? FirstName { get; init; }
 		public string? LastName { get; init; }
 		public int? Age { get; init; }
+		//public Routing? Routing { get; init; }
+
+		public Id Idv3 => "testing";
+		//public Guid Routing { get; init; } = Guid.NewGuid();
+
 		public string? Email { get; init; }
+
+		public string Data { get; init; } = "NOTHING";
 	}
 }
