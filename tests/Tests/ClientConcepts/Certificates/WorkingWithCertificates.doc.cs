868dd8e5e0a02fa269777ed20c6ac3df78743e6c
Steve Gordon <sgordon@hotmail.co.uk>
2022-05-16 09:39:05 +0100

    Fix code snippet in documentation (#6411)

--- tests/Tests/ClientConcepts/Certificates/WorkingWithCertificates.doc.cs
+++ tests/Tests/ClientConcepts/Certificates/WorkingWithCertificates.doc.cs
@@ -226,7 +226,7 @@ namespace Tests.ClientConcepts.Certificates
 		 */		
 		[U] public void CertificateFingerprint()
 		{
-			var pool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
+			var pool = new SingleNodeConnectionPool(new Uri("https://localhost:9200"));
 
 			var settings = new ConnectionSettings(pool)
 				.CertificateFingerprint("94:75:CE:4F:EB:05:32:83:40:B8:18:BB:79:01:7B:E0:F0:B6:C3:01:57:DB:4D:F5:D8:B8:A6:BA:BD:6D:C5:C4");
