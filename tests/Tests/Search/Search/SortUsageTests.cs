0c45b4f29807bdf16da654e279abfdff3c829d28
Steve Gordon <sgordon@hotmail.co.uk>
2022-07-11 14:09:00 +0100

    Fix BOM and update test

--- tests/Tests/Search/Search/SortUsageTests.cs
+++ tests/Tests/Search/Search/SortUsageTests.cs
@@ -1,8 +1,10 @@
-﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Licensed to Elasticsearch B.V under one or more agreements.
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Tests.Core.ManagedElasticsearch.Clusters;
+using Tests.Domain;
 using Tests.Framework.EndpointTests.TestState;
 
 namespace Tests.Search.Search;
@@ -11,24 +13,31 @@ public class SortUsageTests : SearchUsageTestBase
 {
 	public SortUsageTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
 
-	//protected override object ExpectJson =>
-	//	new
-	//	{
-	//		sort = "startedOn"
-	//	};
+	protected override object ExpectJson =>
+		new
+		{
+			sort = new object[]
+			{
+				new { startedOn = new { order = "asc" } },
+				new { name = new { order = "desc" } }
+			}
+		};
 
-	//protected override Action<SearchRequestDescriptor<Project>> Fluent => s => s
-	//	.Sort(new Sort
-	//	{
-	//		new SortCombinations("startedOn")
-	//	});
+	// TODO - Update once we code-generate the fluent syntax
+	protected override Action<SearchRequestDescriptor<Project>> Fluent => s => s
+		.Sort(new[]
+		{
+			new SortCombinations(SortOptions.Field(Infer.Field<Project>(f => f.StartedOn), new FieldSort { Order = SortOrder.Asc })),
+			new SortCombinations(SortOptions.Field(Infer.Field<Project>(f => f.Name), new FieldSort { Order = SortOrder.Desc }))
+		});
 
-	//protected override SearchRequest<Project> Initializer =>
-	//	new()
-	//	{
-	//		Sort = new Sort
-	//		{
-	//			new SortCombinations(new SortOptions())
-	//		}
-	//	};
+	protected override SearchRequest<Project> Initializer =>
+		new()
+		{
+			Sort = new[]
+			{
+				new SortCombinations(SortOptions.Field(Infer.Field<Project>(f => f.StartedOn), new FieldSort { Order = SortOrder.Asc })),
+				new SortCombinations(SortOptions.Field(Infer.Field<Project>(f => f.Name), new FieldSort { Order = SortOrder.Desc }))
+			}
+		};
 }
