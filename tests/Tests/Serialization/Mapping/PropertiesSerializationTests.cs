53ca00824fb7c0689e84f000e5a80e8cf16a87ed
Steve Gordon <sgordon@hotmail.co.uk>
2022-07-20 11:18:39 +0100

    Add properties serialization tests

---
+++ tests/Tests/Serialization/Mapping/PropertiesSerializationTests.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Elastic.Clients.Elasticsearch.Mapping;
+using Tests.Domain;
+using VerifyXunit;
+
+namespace Tests.Serialization.Mapping;
+
+[UsesVerify]
+public class PropertiesSerializationTests : SerializerTestBase
+{
+	[U]
+	public async Task CanSerialize_Properties_WithPropertyNameExpression()
+	{
+		var result = await RoundTripAndVerifyJson(new Properties<Project>
+		{
+			{ p => p.Name, new TextProperty { Boost = 1.2 } }
+		});
+
+		var textProperty = result["name"].Should().BeOfType<TextProperty>().Subject;
+		textProperty.Boost.Should().Be(1.2);
+	}
+
+	[U]
+	public async Task CanSerialize_MultipleProperties_WithPropertyNameExpression()
+	{
+		var result = await RoundTripAndVerifyJson(new Properties<Project>
+		{
+			{ p => p.Name, new TextProperty { Boost = 1.2 } },
+			{ p => p.Description, new TextProperty { Boost = 1.4 } }
+		});
+
+		var nameTextProperty = result["name"].Should().BeOfType<TextProperty>().Subject;
+		nameTextProperty.Boost.Should().Be(1.2);
+
+		var descriptionTextProperty = result["description"].Should().BeOfType<TextProperty>().Subject;
+		descriptionTextProperty.Boost.Should().Be(1.4);
+	}
+}
