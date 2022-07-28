23f15e7fe06f8ca03d223d03602fc728e3eda2ae
Steve Gordon <sgordon@hotmail.co.uk>
2022-07-28 13:17:11 +0100

    Fix GetTerms and sub-aggregations (#6627)

--- src/Elastic.Clients.Elasticsearch/Types/Aggregations/TermsBucket.cs
+++ src/Elastic.Clients.Elasticsearch/Types/Aggregations/TermsBucket.cs
@@ -2,22 +2,24 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Elastic.Clients.Elasticsearch.Aggregations;
 
-public sealed class TermsBucket<TKey>
+public sealed class TermsBucket<TKey> : AggregateDictionary
 {
+	public TermsBucket(IReadOnlyDictionary<string, IAggregate> backingDictionary) : base(backingDictionary)
+	{
+	}
+
 	public TKey Key { get; init; }
+
 	public string? KeyAsString { get; init; }
 
 	[JsonInclude]
 	[JsonPropertyName("doc_count_error")]
 	public long? DocCountError { get; init; }
-
-	[JsonInclude]
-	[JsonPropertyName("aggregations")]
-	public Elastic.Clients.Elasticsearch.Aggregations.AggregateDictionary Aggregations { get; init; }
 
 	[JsonInclude]
 	[JsonPropertyName("doc_count")]
