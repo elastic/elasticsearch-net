12c76943f4849649acef3e8ecbd1d3a2540e5ed6
Steve Gordon <sgordon@hotmail.co.uk>
2022-07-29 11:59:19 +0100

    Support sub-aggs for single bucket aggregations (#6633)
    * Aggregates implement IAggregate
    
    * Response to schema changes
    
    * Remove Aggregate class
    
    * Fix single bucket aggregate types

--- src/Elastic.Clients.Elasticsearch/_Generated/Types/Aggregations/CardinalityAggregate.g.cs
+++ src/Elastic.Clients.Elasticsearch/_Generated/Types/Aggregations/CardinalityAggregate.g.cs
@@ -24,7 +24,7 @@ using System.Text.Json.Serialization;
 #nullable restore
 namespace Elastic.Clients.Elasticsearch.Aggregations
 {
-	public sealed partial class CardinalityAggregate : Aggregate
+	public sealed partial class CardinalityAggregate : IAggregate
 	{
 		[JsonInclude]
 		[JsonPropertyName("meta")]
