88d8f55c4236c778a22f768845a5e7e5cd63582f
Steve Gordon <sgordon@hotmail.co.uk>
2022-09-26 09:10:26 +0100

    Update test cases for intervals query serialise

--- tests/Tests/Serialization/Queries/IntervalsQuerySerializationTests.cs
+++ tests/Tests/Serialization/Queries/IntervalsQuerySerializationTests.cs
@@ -2,7 +2,9 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+using System.Threading;
 using System.Threading.Tasks;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 using Tests.Domain;
 using VerifyXunit;
 
@@ -12,7 +14,7 @@ namespace Tests.Serialization;
 public class IntervalsQuerySerializationTests : SerializerTestBase
 {
 	[U]
-	public async Task CanSerialize()
+	public async Task Descriptor_CanSerialize()
 	{
 		var search = new SearchRequestDescriptor<Project>(search => search
 			.Query(q => q
@@ -24,6 +26,28 @@ public class IntervalsQuerySerializationTests : SerializerTestBase
 						.Query("Steve")
 						.MaxGaps(0)
 						.Ordered()))));
+
+		var serialisedJson = await SerializeAndGetJsonStringAsync(search);
+
+		await Verifier.VerifyJson(serialisedJson);
+	}
+
+	[U]
+	public async Task Object_CanSerialize()
+	{
+		var intervalsMatch = new IntervalsMatch
+		{
+			Query = "Steve",
+			MaxGaps = 0,
+			Ordered = true
+		};
+
+		var query = IntervalsQuery.Match(Infer.Field<Project>(f => f.Name), intervalsMatch);
+		query.QueryName = "testing-intervals";
+		query.Boost = 2.0f;
+
+		var search = new SearchRequestDescriptor<Project>(search => search
+			.Query(q => q.Intervals(query)));
 
 		var serialisedJson = await SerializeAndGetJsonStringAsync(search);
 
