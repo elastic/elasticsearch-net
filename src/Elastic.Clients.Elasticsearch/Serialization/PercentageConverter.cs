e9c888c576de19a8d829a61e3cdaf80a98673cf9
Steve Gordon <sgordon@hotmail.co.uk>
2022-10-03 15:05:10 +0100

    Reduce initial API surface (#6704)
    * Reduce initial API surface
    
    * Further supported API modifications

--- src/Elastic.Clients.Elasticsearch/Serialization/PercentageConverter.cs
+++ src/Elastic.Clients.Elasticsearch/Serialization/PercentageConverter.cs
@@ -9,34 +9,32 @@ using System.Text.Json.Serialization;
 
 namespace Elastic.Clients.Elasticsearch
 {
-	internal sealed class PercentageConverter : JsonConverter<Percentage>
-	{
-		public override Percentage Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-		{
-			var token = reader.TokenType;
+	//internal sealed class PercentageConverter : JsonConverter<Percentage>
+	//{
+	//	public override Percentage Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	//	{
+	//		var token = reader.TokenType;
 
-			switch (token)
-			{
-				case JsonTokenType.String:
-				{
-					var value = reader.GetString();
-					var result = (Percentage)Activator.CreateInstance(typeof(Percentage), value);
-					return result;
-				}
-				case JsonTokenType.Number:
-				{
-					var value = reader.GetSingle();
-					var result = (Percentage)Activator.CreateInstance(typeof(Percentage), value);
-					return result;
-				}
-			}
+	//		switch (token)
+	//		{
+	//			case JsonTokenType.String:
+	//			{
+	//				var value = reader.GetString();
+	//				var result = (Percentage)Activator.CreateInstance(typeof(Percentage), value);
+	//				return result;
+	//			}
+	//			case JsonTokenType.Number:
+	//			{
+	//				var value = reader.GetSingle();
+	//				var result = (Percentage)Activator.CreateInstance(typeof(Percentage), value);
+	//				return result;
+	//			}
+	//		}
 
-			throw new SerializationException();
-		}
+	//		throw new SerializationException();
+	//	}
 
-		public override void Write(Utf8JsonWriter writer, Percentage value, JsonSerializerOptions options) =>
-			throw new NotImplementedException();
-	}
-
-	
+	//	public override void Write(Utf8JsonWriter writer, Percentage value, JsonSerializerOptions options) =>
+	//		throw new NotImplementedException();
+	//}
 }
