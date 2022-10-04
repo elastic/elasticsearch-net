c59412c17620896315a2c95040ce756dc9bf2187
Steve Gordon <sgordon@hotmail.co.uk>
2022-10-04 17:50:51 +0100

    WIP DeleteByQuery tests

---
+++ tests/Tests/Document/Multiple/DeleteByQueryRethrottle/DeleteByQueryRethrottleUrlTests.cs
@@ -0,0 +1,22 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.Threading.Tasks;
+using Tests.Framework.EndpointTests;
+using static Tests.Framework.EndpointTests.UrlTester;
+
+namespace Tests.Document.Multiple.DeleteByQueryRethrottle
+{
+	public class DeleteByQueryRethrottleUrlTests : UrlTestsBase
+	{
+		private readonly Id _taskId = "rhtoNesNR4aXVIY2bRR4GQ:13056";
+
+		[U] public override async Task Urls() =>
+			await POST($"/_delete_by_query/{EscapeUriString(_taskId.ToString())}/_rethrottle")
+				.Fluent(c => c.DeleteByQueryRethrottle(_taskId))
+				.Request(c => c.DeleteByQueryRethrottle(new DeleteByQueryRethrottleRequest(_taskId)))
+				.FluentAsync(c => c.DeleteByQueryRethrottleAsync(_taskId))
+				.RequestAsync(c => c.DeleteByQueryRethrottleAsync(new DeleteByQueryRethrottleRequest(_taskId)));
+	}
+}
