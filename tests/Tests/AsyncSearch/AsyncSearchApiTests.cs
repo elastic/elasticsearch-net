dddcde89c42fa87d668487e9e0c22e99c03fc15c
Steve Gordon <sgordon@hotmail.co.uk>
2022-10-10 07:57:26 +0100

    Add more integration and unit tests (#6725)
    * Format AsyncSearchApiTests
    
    * Fix ClearScrollRequest code-gen
    
    * Port ClearScroll tests
    
    * Add Scroll tests
    
    * WIP DeleteByQuery tests
    
    * Fix failing test
    
    * Fix license headers

--- tests/Tests/AsyncSearch/AsyncSearchApiTests.cs
+++ tests/Tests/AsyncSearch/AsyncSearchApiTests.cs
@@ -14,159 +14,157 @@ using Tests.Framework.EndpointTests;
 using Tests.Framework.EndpointTests.TestState;
 using System;
 
-namespace Tests.AsyncSearch
-{
-	public class AsyncSearchApiTests : CoordinatedIntegrationTestBase<ReadOnlyCluster>
-	{
-		private const string SubmitStep = nameof(SubmitStep);
-		private const string StatusStep = nameof(StatusStep);
-		private const string GetStep = nameof(GetStep);
-		private const string DeleteStep = nameof(DeleteStep);
+namespace Tests.AsyncSearch;
 
-		public AsyncSearchApiTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(new CoordinatedUsage(cluster, usage)
+public class AsyncSearchApiTests : CoordinatedIntegrationTestBase<ReadOnlyCluster>
+{
+	private const string SubmitStep = nameof(SubmitStep);
+	private const string StatusStep = nameof(StatusStep);
+	private const string GetStep = nameof(GetStep);
+	private const string DeleteStep = nameof(DeleteStep);
+
+	public AsyncSearchApiTests(ReadOnlyCluster cluster, EndpointUsage usage) : base(new CoordinatedUsage(cluster, usage)
+	{
 		{
-			{
-				SubmitStep, u =>
-				u.Calls<AsyncSearchSubmitRequestDescriptor<Project>, AsyncSearchSubmitRequest<Project>, AsyncSearchSubmitResponse<Project>>(
-					v => new AsyncSearchSubmitRequest<Project>
+			SubmitStep, u =>
+			u.Calls<AsyncSearchSubmitRequestDescriptor<Project>, AsyncSearchSubmitRequest<Project>, AsyncSearchSubmitResponse<Project>>(
+				v => new AsyncSearchSubmitRequest<Project>
+				{
+					Query = new MatchAllQuery(),
+					KeepOnCompletion = true,
+					WaitForCompletionTimeout = Duration.MinusOne,
+					Aggregations = new TermsAggregation("states")
 					{
-						Query = new MatchAllQuery(),
-						KeepOnCompletion = true,
-						WaitForCompletionTimeout = Duration.MinusOne,
-						Aggregations = new TermsAggregation("states")
+						Field = Infer.Field<Project>(p => p.State.Suffix("keyword")),
+						MinDocCount = 2,
+						Size = 5,
+						ShardSize = 100,
+						ExecutionHint = TermsAggregationExecutionHint.Map,
+						//Missing = "n/a",
+						// TODO - Review terms agg and fix this
+						//Include = new TermsInclude(new[] { StateOfBeing.Stable.ToString(), StateOfBeing.VeryActive.ToString() }),
+						Order =new []
 						{
-							Field = Infer.Field<Project>(p => p.State.Suffix("keyword")),
-							MinDocCount = 2,
-							Size = 5,
-							ShardSize = 100,
-							ExecutionHint = TermsAggregationExecutionHint.Map,
-							//Missing = "n/a",
+							AggregateOrder.KeyAscending,
+							AggregateOrder.CountDescending
+						},
+						Meta = new Dictionary<string, object> { { "foo", "bar" } }
+					}
+				},
+				(v, d) => d
+					.MatchAll()
+					.KeepOnCompletion()
+					.WaitForCompletionTimeout(-1)
+					.Aggregations(a => a
+						.Terms("states", st => st
+							.Field(p => p.State.Suffix("keyword"))
+							.MinDocCount(2)
+							.Size(5)
+							.ShardSize(100)
+							.ExecutionHint(TermsAggregationExecutionHint.Map)
+							//.Missing("n/a")
 							// TODO - Review terms agg and fix this
-							//Include = new TermsInclude(new[] { StateOfBeing.Stable.ToString(), StateOfBeing.VeryActive.ToString() }),
-							Order =new []
+							//.Include(new[] { StateOfBeing.Stable.ToString(), StateOfBeing.VeryActive.ToString() })
+							.Order(new []
 							{
 								AggregateOrder.KeyAscending,
 								AggregateOrder.CountDescending
-							},
-							Meta = new Dictionary<string, object> { { "foo", "bar" } }
-						}
-					},
-					(v, d) => d
-						.MatchAll()
-						.KeepOnCompletion()
-						.WaitForCompletionTimeout(-1)
-						.Aggregations(a => a
-							.Terms("states", st => st
-								.Field(p => p.State.Suffix("keyword"))
-								.MinDocCount(2)
-								.Size(5)
-								.ShardSize(100)
-								.ExecutionHint(TermsAggregationExecutionHint.Map)
-								//.Missing("n/a")
-								// TODO - Review terms agg and fix this
-								//.Include(new[] { StateOfBeing.Stable.ToString(), StateOfBeing.VeryActive.ToString() })
-								.Order(new []
-								{
-									AggregateOrder.KeyAscending,
-									AggregateOrder.CountDescending
-								})
-								.Meta(m => m
-									.Add("foo", "bar")
-								)
+							})
+							.Meta(m => m
+								.Add("foo", "bar")
 							)
-						),
-					(v, c, f) => c.AsyncSearch.Submit(f),
-					(v, c, f) => c.AsyncSearch.SubmitAsync(f),
-					(v, c, r) => c.AsyncSearch.Submit<Project>(r),
-					(v, c, r) => c.AsyncSearch.SubmitAsync<Project>(r),
-					onResponse: (r, values) => values.ExtendedValue("id", r.Id)
-				)
-			},
-			{
-				StatusStep, u =>
-				u.Calls<AsyncSearchStatusRequestDescriptor, AsyncSearchStatusRequest, AsyncSearchStatusResponse>(
-					v => new AsyncSearchStatusRequest(v),
-					(v, d) => d,
-					(v, c, f) => c.AsyncSearch.Status(v, f),
-					(v, c, f) => c.AsyncSearch.StatusAsync(v, f),
-					(v, c, r) => c.AsyncSearch.Status(r),
-					(v, c, r) => c.AsyncSearch.StatusAsync(r),
-					uniqueValueSelector: values => values.ExtendedValue<string>("id")
-				)
-			},
-			{
-				GetStep, u =>
-				u.Calls<GetAsyncSearchRequestDescriptor<Project>, GetAsyncSearchRequest, GetAsyncSearchResponse<Project>>(
-					v => new GetAsyncSearchRequest(v),
-					(v, d) => d,
-					(v, c, f) => c.AsyncSearch.Get(v, f),
-					(v, c, f) => c.AsyncSearch.GetAsync(v, f),
-					(v, c, r) => c.AsyncSearch.Get<Project>(r),
-					(v, c, r) => c.AsyncSearch.GetAsync<Project>(r),
-					uniqueValueSelector: values => values.ExtendedValue<string>("id")
-				)
-			},
-			{
-				DeleteStep, u =>
-				u.Calls<DeleteAsyncSearchRequestDescriptor, DeleteAsyncSearchRequest, DeleteAsyncSearchResponse>(
-					v => new DeleteAsyncSearchRequest(v),
-					(v, d) => d,
-					(v, c, f) => c.AsyncSearch.Delete(v, f),
-					(v, c, f) => c.AsyncSearch.DeleteAsync(v, f),
-					(v, c, r) => c.AsyncSearch.Delete(r),
-					(v, c, r) => c.AsyncSearch.DeleteAsync(r),
-					uniqueValueSelector: values => values.ExtendedValue<string>("id")
-				)
-			},
-		})
-		{ }
-
-		[I]
-		public async Task AsyncSearchSubmitResponse() => await Assert<AsyncSearchSubmitResponse<Project>>(SubmitStep, r =>
+						)
+					),
+				(v, c, f) => c.AsyncSearch.Submit(f),
+				(v, c, f) => c.AsyncSearch.SubmitAsync(f),
+				(v, c, r) => c.AsyncSearch.Submit<Project>(r),
+				(v, c, r) => c.AsyncSearch.SubmitAsync<Project>(r),
+				onResponse: (r, values) => values.ExtendedValue("id", r.Id)
+			)
+		},
 		{
-			r.ShouldBeValid();
-			r.Id.Should().NotBeNullOrEmpty();
-			// TODO - MORE ASSERTIONS
-			r.Response.Should().NotBeNull();
-			r.Response.Took.Should().BeGreaterOrEqualTo(0);
-		});
-
-		[I]
-		public async Task AsyncSearchStatusResponse() => await Assert<AsyncSearchStatusResponse>(StatusStep, r =>
+			StatusStep, u =>
+			u.Calls<AsyncSearchStatusRequestDescriptor, AsyncSearchStatusRequest, AsyncSearchStatusResponse>(
+				v => new AsyncSearchStatusRequest(v),
+				(v, d) => d,
+				(v, c, f) => c.AsyncSearch.Status(v, f),
+				(v, c, f) => c.AsyncSearch.StatusAsync(v, f),
+				(v, c, r) => c.AsyncSearch.Status(r),
+				(v, c, r) => c.AsyncSearch.StatusAsync(r),
+				uniqueValueSelector: values => values.ExtendedValue<string>("id")
+			)
+		},
 		{
-			r.ShouldBeValid();
-			r.StartTimeInMillis.Should().BeGreaterThan(DateTimeOffset.UtcNow.AddMinutes(-10).ToUnixTimeMilliseconds());
-			r.StartTimeInMillis.Should().BeLessOrEqualTo(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
-			r.ExpirationTimeInMillis.Should().BeGreaterOrEqualTo(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
-
-			if (r.IsRunning)
-				r.CompletionStatus.HasValue.Should().BeFalse();
-			else
-				r.CompletionStatus?.Should().Be(200);
-
-			r.Shards.Total.Should().BeGreaterOrEqualTo(1);
-		});
-
-		[I]
-		public async Task AsyncSearchGetResponse() => await Assert<GetAsyncSearchResponse<Project>>(GetStep, (s, r) =>
+			GetStep, u =>
+			u.Calls<GetAsyncSearchRequestDescriptor<Project>, GetAsyncSearchRequest, GetAsyncSearchResponse<Project>>(
+				v => new GetAsyncSearchRequest(v),
+				(v, d) => d,
+				(v, c, f) => c.AsyncSearch.Get(v, f),
+				(v, c, f) => c.AsyncSearch.GetAsync(v, f),
+				(v, c, r) => c.AsyncSearch.Get<Project>(r),
+				(v, c, r) => c.AsyncSearch.GetAsync<Project>(r),
+				uniqueValueSelector: values => values.ExtendedValue<string>("id")
+			)
+		},
 		{
-			r.ShouldBeValid();
-			r.Id.Should().NotBeNullOrEmpty();
-			r.StartTimeInMillis.Should().BeGreaterThan(DateTimeOffset.UtcNow.AddMinutes(-10).ToUnixTimeMilliseconds());
-			r.StartTimeInMillis.Should().BeLessOrEqualTo(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
-			r.ExpirationTimeInMillis.Should().BeGreaterOrEqualTo(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
-			r.Response.Should().NotBeNull();
-			r.Response.Took.Should().BeGreaterOrEqualTo(0);
-			r.Response.Hits.Should().HaveCount(10);
-			var terms = r.Response.Aggregations.GetTerms("states");
-			terms.Should().NotBeNull();
-		});
+			DeleteStep, u =>
+			u.Calls<DeleteAsyncSearchRequestDescriptor, DeleteAsyncSearchRequest, DeleteAsyncSearchResponse>(
+				v => new DeleteAsyncSearchRequest(v),
+				(v, d) => d,
+				(v, c, f) => c.AsyncSearch.Delete(v, f),
+				(v, c, f) => c.AsyncSearch.DeleteAsync(v, f),
+				(v, c, r) => c.AsyncSearch.Delete(r),
+				(v, c, r) => c.AsyncSearch.DeleteAsync(r),
+				uniqueValueSelector: values => values.ExtendedValue<string>("id")
+			)
+		},
+	})
+	{ }
 
-		[I]
-		public async Task AsyncSearchDeleteResponse() => await Assert<DeleteAsyncSearchResponse>(DeleteStep, r =>
-		{
-			r.ShouldBeValid();
-			r.Acknowledged.Should().BeTrue();
-		});
-	}
+	[I]
+	public async Task AsyncSearchSubmitResponse() => await Assert<AsyncSearchSubmitResponse<Project>>(SubmitStep, r =>
+	{
+		r.ShouldBeValid();
+		r.Id.Should().NotBeNullOrEmpty();
+		r.Response.Should().NotBeNull();
+		r.Response.Took.Should().BeGreaterOrEqualTo(0);
+	});
+
+	[I]
+	public async Task AsyncSearchStatusResponse() => await Assert<AsyncSearchStatusResponse>(StatusStep, r =>
+	{
+		r.ShouldBeValid();
+		r.StartTimeInMillis.Should().BeGreaterThan(DateTimeOffset.UtcNow.AddMinutes(-10).ToUnixTimeMilliseconds());
+		r.StartTimeInMillis.Should().BeLessOrEqualTo(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+		r.ExpirationTimeInMillis.Should().BeGreaterOrEqualTo(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+
+		if (r.IsRunning)
+			r.CompletionStatus.HasValue.Should().BeFalse();
+		else
+			r.CompletionStatus?.Should().Be(200);
+
+		r.Shards.Total.Should().BeGreaterOrEqualTo(1);
+	});
+
+	[I]
+	public async Task AsyncSearchGetResponse() => await Assert<GetAsyncSearchResponse<Project>>(GetStep, (s, r) =>
+	{
+		r.ShouldBeValid();
+		r.Id.Should().NotBeNullOrEmpty();
+		r.StartTimeInMillis.Should().BeGreaterThan(DateTimeOffset.UtcNow.AddMinutes(-10).ToUnixTimeMilliseconds());
+		r.StartTimeInMillis.Should().BeLessOrEqualTo(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+		r.ExpirationTimeInMillis.Should().BeGreaterOrEqualTo(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+		r.Response.Should().NotBeNull();
+		r.Response.Took.Should().BeGreaterOrEqualTo(0);
+		r.Response.Hits.Should().HaveCount(10);
+		var terms = r.Response.Aggregations.GetTerms("states");
+		terms.Should().NotBeNull();
+	});
+
+	[I]
+	public async Task AsyncSearchDeleteResponse() => await Assert<DeleteAsyncSearchResponse>(DeleteStep, r =>
+	{
+		r.ShouldBeValid();
+		r.Acknowledged.Should().BeTrue();
+	});
 }
