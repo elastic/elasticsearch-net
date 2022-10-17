d343ee9302486cfc638a4dd2b13c56cbd5efd55e
Steve Gordon <sgordon@hotmail.co.uk>
2022-10-17 17:50:35 +0100

    Fix dictionary key serialization for Name type (#6786)
    * Fix dictionary key serialization for Name type
    
    * Fix BOM

--- src/Elastic.Clients.Elasticsearch/_Generated/Api/SearchResponse.g.cs
+++ src/Elastic.Clients.Elasticsearch/_Generated/Api/SearchResponse.g.cs
@@ -23,7 +23,7 @@ using System.Text.Json.Serialization;
 
 #nullable restore
 namespace Elastic.Clients.Elasticsearch;
-public sealed partial class SearchResponse<TDocument> : ElasticsearchResponseBase
+public partial class SearchResponse<TDocument> : ElasticsearchResponseBase
 {
 	[JsonInclude]
 	[JsonPropertyName("aggregations")]
