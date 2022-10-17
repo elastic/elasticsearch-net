a7420d2e13b3b2432b25e09711145094816af047
Steve Gordon <sgordon@hotmail.co.uk>
2022-10-17 16:39:43 +0100

    Fix dictionary key serialization for Name type

--- src/Elastic.Clients.Elasticsearch/Serialization/EnumStructConverter.cs
+++ src/Elastic.Clients.Elasticsearch/Serialization/EnumStructConverter.cs
@@ -1,4 +1,4 @@
-// Licensed to Elasticsearch B.V under one or more agreements.
+﻿// Licensed to Elasticsearch B.V under one or more agreements.
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
@@ -9,21 +9,16 @@ using System.Text.Json.Serialization;
 
 namespace Elastic.Clients.Elasticsearch.Serialization;
 
-// TODO - In .NET 7 we could review supporting IParsable as a type constraint?
-internal sealed class StringAliasConverter<T> : JsonConverter<T>
+internal sealed class EnumStructConverter<T> : JsonConverter<T> where T : new()
 {
-	public override T ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => Read(ref reader, typeToConvert, options);
-
-	public override void WriteAsPropertyName(Utf8JsonWriter writer, T value, JsonSerializerOptions options) => writer.WritePropertyName(value.ToString());
-
 	public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 	{
 		var value = reader.GetString();
 
 		var instance = (T)Activator.CreateInstance(
 			typeof(T),
-			BindingFlags.Instance | BindingFlags.Public,
-			args: new object[] { value },
+			BindingFlags.Instance | BindingFlags.NonPublic,
+			args: new object[] { value }, // TODO: Perf - Review use of ArrayPool
 			binder: null,
 			culture: null)!;
 
@@ -32,13 +27,11 @@ internal sealed class StringAliasConverter<T> : JsonConverter<T>
 
 	public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
 	{
-		if (value is null)
-		{
-			writer.WriteNullValue();
-		}
-		else
-		{
+		var enumValue = value.ToString();
+
+		if (!string.IsNullOrEmpty(enumValue))
 			writer.WriteStringValue(value.ToString());
-		}
+		else
+			writer.WriteNullValue();
 	}
 }
