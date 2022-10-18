d0d2092af705c8ec391f382d4dc3a933a54a6c3d
Steve Gordon <sgordon@hotmail.co.uk>
2022-10-18 15:27:31 +0100

    Refactor to file-scoped namespaces (#6793)

--- src/Elastic.Clients.Elasticsearch/Common/Infer/Id/Id.cs
+++ src/Elastic.Clients.Elasticsearch/Common/Infer/Id/Id.cs
@@ -9,104 +9,103 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using Elastic.Transport;
 
-namespace Elastic.Clients.Elasticsearch
+namespace Elastic.Clients.Elasticsearch;
+
+[DebuggerDisplay("{DebugDisplay,nq}")]
+[JsonConverter(typeof(IdConverter))]
+public class Id : IEquatable<Id>, IUrlParameter
 {
-	[DebuggerDisplay("{DebugDisplay,nq}")]
-	[JsonConverter(typeof(IdConverter))]
-	public class Id : IEquatable<Id>, IUrlParameter
+	public Id(string id)
 	{
-		public Id(string id)
-		{
-			Tag = 0;
-			StringValue = id;
-		}
-
-		public Id(long id)
-		{
-			Tag = 1;
-			LongValue = id;
-		}
-
-		public Id(object document)
-		{
-			Tag = 2;
-			Document = document;
-		}
-
-		internal object Document { get; }
-		internal long? LongValue { get; }
-		internal string StringOrLongValue => StringValue ?? LongValue?.ToString(CultureInfo.InvariantCulture);
-		internal string StringValue { get; }
-		internal int Tag { get; }
-
-		private string DebugDisplay => StringOrLongValue ?? "Id from instance typeof: " + Document?.GetType().Name;
-
-		private static int TypeHashCode { get; } = typeof(Id).GetHashCode();
-
-		public bool Equals(Id other)
-		{
-			if (Tag + other.Tag == 1)
-				return StringOrLongValue == other.StringOrLongValue;
-			else if (Tag != other.Tag)
-				return false;
-
-			return Tag switch
-			{
-				0 or 1 => StringOrLongValue == other.StringOrLongValue,
-				_ => Document?.Equals(other.Document) ?? false,
-			};
-		}
-
-		string IUrlParameter.GetString(ITransportConfiguration? settings)
-		{
-			var elasticClientSettings = (IElasticsearchClientSettings)settings;
-			return GetString(elasticClientSettings);
-		}
-
-		internal string GetString(IElasticsearchClientSettings settings) => settings.Inferrer.Id(Document) ?? StringOrLongValue;
-
-		public static implicit operator Id(string id) => id.IsNullOrEmpty() ? null : new Id(id);
-
-		public static implicit operator Id(long id) => new(id);
-
-		public static implicit operator Id(Guid id) => new(id.ToString("D"));
-
-		public static Id From<T>(T document) => new(document);
-
-		public override string ToString() => DebugDisplay;
-
-		public override bool Equals(object obj)
-		{
-			switch (obj)
-			{
-				case Id r:
-					return Equals(r);
-				case string s:
-					return Equals(s);
-				case int l:
-					return Equals(l);
-				case long l:
-					return Equals(l);
-				case Guid g:
-					return Equals(g);
-			}
-			return Equals(new Id(obj));
-		}
-
-		public override int GetHashCode()
-		{
-			unchecked
-			{
-				var result = TypeHashCode;
-				result = (result * 397) ^ (StringValue?.GetHashCode() ?? 0);
-				result = (result * 397) ^ (LongValue?.GetHashCode() ?? 0);
-				result = (result * 397) ^ (Document?.GetHashCode() ?? 0);
-				return result;
-			}
-		}
-
-		public static bool operator ==(Id left, Id right) => Equals(left, right);
-
-		public static bool operator !=(Id left, Id right) => !Equals(left, right);
+		Tag = 0;
+		StringValue = id;
 	}
+
+	public Id(long id)
+	{
+		Tag = 1;
+		LongValue = id;
+	}
+
+	public Id(object document)
+	{
+		Tag = 2;
+		Document = document;
+	}
+
+	internal object Document { get; }
+	internal long? LongValue { get; }
+	internal string StringOrLongValue => StringValue ?? LongValue?.ToString(CultureInfo.InvariantCulture);
+	internal string StringValue { get; }
+	internal int Tag { get; }
+
+	private string DebugDisplay => StringOrLongValue ?? "Id from instance typeof: " + Document?.GetType().Name;
+
+	private static int TypeHashCode { get; } = typeof(Id).GetHashCode();
+
+	public bool Equals(Id other)
+	{
+		if (Tag + other.Tag == 1)
+			return StringOrLongValue == other.StringOrLongValue;
+		else if (Tag != other.Tag)
+			return false;
+
+		return Tag switch
+		{
+			0 or 1 => StringOrLongValue == other.StringOrLongValue,
+			_ => Document?.Equals(other.Document) ?? false,
+		};
+	}
+
+	string IUrlParameter.GetString(ITransportConfiguration? settings)
+	{
+		var elasticClientSettings = (IElasticsearchClientSettings)settings;
+		return GetString(elasticClientSettings);
+	}
+
+	internal string GetString(IElasticsearchClientSettings settings) => settings.Inferrer.Id(Document) ?? StringOrLongValue;
+
+	public static implicit operator Id(string id) => id.IsNullOrEmpty() ? null : new Id(id);
+
+	public static implicit operator Id(long id) => new(id);
+
+	public static implicit operator Id(Guid id) => new(id.ToString("D"));
+
+	public static Id From<T>(T document) => new(document);
+
+	public override string ToString() => DebugDisplay;
+
+	public override bool Equals(object obj)
+	{
+		switch (obj)
+		{
+			case Id r:
+				return Equals(r);
+			case string s:
+				return Equals(s);
+			case int l:
+				return Equals(l);
+			case long l:
+				return Equals(l);
+			case Guid g:
+				return Equals(g);
+		}
+		return Equals(new Id(obj));
+	}
+
+	public override int GetHashCode()
+	{
+		unchecked
+		{
+			var result = TypeHashCode;
+			result = (result * 397) ^ (StringValue?.GetHashCode() ?? 0);
+			result = (result * 397) ^ (LongValue?.GetHashCode() ?? 0);
+			result = (result * 397) ^ (Document?.GetHashCode() ?? 0);
+			return result;
+		}
+	}
+
+	public static bool operator ==(Id left, Id right) => Equals(left, right);
+
+	public static bool operator !=(Id left, Id right) => !Equals(left, right);
 }
--- src/Elastic.Clients.Elasticsearch/Common/UrlParameters/DataStreamNames/DataStreamNames.cs
+++ src/Elastic.Clients.Elasticsearch/Common/UrlParameters/DataStreamNames/DataStreamNames.cs
@@ -11,112 +11,111 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using Elastic.Transport;
 
-namespace Elastic.Clients.Elasticsearch
+namespace Elastic.Clients.Elasticsearch;
+
+[JsonConverter(typeof(DataStreamNamesConverter))]
+[DebuggerDisplay("{DebugDisplay,nq}")]
+public sealed class DataStreamNames : IUrlParameter, IEnumerable<DataStreamName>, IEquatable<DataStreamNames>
 {
-	[JsonConverter(typeof(DataStreamNamesConverter))]
-	[DebuggerDisplay("{DebugDisplay,nq}")]
-	public sealed class DataStreamNames : IUrlParameter, IEnumerable<DataStreamName>, IEquatable<DataStreamNames>
+	internal readonly IList<DataStreamName> Names;
+
+	internal DataStreamNames() => Names = new List<DataStreamName>();
+
+	internal DataStreamNames(IEnumerable<DataStreamName> dataStreamNames) => Names = dataStreamNames.ToList();
+
+	internal DataStreamNames(IList<DataStreamName> dataStreamNames) => Names = dataStreamNames;
+
+	private string DebugDisplay =>
+		$"Count: {Names.Count} [" + string.Join(",", Names.Select((t, i) => $"({i + 1}: {t?.DebugDisplay ?? "NULL"})")) + "]";
+
+	public override string ToString() => string.Join(",", Names.Where(f => f is not null).Select(f => f.Name));
+
+	public static implicit operator DataStreamNames(string[] dataStreamNames) => dataStreamNames.IsEmpty() ? null : new DataStreamNames(dataStreamNames.Select(f => new DataStreamName(f)));
+
+	public static implicit operator DataStreamNames(string dataStreamName) => dataStreamName.IsNullOrEmptyCommaSeparatedList(out var split)
+		? null
+		: new DataStreamNames(split.Select(f => new DataStreamName(f)));
+
+	public static implicit operator DataStreamNames(DataStreamName dataStreamName) => dataStreamName == null ? null : new DataStreamNames(new[] { dataStreamName });
+
+	public static implicit operator DataStreamNames(DataStreamName[] dataStreamNames) => dataStreamNames.IsEmpty() ? null : new DataStreamNames(dataStreamNames);
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	public IEnumerator<DataStreamName> GetEnumerator() => Names.GetEnumerator();
+
+	public static bool operator ==(DataStreamNames left, DataStreamNames right) => Equals(left, right);
+
+	public static bool operator !=(DataStreamNames left, DataStreamNames right) => !Equals(left, right);
+
+	public override bool Equals(object obj) =>
+		obj switch
+		{
+			DataStreamNames f => Equals(f),
+			string s => Equals(s),
+			DataStreamName fn => Equals(fn),
+			DataStreamName[] fns => Equals(fns),
+			_ => false,
+		};
+
+	public bool Equals(DataStreamNames other) => EqualsAllNames(Names, other.Names);
+
+	private static bool EqualsAllNames(IList<DataStreamName> thisTypes, IList<DataStreamName> otherTypes)
 	{
-		internal readonly IList<DataStreamName> Names;
+		if (thisTypes is null && otherTypes is null)
+			return true;
 
-		internal DataStreamNames() => Names = new List<DataStreamName>();
+		if (thisTypes is null || otherTypes is null)
+			return false;
 
-		internal DataStreamNames(IEnumerable<DataStreamName> dataStreamNames) => Names = dataStreamNames.ToList();
+		if (thisTypes.Count != otherTypes.Count)
+			return false;
 
-		internal DataStreamNames(IList<DataStreamName> dataStreamNames) => Names = dataStreamNames;
-
-		private string DebugDisplay =>
-			$"Count: {Names.Count} [" + string.Join(",", Names.Select((t, i) => $"({i + 1}: {t?.DebugDisplay ?? "NULL"})")) + "]";
-
-		public override string ToString() => string.Join(",", Names.Where(f => f is not null).Select(f => f.Name));
-
-		public static implicit operator DataStreamNames(string[] dataStreamNames) => dataStreamNames.IsEmpty() ? null : new DataStreamNames(dataStreamNames.Select(f => new DataStreamName(f)));
-
-		public static implicit operator DataStreamNames(string dataStreamName) => dataStreamName.IsNullOrEmptyCommaSeparatedList(out var split)
-			? null
-			: new DataStreamNames(split.Select(f => new DataStreamName(f)));
-
-		public static implicit operator DataStreamNames(DataStreamName dataStreamName) => dataStreamName == null ? null : new DataStreamNames(new[] { dataStreamName });
-
-		public static implicit operator DataStreamNames(DataStreamName[] dataStreamNames) => dataStreamNames.IsEmpty() ? null : new DataStreamNames(dataStreamNames);
-
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-		public IEnumerator<DataStreamName> GetEnumerator() => Names.GetEnumerator();
-
-		public static bool operator ==(DataStreamNames left, DataStreamNames right) => Equals(left, right);
-
-		public static bool operator !=(DataStreamNames left, DataStreamNames right) => !Equals(left, right);
-
-		public override bool Equals(object obj) =>
-			obj switch
-			{
-				DataStreamNames f => Equals(f),
-				string s => Equals(s),
-				DataStreamName fn => Equals(fn),
-				DataStreamName[] fns => Equals(fns),
-				_ => false,
-			};
-
-		public bool Equals(DataStreamNames other) => EqualsAllNames(Names, other.Names);
-
-		private static bool EqualsAllNames(IList<DataStreamName> thisTypes, IList<DataStreamName> otherTypes)
-		{
-			if (thisTypes is null && otherTypes is null)
-				return true;
-
-			if (thisTypes is null || otherTypes is null)
-				return false;
-
-			if (thisTypes.Count != otherTypes.Count)
-				return false;
-
-			return !thisTypes.Except(otherTypes).Any();
-		}
-
-		public override int GetHashCode()
-		{
-			var hashCodes = new List<int>(Names.Count);
-			foreach (var item in Names)
-			{
-				hashCodes.Add(item.GetHashCode());
-			}
-
-			hashCodes.Sort();
-
-			unchecked
-			{
-				var hash = 17;
-				foreach (var hashCode in hashCodes)
-				{
-					hash = hash * 23 + hashCode;
-				}
-				return typeof(DataStreamName).GetHashCode() ^ hash;
-			}
-		}
-
-		string IUrlParameter.GetString(ITransportConfiguration? settings) => ToString();
+		return !thisTypes.Except(otherTypes).Any();
 	}
 
-	internal sealed class DataStreamNamesConverter : JsonConverter<DataStreamNames>
+	public override int GetHashCode()
 	{
-		public override DataStreamNames? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		var hashCodes = new List<int>(Names.Count);
+		foreach (var item in Names)
 		{
-			if (reader.TokenType != JsonTokenType.StartArray)
-				throw new JsonException($"Expected {JsonTokenType.StartArray} token but read '{reader.TokenType}' token for DataStreamNames.");
-
-			return JsonSerializer.Deserialize<string[]>(ref reader, options);
+			hashCodes.Add(item.GetHashCode());
 		}
 
-		public override void Write(Utf8JsonWriter writer, DataStreamNames value, JsonSerializerOptions options)
+		hashCodes.Sort();
+
+		unchecked
 		{
-			if (value is null)
+			var hash = 17;
+			foreach (var hashCode in hashCodes)
 			{
-				writer.WriteNullValue();
-				return;
+				hash = hash * 23 + hashCode;
 			}
-
-			JsonSerializer.Serialize(writer, value.Names, options);
+			return typeof(DataStreamName).GetHashCode() ^ hash;
 		}
+	}
+
+	string IUrlParameter.GetString(ITransportConfiguration? settings) => ToString();
+}
+
+internal sealed class DataStreamNamesConverter : JsonConverter<DataStreamNames>
+{
+	public override DataStreamNames? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		if (reader.TokenType != JsonTokenType.StartArray)
+			throw new JsonException($"Expected {JsonTokenType.StartArray} token but read '{reader.TokenType}' token for DataStreamNames.");
+
+		return JsonSerializer.Deserialize<string[]>(ref reader, options);
+	}
+
+	public override void Write(Utf8JsonWriter writer, DataStreamNames value, JsonSerializerOptions options)
+	{
+		if (value is null)
+		{
+			writer.WriteNullValue();
+			return;
+		}
+
+		JsonSerializer.Serialize(writer, value.Names, options);
 	}
 }
