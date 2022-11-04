c3317b74a5b70dff3696ae7a724971688d200d34
Steve Gordon <sgordon@hotmail.co.uk>
2022-11-04 09:13:36 +0000

    Add TryGet methods for internally tagged unions (#6916)

--- src/Elastic.Clients.Elasticsearch/_Generated/Types/Analysis/Normalizers.g.cs
+++ src/Elastic.Clients.Elasticsearch/_Generated/Types/Analysis/Normalizers.g.cs
@@ -20,6 +20,7 @@ using Elastic.Clients.Elasticsearch.Serialization;
 using Elastic.Transport;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -37,6 +38,19 @@ public partial class Normalizers : IsADictionary<string, INormalizer>
 	}
 
 	public void Add(string name, INormalizer normalizer) => BackingDictionary.Add(Sanitize(name), normalizer);
+	public bool TryGetNormalizer(string name, [NotNullWhen(returnValue: true)] out INormalizer normalizer) => BackingDictionary.TryGetValue(name, out normalizer);
+	public bool TryGetNormalizer<T>(string name, [NotNullWhen(returnValue: true)] out T? normalizer)
+		where T : class, INormalizer
+	{
+		if (BackingDictionary.TryGetValue(name, out var matchedValue) && matchedValue is T finalValue)
+		{
+			normalizer = finalValue;
+			return true;
+		}
+
+		normalizer = null;
+		return false;
+	}
 }
 
 public sealed partial class NormalizersDescriptor : IsADictionaryDescriptor<NormalizersDescriptor, Normalizers, string, INormalizer>
