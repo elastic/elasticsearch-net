f62aa959638afdee6c0c47fc9d26e91e9538daef
Steve Gordon <sgordon@hotmail.co.uk>
2022-11-10 13:32:42 +0000

    Make ElasticsearchClient more mockable. (#6951)

--- src/Elastic.Clients.Elasticsearch/Client/NamespacedClientProxy.cs
+++ src/Elastic.Clients.Elasticsearch/Client/NamespacedClientProxy.cs
@@ -11,14 +11,17 @@ using Elastic.Transport.Products.Elasticsearch;
 
 namespace Elastic.Clients.Elasticsearch;
 
-/// <summary>
-/// </summary>
-/// <remarks>
-///     Not intended to be used directly.
-/// </remarks>
 public abstract class NamespacedClientProxy
 {
+	private const string InvalidOperation = "The client has not been initialised for proper usage as may have been partially mocked. Ensure you are using a " +
+		"new instance of ElasticsearchClient to perform requests over a network to Elasticsearch.";
+
 	private readonly ElasticsearchClient _client;
+	
+	/// <summary>
+	/// Initializes a new instance for mocking.
+	/// </summary>
+	protected NamespacedClientProxy() { }
 
 	internal NamespacedClientProxy(ElasticsearchClient client) => _client = client;
 
@@ -28,16 +31,39 @@ public abstract class NamespacedClientProxy
 		Action<IRequestConfiguration>? forceConfiguration = null)
 		where TRequest : Request<TRequestParameters>
 		where TResponse : ElasticsearchResponse, new()
-		where TRequestParameters : RequestParameters, new() =>
-			_client.DoRequest<TRequest, TResponse, TRequestParameters>(request, parameters, forceConfiguration);
+		where TRequestParameters : RequestParameters, new()
+	{
+		if (_client is null)
+			ThrowHelper.ThrowInvalidOperationException(InvalidOperation);
+
+		return _client.DoRequest<TRequest, TResponse, TRequestParameters>(request, parameters, forceConfiguration);
+	}
 
 	internal TResponse DoRequest<TRequest, TResponse, TRequestParameters>(
 		TRequest request,
 		Action<IRequestConfiguration>? forceConfiguration = null)
 		where TRequest : Request<TRequestParameters>
 		where TResponse : ElasticsearchResponse, new()
-		where TRequestParameters : RequestParameters, new() =>
-			_client.DoRequest<TRequest, TResponse, TRequestParameters>(request, forceConfiguration);
+		where TRequestParameters : RequestParameters, new()
+	{
+		if (_client is null)
+			ThrowHelper.ThrowInvalidOperationException(InvalidOperation);
+
+		return _client.DoRequest<TRequest, TResponse, TRequestParameters>(request, forceConfiguration);
+	}
+
+	internal Task<TResponse> DoRequestAsync<TRequest, TResponse, TRequestParameters>(
+		TRequest request,
+		CancellationToken cancellationToken = default)
+		where TRequest : Request<TRequestParameters>
+		where TResponse : ElasticsearchResponse, new()
+		where TRequestParameters : RequestParameters, new()
+	{
+		if (_client is null)
+			ThrowHelper.ThrowInvalidOperationException(InvalidOperation);
+
+		return _client.DoRequestAsync<TRequest, TResponse, TRequestParameters>(request, cancellationToken: cancellationToken);
+	}
 
 	internal Task<TResponse> DoRequestAsync<TRequest, TResponse, TRequestParameters>(
 		TRequest request,
@@ -45,16 +71,13 @@ public abstract class NamespacedClientProxy
 		CancellationToken cancellationToken = default)
 		where TRequest : Request<TRequestParameters>
 		where TResponse : ElasticsearchResponse, new()
-		where TRequestParameters : RequestParameters, new() =>
-			_client.DoRequestAsync<TRequest, TResponse, TRequestParameters>(request, parameters, cancellationToken: cancellationToken);
+		where TRequestParameters : RequestParameters, new()
+	{
+		if (_client is null)
+			ThrowHelper.ThrowInvalidOperationException(InvalidOperation);
 
-	internal Task<TResponse> DoRequestAsync<TRequest, TResponse, TRequestParameters>(
-		TRequest request,
-		CancellationToken cancellationToken = default)
-		where TRequest : Request<TRequestParameters>
-		where TResponse : ElasticsearchResponse, new()
-		where TRequestParameters : RequestParameters, new() =>
-			_client.DoRequestAsync<TRequest, TResponse, TRequestParameters>(request, cancellationToken: cancellationToken);
+		return _client.DoRequestAsync<TRequest, TResponse, TRequestParameters>(request, parameters, cancellationToken: cancellationToken);
+	}
 
 	internal Task<TResponse> DoRequestAsync<TRequest, TResponse, TRequestParameters>(
 		TRequest request,
@@ -63,6 +86,11 @@ public abstract class NamespacedClientProxy
 		CancellationToken cancellationToken = default)
 		where TRequest : Request<TRequestParameters>
 		where TResponse : ElasticsearchResponse, new()
-		where TRequestParameters : RequestParameters, new() =>
-			_client.DoRequestAsync<TRequest, TResponse, TRequestParameters>(request, parameters, forceConfiguration, cancellationToken);
+		where TRequestParameters : RequestParameters, new()
+	{
+		if (_client is null)
+			ThrowHelper.ThrowInvalidOperationException(InvalidOperation);
+
+		return _client.DoRequestAsync<TRequest, TResponse, TRequestParameters>(request, parameters, forceConfiguration, cancellationToken);
+	}
 }
