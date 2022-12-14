a8d49060a9b0ffda7cf0176ccf47a1fe09941c94
Steve Gordon <sgordon@hotmail.co.uk>
2022-12-14 12:13:16 +0000

    Update to Elastic.Transport 0.4.5 (#7102)

--- tests/Tests.Core/Extensions/ShouldExtensions.cs
+++ tests/Tests.Core/Extensions/ShouldExtensions.cs
@@ -4,7 +4,6 @@
 
 using Elastic.Transport;
 using FluentAssertions;
-using Elastic.Clients.Elasticsearch;
 using Tests.Core.Client;
 using Elastic.Transport.Products.Elasticsearch;
 
@@ -23,23 +22,6 @@ namespace Tests.Core.Extensions
 
 		public static void ShouldNotBeValid(this ElasticsearchResponse response) =>
 			response.IsValidResponse.Should().BeFalse("{0}", response.DebugInformation);
-
-		public static void ShouldBeSuccess(this ElasticsearchResponse response) =>
-			response.ApiCallDetails.HasSuccessfulStatusCode.Should().BeTrue("{0}", response.DebugInformation);
-
-		//public static void ShouldAdhereToSourceSerializerWhenSet(this Project project)
-		//{
-		//	var usingSourceSerializer = TestClient.Configuration.Random.SourceSerializer;
-		//	project.Should().NotBeNull();
-		//	if (!usingSourceSerializer)
-		//	{
-		//		project.SourceOnly.Should().BeNull();
-		//		return;
-		//	}
-		//	project.SourceOnly.Should().NotBeNull();
-		//	project.SourceOnly.NotWrittenByDefaultSerializer.Should().Be("written");
-		//	project.SourceOnly.NotReadByDefaultSerializer.Should().Be("read");
-		//}
 
 		public static void ShouldBeTrue(this bool b, IUrlParameter p) =>
 			b.Should().BeTrue(p?.GetString(TestClient.DefaultInMemoryClient.ElasticsearchClientSettings) ?? "NULL");
