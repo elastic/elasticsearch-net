0d7793fe283aa973613daad465716385f5924349
Steve Gordon <sgordon@hotmail.co.uk>
2022-12-21 09:45:05 +0000

    Implement metrics (#7130)

--- src/Elastic.Clients.Elasticsearch/Core/Infer/Metric/Metrics.cs
+++ src/Elastic.Clients.Elasticsearch/Core/Infer/Metric/Metrics.cs
@@ -3,51 +3,87 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Elastic.Transport;
 
 namespace Elastic.Clients.Elasticsearch;
 
+/// <summary>
+/// Represents a collection of unique metric names to be included in URL paths to limit the request.
+/// </summary>
 public sealed class Metrics : IEquatable<Metrics>, IUrlParameter
 {
-	// TODO: Complete this
+	private static readonly HashSet<string> EmptyMetrics = new();
 
-	//internal Metrics(IndicesStatsMetric metric) => Value = metric;
+	/// <summary>
+	/// An instance of <see cref="Metrics"/> representing all statistics.
+	/// </summary>
+	public static Metrics All { get; } = new("_all");
 
-	//internal Metrics(NodesStatsMetric metric) => Value = metric;
+	/// <summary>
+	///	Initializes a new instance of the <see cref="Metrics"/> class containing a single metric name.
+	/// </summary>
+	public Metrics(string metric)
+	{
+		if (string.IsNullOrEmpty(metric))
+			Values = EmptyMetrics;
 
-	//internal Metrics(NodesInfoMetric metric) => Value = metric;
+		Values = new HashSet<string>()
+		{
+			metric
+		};
+	}
 
-	//internal Metrics(ClusterStateMetric metric) => Value = metric;
+	/// <summary>
+	///	Initializes a new instance of the <see cref="Metrics"/> class containing a collection of metric names.
+	/// </summary>
+	public Metrics(IEnumerable<string> metrics)
+	{
+		if (metrics is null)
+			Values = EmptyMetrics;
 
-	//internal Metrics(WatcherStatsMetric metric) => Value = metric;
+		Values = new HashSet<string>(metrics);
+	}
 
-	//internal Metrics(NodesUsageMetric metric) => Value = metric;
+	private HashSet<string> Values { get; }
 
-	internal Enum Value { get; }
+	/// <inheritdoc />
+	public bool Equals(Metrics other)
+	{
+		if (other is null) return false;
 
-	public bool Equals(Metrics other) => Value.Equals(other.Value);
+		// Equality is true when the metrics names in both instances are equal, regardless of their order in the set.
+		return Values.OrderBy(t => t).SequenceEqual(other.Values.OrderBy(t => t));
+	}
 
-	string IUrlParameter.GetString(ITransportConfiguration settings) => string.Empty; // TODO Value.GetStringValue();
+	string IUrlParameter.GetString(ITransportConfiguration settings) => GetString();
 
-	//public static implicit operator Metrics(IndicesStatsMetric metric) => new Metrics(metric);
+	/// <inheritdoc />
+	public override string ToString() => GetString();
 
-	//public static implicit operator Metrics(NodesStatsMetric metric) => new Metrics(metric);
+	private string GetString()
+	{
+		if (Values == EmptyMetrics)
+			return string.Empty;
 
-	//public static implicit operator Metrics(NodesInfoMetric metric) => new Metrics(metric);
+		return string.Join(",", Values);
+	}
 
-	//public static implicit operator Metrics(ClusterStateMetric metric) => new Metrics(metric);
-
-	//public static implicit operator Metrics(WatcherStatsMetric metric) => new Metrics(metric);
-
-	//public static implicit operator Metrics(NodesUsageMetric metric) => new Metrics(metric);
-
-	public bool Equals(Enum other) => Value.Equals(other);
-
-	public override bool Equals(object obj) => obj is Enum e ? Equals(e) : obj is Metrics m && Equals(m.Value);
-
-	public override int GetHashCode() => Value != null ? Value.GetHashCode() : 0;
+	/// <inheritdoc />
+	public override int GetHashCode() => Values != null ? Values.GetHashCode() : 0;
 
 	public static bool operator ==(Metrics left, Metrics right) => Equals(left, right);
-
 	public static bool operator !=(Metrics left, Metrics right) => !Equals(left, right);
+
+	public static implicit operator Metrics(string metric) => new(metric);
+	public static implicit operator Metrics(string[] metrics) => new(metrics);
+
+	/// <inheritdoc />
+	public override bool Equals(object obj)
+	{
+		if (obj is not Metrics metrics) return false;
+
+		return Equals(metrics);
+	}
 }
