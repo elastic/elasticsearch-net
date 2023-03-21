0727292b85764d0659d0d9aee2b861c723a309b7
Steve Gordon <sgordon@hotmail.co.uk>
2023-03-21 11:50:20 +0000

    Manual code enhancements

---
+++ src/Elastic.Clients.Elasticsearch/Api/Ingest/GetPipelineResponse.cs
@@ -0,0 +1,15 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace Elastic.Clients.Elasticsearch.Ingest;
+
+public partial class GetPipelineResponse
+{
+	[JsonIgnore]
+	public IReadOnlyDictionary<string, Pipeline> Pipelines => BackingDictionary;
+}
