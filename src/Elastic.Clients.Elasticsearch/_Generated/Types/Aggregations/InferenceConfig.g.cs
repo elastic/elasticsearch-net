ca34b156f5dad7046d7620c264819f4e828100cd
Steve Gordon <sgordon@hotmail.co.uk>
2023-03-22 11:35:40 +0000

    Support weight score on FunctionScore. (#7429)
    * Fix unrelated typo
    
    * Add initial function score query test
    
    * Fix formatting in test
    
    * Add manual WeightScore methods
    
    * Converter serialization updates for empty containers

--- src/Elastic.Clients.Elasticsearch/_Generated/Types/Aggregations/InferenceConfig.g.cs
+++ src/Elastic.Clients.Elasticsearch/_Generated/Types/Aggregations/InferenceConfig.g.cs
@@ -86,15 +86,18 @@ internal sealed partial class InferenceConfigConverter : JsonConverter<Inference
 	public override void Write(Utf8JsonWriter writer, InferenceConfig value, JsonSerializerOptions options)
 	{
 		writer.WriteStartObject();
-		writer.WritePropertyName(value.VariantName);
-		switch (value.VariantName)
+		if (value.VariantName is not null & value.Variant is not null)
 		{
-			case "classification":
-				JsonSerializer.Serialize<Elastic.Clients.Elasticsearch.Ml.ClassificationInferenceOptions>(writer, (Elastic.Clients.Elasticsearch.Ml.ClassificationInferenceOptions)value.Variant, options);
-				break;
-			case "regression":
-				JsonSerializer.Serialize<Elastic.Clients.Elasticsearch.Ml.RegressionInferenceOptions>(writer, (Elastic.Clients.Elasticsearch.Ml.RegressionInferenceOptions)value.Variant, options);
-				break;
+			writer.WritePropertyName(value.VariantName);
+			switch (value.VariantName)
+			{
+				case "classification":
+					JsonSerializer.Serialize<Elastic.Clients.Elasticsearch.Ml.ClassificationInferenceOptions>(writer, (Elastic.Clients.Elasticsearch.Ml.ClassificationInferenceOptions)value.Variant, options);
+					break;
+				case "regression":
+					JsonSerializer.Serialize<Elastic.Clients.Elasticsearch.Ml.RegressionInferenceOptions>(writer, (Elastic.Clients.Elasticsearch.Ml.RegressionInferenceOptions)value.Variant, options);
+					break;
+			}
 		}
 
 		writer.WriteEndObject();
