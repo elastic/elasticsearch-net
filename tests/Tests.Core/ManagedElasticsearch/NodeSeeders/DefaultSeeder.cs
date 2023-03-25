885fb221cd111b343b499f8d0daae5e2c5bae2ca
Steve Gordon <sgordon@hotmail.co.uk>
2023-03-25 12:50:00 +0000

    Add support for additional bucket aggregations. (#7485)
    * Generate additional bucket aggregations
    
    * Add tests for bucket aggregations
    
    * Rename verify files
    
    * Skip IP prefix test in < 8.1
    
    * Add release notes for 8.0.8 (#7465)

--- tests/Tests.Core/ManagedElasticsearch/NodeSeeders/DefaultSeeder.cs
+++ tests/Tests.Core/ManagedElasticsearch/NodeSeeders/DefaultSeeder.cs
@@ -249,6 +249,7 @@ namespace Tests.Core.ManagedElasticsearch.NodeSeeders
 
 					properties = new Dictionary<string, object>
 					{
+						{ "leadDeveloper", new { type = "object", properties = new Dictionary<string, object> { { "ipAddress", new { type = "ip" } } } } },
 						{ "locationPoint", new { type = "geo_point" } },
 						{ "name", new { type = "keyword", store = true, fields = new { suggest = new { type = "completion" }, standard = new { analyzer = "standard", type = "text" } } } },
 						{ "numberOfCommits", new { type = "integer", store = true } },
