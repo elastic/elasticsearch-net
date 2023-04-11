1e2ff72dc516346314181ce39cce146095a85391
Steve Gordon <sgordon@hotmail.co.uk>
2023-04-11 15:12:45 +0100

    Avoid side effects when combining with And operator (#7615)
    * Avoid modifying an existing bool query when combining
    
    * Update dictionary exclusions

--- src/Elastic.Clients.Elasticsearch/Types/QueryDsl/SearchQuery.cs
+++ src/Elastic.Clients.Elasticsearch/Types/QueryDsl/SearchQuery.cs
@@ -11,52 +11,55 @@ namespace Elastic.Clients.Elasticsearch.QueryDsl;
 /// </summary>
 public abstract partial class SearchQuery
 {
-	//always evaluate to false so that each side of && equation is evaluated
+	// Always evaluate to false so that each side of && equation is evaluated
 	public static bool operator false(SearchQuery _) => false;
 
-	//always evaluate to false so that each side of && equation is evaluated
+	// Always evaluate to false so that each side of && equation is evaluated
 	public static bool operator true(SearchQuery _) => false;
 
-	public static SearchQuery operator &(SearchQuery leftQuery, SearchQuery rightQuery) => Combine(leftQuery, rightQuery, (l, r) => l && r);
+	public static SearchQuery? operator &(SearchQuery? leftQuery, SearchQuery? rightQuery) => Combine(leftQuery, rightQuery, (l, r) => l && r);
 
-	public static SearchQuery? operator |(SearchQuery leftQuery, SearchQuery rightQuery) => Combine(leftQuery, rightQuery, (l, r) => l || r);
+	public static SearchQuery? operator |(SearchQuery? leftQuery, SearchQuery? rightQuery) => Combine(leftQuery, rightQuery, (l, r) => l || r);
 
-	public static SearchQuery? operator !(SearchQuery query) => query is null 
+	public static SearchQuery? operator !(SearchQuery? query) => query is null
 		? null
 		: new BoolQuery { MustNot = new Query[] { query } };
 
-	public static SearchQuery? operator +(SearchQuery query) => query is null
+	public static SearchQuery? operator +(SearchQuery? query) => query is null
 		? null
 		: new BoolQuery { Filter = new Query[] { query } };
 
-	private static SearchQuery Combine(SearchQuery leftQuery, SearchQuery rightQuery, Func<Query?, Query?, Query> combine)
+	private static SearchQuery? Combine(SearchQuery? leftQuery, SearchQuery? rightQuery, Func<Query?, Query?, Query> combine)
 	{
+		// If both sides are null, return null
 		if (leftQuery is null && rightQuery is null)
+		{
 			return null;
+		}
 
+		// If the left side is null, return the right side
 		if (leftQuery is null)
+		{
 			return rightQuery;
+		}
 
+		// If the right side is null, return the left side
 		if (rightQuery is null)
+		{
 			return leftQuery;
+		}
 
 		var container = combine(leftQuery, rightQuery);
 
-		if (container.TryGet<BoolQuery>(out var query))
+		if (container.TryGet<BoolQuery>(out var boolQuery))
 		{
-			return new BoolQuery
-			{
-				Must = query.Must,
-				MustNot = query.MustNot,
-				Should = query.Should,
-				Filter = query.Filter,
-			};
+			return boolQuery;
 		}
 
 		throw new Exception("Unable to combine queries.");
 	}
 
-	public static implicit operator Query?(SearchQuery query) => query is null ? null : new Query(query);
+	public static implicit operator Query?(SearchQuery? query) => query is null ? null : new Query(query);
 
 	// We no longer (currently) support verbatim/strict query containers so this may be simplified to a direct abstract method in the future.
 	internal void WrapInContainer(Query container) => InternalWrapInContainer(container);
