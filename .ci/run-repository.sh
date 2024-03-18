94dc7c0941eace42a911f70f6adc16f73fdacf8b
gpetrou <4172445+gpetrou@users.noreply.github.com>
2024-03-18 15:25:50 +0200

    Update TargetFrameworks (#8052)

--- .ci/run-repository.sh
+++ .ci/run-repository.sh
@@ -9,7 +9,7 @@ script_path=$(dirname $(realpath -s $0))
 source $script_path/functions/imports.sh
 set -euo pipefail
 
-DOTNET_VERSION=${DOTNET_VERSION-6.0.403}
+DOTNET_VERSION=${DOTNET_VERSION-8.0.100}
 ELASTICSEARCH_URL=${ELASTICSEARCH_URL-"$elasticsearch_url"}
 elasticsearch_container=${elasticsearch_container-}
 
