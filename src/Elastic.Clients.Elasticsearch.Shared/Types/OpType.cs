5f4be58be91ef05703de0e289d52c7eab20a0987
Florian Bernd <git@flobernd.de>
2024-04-09 15:35:50 +0200

    Fix serialization of `OpType` (#8105)

--- src/Elastic.Clients.Elasticsearch.Shared/Types/OpType.cs
+++ src/Elastic.Clients.Elasticsearch.Shared/Types/OpType.cs
@@ -2,6 +2,10 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 using Elastic.Transport;
 
 #if ELASTICSEARCH_SERVERLESS
@@ -10,6 +14,7 @@ namespace Elastic.Clients.Elasticsearch.Serverless;
 namespace Elastic.Clients.Elasticsearch;
 #endif
 
+[JsonConverter(typeof(OpTypeConverter))]
 public partial struct OpType : IStringable
 {
 	public static OpType Index = new("index");
@@ -22,4 +27,27 @@ public partial struct OpType : IStringable
 	public static implicit operator OpType(string v) => new(v);
 
 	public string GetString() => Value ?? string.Empty;
+}
+
+internal sealed class OpTypeConverter :
+	JsonConverter<OpType>
+{
+	public override OpType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		if (reader.TokenType != JsonTokenType.String)
+		{
+			throw new JsonException("Unexpected token.");
+		}
+
+		var value = reader.GetString();
+
+		return value switch
+		{
+			"index" => OpType.Index,
+			"create" => OpType.Create,
+			_ => throw new JsonException($"Unsupported value '{value}' for '{nameof(OpType)}' enum.")
+		};
+	}
+
+	public override void Write(Utf8JsonWriter writer, OpType value, JsonSerializerOptions options) => writer.WriteStringValue(value.Value);
 }
