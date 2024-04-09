69f07c42be4aef493c624326ebb30e487f255a8b
Florian Bernd <git@flobernd.de>
2024-04-09 11:15:01 +0200

    Re-implement `Exists` property (#8102)

---
+++ src/Elastic.Clients.Elasticsearch.Shared/Api/ExistsResponse.cs
@@ -0,0 +1,19 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using Elastic.Transport.Products.Elasticsearch;
+
+#if ELASTICSEARCH_SERVERLESS
+namespace Elastic.Clients.Elasticsearch.Serverless;
+#else
+namespace Elastic.Clients.Elasticsearch;
+#endif
+
+public sealed partial class ExistsResponse : ElasticsearchResponse
+{
+	public bool Exists => ApiCallDetails is
+	{
+		HasSuccessfulStatusCode: true, HttpStatusCode: 200
+	};
+}
