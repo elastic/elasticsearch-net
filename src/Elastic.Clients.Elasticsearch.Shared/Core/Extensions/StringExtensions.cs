9881e960cd2b3ff1cd437aa661e09b186659d2d1
Florian Bernd <git@flobernd.de>
2024-07-30 12:36:11 +0200

    Improve `ToCamelCase` to match `STJ` behavior (#8278)

--- src/Elastic.Clients.Elasticsearch.Shared/Core/Extensions/StringExtensions.cs
+++ src/Elastic.Clients.Elasticsearch.Shared/Core/Extensions/StringExtensions.cs
@@ -2,6 +2,8 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 #if ELASTICSEARCH_SERVERLESS
 namespace Elastic.Clients.Elasticsearch.Serverless;
 #else
@@ -10,18 +12,53 @@ namespace Elastic.Clients.Elasticsearch;
 
 internal static class StringExtensions
 {
-	internal static string ToCamelCase(this string s)
+	// Taken from:
+	// https://github.com/dotnet/runtime/blob/main/src/libraries/System.Text.Json/Common/JsonCamelCaseNamingPolicy.cs
+
+	internal static string ToCamelCase(this string name)
 	{
-		if (string.IsNullOrEmpty(s))
-			return s;
+		if (string.IsNullOrEmpty(name) || !char.IsUpper(name[0]))
+		{
+			return name;
+		}
 
-		if (!char.IsUpper(s[0]))
-			return s;
+#if NET
+		return string.Create(name.Length, name, (chars, name) =>
+		{
+			name.CopyTo(chars);
+			FixCasing(chars);
+		});
+#else
+		var chars = name.ToCharArray();
+		FixCasing(chars);
+		return new string(chars);
+#endif
+	}
 
-		var camelCase = char.ToLowerInvariant(s[0]).ToString();
-		if (s.Length > 1)
-			camelCase += s.Substring(1);
+	private static void FixCasing(Span<char> chars)
+	{
+		for (var i = 0; i < chars.Length; i++)
+		{
+			if (i == 1 && !char.IsUpper(chars[i]))
+			{
+				break;
+			}
 
-		return camelCase;
+			var hasNext = (i + 1 < chars.Length);
+
+			// Stop when next char is already lowercase.
+			if (i > 0 && hasNext && !char.IsUpper(chars[i + 1]))
+			{
+				// If the next char is a space, lowercase current char before exiting.
+				if (chars[i + 1] == ' ')
+				{
+					chars[i] = char.ToLowerInvariant(chars[i]);
+				}
+
+				break;
+			}
+
+			chars[i] = char.ToLowerInvariant(chars[i]);
+		}
 	}
 }
