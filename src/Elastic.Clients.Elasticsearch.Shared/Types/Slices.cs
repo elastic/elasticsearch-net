86191bbf3714c706a0fc95870c041acedeb72a8a
Florian Bernd <florian.bernd@elastic.co>
2024-09-04 10:06:05 +0200

    Fix `Slices` url parameter serialization

---
+++ src/Elastic.Clients.Elasticsearch.Shared/Types/Slices.cs
@@ -0,0 +1,26 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Globalization;
+
+using Elastic.Transport;
+
+#if ELASTICSEARCH_SERVERLESS
+namespace Elastic.Clients.Elasticsearch.Serverless;
+#else
+namespace Elastic.Clients.Elasticsearch;
+#endif
+
+public partial class Slices :
+	IUrlParameter
+{
+	public string GetString(ITransportConfiguration settings) =>
+		Tag switch
+		{
+			0 => Item1.ToString(CultureInfo.InvariantCulture),
+			1 => UrlFormatter.CreateString(Item2, settings)!,
+			_ => throw new InvalidOperationException()
+		};
+}
