99aa6b2d52a7a7946bf4f4f44d299ba020533f98
Florian Bernd <git@flobernd.de>
2024-09-05 10:24:39 +0200

    Fix `SourceConfigParam` url parameter serialization (#8328)

---
+++ src/Elastic.Clients.Elasticsearch.Shared/Types/Core/Search/SourceConfigParam.cs
@@ -0,0 +1,25 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+using Elastic.Transport;
+
+#if ELASTICSEARCH_SERVERLESS
+namespace Elastic.Clients.Elasticsearch.Serverless.Core.Search;
+#else
+namespace Elastic.Clients.Elasticsearch.Core.Search;
+#endif
+
+public partial class SourceConfigParam :
+	IUrlParameter
+{
+	public string GetString(ITransportConfiguration settings) =>
+		Tag switch
+		{
+			0 => UrlFormatter.CreateString(Item1, settings)!,
+			1 => UrlFormatter.CreateString(Item2, settings)!,
+			_ => throw new InvalidOperationException()
+		};
+}
