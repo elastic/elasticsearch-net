6dff23b792a2254699f20817b30b7ee5746d21a3
Florian Bernd <git@flobernd.de>
2025-04-30 13:39:45 +0200

    9.0.1 fixes (#8505)
    * Fix NRE in `GetAliasResponse`
    
    * Regenerate client

--- src/Elastic.Clients.Elasticsearch/_Shared/Api/IndexManagement/GetAliasResponse.cs
+++ src/Elastic.Clients.Elasticsearch/_Shared/Api/IndexManagement/GetAliasResponse.cs
@@ -2,9 +2,6 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
-
 namespace Elastic.Clients.Elasticsearch.IndexManagement;
 
 public partial class GetAliasResponse
@@ -17,5 +14,5 @@ public partial class GetAliasResponse
 	/// the client considers the response to be valid.
 	/// </para>
 	/// </summary>
-	public override bool IsValidResponse => base.IsValidResponse || Aliases.Count > 0;
+	public override bool IsValidResponse => base.IsValidResponse || Aliases?.Count > 0;
 }
