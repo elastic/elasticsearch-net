0194d2801e7a89ccea9f53a1591b5974c08fab24
Florian Bernd <git@flobernd.de>
2025-06-18 13:22:07 +0200

    Add deserialization support for `GeoBounds` and `GeoLocation` (#8586)
    (cherry picked from commit 6a41bd965bfc9c95f3ce2e6f43f5f765e17280d1)

--- src/Elastic.Clients.Elasticsearch/_Shared/Types/GeoBounds.cs
+++ src/Elastic.Clients.Elasticsearch/_Shared/Types/GeoBounds.cs
@@ -19,9 +19,70 @@ public sealed partial class GeoBounds
 internal sealed class GeoBoundsConverter :
 	JsonConverter<GeoBounds>
 {
+	// Coordinates.
+
+	private static readonly JsonEncodedText PropBottom = JsonEncodedText.Encode("bottom");
+	private static readonly JsonEncodedText PropLeft = JsonEncodedText.Encode("left");
+	private static readonly JsonEncodedText PropRight = JsonEncodedText.Encode("right");
+	private static readonly JsonEncodedText PropTop = JsonEncodedText.Encode("top");
+
+	// TopLeftBottomRight.
+
+	private static readonly JsonEncodedText PropBottomRight = JsonEncodedText.Encode("bottom_right");
+	private static readonly JsonEncodedText PropTopLeft = JsonEncodedText.Encode("top_left");
+
+	// TopRightBottomLeft.
+
+	private static readonly JsonEncodedText PropBottomLeft = JsonEncodedText.Encode("bottom_left");
+	private static readonly JsonEncodedText PropTopRight = JsonEncodedText.Encode("top_right");
+
+	// WKT.
+
+	private static readonly JsonEncodedText PropWkt = JsonEncodedText.Encode("wkt");
+
 	public override GeoBounds? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 	{
-		throw new InvalidOperationException();
+		reader.ValidateToken(JsonTokenType.StartObject);
+
+		var readerSnapshot = reader;
+		reader.Read();
+
+		GeoBounds.Kind? kind = null;
+		if (reader.TokenType is JsonTokenType.PropertyName)
+		{
+			if (reader.ValueTextEquals(PropBottom) || reader.ValueTextEquals(PropLeft) ||
+				reader.ValueTextEquals(PropRight) || reader.ValueTextEquals(PropTop))
+			{
+				kind = GeoBounds.Kind.Coordinates;
+			}
+
+			if (reader.ValueTextEquals(PropBottomRight) || reader.ValueTextEquals(PropTopLeft))
+			{
+				kind = GeoBounds.Kind.TopLeftBottomRight;
+			}
+
+			if (reader.ValueTextEquals(PropBottomLeft) || reader.ValueTextEquals(PropTopRight))
+			{
+				kind = GeoBounds.Kind.TopRightBottomLeft;
+			}
+
+			if (reader.ValueTextEquals(PropWkt))
+			{
+				kind = GeoBounds.Kind.Wkt;
+			}
+		}
+
+		reader = readerSnapshot;
+
+		return kind switch
+		{
+			GeoBounds.Kind.Coordinates => new(reader.ReadValue<CoordsGeoBounds>(options)),
+			GeoBounds.Kind.TopLeftBottomRight => new(reader.ReadValue<TopLeftBottomRightGeoBounds>(options)),
+			GeoBounds.Kind.TopRightBottomLeft => new(reader.ReadValue<TopRightBottomLeftGeoBounds>(options)),
+			GeoBounds.Kind.Wkt => new(reader.ReadValue<WktGeoBounds>(options)),
+			null => throw new JsonException($"Unrecognized '{typeof(GeoBounds)}' variant."),
+			_ => throw new InvalidOperationException("unreachable")
+		};
 	}
 
 	public override void Write(Utf8JsonWriter writer, GeoBounds value, JsonSerializerOptions options)
