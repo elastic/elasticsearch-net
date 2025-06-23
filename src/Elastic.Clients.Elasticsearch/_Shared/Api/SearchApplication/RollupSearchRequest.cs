897fa6e290396760bb5b7c13e1121dce4b960e0d
Florian Bernd <git@flobernd.de>
2025-06-23 12:45:02 +0200

    Set `typed_keys` to `true` by default (#8609)
    (cherry picked from commit 85f1886f71a1790bd4a27b6a8637d290e4dc5aa9)

--- src/Elastic.Clients.Elasticsearch/_Shared/Api/SearchApplication/RollupSearchRequest.cs
+++ src/Elastic.Clients.Elasticsearch/_Shared/Api/SearchApplication/RollupSearchRequest.cs
@@ -2,9 +2,9 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-namespace Elastic.Clients.Elasticsearch.AsyncSearch;
+namespace Elastic.Clients.Elasticsearch.SearchApplication;
 
-public partial class GetAsyncSearchRequest
+public partial class SearchApplicationSearchRequest
 {
 	// Any request may contain aggregations so we force `typed_keys` in order to successfully
 	// deserialize them.
