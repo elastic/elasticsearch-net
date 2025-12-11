fe37720bbfad5d5bd46d1576518e6669e7dab11a
Florian Bernd <florian.bernd@elastic.co>
2025-12-11 17:03:03 +0100

    Refactor vector ingest benchmark

--- src/Playground/Program.cs
+++ src/Playground/Program.cs
@@ -9,36 +9,124 @@ using System.Text.Json.Serialization;
 using Elastic.Clients.Elasticsearch;
 using Elastic.Clients.Elasticsearch.Mapping;
 using Elastic.Clients.Elasticsearch.Serialization;
+using Elastic.Elasticsearch.Ephemeral;
+using Elastic.Stack.ArtifactsApi;
 using Elastic.Transport;
+using ProcNet.Std;
 
-var baseOptions = new VectorIngestOptions
+var datasetFile = Environment.GetEnvironmentVariable("BENCHMARK_DATASET_FILE") ?? 
+				  "C:\\Users\\Florian\\Desktop\\bench\\open_ai_corpus-initial-indexing-1k.json";
+
+if (string.IsNullOrEmpty(datasetFile))
 {
-	DatasetFile = "C:\\Users\\Florian\\Desktop\\bench\\open_ai_corpus-initial-indexing-1k.json",
-	Repetitions = 1,
-	MaxDegreeOfParallelism = 1,
-	WarmupPasses = 5,
-	MeasuredPasses = 3,
-	ElasticsearchEndpoint = new Uri("http://192.168.100.87:9200"),
-	ElasticsearchUsername = "elastic",
-	ElasticsearchPassword = "julCIvcZ"
+	Console.WriteLine("BENCHMARK_DATASET_FILE environment variable is not set. Exiting.");
+	return;
+}
+
+var elasticsearchUrl = Environment.GetEnvironmentVariable("ELASTICSEARCH_URL");
+var elasticsearchUser = Environment.GetEnvironmentVariable("ELASTICSEARCH_USER");
+var elasticsearchPass = Environment.GetEnvironmentVariable("ELASTICSEARCH_PASS");
+
+Console.WriteLine("Do you want me to start an ephemeral Elasticsearch instance for the benchmark?");
+Console.Write("[y|n]: ");
+var answer = Console.ReadKey();
+Console.WriteLine();
+
+EphemeralCluster? cluster = null;
+
+if (answer.KeyChar is 'y' or 'Y')
+{
+	Console.WriteLine("Initializing ephemeral Elasticsearch cluster. This might take a few minutes.");
+	cluster = new EphemeralCluster(new EphemeralClusterConfiguration(
+		ElasticVersion.From("9.3.0-SNAPSHOT"), ClusterFeatures.XPack | ClusterFeatures.Security)
+	{
+		StartingPortNumber = 9202,
+		TrialMode = XPackTrialMode.Trial
+	});
+
+	Console.WriteLine("Starting ephemeral Elasticsearch cluster ...");
+	cluster.Start(NoopConsoleLineWriter.Instance, TimeSpan.FromHours(1));
+
+	elasticsearchUrl = $"http://localhost:{cluster.ClusterConfiguration.StartingPortNumber}";
+	elasticsearchUser = ClusterAuthentication.Admin.Username;
+	elasticsearchPass = ClusterAuthentication.Admin.Password;
+}
+else
+{
+	if (string.IsNullOrEmpty(elasticsearchUrl))
+	{
+		Console.WriteLine("ELASTICSEARCH_URL environment variable is not set. Exiting.");
+		return;
+	}
+
+	if (string.IsNullOrEmpty(elasticsearchUser))
+	{
+		Console.WriteLine("ELASTICSEARCH_USER environment variable is not set. Exiting.");
+		return;
+	}
+
+	if (string.IsNullOrEmpty(elasticsearchPass))
+	{
+		Console.WriteLine("ELASTICSEARCH_PASS environment variable is not set. Exiting.");
+		return;
+	}
+}
+
+Console.WriteLine($"Using existing Elasticsearch instance at '{elasticsearchUrl}'");
+
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (_, e) =>
+{
+	e.Cancel = true;
+	if (cts.IsCancellationRequested)
+	{
+		return;
+	}
+
+	cts.Cancel();
 };
 
-var cases = new VectorIngestOptions[]
+try
 {
-	baseOptions with { UseBase64VectorEncoding = false, ChunkSize = 100  },
-	baseOptions with { UseBase64VectorEncoding = false, ChunkSize = 250  },
-	baseOptions with { UseBase64VectorEncoding = false, ChunkSize = 500  },
-	baseOptions with { UseBase64VectorEncoding = false, ChunkSize = 1000 },
-	baseOptions with { UseBase64VectorEncoding = true , ChunkSize = 100  },
-	baseOptions with { UseBase64VectorEncoding = true , ChunkSize = 250  },
-	baseOptions with { UseBase64VectorEncoding = true , ChunkSize = 500  },
-	baseOptions with { UseBase64VectorEncoding = true , ChunkSize = 1000 }
-};
+	var options = new VectorIngestOptions
+	{
+		DatasetFile = "C:\\Users\\Florian\\Desktop\\bench\\open_ai_corpus-initial-indexing-1k.json",
+		Repetitions = 20,
+		MaxDegreeOfParallelism = 1,
+		WarmupPasses = 5,
+		MeasuredPasses = 3,
+		ElasticsearchEndpoint = new Uri(elasticsearchUrl),
+		ElasticsearchUsername = elasticsearchUser,
+		ElasticsearchPassword = elasticsearchPass
+	};
 
-foreach (var testcase in cases)
+	var results = await VectorIngestBenchmark.Benchmark(options, cts.Token).ConfigureAwait(false);
+	Console.WriteLine(JsonSerializer.Serialize(results.ToArray(), BenchmarkJsonSerializerContext.Default.VectorIngestResultArray));
+}
+catch (Exception e)
 {
-	Console.Write($"Base64: {(testcase.UseBase64VectorEncoding ? '1' : '0')}, Chunk size: {testcase.ChunkSize,4} == ");
-	await VectorIngest.Ingest(testcase);
+	Console.WriteLine(e.Message);
+}
+finally
+{
+	cluster?.Dispose();
+}
+
+cluster?.WaitForExit(TimeSpan.FromHours(1));
+
+internal class NoopConsoleLineWriter :
+	IConsoleLineHandler
+{
+	public static NoopConsoleLineWriter Instance { get; } = new NoopConsoleLineWriter();
+
+	public void Handle(LineOut lineOut)
+	{
+	}
+
+	public void Handle(Exception e)
+	{
+	}
 }
 
 public sealed record VectorIngestOptions
@@ -52,16 +140,6 @@ public sealed record VectorIngestOptions
 	/// The number of times the dataset is repeated.
 	/// </summary>
 	public int Repetitions { get; init; } = 1;
-
-	/// <summary>
-	/// The chunk size for the individual bulk requests.
-	/// </summary>
-	public int ChunkSize { get; init; } = 100;
-
-	/// <summary>
-	/// Configures whether vector data is encoded in <c>base64</c> format.
-	/// </summary>
-	public bool UseBase64VectorEncoding { get; init; } = true;
 
 	/// <summary>
 	/// The maximum number of concurrent bulk operations allowed when processing tasks in parallel.
@@ -83,28 +161,113 @@ public sealed record VectorIngestOptions
 	public required string ElasticsearchPassword { get; init; }
 }
 
-public static class VectorIngest
+public sealed record VectorIngestResult
 {
-	public static async Task Ingest(VectorIngestOptions options)
-	{
-		var docs = LoadDataset(options.DatasetFile, options.Repetitions);
+	[JsonPropertyName("dataset_size")]
+	public required int DatasetSize { get; init; }
 
+	[JsonPropertyName("chunk_size")]
+	public required int ChunkSize { get; init; }
+
+	[JsonPropertyName("float32")]
+	public required VectorIngestEncodingResult Float32 { get; init; }
+
+	[JsonPropertyName("base64")]
+	public required VectorIngestEncodingResult Base64 { get; init; }
+}
+
+public sealed record VectorIngestEncodingResult
+{
+	/// <summary>
+	/// The duration in milliseconds.
+	/// </summary>
+	[JsonPropertyName("duration")]
+	public required long Duration { get; init; }
+}
+
+public static class VectorIngestBenchmark
+{
+	public static async Task<IEnumerable<VectorIngestResult>> Benchmark(VectorIngestOptions options, CancellationToken cancellationToken = default)
+	{
+		var documents = LoadDataset(options.DatasetFile, options.Repetitions);
+
+		var chunkSizes = new[]
+		{
+			100, 250 ,500, 1000
+		};
+
+		var result = new List<VectorIngestResult>();
+
+		foreach (var chunkSize in chunkSizes)
+		{
+			if (cancellationToken.IsCancellationRequested)
+			{
+				break;
+			}
+
+			Console.WriteLine($"== Chunk size {chunkSize,4} == ");
+
+			Console.Write("Float32 : ");
+			var elapsedSecondsFloat32 = await Ingest(options, documents, chunkSize, FloatVectorDataEncoding.Legacy, cancellationToken);
+			Console.WriteLine($"{((int)(elapsedSecondsFloat32 * 1000)),4}ms / {((int)(documents.Length / elapsedSecondsFloat32)),4} docs/s");
+
+			if (cancellationToken.IsCancellationRequested)
+			{
+				break;
+			}
+
+			Console.Write("Base64 : ");
+			var elapsedSecondsBase64 = await Ingest(options, documents, chunkSize, FloatVectorDataEncoding.Base64, cancellationToken);
+			Console.WriteLine($"{((int)(elapsedSecondsBase64 * 1000)),4}ms / {((int)(documents.Length / elapsedSecondsBase64)),4} docs/s");
+
+			Console.WriteLine($"Speedup: {(elapsedSecondsFloat32 / elapsedSecondsBase64):0.00}x");
+
+			result.Add(new VectorIngestResult
+			{
+				DatasetSize = documents.Length,
+				ChunkSize = chunkSize,
+				Float32 = new VectorIngestEncodingResult
+				{
+					Duration = (int)(elapsedSecondsFloat32 * 1000)
+				},
+				Base64 = new VectorIngestEncodingResult
+				{
+					Duration = (int)(elapsedSecondsBase64 * 1000)
+				}
+			});
+		}
+
+		return result;
+	}
+
+	private static async Task<double> Ingest(
+		VectorIngestOptions options,
+		OpenAiBenchmarkDocument[] documents,
+		int chunkSize,
+		FloatVectorDataEncoding encoding,
+		CancellationToken cancellationToken = default)
+	{
 		var sw = new Stopwatch();
 		var elapsedSeconds = 0.0d;
 
-		var client = CreateClient(options);
+		var client = CreateClient(options, encoding);
 		var indexName = $"bench-{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
 
 		for (var i = 0; i < options.WarmupPasses + options.MeasuredPasses; i++)
 		{
-			await InitializeIndex(client, indexName, docs.First().Embedding.Length);
+			if (cancellationToken.IsCancellationRequested)
+			{
+				break;
+			}
+
+			await InitializeIndex(client, indexName, documents.First().Embedding.Length, cancellationToken);
 
 			sw.Restart();
 			client
-				.BulkAll(docs, x => x
+				.BulkAll(documents, x => x
 					.Index(indexName)
 					.MaxDegreeOfParallelism(options.MaxDegreeOfParallelism)
-					.Size(options.ChunkSize)
+					.Size(chunkSize)
 				)
 				.Wait(TimeSpan.FromHours(1), _ => {});
 			sw.Stop();
@@ -114,21 +277,19 @@ public static class VectorIngest
 				elapsedSeconds += sw.Elapsed.TotalSeconds;
 			}
 
-			await client.Indices.RefreshAsync(x => x.Indices(indexName)).ConfigureAwait(false);
+			await client.Indices.RefreshAsync(x => x.Indices(indexName), cancellationToken).ConfigureAwait(false);
 
-			var count = await client.CountAsync(x => x.Indices(indexName)).ConfigureAwait(false);
-			if (count.Count != docs.Length)
+			var count = await client.CountAsync(x => x.Indices(indexName), cancellationToken).ConfigureAwait(false);
+			if (!cancellationToken.IsCancellationRequested && (count.Count != documents.Length))
 			{
-				throw new Exception($"Document count mismatch: expected {docs.Length}, got {count.Count}");
+				throw new Exception($"Document count mismatch: expected {documents.Length}, got {count.Count}");
 			}
 		}
 
+		// ReSharper disable once MethodSupportsCancellation
 		await client.Indices.DeleteAsync(indexName).ConfigureAwait(false);
 
-		elapsedSeconds /= options.MeasuredPasses;
-		var docsPerSec = docs.Length / elapsedSeconds;
-
-		Console.WriteLine($"{((int)(elapsedSeconds * 1000)),4}ms / {((int)docsPerSec),4} docs/s");
+		return elapsedSeconds / options.MeasuredPasses;
 	}
 
 	private static OpenAiBenchmarkDocument[] LoadDataset(string filename, int repetitions)
@@ -146,7 +307,7 @@ public static class VectorIngest
 		}
 	}
 
-	private static ElasticsearchClient CreateClient(VectorIngestOptions options)
+	private static ElasticsearchClient CreateClient(VectorIngestOptions options, FloatVectorDataEncoding encoding)
 	{
 		var pool = new SingleNodePool(options.ElasticsearchEndpoint);
 
@@ -157,16 +318,16 @@ public static class VectorIngest
 			.Authentication(new BasicAuthentication(options.ElasticsearchUsername, options.ElasticsearchPassword))
 			.MemoryStreamFactory(new RecyclableMemoryStreamFactory())
 			.EnableHttpCompression(false)
-			.FloatVectorDataEncoding(options.UseBase64VectorEncoding ? FloatVectorDataEncoding.Base64 : FloatVectorDataEncoding.Legacy);
+			.FloatVectorDataEncoding(encoding);
 
 		return new ElasticsearchClient(settings);
 	}
 
-	private static async Task InitializeIndex(ElasticsearchClient client, string indexName, int vectorDimensions)
+	private static async Task InitializeIndex(ElasticsearchClient client, string indexName, int vectorDimensions, CancellationToken cancellationToken = default)
 	{
-		if (await client.Indices.ExistsAsync(indexName).ConfigureAwait(false) is { Exists: true })
+		if (await client.Indices.ExistsAsync(indexName, cancellationToken).ConfigureAwait(false) is { Exists: true })
 		{
-			await client.Indices.DeleteAsync(indexName).ConfigureAwait(false);
+			await client.Indices.DeleteAsync(indexName, cancellationToken).ConfigureAwait(false);
 		}
 
 		await client.Indices
@@ -186,17 +347,19 @@ public static class VectorIngest
 						.Text(x => x.Text)
 					)
 				)
-				.WaitForActiveShards(1)
+				.WaitForActiveShards(1),
+				cancellationToken
 			)
 			.ConfigureAwait(false);
 
-		await client.Indices.RefreshAsync(x => x.Indices(indexName)).ConfigureAwait(false);
+		await client.Indices.RefreshAsync(x => x.Indices(indexName), cancellationToken).ConfigureAwait(false);
 	}
 }
 
 [JsonSerializable(typeof(OpenAiBenchmarkDocument))]
 [JsonSerializable(typeof(OpenAiBenchmarkDocument[]))]
-[JsonSerializable(typeof(object))]
+[JsonSerializable(typeof(VectorIngestResult[]))]
+[JsonSourceGenerationOptions(WriteIndented = true)]
 public sealed partial class BenchmarkJsonSerializerContext :
 	JsonSerializerContext
 {
