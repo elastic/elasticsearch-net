819724cd9bc00355317142be16013bafaf624943
Florian Bernd <git@flobernd.de>
2026-09-25 12:08:43 +0000

    Regenerate client

--- src/Elastic.Clients.Elasticsearch/_Generated/Api/MachineLearning/UpdateDatafeedResponse.g.cs
+++ src/Elastic.Clients.Elasticsearch/_Generated/Api/MachineLearning/UpdateDatafeedResponse.g.cs
@@ -48,6 +48,7 @@ public sealed partial class UpdateDatafeedResponse : Elastic.Transport.Products.
 	public required System.Collections.Generic.IReadOnlyCollection<string> Indices { get; set; }
 	public Elastic.Clients.Elasticsearch.IndicesOptions? IndicesOptions { get; set; }
 	public required string JobId { get; set; }
+	public int? MaxConsecutiveExtractionFailures { get; set; }
 	public int? MaxEmptySearches { get; set; }
 	public required Elastic.Clients.Elasticsearch.QueryDsl.Query Query { get; set; }
 	public required Elastic.Clients.Elasticsearch.Duration QueryDelay { get; set; }
--- src/RequestConverter/_Generated/Api/MachineLearning/PutDatafeedRequest.g.cs
+++ src/RequestConverter/_Generated/Api/MachineLearning/PutDatafeedRequest.g.cs
@@ -139,6 +139,12 @@ public partial class PutDatafeedRequest : RequestConverter.IClientCallFormattabl
 				JobId.FormatCode(writer);
 			}
 
+			if (MaxConsecutiveExtractionFailures is not null)
+			{
+				initializer.Property("MaxConsecutiveExtractionFailures");
+				writer.WriteValue(MaxConsecutiveExtractionFailures.Value);
+			}
+
 			if (MaxEmptySearches is not null)
 			{
 				initializer.Property("MaxEmptySearches");
@@ -263,6 +269,11 @@ public partial class PutDatafeedRequest : RequestConverter.IClientCallFormattabl
 		if (JobId is not null)
 		{
 			writer.WriteFluentCall("JobId", (w) => { JobId.FormatCode(w); });
+		}
+
+		if (MaxConsecutiveExtractionFailures is not null)
+		{
+			writer.WriteFluentCall("MaxConsecutiveExtractionFailures", (w) => { w.WriteValue(MaxConsecutiveExtractionFailures.Value); });
 		}
 
 		if (MaxEmptySearches is not null)
